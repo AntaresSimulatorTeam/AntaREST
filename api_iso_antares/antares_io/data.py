ba3bff413665e787a5cd7c0a3b482f2769716214
François Jolain <fjolain@gmail.com>
2020-10-05 16:51:11 +0200

    close #9. implement jsonschema validator

--- api_iso_antares/antares_io/data.py
+++ api_iso_antares/antares_io/data.py
@@ -1,8 +1,3 @@
-from configparser import ConfigParser
 from typing import Any, Dict
 
-
-def to_json_with_jsonschema(
-    config: ConfigParser, jsonschema: Dict
-) -> Dict[str, Any]:
-    pass
+from jsonschema import validate
--- tests/antares_io/test_data.py
+++ tests/antares_io/test_data.py
@@ -1,58 +1,81 @@
-import configparser
+import pytest
 import json
 
-from api_iso_antares.antares_io.data import to_json_with_jsonschema
+from jsonschema import ValidationError
 
+from api_iso_antares.antares_io.data import validate
 
-def test_to_json_with_jsonschema() -> None:
-    jsonschema_litteral = """
-    {
-      "$id": "http://json-schema.org/draft-07/schema#",
-      "$schema": "http://json-schema.org/draft-07/schema#",
-      "description": "A small exemple.",
+jsonschema_litteral = """
+{
+  "$id": "http://json-schema.org/draft-07/schema#",
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "description": "A small exemple.",
+  "type": "object",
+  "properties": {
+    "part1": {
+      "type": "object",
+      "required": [
+        "key_int",
+        "key_str"
+      ],
+      "properties": {
+        "key_int": {
+          "type": "integer",
+          "description": "A description"
+        },
+        "key_str": {
+          "type": "string",
+          "description": "An other description"
+        }
+      }
+    },
+    "part2": {
       "type": "object",
       "properties": {
-        "part1": {
-          "type": "object",
-          "required": [
-            "key_int",
-            "key_str"
-          ],
-          "properties": {
-            "key_int": {
-              "type": "integer",
-              "description": "A description"
-            },
-            "key_str": {
-              "type": "string",
-              "description": "An other description"
-            }
-          }
+        "key_bool": {
+          "type": "boolean",
+          "description": "A description"
         },
-        "part2": {
-          "type": "object",
-          "properties": {
-            "key_bool": {
-              "type": "boolean",
-              "description": "A description"
-            },
-            "key_bool2": {
-              "type": "boolean"
-            }
-          }
+        "key_bool2": {
+          "type": "boolean"
         }
       }
     }
-    """
+  }
+}
+"""
 
+
+def test_validate_json_ok() -> None:
     jsonschema = json.loads(jsonschema_litteral)
 
-    expected_json = {
+    jsondata = {
         "part1": {"key_int": 1, "key_str": "value1"},
         "part2": {"key_bool": True, "key_bool2": False},
     }
-    config = configparser.ConfigParser()
-    config.read_dict(expected_json)
 
-    result_json = to_json_with_jsonschema(config, jsonschema)
-    assert expected_json == result_json
+    validate(jsondata, jsonschema)
+
+
+def test_validate_json_wrong_key() -> None:
+    jsonschema = json.loads(jsonschema_litteral)
+
+    jsondata = {
+        "part1": {"WRONG_KEY": 1, "key_str": "value1"},
+        "part2": {"key_bool": True, "key_bool2": False},
+    }
+
+    with pytest.raises(ValidationError):
+        validate(jsondata, jsonschema)
+
+
+def test_validate_json_wrong_type() -> None:
+    jsonschema = json.loads(jsonschema_litteral)
+
+    jsondata = {
+        "part1": {"key_int": 1.9, "key_str": "value1"},
+        "part2": {"key_bool": True, "key_bool2": False},
+    }
+
+    with pytest.raises(ValidationError):
+        validate(jsondata, jsonschema)
