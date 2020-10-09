ee91371cd688ce6b0c522a81c746a03a0e266ef4
Charly BION <37449809+Hyralc@users.noreply.github.com>
2020-10-09 15:58:20 +0200

    feat<#15>: get file data by url

--- tests/web/test_server.py
+++ tests/web/test_server.py
@@ -1,9 +1,13 @@
+from pathlib import Path
 from unittest.mock import Mock
 
 import pytest
 
 from api_iso_antares.engine.url_engine import UrlNotMatchJsonDataError
-from api_iso_antares.web.request_handler import RequestHandlerParameters
+from api_iso_antares.web.request_handler import (
+    RequestHandlerParameters,
+    RequestHandler,
+)
 from api_iso_antares.web.server import create_server
 
 
@@ -16,7 +20,7 @@ def test_server() -> None:
 
     app = create_server(mock_handler)
     client = app.test_client()
-    client.get("/api/studies/study1/settings/general/params")
+    client.get("/metadata/study1/settings/general/params")
 
     mock_handler.get.assert_called_once_with(
         "study1/settings/general/params", parameters
@@ -30,10 +34,10 @@ def test_404() -> None:
 
     app = create_server(mock_handler)
     client = app.test_client()
-    result = client.get("/api/studies/study1/settings/general/params")
+    result = client.get("/metadata/study1/settings/general/params")
     assert result.status_code == 404
 
-    result = client.get("/api/studies/WRONG_STUDY")
+    result = client.get("/metadata/WRONG_STUDY")
     assert result.status_code == 404
 
 
@@ -45,16 +49,35 @@ def test_server_with_parameters() -> None:
 
     app = create_server(mock_handler)
     client = app.test_client()
-    result = client.get("/api/studies/study1?depth=4")
+    result = client.get("/metadata/study1?depth=4")
 
     parameters = RequestHandlerParameters(depth=4)
 
     assert result.status_code == 200
     mock_handler.get.assert_called_once_with("study1", parameters)
 
-    result = client.get("/api/studies/study2?depth=WRONG_TYPE")
+    result = client.get("/metadata/study2?depth=WRONG_TYPE")
 
     excepted_parameters = RequestHandlerParameters()
 
     assert result.status_code == 200
     mock_handler.get.assert_called_with("study2", excepted_parameters)
+
+
+@pytest.mark.unit_test
+def test_matrix(tmp_path: str):
+    tmp = Path(tmp_path)
+    (tmp / "study1").mkdir()
+    (tmp / "study1" / "matrix").write_text("toto")
+
+    request_handler = RequestHandler(
+        study_reader=Mock(), url_engine=Mock(), path_to_studies=Path(tmp_path)
+    )
+    app = create_server(request_handler)
+    client = app.test_client()
+    result_right = client.get("/data/study1/matrix")
+
+    assert result_right.data == b"toto"
+
+    result_wrong = client.get("/data/study1/WRONG_MATRIX")
+    assert result_wrong.status_code == 404
