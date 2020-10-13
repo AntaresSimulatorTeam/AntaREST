9f1d30ec077eecf0bc681e147ee46d6056c68990
GPivette <pivette.guillaume@gmail.com>
2020-10-13 16:46:28 +0200

    feat<#28> : closing #28

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,52 +28,14 @@ def lite_jsonschema() -> JSON:
         "title": "The root schema",
         "required": ["file1.ini", "folder1", "folder3"],
         "properties": {
-            "file1.ini": {
-                "$id": "#/properties/file1.ini",
-                "type": "object",
-                "title": "The file1.ini schema",
-                "properties": {
-                    "section": {
-                        "$id": "#/properties/file1.ini/properties/section",
-                        "type": "object",
-                        "required": ["parms"],
-                        "properties": {
-                            "parms": {
-                                "$id": "#/properties/file1.ini/properties/section/properties/parms",
-                                "type": "integer",
-                                "title": "The parms schema",
-                            }
-                        },
-                    }
-                },
-            },
+            "file1.ini": {"$ref": "#definitions/file.ini"},
             "folder1": {
                 "$id": "#/properties/folder1",
                 "type": "object",
                 "title": "The folder1 schema",
                 "required": ["file2.ini", "matrice1.txt", "folder2"],
                 "properties": {
-                    "file2.ini": {
-                        "$id": "#/properties/folder1/properties/file2.ini",
-                        "type": "object",
-                        "title": "The file2.ini schema",
-                        "required": ["section"],
-                        "properties": {
-                            "section": {
-                                "$id": "#/properties/folder1/properties/file2.ini/properties/section",
-                                "type": "object",
-                                "title": "The section schema",
-                                "required": ["parms"],
-                                "properties": {
-                                    "parms": {
-                                        "$id": "#/properties/folder1/properties/file2.ini/properties/section/properties/parms",
-                                        "type": "integer",
-                                        "title": "The parms schema",
-                                    }
-                                },
-                            }
-                        },
-                    },
+                    "file2.ini": {"$ref": "#definitions/file.ini"},
                     "matrice1.txt": {
                         "$id": "#/properties/folder1/properties/matrice1.txt",
                         "type": "string",
@@ -99,29 +61,27 @@ def lite_jsonschema() -> JSON:
                 "type": "object",
                 "title": "The folder3 schema",
                 "required": ["file3.ini"],
+                "properties": {"file3.ini": {"$ref": "#definitions/file.ini"}},
+            },
+        },
+        "definitions": {
+            "file.ini": {
+                "type": "object",
+                "title": "The file3.ini schema",
+                "required": ["section"],
                 "properties": {
-                    "file3.ini": {
-                        "$id": "#/properties/folder3/properties/file3.ini",
+                    "section": {
                         "type": "object",
-                        "title": "The file3.ini schema",
-                        "required": ["section"],
+                        "title": "The section schema",
+                        "required": ["parms"],
                         "properties": {
-                            "section": {
-                                "$id": "#/properties/folder3/properties/file3.ini/properties/section",
-                                "type": "object",
-                                "title": "The section schema",
-                                "required": ["parms"],
-                                "properties": {
-                                    "parms": {
-                                        "$id": "#/properties/folder3/properties/file3.ini/properties/section/properties/parms",
-                                        "type": "integer",
-                                        "title": "The parms schema",
-                                    }
-                                },
+                            "parms": {
+                                "type": "integer",
+                                "title": "The parms schema",
                             }
                         },
                     }
                 },
-            },
+            }
         },
     }
