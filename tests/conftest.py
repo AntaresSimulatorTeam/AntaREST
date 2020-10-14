343e95ccc93bd482cd03ea32625f13e03fc71345
Quentin De Smedt <quentin.desmedt@gmail.com>
2020-10-14 10:14:49 +0200

    feat<jsonschema>: handle array

--- tests/conftest.py
+++ tests/conftest.py
@@ -59,7 +59,6 @@ def lite_jsonschema() -> JSON:
             "folder3": {
                 "$id": "#/properties/folder3",
                 "type": "object",
-                "title": "The folder3 schema",
                 "required": ["file3.ini"],
                 "properties": {
                     "file3.ini": {"$ref": "#/definitions/file.ini"}
@@ -82,8 +81,177 @@ def lite_jsonschema() -> JSON:
                                 "title": "The parms schema",
                             }
                         },
+                    },
+                    "areas": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "name": {"type": "string"},
+                                "matrice1.txt": {"type": "string"},
+                                "file4.ini": {
+                                    "type": "object",
+                                    "required": ["section"],
+                                    "properties": {
+                                        "section": {
+                                            "type": "object",
+                                            "required": ["params"],
+                                            "properties": {
+                                                "params": {
+                                                    "type": "integer",
+                                                }
+                                            },
+                                        }
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+        "definitions": {
+            "area": {
+                "type": "object",
+                "properties": {
+                    "name": {"type": "string"},
+                    "matrice1.txt": {"type": "string"},
+                    "file4.ini": {
+                        "type": "object",
+                        "required": ["section"],
+                        "properties": {
+                            "section": {
+                                "type": "object",
+                                "required": ["params"],
+                                "properties": {
+                                    "params": {
+                                        "type": "integer",
+                                    }
+                                },
+                            }
+                        },
+                    },
+                },
+            },
+            "file": {
+                "type": "object",
+                "required": ["section"],
+                "properties": {
+                    "section": {
+                        "type": "object",
+                        "required": ["params"],
+                        "properties": {
+                            "params": {
+                                "type": "integer",
+                            }
+                        },
                     }
                 },
             }
         },
     }
+
+
+@pytest.fixture
+def lite_jsondata() -> JSON:
+    file_content = {"section": {"params": 123}}
+    return {
+        "file1.ini": file_content,
+        "folder1": {
+            "file2.ini": file_content,
+            "matrice1.txt": str(Path("matrices/folder1/matrice1.txt")),
+            "folder2": {
+                "matrice2.txt": str(
+                    Path("matrices/folder1/folder2/matrice2.txt")
+                ),
+            },
+        },
+        "folder3": {
+            "file3.ini": file_content,
+            "areas": [
+                {
+                    "name": "area1",
+                    "matrice1.txt": str(
+                        Path("matrices/folder3/areas/area1/matrice1.txt")
+                    ),
+                    "file4.ini": file_content,
+                },
+                {
+                    "name": "area2",
+                    "matrice1.txt": str(
+                        Path("matrices/folder3/areas/area2/matrice1.txt")
+                    ),
+                    "file4.ini": file_content,
+                },
+                {
+                    "name": "area3",
+                    "matrice1.txt": str(
+                        Path("matrices/folder3/areas/area3/matrice1.txt")
+                    ),
+                    "file4.ini": file_content,
+                },
+            ],
+        },
+    }
+
+
+@pytest.fixture
+def lite_path(tmp_path: Path) -> Path:
+
+    """
+    root1
+    |
+    _ file1.ini
+    |_folder1
+        |_ file2.ini
+        |_ matrice1.txt
+        |_ folder2
+            |_ matrice2.txt
+    |_folder3
+        |_ file3.ini
+        |_ areas
+            |_ area1
+                |_ matrice3.txt
+                |_ file4.ini
+            |_ area2
+                |_ matrice3.txt
+                |_ file4.ini
+            |_ area3
+                |_ matrice3.txt
+                |_ file4.ini
+    """
+
+    str_content_ini = """
+        [section]
+        params = 123
+    """
+
+    path = Path(tmp_path) / "root1"
+    path_folder = Path(path)
+    path_folder.mkdir()
+    (path / "file1.ini").write_text(str_content_ini)
+    path /= "folder1"
+    path.mkdir()
+    (path / "file2.ini").write_text(str_content_ini)
+    (path / "matrice1.txt").touch()
+    path /= "folder2"
+    path.mkdir()
+    (path / "matrice2.txt").touch()
+    path = Path(path_folder) / "folder3"
+    path.mkdir()
+    (path / "file3.ini").write_text(str_content_ini)
+
+    path /= "areas"
+    path.mkdir()
+
+    def create_area(path_areas: Path, area: str) -> None:
+        path_area = path_areas / area
+        path_area.mkdir()
+        (path_area / "file4.ini").write_text(str_content_ini)
+        (path_area / "matrice1.txt").touch()
+
+    create_area(path, "area1")
+    create_area(path, "area2")
+    create_area(path, "area3")
+
+    return path_folder
