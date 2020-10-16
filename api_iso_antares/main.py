77d647162ef2346e081e30e543a3e68593790fd0
Charly BION <37449809+Hyralc@users.noreply.github.com>
2020-10-16 13:53:02 +0200

    feat<pyinstaller>: generate binary and update workflow

--- api_iso_antares/main.py
+++ api_iso_antares/main.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 from pathlib import Path
 
@@ -11,9 +12,29 @@ from api_iso_antares.engine import UrlEngine
 from api_iso_antares.web import RequestHandler
 from api_iso_antares.web.server import create_server
 
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-j",
+        "--json-schema",
+        dest="jsm_path",
+        help="Path to the Json Schema file",
+    )
+    parser.add_argument(
+        "-s",
+        "--studies",
+        dest="studies_path",
+        help="Path to the studies directory",
+    )
+    return parser.parse_args(sys.argv[1:])
+
+
 if __name__ == "__main__":
+    arguments: argparse.Namespace = parse_arguments()
+
     project_dir: Path = Path(__file__).resolve().parents[2]
-    jsonschema = JsmReader.read(Path(sys.argv[1]))
+    jsonschema = JsmReader.read(Path(arguments.jsm_path))
     request_handler = RequestHandler(
         study_reader=FolderReaderEngine(
             ini_reader=IniReader(),
@@ -22,7 +43,7 @@ if __name__ == "__main__":
             jsm_validator=JsmValidator(jsm=jsonschema),
         ),
         url_engine=UrlEngine(jsm={}),
-        path_studies=Path(sys.argv[2]),
+        path_studies=Path(arguments.studies_path),
     )
     application = create_server(request_handler)
 
