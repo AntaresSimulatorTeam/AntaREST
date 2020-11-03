5ddbee6ecccfdd80f7dcfa963f658d561779a3f3
Quentin De Smedt <quentin.desmedt@gmail.com>
2020-11-03 16:22:50 +0100

    feat<parser>: add writers to file engine

--- api_iso_antares/engine/filesystem/engine.py
+++ api_iso_antares/engine/filesystem/engine.py
@@ -11,9 +11,11 @@ class FileSystemEngine:
         self,
         jsm: JsonSchema,
         readers: Dict[str, Any],
+        writers: Dict[str, Any],
     ) -> None:
         self.jsm = jsm
         self.node_factory = NodeFactory(readers=readers)
+        self.writers = writers
 
     def parse(self, path: Path) -> JSON:
         root_node = self.node_factory.build(
@@ -22,3 +24,9 @@ class FileSystemEngine:
             jsm=self.jsm,
         )
         return cast(JSON, root_node.get_content())
+
+    def get_reader(self, reader: str = "default") -> Any:
+        return self.node_factory.readers[reader]
+
+    def get_writer(self, reader: str = "default") -> Any:
+        return self.writers[reader]
