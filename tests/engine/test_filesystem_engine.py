bba0450293d05adcf3499b7d8f602b2331ae44fa
Quentin De Smedt <quentin.desmedt@gmail.com>
2020-11-06 16:56:40 +0100

    test: engine writing test

--- tests/engine/test_filesystem_engine.py
+++ tests/engine/test_filesystem_engine.py
@@ -29,3 +29,25 @@ def test_read_filesystem(
 
     assert res == lite_jsondata
     assert ini_reader.read.call_count == 6
+
+
+@pytest.mark.unit_test
+def test_write_filesystem(
+    tmp_path: str, lite_path: Path, lite_jsonschema: JSON, lite_jsondata: JSON
+) -> None:
+
+    file_content = {"section": {"params": 123}}
+    ini_reader = Mock()
+    ini_reader.read.return_value = file_content
+    ini_writer = Mock()
+
+    readers = {"default": ini_reader}
+    writers = {"default": ini_writer}
+    jsm = JsonSchema(lite_jsonschema)
+
+    folder_reader = FileSystemEngine(jsm=jsm, readers=readers, writers=writers)
+
+    writed_path = Path(tmp_path)
+    folder_reader.write(writed_path, lite_jsondata)
+
+    assert folder_reader.parse(lite_path) == folder_reader.parse(writed_path)
