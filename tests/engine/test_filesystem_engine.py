5211efed03b059a912dc0a18af6b4276cf402563
Quentin De Smedt <quentin.desmedt@gmail.com>
2020-11-13 15:41:05 +0100

    fix<test>: rewrite on already created folder

--- tests/engine/test_filesystem_engine.py
+++ tests/engine/test_filesystem_engine.py
@@ -1,9 +1,8 @@
 from pathlib import Path
+from typing import Callable
 from unittest.mock import Mock
 
 import pytest
-import random
-import string
 
 from api_iso_antares.custom_types import JSON
 from api_iso_antares.engine.filesystem.engine import (
@@ -12,22 +11,43 @@ from api_iso_antares.engine.filesystem.engine import (
 from api_iso_antares.jsm import JsonSchema
 
 
-@pytest.mark.unit_test
-def test_read_filesystem(
-    lite_path: Path, lite_jsonschema: JSON, lite_jsondata: JSON
-) -> None:
+def get_mocked_filesystem_engine(
+    jsm: JSON, ini_cleaner: Callable
+) -> FileSystemEngine:
 
     file_content = {"section": {"params": 123}}
+
     ini_reader = Mock()
     ini_reader.read.return_value = file_content
+
     ini_writer = Mock()
+    ini_writer.write = lambda data, path: path.touch()
+
+    matrix_writer = Mock()
+    matrix_writer.write = lambda data, path: path.touch()
 
     readers = {"default": ini_reader}
-    writers = {"default": ini_writer}
-    jsm = JsonSchema(lite_jsonschema)
-    folder_reader = FileSystemEngine(jsm=jsm, readers=readers, writers=writers)
+    writers = {"default": ini_writer, "matrix": matrix_writer}
+
+    filesystem_engine = FileSystemEngine(
+        jsm=JsonSchema(jsm), readers=readers, writers=writers
+    )
+
+    return filesystem_engine
+
+
+@pytest.mark.unit_test
+def test_read_filesystem(
+    lite_path: Path,
+    lite_jsonschema: JSON,
+    lite_jsondata: JSON,
+    ini_cleaner: Callable,
+) -> None:
+
+    folder_reader = get_mocked_filesystem_engine(lite_jsonschema, ini_cleaner)
 
     res = folder_reader.parse(lite_path)
+    ini_reader = folder_reader.get_reader()
 
     assert res == lite_jsondata
     assert ini_reader.read.call_count == 6
@@ -35,28 +55,35 @@ def test_read_filesystem(
 
 @pytest.mark.unit_test
 def test_write_filesystem(
-    tmpdir_factory, lite_path: Path, lite_jsonschema: JSON, lite_jsondata: JSON
+    tmp_path,
+    lite_path: Path,
+    lite_jsonschema: JSON,
+    lite_jsondata: JSON,
+    ini_cleaner: Callable,
 ) -> None:
 
-    # TODO
-
-    file_content = {"section": {"params": 123}}
-    ini_reader = Mock()
-    ini_reader.read.return_value = file_content
-    ini_writer = Mock()
-
-    readers = {"default": ini_reader}
-    writers = {"default": ini_writer}
-    jsm = JsonSchema(lite_jsonschema)
-
-    folder_reader = FileSystemEngine(jsm=jsm, readers=readers, writers=writers)
-
-    unique_path = Path(
-        tmpdir_factory.getbasetemp()
-        / "".join(random.choices(string.ascii_uppercase + string.digits, k=30))
+    filesystem_engine = get_mocked_filesystem_engine(
+        lite_jsonschema, ini_cleaner
     )
-    unique_path.mkdir()
-    write_path = unique_path / "root1"
-    folder_reader.write(write_path, lite_jsondata)
 
-    assert folder_reader.parse(lite_path) == folder_reader.parse(write_path)
+    study_name_destination = "copy_of_lite_study"
+    write_path = Path(tmp_path) / study_name_destination
+    filesystem_engine.write(write_path, lite_jsondata)
+
+    study_name_source = lite_path.parts[-1]
+    data_source = filesystem_engine.parse(lite_path)
+    data_destination = filesystem_engine.parse(write_path)
+
+    def replace_study_name(data: JSON) -> None:
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if isinstance(value, str) and value.startswith("file/"):
+                    data[key] = value.replace(
+                        study_name_destination, study_name_source
+                    )
+                else:
+                    replace_study_name(value)
+
+    replace_study_name(data_destination)
+
+    assert data_destination == data_source
