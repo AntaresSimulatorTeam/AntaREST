953b4fbd8979f19df00ea3951ade0bc38f051fc9
Quentin De Smedt <quentin.desmedt@gmail.com>
2020-11-13 15:41:37 +0100

    test: add test on ini writer with capitals

--- tests/antares_io/writer/test_ini_writer.py
+++ tests/antares_io/writer/test_ini_writer.py
@@ -11,19 +11,24 @@ def test_write(tmp_path: str, ini_cleaner: Callable) -> None:
     path = Path(tmp_path) / "test.ini"
 
     ini_content = """
-        [part1]
+        [part]
         key_int = 1
         key_float = 2.1
         key_str = value1
         
-        [part2]
+        [partWithCapitals]
         key_bool = True
         key_bool2 = False
+        keyWithCapital = True
     """
 
     json_data = {
-        "part1": {"key_int": 1, "key_float": 2.1, "key_str": "value1"},
-        "part2": {"key_bool": True, "key_bool2": False},
+        "part": {"key_int": 1, "key_float": 2.1, "key_str": "value1"},
+        "partWithCapitals": {
+            "key_bool": True,
+            "key_bool2": False,
+            "keyWithCapital": True,
+        },
     }
     writer = IniWriter()
     writer.write(json_data, path)
