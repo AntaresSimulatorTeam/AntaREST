37b6fa9cc376bb95df67e62802bf32a83c8a35da
François Jolain <4466185+FrancoisJ@users.noreply.github.com>
2020-11-16 12:46:28 +0100

    wip<#100> : compact zip method implemented.

--- api_iso_antares/antares_io/exporter/export_file.py
+++ api_iso_antares/antares_io/exporter/export_file.py
@@ -1,20 +1,45 @@
 import glob
+import json
 import os
+import re
+import uuid
 from io import BytesIO
 from pathlib import Path
 from zipfile import ZipFile, ZIP_DEFLATED
+
+from api_iso_antares.custom_types import JSON
 
 
 class Exporter:
     def export_file(self, path_study: Path) -> BytesIO:
         data = BytesIO()
         zipf = ZipFile(data, "w", ZIP_DEFLATED)
+
         root = path_study.name + os.sep
-        root_abs = path_study.absolute()
         os.chdir(path_study)
-        a = glob.glob("**", recursive=True)
-        for path in a:
-            zipf.write(root_abs / path, root + path)
+
+        for path in glob.glob("**", recursive=True):
+            zipf.write(path, root + path)
+
         zipf.close()
         data.seek(0)
         return data
+
+    def export_compact(self, path_study: Path, data: JSON) -> BytesIO:
+        zip = BytesIO()
+        zipf = ZipFile(zip, "w", ZIP_DEFLATED)
+
+        root = path_study.parent.absolute()
+        jsonify = json.dumps(data)
+
+        for url in re.findall(r"file\/[\w\d\-.\/]*", jsonify):
+            uuid4 = str(uuid.uuid4())
+            jsonify = jsonify.replace(url, uuid4)
+            url = url.replace("file/", "")
+            zipf.write(root / url, f"res/{uuid4}")
+
+        zipf.writestr("data.json", jsonify)
+
+        zipf.close()
+        zip.seek(0)
+        return zip
--- tests/antares_io/exporter/test_export_file.py
+++ tests/antares_io/exporter/test_export_file.py
@@ -1,4 +1,5 @@
 import base64
+import json
 from io import BytesIO
 from pathlib import Path
 from zipfile import ZipFile
@@ -19,3 +20,27 @@ def test_export_file(tmp_path: Path):
     assert "folder/file.txt" in zipf.namelist()
     assert "folder/test/" in zipf.namelist()
     assert "folder/test/file.txt" in zipf.namelist()
+
+
+def test_export_compact(tmp_path: Path):
+    root = tmp_path / "folder"
+    root.mkdir()
+    (root / "input").mkdir()
+    (root / "input/file.txt").write_text("Bonjour")
+    (root / "output").mkdir()
+    (root / "output/file.txt").write_text("Hello, World")
+
+    data = {
+        "input": {"file": "file/folder/input/file.txt"},
+        "output": {"file": "file/folder/output/file.txt"},
+    }
+
+    buffer = Exporter().export_compact(root, data)
+    zipf = ZipFile(buffer)
+
+    zipf.extract("data.json", str(tmp_path.absolute()))
+    data_res_path = tmp_path / "data.json"
+    data_res = json.loads(data_res_path.read_text())
+
+    assert f"res/{data_res['input']['file']}" in zipf.namelist()
+    assert f"res/{data_res['output']['file']}" in zipf.namelist()
