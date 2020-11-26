7b9a06afa5cd2a8c66314c73884f1ba9c35c6e23
Quentin De Smedt <quentin.desmedt@gmail.com>
2020-11-26 09:58:20 +0100

    refacto: fix mypy

--- api_iso_antares/wsgi.py
+++ api_iso_antares/wsgi.py
@@ -1,12 +1,24 @@
 import os
 from pathlib import Path
 
+
 from api_iso_antares.main import (
     get_flask_application,
 )
 
-jsm_path = Path(os.getenv("API_ANTARES_JSM_PATH"))
-studies_path = Path(os.getenv("API_ANTARES_STUDIES_PATH"))
+
+def get_env_var(env_var_name: str) -> str:
+    env_var = os.getenv(env_var_name)
+    if env_var is None:
+        raise EnvironmentError(f"API need the env var: {env_var_name}.")
+    return env_var
+
+
+env_var_jsm_path = get_env_var("API_ANTARES_JSM_PATH")
+jsm_path = Path(env_var_jsm_path)
+
+env_var_studies_path = get_env_var("API_ANTARES_STUDIES_PATH")
+studies_path = Path(env_var_studies_path)
 
 app = get_flask_application(jsm_path, studies_path)
 
