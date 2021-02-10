f56881f9e45919428eadd2865388d42e7f5205f1
Paul Bui-Quang <paul.buiquang@rte-france.com>
2021-02-10 13:54:27 +0100

    Fix repository draft
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- antarest/storage/repository/metadata.py
+++ antarest/storage/repository/metadata.py
@@ -1,32 +1,30 @@
 from typing import Optional, List
 
 from sqlalchemy.engine import Engine  # type: ignore
+from sqlalchemy.orm import Session
 
 from antarest.common.persistence import session_scope
 from antarest.storage.model import Metadata
 
 
 class StudyMetadataRepository:
-    def __init__(self, engine: Engine) -> None:
-        self.engine = engine
+    def __init__(self, session: Session) -> None:
+        self.session = session
 
     def save(self, metadata: Metadata) -> Metadata:
-        with session_scope(self.engine) as sess:
-            sess.add(metadata)
-            sess.commit()
-            return metadata
+        self.session.add(metadata)
+        self.session.commit()
+        return metadata
 
     def get(self, id: int) -> Optional[Metadata]:
-        with session_scope(self.engine) as sess:
-            metadata: Metadata = sess.query(Metadata).get(id)
-            return metadata
+        metadata: Metadata = self.session.query(Metadata).get(id)
+        return metadata
 
     def get_all(self) -> List[Metadata]:
-        with session_scope(self.engine) as sess:
-            metadatas: List[Metadata] = sess.query(Metadata).all()
-            return metadatas
+        metadatas: List[Metadata] = self.session.query(Metadata).all()
+        return metadatas
 
     def delete(self, id: int) -> None:
-        with session_scope(self.engine) as sess:
-            u: Metadata = sess.query(Metadata).get(id)
-            sess.delete(u)
+        u: Metadata = self.session.query(Metadata).get(id)
+        self.session.delete(u)
+        self.session.flush()
--- tests/storage/repository/test_metadata.py
+++ tests/storage/repository/test_metadata.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from sqlalchemy import create_engine
 
-from antarest.common.persistence import Base
+from antarest.common.persistence import Base, session_scope
 from antarest.storage.model import Metadata
 from antarest.storage.repository.metadata import StudyMetadataRepository
 
@@ -12,28 +12,28 @@ def test_cyclelife():
     engine = create_engine("sqlite:///:memory:", echo=True)
 
     Base.metadata.create_all(engine)
+    with session_scope(engine) as sess:
+        repo = StudyMetadataRepository(session=sess)
+        a = Metadata(
+            name="a",
+            version="42",
+            author="John Smith",
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
+        )
+        b = Metadata(
+            name="b",
+            version="43",
+            author="Morpheus",
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
+        )
 
-    repo = StudyMetadataRepository(engine=engine)
-    a = Metadata(
-        name="a",
-        version="42",
-        author="John Smith",
-        created_at=datetime.now(),
-        updated_at=datetime.now(),
-    )
-    b = Metadata(
-        name="b",
-        version="43",
-        author="Morpheus",
-        created_at=datetime.now(),
-        updated_at=datetime.now(),
-    )
+        a = repo.save(a)
+        b = repo.save(b)
+        assert b.id
+        c = repo.get(a.id)
+        assert a == c
 
-    a = repo.save(a)
-    b = repo.save(b)
-    assert b.id
-    c = repo.get(a.id)
-    assert a == c
-
-    repo.delete(a.id)
-    assert repo.get(a.id) is None
+        repo.delete(a.id)
+        assert repo.get(a.id) is None
