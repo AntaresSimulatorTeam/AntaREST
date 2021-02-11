32b8f5946d6ea9483a61e58e4f948fbc5ce524ff
Paul Bui-Quang <paul.buiquang@rte-france.com>
2021-02-11 17:17:26 +0100

    Fix server version test
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AntaREST",
-    version="0.0.1",
+    version="0.3.0",
     description="Antares Server",
     long_description=long_description,
     long_description_content_type="text/markdown",
