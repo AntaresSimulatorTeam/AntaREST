aaa700d70e5b04f52ee7c1c29d0736fb5f6fc333
Charly BION <37449809+Hyralc@users.noreply.github.com>
2021-02-19 16:11:28 +0100

    feat: implement launcher service and tests

--- antarest/launcher/service.py
+++ antarest/launcher/service.py
@@ -1,18 +1,30 @@
+from uuid import UUID
+
 from antarest.common.config import Config
 from antarest.launcher.factory_launcher import FactoryLauncher
+from antarest.launcher.model import ExecutionResult
 from antarest.storage.service import StorageService
 
 
 class LauncherService:
     def __init__(
-        self, config: Config, storage_service: StorageService
+        self,
+        config: Config,
+        storage_service: StorageService,
+        factory_launcher=FactoryLauncher,
     ) -> None:
         self.config = config
         self.storage_service = storage_service
-        self.launcher = FactoryLauncher.build_launcher(config)
+        self.launcher = factory_launcher.build_launcher(config)
 
-    def run_study(self):
-        self.launcher.run_study(study_path, version)
+    def run_study(self, study_uuid: str) -> UUID:
+        study_info = self.storage_service.get_study_information(
+            uuid=study_uuid
+        )
+        study_version = study_info["antares"]["version"]
+        study_path = self.storage_service.get_study_path(study_uuid)
+        job_uuid = self.launcher.run_study(study_path, study_version)
+        return job_uuid
 
-    def get_status(self):
-        self.launcher.get_result()
+    def get_result(self, job_uuid: UUID) -> ExecutionResult:
+        return self.launcher.get_result(uuid=job_uuid)
---
+++ tests/launcher/test_service.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+from unittest.mock import Mock
+from uuid import uuid4
+
+import pytest
+
+from antarest.common.config import Config
+from antarest.launcher.model import ExecutionResult, ExecutionStatus
+from antarest.launcher.service import LauncherService
+from antarest.storage.service import StorageService
+
+
+@pytest.mark.unit_test
+def test_service_run_study():
+    storage_service_mock = Mock()
+    storage_service_mock.get_study_information.return_value = {
+        "antares": {"version": "42"}
+    }
+    storage_service_mock.get_study_path.return_value = Path("path/to/study")
+
+    uuid = uuid4()
+    launcher_mock = Mock()
+    launcher_mock.run_study.return_value = uuid
+    factory_launcher_mock = Mock()
+    factory_launcher_mock.build_launcher.return_value = launcher_mock
+
+    launcher_service = LauncherService(
+        config=Config(),
+        storage_service=storage_service_mock,
+        factory_launcher=factory_launcher_mock,
+    )
+
+    launcher_service.launcher.run_study.return_value = uuid
+
+    job_id = launcher_service.run_study("study_uuid")
+
+    assert job_id == uuid
+
+
+@pytest.mark.unit_test
+def test_service_get_result():
+    storage_service_mock = Mock()
+    launcher_mock = Mock()
+    fake_execution_result = ExecutionResult(
+        ExecutionStatus.SUCCESS, msg="Hello, World!", exit_code=0
+    )
+    launcher_mock.get_result.return_value = fake_execution_result
+    factory_launcher_mock = Mock()
+    factory_launcher_mock.build_launcher.return_value = launcher_mock
+
+    launcher_service = LauncherService(
+        config=Config(),
+        storage_service=storage_service_mock,
+        factory_launcher=factory_launcher_mock,
+    )
+
+    assert (
+        launcher_service.get_result(job_uuid=uuid4()) == fake_execution_result
+    )
