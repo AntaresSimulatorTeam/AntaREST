e811200d31d9abbf2cf96cda2c1c9573774b24b6
Paul Bui-Quang <paul.buiquang@rte-france.com>
2021-03-29 18:17:07 +0200

    Setup registered config system
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- antarest/common/config.py
+++ antarest/common/config.py
@@ -3,17 +3,34 @@ from copy import deepcopy
 from pathlib import Path
 from typing import Any, Optional
 
+import typing
 import yaml
+from dataclasses_json import DataClassJsonMixin
 
 from antarest.common.custom_types import JSON
+
+config_definitions: typing.Dict[str, typing.Type[DataClassJsonMixin]] = {}
+T = typing.TypeVar("T", bound=DataClassJsonMixin)
 
 
 class Config:
     def __init__(self, data: Optional[JSON] = None):
-        self.data = data or dict()
+        self.raw_data = data or dict()
+        self.data: typing.Dict[str, Any] = {}
+        self._init()
 
     def __getitem__(self, item: str) -> Any:
         return self._get(item)
+
+    def _init(self):
+        global config_definitions
+        for k in self.raw_data:
+            if k in config_definitions:
+                self.data[k] = config_definitions[k].from_dict(
+                    self.raw_data[k]
+                )
+            else:
+                self.data[k] = self.raw_data[k]
 
     def _get(self, key: str) -> Any:
         parts = key.split(".")
@@ -36,3 +53,13 @@ class ConfigYaml(Config):
         data["_internal"] = {}
         data["_internal"]["resources_path"] = res
         Config.__init__(self, data)
+
+
+def register_config(key: str, class_name: T) -> typing.Callable[[Config], T]:
+    global config_definitions
+    config_definitions[key] = class_name
+
+    def _get_config(config: Config) -> T:
+        return typing.cast(T, config[key])
+
+    return _get_config
--- tests/common/test_config.py
+++ tests/common/test_config.py
@@ -1,9 +1,12 @@
 import os
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Dict, cast
 
 import pytest
+from dataclasses_json.api import dataclass_json
 
-from antarest.common.config import ConfigYaml
+from antarest.common.config import ConfigYaml, register_config, Config
 
 
 @pytest.mark.unit_test
@@ -25,3 +28,30 @@ def test_env_yaml(project_path: Path):
     assert config["main.hello"] == "World"
     os.environ["MAIN_HELLO"] = "antarest"
     assert config["main.hello"] == "antarest"
+
+
+@dataclass
+class Bar:
+    arg: Dict[str, str]
+
+
+@dataclass_json
+@dataclass
+class Foo:
+    bar: Bar
+    baz: str = "hello"
+
+
+def test_custom_typed_conf():
+    get_config = register_config("managed", Foo)
+    json_config = {
+        "managed": {"bar": {"arg": {"hello": "world"}}},
+        "unmanaged": {"hello": "world"},
+    }
+    config = Config(json_config)
+    assert config is not None
+
+    custom_config = get_config(config)
+    assert custom_config.bar.arg == {"hello": "world"}
+    assert custom_config.baz == "hello"
+    assert config["unmanaged"] == {"hello": "world"}
