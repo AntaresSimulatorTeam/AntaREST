86c019ebe892b8d42ee4b2dabde5d8e7f5c5df37
Charly BION <37449809+Hyralc@users.noreply.github.com>
2021-04-22 13:00:34 +0200

    feat: implement config and input data for slurm launcher

--- antarest/launcher/business/slurm_launcher/slurm_launcher.py
+++ antarest/launcher/business/slurm_launcher/slurm_launcher.py
@@ -1,23 +1,15 @@
-import os
-
 from pathlib import Path
 from typing import Callable, List
 from uuid import UUID
 
+from antareslauncher.main import MainParameters, run_with
+from antareslauncher.main_option_parser import (
+    MainOptionParser,
+    MainOptionsParameters,
+)
 from antarest.common.config import Config
 from antarest.common.requests import RequestParameters
 from antarest.launcher.business.ilauncher import ILauncher
-from antarest.launcher.business.slurm_launcher.antares_launcher.antareslauncher import (
-    definitions,
-)
-from antarest.launcher.business.slurm_launcher.antares_launcher.antareslauncher.main import (
-    run_with,
-    MainParameters,
-)
-from antarest.launcher.business.slurm_launcher.antares_launcher.antareslauncher.main_option_parser import (
-    MainOptionParser,
-    look_for_default_ssh_conf_file,
-)
 from antarest.launcher.model import JobResult
 from antarest.storage.service import StorageService
 
@@ -33,26 +25,38 @@ class SlurmLauncher(ILauncher):
         self.callbacks.append(callback)
 
     def _init_launcher_arguments(self):
-        parser: MainOptionParser = MainOptionParser()
+        main_options_parameters = MainOptionsParameters(
+            default_wait_time=self.config.launcher.slurm.default_wait_time,
+            default_time_limit=self.config.launcher.slurm.default_time_limit,
+            default_n_cpu=self.config.launcher.slurm.default_n_cpu,
+            studies_in_dir=str(
+                (
+                    Path(self.config.launcher.slurm.local_workspace)
+                    / "STUDIES_IN"
+                )
+            ),
+            log_dir=str(
+                (Path(self.config.launcher.slurm.local_workspace) / "LOGS")
+            ),
+            finished_dir=str(
+                (Path(self.config.launcher.slurm.local_workspace) / "OUTPUT")
+            ),
+            ssh_config_file_is_required=False,
+            ssh_configfile_path_prod_cwd=None,
+            ssh_configfile_path_prod_user=None,
+        )
+
+        parser: MainOptionParser = MainOptionParser(main_options_parameters)
         parser.add_basic_arguments()
         parser.add_advanced_arguments()
         arguments = parser.parse_args()
 
         arguments.wait_mode = False
-        arguments.wait_time = definitions.DEFAULT_WAIT_TIME
-        arguments.studies_in = str(
-            (Path(self.config["launcher.slurm.workspace"]) / "STUDIES_IN")
-        )
-        arguments.output_dir = str(
-            (Path(self.config["launcher.slurm.workspace"]) / "OUTPUT")
-        )
         arguments.check_queue = False
-        arguments.time_limit = definitions.DEFAULT_TIME_LIMIT
-        arguments.json_ssh_config = look_for_default_ssh_conf_file()
+        arguments.json_ssh_config = None
         arguments.log_dir = str(
-            (Path(self.config["launcher.slurm.workspace"]) / "logs")
+            (Path(self.config.launcher.slurm.local_workspace / "logs"))
         )
-        arguments.n_cpu = definitions.DEFAULT_N_CPU
         arguments.job_id_to_kill = None
         arguments.xpansion_mode = False
         arguments.version = False
@@ -60,25 +64,22 @@ class SlurmLauncher(ILauncher):
         return arguments
 
     def _init_launcher_parameters(self) -> MainParameters:
-        main_parameters = MainParameters(json_dir=,default_json_db_name=,slurm_script_path=,antares_versions_on_remote_server=,default_ssh_dict_from_embedded_json=,db_primary_key=)
-        pass
-
-    def run_study(
-        self, study_uuid: str, version: str, params: RequestParameters
-    ) -> UUID:
-        arguments = self._init_launcher_arguments()
-        main_antares_launcher_parameters = self._init_launcher_parameters()
-
-        # export study
-        self._export_study_to_studies_in_directory(
-            study_uuid, arguments.studies_in, params
+        main_parameters = MainParameters(
+            json_dir=self.config.launcher.slurm.local_workspace,
+            default_json_db_name=self.config.launcher.slurm.default_json_db_name,
+            slurm_script_path=self.config.launcher.slurm.slurm_script_path,
+            antares_versions_on_remote_server=self.config.launcher.slurm.antares_versions_on_remote_server,
+            default_ssh_dict_from_embedded_json={
+                "username": self.config.launcher.slurm.username,
+                "hostname": self.config.launcher.slurm.hostname,
+                "port": self.config.launcher.slurm.port,
+                "private_key_file": self.config.launcher.slurm.private_key_file,
+                "key_password": self.config.launcher.slurm.key_password,
+                "password": self.config.launcher.slurm.password,
+            },
+            db_primary_key=self.config.launcher.slurm.db_primary_key,
         )
-
-        run_with(arguments)
-
-        self._delete_input_study()
-
-        # delete exported study
+        return main_parameters
 
     def _export_study_to_studies_in_directory(
         self,
@@ -86,19 +87,24 @@ class SlurmLauncher(ILauncher):
         studies_in_directory: str,
         params: RequestParameters,
     ):
-        zip_data = self.storage_service.export_study(
-            uuid=study_uuid, params=params, compact=False, outputs=False
-        )
-        os.makedirs(studies_in_directory, exist_ok=True)
-
-        zipfile_path = studies_in_directory + "/study.zip"
-        with open(zipfile_path, "wb") as f:
-            f.write(zip_data.getvalue())
-
-        import zipfile
-
-        with zipfile.ZipFile(zipfile_path, "r") as zip_ref:
-            zip_ref.extractall(studies_in_directory)
+        pass
 
     def _delete_input_study(self):
         pass
+
+    def run_study(
+        self, study_uuid: str, version: str, params: RequestParameters
+    ) -> UUID:
+        arguments = self._init_launcher_arguments()
+        antares_launcher_parameters = self._init_launcher_parameters()
+
+        # export study
+        self._export_study_to_studies_in_directory(
+            study_uuid, arguments.studies_in, params
+        )
+
+        run_with(arguments, antares_launcher_parameters)
+
+        self._delete_input_study()
+
+        # delete exported study
