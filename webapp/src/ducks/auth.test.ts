21277fd30c4f09fc080e8c191a7a4dfff6380bea
3lbanna <contact@elbanna.io>
2021-05-07 17:30:42 +0200

    Auth reducer test

---
+++ webapp/src/ducks/auth.test.ts
@@ -0,0 +1,64 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import thunk from 'redux-thunk'
+import configureMockStore from 'redux-mock-store'
+import auth, { AuthState, logoutAction, LoginAction, LogoutAction} from './auth';
+import { UserInfo } from '../common/types';
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+
+/** ******************************************* */
+/* Test reducers                                */
+/** ******************************************* */
+
+describe('Test reducers', (): void => {
+
+    /** ******************************************* */
+    /* Variables needed for the tests               */
+    /** ******************************************* */
+    const dummyUserInfo : UserInfo = {
+        user: "Dummy",
+        accessToken: "dummyAccessToken",
+        refreshToken: "dummyRefreshToken"
+    };
+
+    const state : AuthState = {
+        user: dummyUserInfo
+    }
+
+
+    it('should test auth reducer after a dummy action', () : void => {
+        
+        const inputState = state;
+        const outputState = state;
+        const testDummyAction = {
+            type: 'AUTH/DUMMY_STATE'
+        }
+
+        expect(auth(inputState, testDummyAction)).toEqual(outputState);
+    });
+
+    it('should test auth reducer after LoginAction', () : void => {
+        const inputState = {user : ''};
+        const outputState = state;
+        const testLoginAction : LoginAction = {
+            type: 'AUTH/LOGIN',
+            payload: dummyUserInfo
+        }
+
+        expect(auth(inputState, testLoginAction)).toEqual(outputState);
+    });
+    
+    it('should test auth reducer after LogoutAction', () : void => {
+        const inputState = state;
+        const outputState = {};
+        const testLogoutAction : LogoutAction = {
+            type: 'AUTH/LOGOUT'
+        }
+        expect(auth(inputState, testLogoutAction)).toEqual(outputState);
+    });
+
+})
+
