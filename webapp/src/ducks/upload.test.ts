b5009d71de3d853e6bb3aa27fa0fe5d83761e49f
3lbanna <76211863+3lbanna@users.noreply.github.com>
2021-05-11 11:58:40 +0200

    Introduction of webapp tests (#315)
    * Introduction of Auth reducer tests
    
    * Addition of test file templates

---
+++ webapp/src/ducks/upload.test.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import upload from './upload';
+
+
+/** ******************************************* */
+/* Test reducer                                 */
+/** ******************************************* */
+
+describe('Test upload reducer', (): void => {
+
+    it('Test 1', () : void => {
+        
+    });
+})
