95d827509f7d4c9e649d15f1d6aa3718b6d577c2
3lbanna <contact@elbanna.io>
2021-06-25 16:24:05 +0200

    Change datasheet lib

--- webapp/src/components/StudyView/StudyDataView/MatrixView/index.tsx
+++ webapp/src/components/StudyView/StudyDataView/MatrixView/index.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect, useRef } from 'react';
-import jspreadsheet from 'jspreadsheet-ce';
+import React, { useEffect, useState } from 'react';
+import ReactDataSheet from 'react-datasheet';
+import 'react-datasheet/lib/react-datasheet.css';
+// import "./styles.css";
+// import jspreadsheet from 'jspreadsheet-ce';
 import { MatrixType } from '../../../../common/types';
-import './jexcel.css';
-import './jsuites.css';
+import theme from '../../../../App/theme';
+// import './jexcel.css';
+// import './jsuites.css';
 
 interface PropTypes {
   data: MatrixType;
 }
 
-export default function MatrixView(props: PropTypes) {
+type CellType = Array<{value: number | string; readOnly: boolean | undefined}>
+
+/* export default function MatrixView(props: PropTypes) {
   // eslint-disable-next-line react/destructuring-assignment
   const { data = [], columns = [], index = [] } = props.data;
   const jRef = useRef(null);
@@ -36,6 +42,45 @@ export default function MatrixView(props: PropTypes) {
   return (
     <div>
       <div ref={jRef} />
+    </div>
+  );
+}
+*/
+export default function MatrixView(props: PropTypes) {
+  // eslint-disable-next-line react/destructuring-assignment
+  const { data = [], columns = [], index = [] } = props.data;
+  const prependIndex = index.length > 0 && typeof index[0] === 'string';
+  const [grid, setGrid] = useState<Array<CellType>>([]);
+
+  useEffect(() => {
+    const columns_data: CellType = (prependIndex ? [{ value: 'Time', readOnly: true }] : []).concat(
+      columns.map((title) => ({ value: String(title), readOnly: true })),
+    );
+    const tmp_grid = [columns_data];
+    const tmp_data = data.map((row, i) => {
+      const tmp_row = row.map((item) => ({ value: item, readOnly: false }));
+
+      return (
+        prependIndex ? [{ value: index[i], readOnly: true }].concat(tmp_row) : tmp_row
+      );
+    });
+    tmp_grid.concat(tmp_data);
+    setGrid(tmp_grid.concat(tmp_data));
+  }, [columns, data, index, prependIndex]);
+
+  return (
+    <div>
+      <ReactDataSheet
+        data={grid}
+        valueRenderer={(cell) => cell.value}
+        onCellsChanged={(changes) => {
+          const new_grid = grid.map((row) => [...row]);
+          changes.forEach(({ cell, row, col, value }) => {
+            new_grid[row][col] = value ? { value, readOnly: false } : new_grid[row][col];
+          });
+          setGrid(new_grid);
+        }}
+      />
     </div>
   );
 }
