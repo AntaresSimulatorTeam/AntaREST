4fe3c68a423137b3ed58249ae5172d9e7b1a4f8c
Paul Bui-Quang <paul.buiquang@rte-france.com>
2021-07-02 20:40:07 +0200

    Type some rawnode into matrixnode
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- antarest/storage/repository/filesystem/root/input/reserves/area.py
+++ antarest/storage/repository/filesystem/root/input/reserves/area.py
@@ -1,5 +1,10 @@
-from antarest.storage.repository.filesystem.raw_file_node import RawFileNode
+from antarest.storage.repository.filesystem.config.model import StudyConfig
+from antarest.storage.repository.filesystem.context import ContextServer
+from antarest.storage.repository.filesystem.matrix.input_series_matrix import (
+    InputSeriesMatrix,
+)
 
 
-class InputReservesArea(RawFileNode):
-    pass
+class InputReservesArea(InputSeriesMatrix):
+    def __init__(self, context: ContextServer, config: StudyConfig):
+        super(InputReservesArea, self).__init__(context, config)
