97e9a2ab16a621f51a9b7077c800202e5b0ff2a0
Paul Bui-Quang <paul.buiquang@rte-france.com>
2021-07-13 19:16:53 +0200

    Fix mypy
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- antarest/db.py
+++
@@ -1,9 +0,0 @@
-from alembic.config import Config
-from alembic import command
-
-alembic_cfg = Config("alembic.ini")
-
-
-def get_current():
-    command.current(alembic_cfg)
-    command.upgrade(alembic_cfg, "head")
