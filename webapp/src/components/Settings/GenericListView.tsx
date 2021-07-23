b59e13e6bef98b843195139636a7a1eb402ca6ad
3lbanna <contact@elbanna.io>
2021-07-23 19:49:17 +0200

    Generic List UI refactoring

--- webapp/src/components/Settings/GenericListView.tsx
+++ webapp/src/components/Settings/GenericListView.tsx
@@ -4,14 +4,12 @@ import {
   createStyles,
   Theme,
   Typography,
-  Button,
   List,
   ListItem,
 } from '@material-ui/core';
 import CreateIcon from '@material-ui/icons/Create';
 import VisibilityIcon from '@material-ui/icons/Visibility';
-import DeleteIcon from '@material-ui/icons/Delete';
-
+import DeleteIcon from '@material-ui/icons/HighlightOff';
 import { UserDTO, BotDTO, MatrixDataSetDTO, IDType } from '../../common/types';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -29,12 +27,18 @@ const useStyles = makeStyles((theme: Theme) =>
     userItem: {
       display: 'flex',
       padding: theme.spacing(1),
+      paddingLeft: theme.spacing(2),
       flexFlow: 'row nowrap',
       justifyContent: 'flex-start',
       color: theme.palette.primary.main,
       backgroundColor: 'white',
-      border: `1px solid ${theme.palette.primary.main}`,
+      borderRadius: theme.shape.borderRadius,
+      border: `1px solid ${theme.palette.action.focus}`,
       margin: theme.spacing(0.2),
+      height: '50px',
+      '&:hover': {
+        backgroundColor: theme.palette.action.hover,
+      },
     },
     iconsContainer: {
       flex: '1',
@@ -43,11 +47,26 @@ const useStyles = makeStyles((theme: Theme) =>
       justifyContent: 'flex-end',
       alignItems: 'center',
     },
+    text: {
+      backgroundColor: theme.palette.action.selected,
+      fontWeight: 'bold',
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+      borderRadius: theme.shape.borderRadius,
+    },
     deleteIcon: {
-      color: theme.palette.error.main,
+      color: theme.palette.error.light,
+      marginLeft: theme.spacing(2),
+      marginRight: theme.spacing(2),
+      '&:hover': {
+        color: theme.palette.error.main,
+      },
     },
     actionIcon: {
       color: theme.palette.primary.main,
+      '&:hover': {
+        color: theme.palette.primary.light,
+      },
     },
   }));
 
@@ -76,19 +95,15 @@ const GenericListView = (props: PropTypes) => {
           !excludeName?.find((elm) => elm === item.name) &&
           matchFilter(item.name) && (
             <Fragment key={item.id}>
-              <ListItem className={classes.userItem} button>
-                <Typography>{item.name}</Typography>
+              <ListItem className={classes.userItem}>
+                <Typography className={classes.text}>{item.name}</Typography>
                 <div className={classes.iconsContainer}>
-                  <Button onClick={() => onActionClick(item.id)}>
-                    {view ? (
-                      <VisibilityIcon className={classes.actionIcon} />
-                    ) : (
-                      <CreateIcon className={classes.actionIcon} />
-                    )}
-                  </Button>
-                  <Button onClick={() => onDeleteClick(item.id)}>
-                    <DeleteIcon className={classes.deleteIcon} />
-                  </Button>
+                  {view ? (
+                    <VisibilityIcon className={classes.actionIcon} onClick={() => onActionClick(item.id)} />
+                  ) : (
+                    <CreateIcon className={classes.actionIcon} onClick={() => onActionClick(item.id)} />
+                  )}
+                  <DeleteIcon className={classes.deleteIcon} onClick={() => onDeleteClick(item.id)} />
                 </div>
               </ListItem>
             </Fragment>
