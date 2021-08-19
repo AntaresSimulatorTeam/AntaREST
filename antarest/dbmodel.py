608c47fecdbbefafba474314d53160a44585d2db
Paul Bui-Quang <paul.buiquang@rte-france.com>
2021-08-19 15:02:10 +0200

    wip
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- antarest/dbmodel.py
+++ antarest/dbmodel.py
@@ -3,5 +3,6 @@ from antarest.study import model as studymodel
 from antarest.launcher import model as launchermodel
 from antarest.login import model as loginmodel
 from antarest.matrixstore import model as matrixstoremodel
+from antarest.core.tasks import model as tasksmodel
 
 Base = PersistenceBase
--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -4,5 +4,5 @@ CURDIR=$(cd `dirname $0` && pwd)
 BASEDIR=`dirname $CURDIR`
 
 cd $BASEDIR
-alembic downgrade 15f3bce09264
+alembic downgrade ae61e887e309
 cd -
