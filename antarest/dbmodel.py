8275cc1806e88dd47fd40891936024c427860b86
CHEIKH Bilal Ext <cheikhbil@gm0winl229.bureau.si.interne>
2021-08-21 00:44:03 +0200

    fix dbmodel

--- antarest/dbmodel.py
+++ antarest/dbmodel.py
@@ -1,3 +1,9 @@
 from antarest.core.persistence import Base as PersistenceBase
+from antarest.study import model as studymodel
+from antarest.launcher import model as launchermodel
+from antarest.login import model as loginmodel
+from antarest.matrixstore import model as matrixstoremodel
+from antarest.core.tasks import model as tasksmodel
+from antarest.study.storage.variantstudy.model import dbmodel as variantmodel
 
 Base = PersistenceBase
