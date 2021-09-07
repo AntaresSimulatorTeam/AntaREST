dd173b9c41391455559f62c44a0a59573ec4f712
Paul Bui-Quang <paul.buiquang@rte-france.com>
2021-09-07 17:50:35 +0200

    Add simple cli vm tool
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

---
+++ antarest/tools/lib.py
@@ -0,0 +1,67 @@
+import json
+import requests
+from pathlib import Path
+from typing import List, Optional
+
+from pydantic import BaseModel
+from requests import Session
+
+from antarest.study.storage.variantstudy.model import (
+    CommandDTO,
+    GenerationResultInfoDTO,
+)
+
+
+class CommandList(BaseModel):
+    __root__: List[CommandDTO]
+
+
+class CLIVariantManager:
+    def __init__(
+        self,
+        host: Optional[str] = None,
+        token: Optional[str] = None,
+        session: Optional[Session] = None,
+    ):
+        self.session = session or requests.session()
+        self.host = host
+        if session is None and host is None:
+            raise ValueError("Missing either session or host")
+        if token is not None:
+            self.session.headers.update({"Authorization": f"Bearer {token}"})
+
+    def parse_commands(self, file: Path) -> List[CommandDTO]:
+        with open(file, "r") as fh:
+            json_commands = json.load(fh)
+
+        commands: List[CommandDTO] = []
+        for command in json_commands:
+            commands.append(CommandDTO.parse_obj(command))
+
+        return commands
+
+    def apply_commands(
+        self,
+        study_id: str,
+        commands: List[CommandDTO],
+    ) -> GenerationResultInfoDTO:
+        study = self.session.get(
+            self.build_url(f"/v1/studies/{study_id}")
+        ).json()
+        assert study is not None
+
+        res = self.session.post(
+            self.build_url(f"/v1/studies/{study_id}/commands"),
+            json=CommandList(__root__=commands).dict(),
+        )
+        assert res.status_code == 200
+
+        res = self.session.get(
+            self.build_url(f"/v1/studies/{study_id}/generate")
+        )
+        return GenerationResultInfoDTO.parse_obj(res.json())
+
+    def build_url(self, url):
+        if self.host is not None:
+            return f"{self.host}/{url}"
+        return url
