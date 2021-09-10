c19b692f16a30152fbc13e2d72510e335b8966df
Paul Bui-Quang <paul.buiquang@rte-france.com>
2021-09-10 15:44:58 +0200

    Fix migrations
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- alembic/versions/c83b8bb38a7c_fix_permissions.py
+++ alembic/versions/c83b8bb38a7c_fix_permissions.py
@@ -21,7 +21,6 @@ def upgrade():
     if op.get_context().dialect.name == 'sqlite':
         return
     op.execute("GRANT USAGE, SELECT ON SEQUENCE tasklog_id_sequence TO public;")
-    op.execute("GRANT USAGE, SELECT ON SEQUENCE tasklogjob_id_seq TO public;")
     #op.execute("GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO public;")
     # ### end Alembic commands ###
 
--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -4,5 +4,5 @@ CURDIR=$(cd `dirname $0` && pwd)
 BASEDIR=`dirname $CURDIR`
 
 cd $BASEDIR
-alembic downgrade 3292cdb4557a
+alembic downgrade ae61e887e309
 cd -
