1a0e9fa81c37520b25a0adbc83a53633c7ea1ca0
3lbanna <76211863+3lbanna@users.noreply.github.com>
2021-09-28 18:06:42 +0200

    add list virtualization (#516)

--- webapp/src/components/Variants/Edition/DraggableCommands/CommandListItem.tsx
+++ webapp/src/components/Variants/Edition/DraggableCommands/CommandListItem.tsx
@@ -1,22 +1,36 @@
-import React, { useState } from 'react';
-import { Draggable } from 'react-beautiful-dnd';
+// @flow
+import React, { CSSProperties, useState } from 'react';
+import clsx from 'clsx';
+import { DraggableProvided } from 'react-beautiful-dnd';
 import ReactJson, { InteractionProps } from 'react-json-view';
-import { Accordion, AccordionDetails, AccordionSummary, Container, createStyles, Theme, Typography } from '@material-ui/core';
-import makeStyles from '@material-ui/core/styles/makeStyles';
+import DeleteIcon from '@material-ui/icons/HighlightOff';
+import { Accordion, AccordionDetails, AccordionSummary, createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import SaveOutlinedIcon from '@material-ui/icons/SaveOutlined';
-import DeleteIcon from '@material-ui/icons/HighlightOff';
 import { CommandItem } from '../CommandTypes';
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
-  container: {
+  item: {
     boxSizing: 'border-box',
     display: 'flex',
     flexFlow: 'row nowrap',
     justifyContent: 'space-between',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     width: '100%',
     height: 'auto',
+    userSelect: 'none',
+    maxWidth: '800px',
+  },
+  itemContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  onTopVisible: {
+    zIndex: 10000,
+  },
+  onTopInvisible: {
+    zIndex: 9999,
+    transition: '1s',
   },
   normalItem: {
     flex: 1,
@@ -26,7 +40,25 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
   },
   draggingListItem: {
     flex: 1,
-    background: 'rgb(235,235,235)',
+    borderColor: theme.palette.secondary.main,
+    boxShadow: `0px 0px 2px rgb(8, 58, 30), 0px 0px 10px ${theme.palette.secondary.main}`,
+  },
+  deleteIcon: {
+    flex: '0 0 24px',
+    color: theme.palette.error.light,
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    cursor: 'pointer',
+    '&:hover': {
+      color: theme.palette.error.main,
+    },
+  },
+  infos: {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    boxSizing: 'border-box',
   },
   details: {
     width: '100%',
@@ -64,87 +96,101 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
     zIndex: 999, // for json edition modal to be up everything else
-  },
-  deleteIcon: {
-    flex: '0 0 24px',
-    color: theme.palette.error.light,
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    '&:hover': {
-      color: theme.palette.error.main,
-    },
-  },
-  infos: {
-    display: 'flex',
-    flexFlow: 'column nowrap',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    boxSizing: 'border-box',
+    maxHeight: '400px',
+    overflow: 'scroll',
   },
 }));
 
-export type DraggableListItemProps = {
-  item: CommandItem;
-  index: number;
-  onDelete: (index: number) => void;
-  onArgsUpdate: (index: number, json: object) => void;
-  onSave: (index: number) => void;
-};
+interface StyleType {
+    provided: DraggableProvided;
+    style: CSSProperties;
+    isDragging: boolean;
+}
+function getStyle({ provided, style, isDragging }: StyleType) {
+  // If you don't want any spacing between your items
+  // then you could just return this.
+  // I do a little bit of magic to have some nice visual space
+  // between the row items
+  const combined = {
+    ...style,
+    ...provided.draggableProps.style,
+  };
 
-const CommandListItem = ({ item, index, onDelete, onArgsUpdate, onSave }: DraggableListItemProps) => {
+  const marginBottom = 8;
+  const withSpacing = {
+    ...combined,
+    height: isDragging ? combined.height : (combined.height as number) - marginBottom,
+    marginBottom,
+  };
+  return withSpacing;
+}
+
+interface PropsType {
+    provided: DraggableProvided;
+    item: CommandItem;
+    style: CSSProperties;
+    isDragging: boolean;
+    index: number;
+    onDelete: (index: number) => void;
+    onArgsUpdate: (index: number, json: object) => void;
+    onSave: (index: number) => void;
+}
+
+function Item({ provided, item, style, isDragging, index, onDelete, onArgsUpdate, onSave }: PropsType) {
   const classes = useStyles();
   const [jsonData, setJsonData] = useState<object>(item.args);
+  const [isExpanded, setExpanded] = useState<boolean>(false);
 
   const updateJson = (e: InteractionProps) => {
     setJsonData(e.updated_src);
     onArgsUpdate(index, e.updated_src);
   };
-
   return (
-    <Draggable draggableId={`${item.id}${index}`} index={index}>
-      {(provided, snapshot) => (
-        <Container
-          className={classes.container}
-          ref={provided.innerRef}
-        // eslint-disable-next-line react/jsx-props-no-spreading
-          {...provided.draggableProps}
-        // eslint-disable-next-line react/jsx-props-no-spreading
-          {...provided.dragHandleProps}
+    <div
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...provided.draggableProps}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...provided.dragHandleProps}
+      ref={provided.innerRef}
+      style={getStyle({ provided, style, isDragging })}
+      className={isExpanded ? clsx(classes.itemContainer, classes.onTopVisible) : clsx(classes.itemContainer, classes.onTopInvisible)}
+    >
+      <div className={classes.item}>
+        <Accordion
+          className={isDragging ? classes.draggingListItem : classes.normalItem}
         >
-          <Accordion
-            className={snapshot.isDragging ? classes.draggingListItem : classes.normalItem}
+          <AccordionSummary
+            expandIcon={<ExpandMore />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            onClick={() => setExpanded(!isExpanded)}
           >
-            <AccordionSummary
-              expandIcon={<ExpandMore />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-              style={{ width: '90%' }}
-            >
-              <div className={classes.infos}>
-                <Typography color="primary" style={{ fontSize: '0.9em' }}>{item.action}</Typography>
-                <Typography style={{ fontSize: '0.8em', color: 'gray' }}>{item.id}</Typography>
+            <div className={classes.infos}>
+              <Typography color="primary" style={{ fontSize: '0.9em' }}>{item.action}</Typography>
+              <Typography style={{ fontSize: '0.8em', color: 'gray' }}>{item.id}</Typography>
+            </div>
+          </AccordionSummary>
+          <AccordionDetails className={classes.details}>
+            <div className={classes.details}>
+              <div className={classes.header}>
+                {item.updated && <SaveOutlinedIcon className={classes.headerIcon} onClick={() => onSave(index)} />}
+                {
+                    // <CloudDownloadOutlinedIcon className={classes.headerIcon} />
+                    // <CloudUploadOutlinedIcon className={classes.headerIcon} />
+                    }
               </div>
-            </AccordionSummary>
-            <AccordionDetails className={classes.details}>
-              <div className={classes.details}>
-                <div className={classes.header}>
-                  {item.updated && <SaveOutlinedIcon className={classes.headerIcon} onClick={() => onSave(index)} />}
-                  {
-                   // <CloudDownloadOutlinedIcon className={classes.headerIcon} />
-                   // <CloudUploadOutlinedIcon className={classes.headerIcon} />
-                  }
-                </div>
-                <div className={classes.json}>
-                  <ReactJson src={jsonData} onEdit={updateJson} onDelete={updateJson} onAdd={updateJson} />
-                </div>
+              <div className={classes.json}>
+                <ReactJson src={jsonData} onEdit={updateJson} onDelete={updateJson} onAdd={updateJson} />
               </div>
-            </AccordionDetails>
-          </Accordion>
+            </div>
+          </AccordionDetails>
+        </Accordion>
+        <div style={{ height: '100%', display: 'flex', alignItems: 'center' }}>
           <DeleteIcon className={classes.deleteIcon} onClick={() => onDelete(index)} />
-        </Container>
-      )}
-    </Draggable>
+        </div>
+      </div>
+    </div>
   );
-};
+}
 
-export default CommandListItem;
+export default Item;
