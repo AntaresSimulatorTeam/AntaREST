dc7bb4cc990ef406c4665a673542c971d5325fbf
Paul Bui-Quang <paul.buiquang@rte-france.com>
2021-10-07 16:44:27 +0200

    Add task refresh on get
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- antarest/core/tasks/repository.py
+++ antarest/core/tasks/repository.py
@@ -15,6 +15,7 @@ class TaskJobRepository:
 
     def get(self, id: str) -> Optional[TaskJob]:
         task: TaskJob = db.session.get(TaskJob, id)
+        db.session.refresh(task)
         return task
 
     def list(
