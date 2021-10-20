64364c030f664d4d135c1a7a22147747759b7726
3lbanna <contact@elbanna.io>
2021-10-20 15:05:36 +0200

    remove print

--- webapp/src/services/utils/globalWsListeners.ts
+++ webapp/src/services/utils/globalWsListeners.ts
@@ -9,15 +9,12 @@ const studyListener = (reduxStore: Store<AppState>) => async (ev: WSMessage) => 
   const studySummary = ev.payload as StudySummary;
   switch (ev.type) {
     case WSEvent.STUDY_CREATED:
-      console.log('YES SIR - STUDY CREATED: ', studySummary.id);
       reduxStore.dispatch(addStudies([await getStudyMetadata(studySummary.id)]));
       break;
     case WSEvent.STUDY_DELETED:
-      console.log('YES SIR - STUDY DELETED: ', studySummary.id);
       reduxStore.dispatch(removeStudies([studySummary.id]));
       break;
     case WSEvent.STUDY_EDITED:
-      console.log('YES SIR - STUDY EDITED: ', studySummary.id);
       reduxStore.dispatch(addStudies([await getStudyMetadata(studySummary.id)]));
       break;
 
