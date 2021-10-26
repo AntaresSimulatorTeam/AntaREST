a041880239276fc0e4a5016316c03cdfe74f7396
Alexis <alexis.martinet@rte-france.com>
2021-10-26 17:53:38 +0200

    button table or graph

--- webapp/src/components/ui/MatrixView.tsx
+++ webapp/src/components/ui/MatrixView.tsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import Plot from 'react-plotly.js';
 import { HotTable, HotColumn } from '@handsontable/react';
-import { createStyles, makeStyles, ButtonGroup, Button } from '@material-ui/core';
+import { createStyles, makeStyles, Theme, ButtonGroup, Button } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { MatrixType } from '../../common/types';
 import 'handsontable/dist/handsontable.min.css';
 
-const useStyles = makeStyles(() => createStyles({
+const useStyles = makeStyles((theme: Theme) => createStyles({
   root: {
     width: '100%',
     height: '100%',
     overflow: 'auto',
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center',
     alignItems: 'center',
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
   },
 }));
 
@@ -28,12 +32,12 @@ type ColumnsType = {title: string; readOnly: boolean};
 export default function MatrixView(props: PropTypes) {
   // eslint-disable-next-line react/destructuring-assignment
   const { readOnly, matrix } = props;
-  console.log(matrix);
   const { data = [], columns = [], index = [] } = matrix;
   const classes = useStyles();
   const prependIndex = index.length > 0 && typeof index[0] === 'string';
   const [grid, setGrid] = useState<Array<CellType>>([]);
   const [formatedColumns, setColumns] = useState<Array<ColumnsType>>([]);
+  const [toggleView, setToggleView] = useState<boolean>(true);
 
   useEffect(() => {
     const columnsData: Array<ColumnsType> = (prependIndex ? [{ title: 'Time', readOnly }] : []).concat(
@@ -47,38 +51,69 @@ export default function MatrixView(props: PropTypes) {
     setGrid(tmpData);
   }, [columns, data, index, prependIndex, readOnly]);
 
-  console.log(data.map((a) => a[0]));
-  console.log(index);
-  /*      
-            <Plot
-        data={[
+  const handleView = () => {
+    let view;
+    if (toggleView) {
+      view = (
+        <HotTable
+          data={grid}
+          licenseKey="non-commercial-and-evaluation"
+          width="100%"
+          height="100%"
+        >
           {
-            x: index,
-            y: data.map((a) => a[0]),
-            type: 'scatter',
-            mode: 'lines',
-            marker: { color: 'red' },
-          },
-        ]}
-        layout={{ width: 960, height: 720 }}
-      />*/
+            formatedColumns.map((column) =>
+              <HotColumn key={column.title} settings={column} />)
+          }
+        </HotTable>
+      );
+    } else {
+      view = (
+        <Plot
+          data={columns.map((val, i) => (
+            {
+              x: index,
+              y: data.map((a) => a[i]),
+              type: 'scatter',
+              mode: 'lines',
+            }
+          ))}
+          layout={{ width: 960, height: 720 }}
+        />
+      );
+    }
+    return view;
+  };
+
+  const changeView = () => setToggleView(!toggleView);
+
+  useEffect(() => {
+    handleView();
+  }, [toggleView]);
+
   return (
     <div className={classes.root}>
-      <ButtonGroup variant="contained">
-        <Button>One</Button>
-        <Button>Two</Button>
+      <ButtonGroup className={classes.button} variant="contained">
+        {toggleView ? (
+          <Button color="primary" disabled>
+            <FontAwesomeIcon size="2x" icon="table" />
+          </Button>
+        ) : (
+          <Button color="primary" onClick={changeView}>
+            <FontAwesomeIcon size="2x" icon="table" />
+          </Button>
+        )}
+        {toggleView ? (
+          <Button color="primary" onClick={changeView}>
+            <FontAwesomeIcon size="2x" icon="chart-area" />
+          </Button>
+        ) : (
+          <Button color="primary" disabled>
+            <FontAwesomeIcon size="2x" icon="chart-area" />
+          </Button>
+        )}
       </ButtonGroup>
-      <HotTable
-        data={grid}
-        licenseKey="non-commercial-and-evaluation"
-        width="100%"
-        height="100%"
-      >
-        {
-          formatedColumns.map((column) =>
-            <HotColumn key={column.title} settings={column} />)
-        }
-      </HotTable>
+      {handleView()}
     </div>
   );
 }
