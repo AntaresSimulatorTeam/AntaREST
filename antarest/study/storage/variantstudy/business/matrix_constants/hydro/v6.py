4174d17721d17e276383a26dfead3999dd5c2d8c
Charly Bion <charly.bion@rte-france.com>
2021-10-27 16:12:00 +0200

    fix tests

--- antarest/study/storage/variantstudy/business/matrix_constants/hydro/v6.py
+++ antarest/study/storage/variantstudy/business/matrix_constants/hydro/v6.py
@@ -1,1 +1,5 @@
-reservoir = [[0, 0.5, 1]] * 12
+from typing import List
+
+from antarest.matrixstore.model import MatrixData
+
+reservoir: List[List[MatrixData]] = [[0.0, 0.5, 1.0]] * 12
--- antarest/study/storage/variantstudy/business/matrix_constants/hydro/v7.py
+++ antarest/study/storage/variantstudy/business/matrix_constants/hydro/v7.py
@@ -2,7 +2,7 @@ from typing import List
 
 from antarest.matrixstore.model import MatrixData
 
-credit_modulations: List[List[MatrixData]] = [[1] * 101] * 2
-inflow_pattern: List[List[MatrixData]] = [[1]] * 365
-max_power: List[List[MatrixData]] = [[0, 24, 0, 24]] * 365
-reservoir: List[List[MatrixData]] = [[0, 0.5, 1]] * 365
+credit_modulations: List[List[MatrixData]] = [[1.0] * 101] * 2
+inflow_pattern: List[List[MatrixData]] = [[1.0]] * 365
+max_power: List[List[MatrixData]] = [[0.0, 24.0, 0.0, 24.0]] * 365
+reservoir: List[List[MatrixData]] = [[0.0, 0.5, 1.0]] * 365
