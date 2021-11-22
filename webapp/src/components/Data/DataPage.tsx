bab389970147f6f5466e1636d908839cba740bdb
Alexis <alexis.martinet@rte-france.com>
2021-11-22 17:28:07 +0100

    Skeleton Loader in DataPage

--- webapp/src/components/Data/DataPage.tsx
+++ webapp/src/components/Data/DataPage.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
+import ContentLoader from 'react-content-loader';
 import { AxiosError } from 'axios';
+import { makeStyles, createStyles } from '@material-ui/core';
 import { useSnackbar } from 'notistack';
 import { useTranslation } from 'react-i18next';
 import { AppState } from '../../App/reducers';
@@ -13,6 +15,25 @@ import ConfirmationModal from '../ui/ConfirmationModal';
 import MatrixModal from './MatrixModal';
 import enqueueErrorSnackbar from '../ui/ErrorSnackBar';
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    contentloader: {
+      width: '100%',
+      height: '100%',
+    },
+    contentloader1: {
+      width: '100%',
+      height: '100%',
+      zIndex: 0,
+      position: 'absolute',
+    },
+    contentloader2: {
+      width: '100%',
+      height: '100%',
+      zIndex: 10,
+      position: 'absolute',
+    },
+  }));
 const mapState = (state: AppState) => ({
   user: state.auth.user,
 });
@@ -24,10 +45,12 @@ type PropTypes = ReduxProps;
 const Data = (props: PropTypes) => {
   const [t] = useTranslation();
   const { enqueueSnackbar } = useSnackbar();
+  const classes = useStyles();
   const [dataList, setDataList] = useState<Array<MatrixDataSetDTO>>([]);
   const [idForDeletion, setIdForDeletion] = useState<IDType>(-1);
   const [filter, setFilter] = useState<string>('');
   const { user } = props;
+  const [loaded, setLoaded] = useState(false);
 
   // User modal
   const [openModal, setOpenModal] = useState<boolean>(false);
@@ -95,6 +118,8 @@ const Data = (props: PropTypes) => {
         setDataList(matrix);
       } catch (e) {
         enqueueErrorSnackbar(enqueueSnackbar, t('data:matrixError'), e as AxiosError);
+      } finally {
+        setLoaded(true);
       }
     };
     init();
@@ -110,7 +135,7 @@ const Data = (props: PropTypes) => {
       buttonValue={t('data:createMatrix')}
       onButtonClick={() => createNewData()}
     >
-
+      {loaded && (
       <DataView
         data={dataList}
         filter={filter}
@@ -119,6 +144,61 @@ const Data = (props: PropTypes) => {
         onUpdateClick={onUpdateClick}
         onMatrixClick={onMatrixClick}
       />
+      )}
+      {!loaded && (
+      <div className={classes.contentloader}>
+        <ContentLoader
+          speed={2}
+          backgroundColor="#dedede"
+          foregroundColor="#ececec"
+          className={classes.contentloader1}
+        >
+          <rect x="8%" y="3%" rx="2" ry="2" width="64%" height="6%" />
+          <rect x="8%" y="9.4%" rx="2" ry="2" width="64%" height="6%" />
+          <rect x="8%" y="15.8%" rx="2" ry="2" width="64%" height="6%" />
+          <rect x="8%" y="22.2%" rx="2" ry="2" width="64%" height="6%" />
+          <rect x="8%" y="28.6%" rx="2" ry="2" width="64%" height="6%" />
+        </ContentLoader>
+        <ContentLoader
+          speed={2}
+          backgroundColor="#B9B9B9"
+          foregroundColor="#ececec"
+          className={classes.contentloader2}
+        >
+          <rect x="8%" y="3%" rx="4" ry="4" width=".5%" height="6%" />
+          <rect x="8%" y="9.4%" rx="4" ry="4" width=".5%" height="6%" />
+          <rect x="8%" y="15.8%" rx="4" ry="4" width=".5%" height="6%" />
+          <rect x="8%" y="22.2%" rx="4" ry="4" width=".5%" height="6%" />
+          <rect x="8%" y="28.6%" rx="4" ry="4" width=".5%" height="6%" />
+
+          <rect x="9.7%" y="4.5%" rx="4" ry="4" width="5%" height="3%" />
+          <rect x="9.7%" y="10.9%" rx="4" ry="4" width="7%" height="3%" />
+          <rect x="9.7%" y="17.3%" rx="4" ry="4" width="3%" height="3%" />
+          <rect x="9.7%" y="23.7%" rx="4" ry="4" width="10%" height="3%" />
+          <rect x="9.7%" y="30.1%" rx="4" ry="4" width="6%" height="3%" />
+
+          <rect x="65.4%" y="4.75%" rx="2" ry="2" width="1.5%" height="2.75%" />
+          <rect x="67.6%" y="4.75%" rx="2" ry="2" width="1.5%" height="2.75%" />
+          <rect x="69.8%" y="4.75%" rx="2" ry="2" width="1.5%" height="2.75%" />
+
+          <rect x="65.4%" y="11.15%" rx="2" ry="2" width="1.5%" height="2.75%" />
+          <rect x="67.6%" y="11.15%" rx="2" ry="2" width="1.5%" height="2.75%" />
+          <rect x="69.8%" y="11.15%" rx="2" ry="2" width="1.5%" height="2.75%" />
+
+          <rect x="65.4%" y="17.55%" rx="2" ry="2" width="1.5%" height="2.75%" />
+          <rect x="67.6%" y="17.55%" rx="2" ry="2" width="1.5%" height="2.75%" />
+          <rect x="69.8%" y="17.55%" rx="2" ry="2" width="1.5%" height="2.75%" />
+
+          <rect x="65.4%" y="23.95%" rx="2" ry="2" width="1.5%" height="2.75%" />
+          <rect x="67.6%" y="23.95%" rx="2" ry="2" width="1.5%" height="2.75%" />
+          <rect x="69.8%" y="23.95%" rx="2" ry="2" width="1.5%" height="2.75%" />
+
+          <rect x="65.4%" y="30.35%" rx="2" ry="2" width="1.5%" height="2.75%" />
+          <rect x="67.6%" y="30.35%" rx="2" ry="2" width="1.5%" height="2.75%" />
+          <rect x="69.8%" y="30.35%" rx="2" ry="2" width="1.5%" height="2.75%" />
+        </ContentLoader>
+      </div>
+      )}
       {matrixModal && currentMatrix && (
         <MatrixModal
           open={matrixModal} // Why 'openModal &&' ? => Otherwise previous data are still present
