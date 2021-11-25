70b8e6d1c285c252112bde889553b848aa6550ca
Alexis <alexis.martinet@rte-france.com>
2021-11-25 13:21:12 +0100

    Debounce button launch

--- webapp/src/components/StudyListing/StudyListingItemView/StudyListSummaryView.tsx
+++ webapp/src/components/StudyListing/StudyListingItemView/StudyListSummaryView.tsx
@@ -130,13 +130,14 @@ const StudyListSummaryView = (props: StudyListingItemPropTypes) => {
           </ButtonLoader>
         ) : (
           <>
-            <Button
+            <ButtonLoader
               size="small"
               style={{ color: theme.palette.secondary.main }}
               onClick={() => launchStudy(study)}
+              fakeDelay={1000}
             >
               {t('main:launch')}
-            </Button>
+            </ButtonLoader>
             <ButtonLoader
               size="small"
               style={{ color: theme.palette.primary.main }}
