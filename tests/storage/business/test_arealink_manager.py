2bdfdb8212fbbc4535f9d578907e09f57efcf13c
Paul Bui-Quang <paul.buiquang@rte-france.com>
2022-01-11 15:54:52 +0100

    Add some tests
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- tests/storage/business/test_arealink_manager.py
+++ tests/storage/business/test_arealink_manager.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from unittest.mock import Mock
 
 from antarest.study.business.link_management import LinkManager
-from antarest.study.model import RawStudy, Patch, PatchArea
+from antarest.study.model import RawStudy, Patch, PatchArea, PatchCluster
 from antarest.study.business.area_management import (
     AreaManager,
     AreaType,
@@ -12,6 +12,7 @@ from antarest.study.storage.rawstudy.model.filesystem.config.model import (
     Area,
     Set,
     Link,
+    Cluster,
 )
 from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
 from antarest.study.storage.rawstudy.model.filesystem.root.filestudytree import (
@@ -53,7 +54,7 @@ def test_get_all_area():
                     "a2": Link(filters_synthesis=[], filters_year=[]),
                     "a3": Link(filters_synthesis=[], filters_year=[]),
                 },
-                thermals=[],
+                thermals=[Cluster(id="a", name="a", enabled=True)],
                 renewables=[],
                 filters_synthesis=[],
                 filters_year=[],
@@ -77,14 +78,28 @@ def test_get_all_area():
         },
         sets={"s1": Set(areas=["a1"])},
     )
+    file_tree_mock = Mock(spec=FileStudyTree, context=Mock(), config=config)
     raw_study_service.get_raw.return_value = FileStudy(
-        config=config, tree=FileStudyTree(context=Mock(), config=config)
+        config=config, tree=file_tree_mock
     )
 
     area_manager.patch_service = Mock()
     area_manager.patch_service.get.return_value = Patch(
-        areas={"a1": PatchArea(country="fr")}
+        areas={"a1": PatchArea(country="fr")},
+        thermal_clusters={"a1.a": PatchCluster.parse_obj({"code-oi": "1"})},
     )
+    file_tree_mock.get.side_effect = [
+        {
+            "a": {
+                "name": "A",
+                "unitcount": 1,
+                "nominalcapacity": 500,
+                "min-stable-power": 200,
+            }
+        },
+        {},
+        {},
+    ]
 
     expected_areas = [
         {
@@ -92,7 +107,24 @@ def test_get_all_area():
             "type": AreaType.AREA,
             "metadata": {"country": "fr"},
             "set": None,
-            "thermals": [],
+            "thermals": [
+                {
+                    "id": "a",
+                    "name": "A",
+                    "unitcount": 1,
+                    "nominalcapacity": 500,
+                    "group": None,
+                    "min_stable_power": 200,
+                    "min_up_time": None,
+                    "min_down_time": None,
+                    "spinning": None,
+                    "marginal_cost": None,
+                    "spread_cost": None,
+                    "market_bid_cost": None,
+                    "type": None,
+                    "code_oi": "1",
+                }
+            ],
             "id": "a1",
         },
         {
@@ -128,6 +160,7 @@ def test_get_all_area():
     clusters = area_manager.get_all_areas(study, AreaType.DISTRICT)
     assert expected_clusters == [area.dict() for area in clusters]
 
+    file_tree_mock.get.side_effect = [{}, {}, {}]
     expected_all = [
         {
             "name": "a1",
@@ -233,10 +266,60 @@ def test_update_area():
         areas={"a1": PatchArea(country="fr")}
     )
 
-    area_manager.patch_service.patch.side_effect = lambda x, y: y
-
     new_area_info = area_manager.update_area_metadata(
         study, "a1", PatchArea(country="fr")
     )
     assert new_area_info.id == "a1"
     assert new_area_info.metadata == {"country": "fr"}
+
+
+def test_update_clusters():
+    raw_study_service = Mock(spec=RawStudyService)
+    area_manager = AreaManager(
+        storage_service=StudyStorageService(raw_study_service, Mock())
+    )
+
+    study = RawStudy()
+    config = FileStudyTreeConfig(
+        study_path=Path("somepath"),
+        path=Path("somepath"),
+        study_id="",
+        version=-1,
+        areas={
+            "a1": Area(
+                name="a1",
+                links={},
+                thermals=[Cluster(id="a", name="a", enabled=True)],
+                renewables=[],
+                filters_synthesis=[],
+                filters_year=[],
+            )
+        },
+    )
+    file_tree_mock = Mock(spec=FileStudyTree, context=Mock(), config=config)
+    raw_study_service.get_raw.return_value = FileStudy(
+        config=config, tree=file_tree_mock
+    )
+
+    area_manager.patch_service = Mock()
+    area_manager.patch_service.get.return_value = Patch(
+        areas={"a1": PatchArea(country="fr")},
+        thermal_clusters={"a1.a": PatchCluster.parse_obj({"code-oi": "1"})},
+    )
+    file_tree_mock.get.side_effect = [
+        {
+            "a": {
+                "name": "A",
+                "unitcount": 1,
+                "nominalcapacity": 500,
+                "min-stable-power": 200,
+            }
+        }
+    ]
+
+    new_area_info = area_manager.update_thermal_cluster_metadata(
+        study, "a1", {"a": PatchCluster(type="a")}
+    )
+    assert len(new_area_info.thermals) == 1
+    assert new_area_info.thermals[0].type == "a"
+    assert new_area_info.thermals[0].code_oi == None
