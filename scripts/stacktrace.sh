617dd56d80cb65d944570ec54aeae0e573f24574
Paul Bui-Quang <paul.buiquang@rte-france.com>
2022-01-12 14:11:18 +0100

    Add stacktrace script
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- scripts/stacktrace.sh
+++ scripts/stacktrace.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+source /debug_env/bin/activate
+echo "Printing stacktraces"
+ps aux | grep python | awk '{print $2}' | xargs -I{} bash -c "echo {}; pystack --include-greenlet {}"
+echo "Done"
