80cc7aad378c06baaf260e8eccd4733c0ab2325e
3lbanna <contact@elbanna.io>
2022-02-02 14:53:38 +0100

    wip add studies card

--- webapp_v2/src/components/Studies/StudiesList.tsx
+++ webapp_v2/src/components/Studies/StudiesList.tsx
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import { StudyMetadata } from '../../common/types';
+import StudyCard from './StudyCard';
 
 interface PropTypes {
     studies: Array<StudyMetadata>;
 }
 
 const StudiesList = (props: PropTypes) => {
-
+  const { studies } = props;
   return (
     <Box flex={1} height="100%" display="flex" flexDirection="column" justifyContent="flex-start" alignItems="center"
-    boxSizing="border-box" sx={{ overflowX: 'hidden',overflowY: 'auto' }}>
-        BONJOUR
+    boxSizing="border-box" bgcolor='red' sx={{ overflowX: 'hidden',overflowY: 'auto' }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} sx={{ w: '100%', h: '100%', bgColor: 'primary.light'}}>
+          {studies.map((elm, index) => (
+            <Grid item xs={2} sm={4} md={4} key={elm.id}>
+              <StudyCard />
+            </Grid>
+          ))}
+        </Grid>
     </Box>  
   );
 }
