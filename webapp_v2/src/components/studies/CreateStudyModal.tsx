86d2e5191668932fa1debebcb4bdba979318c9ad
CHEIKH Bilal Ext <cheikhbil@gm0winl229.bureau.si.interne>
2022-03-02 20:07:54 +0100

    implement studies creation

--- webapp_v2/src/components/studies/CreateStudyModal.tsx
+++ webapp_v2/src/components/studies/CreateStudyModal.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import debug from 'debug';
 import { Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { connect, ConnectedProps } from 'react-redux';
@@ -8,14 +9,29 @@ import MultiSelect from '../common/SelectMulti';
 import { AppState } from '../../store/reducers';
 import { convertVersions } from '../../services/utils';
 import FilledTextInput from '../common/FilledTextInput';
-import { GenericInfo } from '../../common/types';
+import { GenericInfo, GroupDTO, RoleType, StudyMetadata } from '../../common/types';
 import TextSeparator from '../common/TextSeparator';
+import { createStudy, getStudyMetadata } from '../../services/api/study';
+import { addStudies, initStudiesVersion } from '../../store/study';
+import { getGroups } from '../../services/api/user';
+
+const logErr = debug('antares:createstudyform:error');
+
+interface Inputs {
+  studyname: string;
+  version: number;
+}
 
 const mapState = (state: AppState) => ({
   versions: state.study.versionList,
 });
 
-const connector = connect(mapState);
+const mapDispatch = ({
+  addStudy: (study: StudyMetadata) => addStudies([study]),
+  loadVersions: initStudiesVersion,
+});
+
+const connector = connect(mapState, mapDispatch);
   type PropsFromRedux = ConnectedProps<typeof connector>;
   interface OwnProps {
     open: boolean;
@@ -26,16 +42,52 @@ const connector = connect(mapState);
 
 function CreateStudyModal(props: PropTypes) {
   const [t] = useTranslation();
-  const { versions, open, onClose, onActionButtonClick } = props;
+  const { versions, open, addStudy, onClose, onActionButtonClick } = props;
   const versionList = convertVersions(versions || []);
-  const rightToChangeList: Array<GenericInfo> = []; // Replace by ??
-  const groupList: Array<GenericInfo> = []; // Replace by ??
   const tagList: Array<GenericInfo> = []; // Replace by ??
-  const [version, setVersion] = useState<string>();
+  const [version, setVersion] = useState<string>(versionList[versionList.length - 1].id.toString());
   const [studyName, setStudyName] = useState<string>('');
   const [rightToChange, setRightToChange] = useState<string>('');
   const [group, setGroup] = useState<string>('');
   const [tags, setTags] = useState<Array<string>>([]);
+  const [groupList, setGroupList] = useState<Array<GroupDTO>>([]);
+
+  const onSubmit = async () => {
+    if (studyName && studyName.replace(/\s+/g, '') !== '' && versionList.length > 0) {
+      try {
+        let vrs = versionList[versionList.length - 1].id as number;
+        if (version) {
+          const index = versionList.findIndex((elm) => elm.id.toString() === version);
+          if (index >= 0) { vrs = versionList[index].id as number; }
+        }
+        const sid = await createStudy(studyName, vrs, group !== '' ? [group] : undefined);
+        const metadata = await getStudyMetadata(sid);
+        addStudy(metadata);
+      } catch (e) {
+        logErr('Failed to create new study', studyName, e);
+      }
+    }
+    onClose();
+  };
+
+  const rightToChangeList: Array<GenericInfo> = [
+    { id: RoleType.ADMIN, name: t('settings:adminRole') },
+    { id: RoleType.RUNNER, name: t('settings:runnerRole') },
+    { id: RoleType.WRITER, name: t('settings:writerRole') },
+    { id: RoleType.READER, name: t('settings:readerRole') }];
+
+  const init = async () => {
+    try {
+      const groupRes = await getGroups();
+      setGroupList(groupRes);
+    } catch (error) {
+      logErr(error);
+    }
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
 
   return (
     <BasicModal
@@ -44,7 +96,7 @@ function CreateStudyModal(props: PropTypes) {
       onClose={onClose}
       closeButtonLabel={t('main:cancelButton')}
       actionButtonLabel={t('main:create')}
-      onActionButtonClick={onActionButtonClick}
+      onActionButtonClick={onSubmit}
       rootStyle={{ width: '600px', height: '500px', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', boxSizing: 'border-box' }}
     >
       <Box width="100%" height="100%" display="flex" flexDirection="column" justifyContent="flex-start" alignItems="center" p={2} boxSizing="border-box">
