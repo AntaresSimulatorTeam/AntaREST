01c21d2977868fbdc4c6bbdcadc6afeb16555fc0
Hyralc <charly6203@live.fr>
2022-03-10 14:12:50 +0100

    feature implemented. need tests

---
+++ antarest/matrixstore/business/matrix_editor.py
@@ -0,0 +1,75 @@
+import operator
+from typing import Optional, Any, Callable, List
+
+import xarray as xr
+from pydantic import BaseModel, validator
+
+from antarest.matrixstore.model import MatrixData
+
+
+class MatrixSlice(BaseModel):
+    # row_from <= cells <= row_to
+    # col_from <= cells <= col_to
+    row_from: int
+    row_to: Optional[int] = None
+    column_from: int
+    column_to: Optional[int] = None
+
+    @validator("row_to", always=True)
+    def set_row_to(cls, v: Optional[int], values: Any) -> int:
+        if v is None:
+            return values["row_from"]
+        return v
+
+    @validator("column_to", always=True)
+    def set_column_to(cls, v: Optional[int], values: Any) -> int:
+        if v is None:
+            return values["column_from"]
+        return v
+
+
+class Operation(BaseModel):
+    value: float
+    operation: str
+
+
+class MatrixEditor:
+    @staticmethod
+    def _operation_to_operator(operation: str) -> Callable:
+        operation_dict = {
+            "+": operator.add,
+            "-": operator.sub,
+            "*": operator.mul,
+            "/": operator.truediv,
+            "=": lambda x, y: y,
+            "ABS": abs,
+        }
+        return operation_dict[operation]
+
+    @classmethod
+    def update_matrix_content_with_slices(
+        cls,
+        matrix_data: List[List[MatrixData]],
+        slices: List[MatrixSlice],
+        operation: Operation,
+    ) -> List[List[MatrixData]]:
+
+        data_array = xr.DataArray(matrix_data, dims=["row", "col"])
+
+        mask = xr.zeros_like(data_array)
+        for matrix_slice in slices:
+            mask |= (
+                (data_array.coords["row"] >= matrix_slice.row_from)
+                & (data_array.coords["row"] <= matrix_slice.row_to)
+                & (data_array.coords["col"] >= matrix_slice.column_from)
+                & (data_array.coords["col"] <= matrix_slice.column_to)
+            )
+
+        operator_func = cls._operation_to_operator(operation.operation)
+
+        new_matrix_data = xr.where(
+            mask,
+            operator_func(data_array, operation.value),
+            data_array,
+        ).data.tolist()
+        return new_matrix_data
---
+++ antarest/study/business/matrix_management.py
@@ -0,0 +1,52 @@
+from typing import List
+
+from antarest.matrixstore.business.matrix_editor import Operation, MatrixSlice
+from antarest.study.business.utils import execute_or_add_commands
+from antarest.study.model import Study
+from antarest.study.storage.storage_service import StudyStorageService
+from antarest.study.storage.variantstudy.model.command.replace_matrix import (
+    ReplaceMatrix,
+)
+from antarest.study.storage.variantstudy.model.command.utils import (
+    strip_matrix_protocol,
+)
+
+
+class MatrixManager:
+    def __init__(self, storage_service: StudyStorageService):
+        self.storage_service = storage_service
+
+    def update_matrix(
+        self,
+        study: Study,
+        path: str,
+        slices: List[MatrixSlice],
+        operation: Operation,
+    ):
+        file_study = self.storage_service.get_storage(study).get_raw(study)
+        matrix_service = (
+            self.storage_service.variant_study_service.command_factory.command_context.matrix_service
+        )
+
+        whole_matrix = self.storage_service.get_storage(study).get(
+            metadata=study, url=path
+        )
+        matrix_data = whole_matrix["data"]
+        new_matrix_id = matrix_service.update_matrix(
+            matrix_data, slices, operation
+        )
+
+        command = [
+            ReplaceMatrix(
+                target=path,
+                matrix=strip_matrix_protocol(new_matrix_id),
+                command_context=self.storage_service.variant_study_service.command_factory.command_context,
+            )
+        ]
+
+        execute_or_add_commands(
+            study=study,
+            file_study=file_study,
+            commands=command,
+            storage_service=self.storage_service,
+        )
