0a22dbdc6dc42ff06d449912320d870a70b50ce3
Alexis <alexis.martinet@rte-france.com>
2022-03-16 17:52:53 +0100

    Jobs & downloads

--- webapp/src/App/Pages/Jobs.tsx
+++ webapp/src/App/Pages/Jobs.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles, createStyles } from '@material-ui/core';
+import { connect, ConnectedProps } from 'react-redux';
+import { addListener, removeListener } from '../../ducks/websockets';
 import JobsListing from '../../components/JobsListing';
 
 const useStyles = makeStyles(() =>
@@ -17,6 +19,17 @@ const useStyles = makeStyles(() =>
     },
   }));
 
+const mapState = () => ({});
+
+const mapDispatch = {
+  addWsListener: addListener,
+  removeWsListener: removeListener,
+};
+
+const connector = connect(mapState, mapDispatch);
+type ReduxProps = ConnectedProps<typeof connector>;
+type PropTypes = ReduxProps;
+
 const Jobs = () => {
   const classes = useStyles();
 
@@ -27,4 +40,87 @@ const Jobs = () => {
   );
 };
 
-export default Jobs;
+export default connector(Jobs);
+
+/*
+import React, { useEffect, useState } from 'react';
+import { makeStyles, createStyles, Theme, Breadcrumbs } from '@material-ui/core';
+import { connect, ConnectedProps } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { addListener, removeListener } from '../../ducks/websockets';
+import GenericTab from '../../components/JobListing/TabView';
+import JobManagement from '../../components/JobListing/JobManagement';
+import DownloadsManagement from '../../components/JobListing/DownloadsManagement';
+import OtherJobManagement from '../../components/JobListing/OtherJobManagement';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      flexFlow: 'column nowrap',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      flexDirection: 'column',
+      overflow: 'hidden',
+      boxSizing: 'border-box',
+    },
+    breadcrumbs: {
+      backgroundColor: '#d7d7d7',
+      width: '100%',
+      padding: theme.spacing(1),
+    },
+    breadcrumbsfirstelement: {
+      marginLeft: theme.spacing(1),
+    },
+  }));
+
+const mapState = () => ({});
+
+const mapDispatch = {
+  addWsListener: addListener,
+  removeWsListener: removeListener,
+};
+
+const connector = connect(mapState, mapDispatch);
+type ReduxProps = ConnectedProps<typeof connector>;
+type PropTypes = ReduxProps;
+
+interface MenuTab {
+  [key: string]:
+    () => JSX.Element;
+}
+
+const Jobs = () => {
+  const classes = useStyles();
+  const [navData, setNavData] = useState<MenuTab>({});
+  const [t] = useTranslation();
+
+  useEffect(() => {
+    const newNavData: {[key: string]: () => JSX.Element} = {
+      launches: () => <JobManagement />,
+      exports: () => <DownloadsManagement />,
+      others: () => <OtherJobManagement />,
+    };
+    setNavData(newNavData);
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <Breadcrumbs aria-label="breadcrumb" className={classes.breadcrumbs}>
+        <Link to="/" className={classes.breadcrumbsfirstelement}>
+          {t('main:allStudies')}
+        </Link>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          {t('main:jobs')}
+        </div>
+      </Breadcrumbs>
+      <GenericTab items={navData} initialValue="launches" />
+    </div>
+  );
+};
+
+export default connector(Jobs);
+*/
--- webapp/src/components/JobsListing/index.tsx
+++ webapp/src/components/JobsListing/index.tsx
@@ -1,22 +1,27 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect, ReactNode } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AxiosError } from 'axios';
 import { connect, ConnectedProps } from 'react-redux';
 import debug from 'debug';
 import { useTranslation } from 'react-i18next';
 import { useSnackbar } from 'notistack';
 import moment from 'moment';
-import { makeStyles, createStyles, Theme, useTheme, Typography, Box } from '@material-ui/core';
+import { makeStyles, createStyles, Theme, useTheme, Typography, Box, CircularProgress } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import BlockIcon from '@material-ui/icons/Block';
+import InfoIcon from '@material-ui/icons/Info';
 import MainContentLoader from '../ui/loaders/MainContentLoader';
+import DownloadLink from '../ui/DownloadLink';
+import LogModal from '../ui/LogModal';
+import { addListener, removeListener } from '../../ducks/websockets';
 import JobTableView from './JobTableView';
 import { convertUTCToLocalTime, useNotif } from '../../services/utils';
 import { AppState } from '../../App/reducers';
 import { downloadJobOutput, killStudy, getStudyJobs, getStudies } from '../../services/api/study';
+import { convertFileDownloadDTO, FileDownload, getDownloadUrl, FileDownloadDTO, getDownloadsList } from '../../services/api/downloads';
 import { initStudies } from '../../ducks/study';
-import { LaunchJob } from '../../common/types';
+import { LaunchJob, WSEvent, WSMessage } from '../../common/types';
 import enqueueErrorSnackbar from '../ui/ErrorSnackBar';
 import ConfirmationModal from '../ui/ConfirmationModal';
 
@@ -73,16 +78,27 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
       color: theme.palette.secondary.main,
     },
   },
+  errorIcon: {
+    width: '18px',
+    height: 'auto',
+    cursor: 'pointer',
+    color: theme.palette.error.main,
+    '&:hover': {
+      color: theme.palette.error.dark,
+    },
+  },
 }));
 
 interface JobsType {
-    name: ReactNode;
-    date: ReactNode;
-    action: ReactNode;
+    name: JSX.Element;
+    date: JSX.Element;
+    action: JSX.Element;
 }
 
 const mapState = (state: AppState) => ({
   studies: state.study.studies,
+  addWsListener: addListener,
+  removeWsListener: removeListener,
 });
 
 const mapDispatch = ({
@@ -94,16 +110,18 @@ type ReduxProps = ConnectedProps<typeof connector>;
 type PropTypes = ReduxProps;
 
 const JobsListing = (props: PropTypes) => {
-  const { studies, loadStudies } = props;
+  const { studies, loadStudies, addWsListener, removeWsListener } = props;
   const classes = useStyles();
   const [t] = useTranslation();
   const { enqueueSnackbar } = useSnackbar();
   const theme = useTheme();
   const [jobs, setJobs] = useState<LaunchJob[]>();
+  const [downloads, setDownloads] = useState<FileDownload[]>();
   const [content, setContent] = useState<Array<JobsType>>();
   const createNotif = useNotif();
   const [loaded, setLoaded] = useState(false);
   const [openConfirmationModal, setOpenConfirmationModal] = useState<string | undefined>();
+  const [messageModalOpen, setMessageModalOpen] = useState<string | undefined>();
 
   const init = async () => {
     setLoaded(false);
@@ -114,9 +132,11 @@ const JobsListing = (props: PropTypes) => {
       }
       const allJobs = await getStudyJobs(undefined, false);
       setJobs(allJobs);
+      const dlList = await getDownloadsList();
+      setDownloads(dlList);
     } catch (e) {
       logError('woops', e);
-      createNotif(t('jobs:failedtoretrievejobs'), { variant: 'error' });
+      enqueueErrorSnackbar(createNotif, 'ça marche pas', e as AxiosError);
     } finally {
       setLoaded(true);
     }
@@ -154,12 +174,39 @@ const JobsListing = (props: PropTypes) => {
   };
 
   useEffect(() => {
-    init();
-  }, []);
+    const listener = (ev: WSMessage) => {
+      if (ev.type === WSEvent.DOWNLOAD_CREATED) {
+        setDownloads((downloads || []).concat([convertFileDownloadDTO(ev.payload as FileDownloadDTO)]));
+      } else if (ev.type === WSEvent.DOWNLOAD_READY) {
+        setDownloads((downloads || []).map((d) => {
+          const fileDownload = (ev.payload as FileDownloadDTO);
+          if (d.id === fileDownload.id) {
+            return convertFileDownloadDTO(fileDownload);
+          }
+          return d;
+        }));
+      } else if (ev.type === WSEvent.DOWNLOAD_READY || ev.type === WSEvent.DOWNLOAD_FAILED) {
+        setDownloads((downloads || []).map((d) => {
+          const fileDownload = (ev.payload as FileDownloadDTO);
+          if (d.id === fileDownload.id) {
+            return convertFileDownloadDTO(fileDownload);
+          }
+          return d;
+        }));
+      } else if (ev.type === WSEvent.DOWNLOAD_EXPIRED) {
+        setDownloads((downloads || []).filter((d) => {
+          const fileDownload = (ev.payload as FileDownloadDTO);
+          return d.id !== fileDownload.id;
+        }));
+      }
+    };
+    addWsListener(listener);
+    return () => { removeWsListener(listener); };
+  }, [addWsListener, removeWsListener, downloads]);
 
   useEffect(() => {
     if (jobs) {
-      const tab = jobs
+      const tab: Array<JobsType> = jobs
         .sort((a, b) => (moment(a.completionDate || a.creationDate).isAfter(moment(b.completionDate || b.creationDate)) ? -1 : 1))
         .reduce((prevVal, curVal) => { if (prevVal.find((el) => el.studyId === curVal.studyId)) { return prevVal; } return prevVal.concat([curVal]); }, [] as LaunchJob[])
         .map((job) => ({
@@ -200,9 +247,61 @@ const JobsListing = (props: PropTypes) => {
             </Box>
           ),
         }));
+
+      console.log(tab);
+      //   if (tab.length > 1) {
+      //     if (content) {
+      //       setContent([...content, ...tab]);
+      //     } else {
       setContent(tab);
+    //     }
+    //   }
     }
   }, [jobs]);
+
+  useEffect(() => {
+    if (downloads) {
+      const tab = downloads
+        .sort((a, b) => (moment(a.expirationDate).isAfter(moment(b.expirationDate)) ? -1 : 1))
+        .map((download) => ({
+          name: (
+            <Box className={classes.title}>
+              {download.name}
+            </Box>
+          ),
+          date: (
+            <Box className={classes.dateblock}>
+              {`(${t('downloads:expirationDate')} : ${convertUTCToLocalTime(download.expirationDate)})`}
+            </Box>
+          ),
+          action: (
+            download.failed ? (
+              <InfoIcon className={classes.errorIcon} onClick={() => setMessageModalOpen(download.errorMessage)} />
+            ) : (
+              <Box>
+                {download.ready ? (
+                  <DownloadLink url={getDownloadUrl(download.id)}>
+                    <FontAwesomeIcon className={classes.downloadIcon} icon="download" />
+                  </DownloadLink>
+                ) : <CircularProgress color="primary" style={{ width: '18px', height: '18px' }} />}
+              </Box>
+            )
+          ),
+        }));
+
+      if (tab.length > 1) {
+        if (content) {
+          setContent([...tab, ...content]);
+        } else {
+          setContent(tab);
+        }
+      }
+    }
+  }, [downloads]);
+
+  useEffect(() => {
+    init();
+  }, []);
 
   return (
     <Box className={classes.root}>
@@ -217,6 +316,13 @@ const JobsListing = (props: PropTypes) => {
           handleNo={() => setOpenConfirmationModal(undefined)}
         />
       )}
+      <LogModal
+        isOpen={!!messageModalOpen}
+        title={t('singlestudy:taskLog')}
+        content={messageModalOpen}
+        close={() => setMessageModalOpen(undefined)}
+        style={{ width: '600px', height: '300px' }}
+      />
     </Box>
   );
 };
