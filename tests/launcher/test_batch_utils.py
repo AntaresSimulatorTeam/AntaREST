9bc9c38eeb6defe85826549cbe65ac793fb758e2
Paul Bui-Quang <paul.buiquang@rte-france.com>
2022-03-25 16:29:58 +0100

    Add merge stats test
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- tests/launcher/test_batch_utils.py
+++ tests/launcher/test_batch_utils.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from typing import cast
 from unittest.mock import patch, Mock
 
 from antarest.launcher.adapters.slurm_launcher.batch_job import BatchJobManager
 from antarest.study.storage.rawstudy.model.helpers import FileStudyHelpers
+import numpy as np
 
 
 @patch.object(FileStudyHelpers, "get_playlist")
@@ -26,7 +28,23 @@ def test_batch_compute_params(get_playlist_mock):
 
 
 def test_merge_stats():
-    stats = [(5, 5, 1), (5, 5, 2), (5, 5, 3)]
-    res = BatchJobManager.merge_std_deviation(stats)
-    # todo this fails on purpose just to remember to create some real test
-    assert len(res) == 1
+    data_set = [1, 1, 2, 3, 5, 6, 2, 2, 8, 9, 7, 8, 10, 0, 10, 2, 2, 3, 5]
+    subset_1 = data_set[0:5]
+    subset_2 = data_set[5:12]
+    subset_3 = data_set[12:]
+    subsets = [subset_1, subset_2, subset_3]
+    assert data_set == subset_1 + subset_2 + subset_3
+    mean = np.mean(data_set)
+    sub_set_means = [cast(float, np.mean(subset)) for subset in subsets]
+    variance = np.sqrt(np.var(data_set))
+    subset_variances = [
+        cast(float, np.sqrt(np.var(subset))) for subset in subsets
+    ]
+    res = BatchJobManager.merge_std_deviation(
+        [
+            (sub_set_means[i], subset_variances[i], len(subsets[i]))
+            for i in range(0, 3)
+        ]
+    )
+    assert f"{res[0]:.8f}" == f"{variance:.8f}"
+    assert f"{res[1]:.8f}" == f"{mean:.8f}"
