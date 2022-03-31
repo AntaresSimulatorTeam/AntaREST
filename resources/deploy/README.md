39743c0993f0dfd5dd2450acd198919fb15bdadf
Hyralc <charly6203@live.fr>
2022-03-31 11:23:15 +0200

    packaging for linux

---
+++ resources/deploy/README.md
@@ -0,0 +1,24 @@
+# Antares Web
+
+## Test Server
+
+To launch the test server, run the command
+```
+./TestServer --auto-upgrade-db
+```
+Then go to http://localhost:8080
+
+## Daemon Server
+
+To launch the test server, run the command
+```
+./AntaresWebServer
+```
+
+## Variant manager tool
+
+To use the variant manager tool, run the command
+```
+./AntaresTool
+```
+Further instruction will be provided by the command output.
--- scripts/package_antares_web_win.sh
+++ scripts/package_antares_web_win.sh
@@ -8,13 +8,13 @@ ANTARES_SOLVER_FOLDER_NAME="rte-antares-$ANTARES_SOLVER_FULL_VERSION-installer-6
 ANTARES_SOLVER_ZIPFILE_NAME="$ANTARES_SOLVER_FOLDER_NAME.zip"
 
 LINK="https://github.com/AntaresSimulatorTeam/Antares_Simulator/releases/download/v$ANTARES_SOLVER_FULL_VERSION/$ANTARES_SOLVER_ZIPFILE_NAME"
-DESTINATION="../dist/AntaresWebServer/antares_solver"
+DESTINATION="../dist/AntaresWeb/antares_solver"
 
 echo "Downloading AntaresSimulator from $LINK"
 wget $LINK
 
 echo "Unzipping $ANTARES_SOLVER_ZIPFILE_NAME and move Antares solver to $DESTINATION"
-7z x $ANTARES_SOLVER_ZIPFILE_NAME
+unzip x $ANTARES_SOLVER_ZIPFILE_NAME
 mkdir $DESTINATION
 
 mv "$ANTARES_SOLVER_FOLDER_NAME/bin/antares-$ANTARES_SOLVER_VERSION-solver.exe" $DESTINATION
@@ -23,9 +23,14 @@ mv $ANTARES_SOLVER_FOLDER_NAME/bin/zlib1.dll $DESTINATION
 
 echo "Copy basic configuration files"
 cp -r ../resources/deploy/* ../dist/
-cp ../README.md ../dist/
 sed -i "s/700: path\/to\/700/$ANTARES_SOLVER_FULL_VERSION_INT: .\/AntaresWebServer\/antares_solver\/antares-$ANTARES_SOLVER_VERSION-solver.exe/g" ../dist/config.yaml
+
+
+if [[ "$OSTYPE" == "msys"* ]]; then
 cp ../resources/AntaresWebServerShortcut.lnk ../dist/
+else
+ln -s ../dist/AntaresWebServer ../dist/AntaresWeb/AntaresWebServer
+fi
 
 echo "Cleaning up"
 rm $ANTARES_SOLVER_ZIPFILE_NAME
