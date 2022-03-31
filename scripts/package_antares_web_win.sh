6d9f80046570357a85c4a0e6b48d587a6d5fa8a1
Hyralc <charly6203@live.fr>
2022-03-31 14:25:43 +0200

    wip

--- scripts/package_antares_web_win.sh
+++ scripts/package_antares_web_win.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
 
 ANTARES_SOLVER_VERSION="8.2"
-ANTARES_SOLVER_FULL_VERSION="$ANTARES_SOLVER_VERSION.0"
+ANTARES_SOLVER_FULL_VERSION="$ANTARES_SOLVER_VERSION.1"
 ANTARES_SOLVER_FULL_VERSION_INT=$(echo $ANTARES_SOLVER_FULL_VERSION | sed 's/\.//g')
 
-ANTARES_SOLVER_FOLDER_NAME="rte-antares-$ANTARES_SOLVER_FULL_VERSION-installer-64bits"
-ANTARES_SOLVER_ZIPFILE_NAME="$ANTARES_SOLVER_FOLDER_NAME.zip"
+
+if [[ "$OSTYPE" == "msys"* ]]; then
+    ANTARES_SOLVER_FOLDER_NAME="rte-antares-$ANTARES_SOLVER_FULL_VERSION-installer-64bits"
+    ANTARES_SOLVER_ZIPFILE_NAME="$ANTARES_SOLVER_FOLDER_NAME.zip"
+else
+    ANTARES_SOLVER_FOLDER_NAME="antares-$ANTARES_SOLVER_FULL_VERSION-Ubuntu-20.04"
+    ANTARES_SOLVER_ZIPFILE_NAME="$ANTARES_SOLVER_FOLDER_NAME.tar.gz"
+fi
 
 LINK="https://github.com/AntaresSimulatorTeam/Antares_Simulator/releases/download/v$ANTARES_SOLVER_FULL_VERSION/$ANTARES_SOLVER_ZIPFILE_NAME"
 DESTINATION="../dist/AntaresWeb/antares_solver"
 
 echo "Downloading AntaresSimulator from $LINK"
-wget $LINK
+    wget $LINK
 
 echo "Unzipping $ANTARES_SOLVER_ZIPFILE_NAME and move Antares solver to $DESTINATION"
-unzip $ANTARES_SOLVER_ZIPFILE_NAME
-mkdir $DESTINATION
+    7z x $ANTARES_SOLVER_ZIPFILE_NAME
+    if [[ "$OSTYPE" != "msys"* ]]; then
+        7z x "$ANTARES_SOLVER_FOLDER_NAME.tar"
+    fi
 
-mv "$ANTARES_SOLVER_FOLDER_NAME/bin/antares-$ANTARES_SOLVER_VERSION-solver.exe" $DESTINATION
-mv $ANTARES_SOLVER_FOLDER_NAME/bin/sirius_solver.dll $DESTINATION
-mv $ANTARES_SOLVER_FOLDER_NAME/bin/zlib1.dll $DESTINATION
+    mkdir $DESTINATION
+
+    if [[ "$OSTYPE" != "msys"* ]]; then
+        mv "$ANTARES_SOLVER_FOLDER_NAME/bin/antares-$ANTARES_SOLVER_VERSION-solver.exe" $DESTINATION
+        mv $ANTARES_SOLVER_FOLDER_NAME/bin/sirius_solver.dll $DESTINATION
+        mv $ANTARES_SOLVER_FOLDER_NAME/bin/zlib1.dll $DESTINATION
+    else
+        mv "$ANTARES_SOLVER_FOLDER_NAME/bin/antares-$ANTARES_SOLVER_VERSION-solver" $DESTINATION
+    fi
+
 
 echo "Copy basic configuration files"
-cp -r ../resources/deploy/* ../dist/
-sed -i "s/700: path\/to\/700/$ANTARES_SOLVER_FULL_VERSION_INT: .\/AntaresWebServer\/antares_solver\/antares-$ANTARES_SOLVER_VERSION-solver.exe/g" ../dist/config.yaml
+    cp -r ../resources/deploy/* ../dist/
+    sed -i "s/700: path\/to\/700/$ANTARES_SOLVER_FULL_VERSION_INT: .\/AntaresWebServer\/antares_solver\/antares-$ANTARES_SOLVER_VERSION-solver.exe/g" ../dist/config.yaml
 
 
-if [[ "$OSTYPE" == "msys"* ]]; then
-cp ../resources/AntaresWebServerShortcut.lnk ../dist/
-else
-ln -s ../dist/AntaresWeb/AntaresWebServer ../dist/AntaresWebServer
-fi
+echo "Creating shortcuts"
+    if [[ "$OSTYPE" == "msys"* ]]; then
+    cp ../resources/AntaresWebServerShortcut.lnk ../dist/
+    else
+    ln -s ../dist/AntaresWeb/AntaresWebServer ../dist/AntaresWebServer
+    fi
 
 echo "Cleaning up"
-rm $ANTARES_SOLVER_ZIPFILE_NAME
-rm -rf $ANTARES_SOLVER_FOLDER_NAME
+    rm $ANTARES_SOLVER_ZIPFILE_NAME
+    rm -rf $ANTARES_SOLVER_FOLDER_NAME
 
