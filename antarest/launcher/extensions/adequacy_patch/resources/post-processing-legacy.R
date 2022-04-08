801a390bbd2ea19f32db6c14fe753a0ee60d0637
Paul Bui-Quang <paul.buiquang@rte-france.com>
2022-04-08 08:32:29 +0200

    Fix Radq patch run script
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- antarest/launcher/extensions/adequacy_patch/resources/post-processing-legacy.R
+++ antarest/launcher/extensions/adequacy_patch/resources/post-processing-legacy.R
@@ -8,15 +8,23 @@ opts <- setSimulationPath(".")
 print(readLayout(opts = opts))
 
 config <- read_yaml("user/adequacypatch/config.yml", fileEncoding = "UTF-8", text)
+areas = config$areas
+virutal_areas = config$areas
+mcYears = config$mcYears
+antaresfbzone = config$antaresfbzone
+thresholdFilter = config$thresholdFilter
+core_ahc = config$core_ahc
+calculate_mc_all = config$calculate_mc_all
+extension = config$extension
 
 run_adq(opts = opts,
-					areas = config$areas,
-					virtual_areas = config$areas,
-					mcYears = config$mcYears,
-					antaresfbzone = config$antaresfbzone,
-					ext = NULL,
-					nbcl = config$nbcore, thresholdFilter = config$thresholdFilter,
-					core_ahc = config$core_ahc)
+					areas = areas,
+					virtual_areas = areas,
+					mcYears = mcYears,
+					antaresfbzone = antaresfbzone,
+					ext = extension,
+					nbcl = 12, thresholdFilter = thresholdFilter,
+					core_ahc = core_ahc)
 
 areas <- read_yaml("user/adequacypatch/hourly-areas.yml", fileEncoding = "UTF-8", text)
 for (area in names(areas)) {
--- antarest/launcher/extensions/adequacy_patch/resources/post-processing.R
+++ antarest/launcher/extensions/adequacy_patch/resources/post-processing.R
@@ -9,13 +9,22 @@ print(readLayout(opts = opts))
 
 config <- read_yaml("user/adequacypatch/config.yml", fileEncoding = "UTF-8", text)
 
+areas = config$areas
+virutal_areas = config$areas
+mcYears = config$mcYears
+antaresfbzone = config$antaresfbzone
+thresholdFilter = config$thresholdFilter
+core_ahc = config$core_ahc
+calculate_mc_all = config$calculate_mc_all
+extension = config$extension
+
 run_adq(opts = opts,
-					areas = config$areas,
-					virtual_areas = config$areas,
-					mcYears = config$mcYears,
-					antaresfbzone = config$antaresfbzone,
-					ext = NULL,
-					nbcl = config$nbcore, thresholdFilter = config$thresholdFilter,
-					core_ahc = config$core_ahc,
-                    calculate_mc_all = config$calculate_mc_all,
+					areas = areas,
+					virtual_areas = areas,
+					mcYears = mcYears,
+					antaresfbzone = antaresfbzone,
+					ext = extension,
+					nbcl = 12, thresholdFilter = thresholdFilter,
+					core_ahc = core_ahc,
+                    calculate_mc_all = calculate_mc_all,
                     log_detail = TRUE)
