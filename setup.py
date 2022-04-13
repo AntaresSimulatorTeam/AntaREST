bfcc96a273ea2943a17afc257aedc5637c46c54c
Paul Bui-Quang <paul.buiquang@rte-france.com>
2022-04-13 16:31:33 +0200

    Update version
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AntaREST",
-    version="2.3.2",
+    version="2.3.3",
     description="Antares Server",
     long_description=long_description,
     long_description_content_type="text/markdown",
