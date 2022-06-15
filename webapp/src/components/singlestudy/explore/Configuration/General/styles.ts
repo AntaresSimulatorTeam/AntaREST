129f47877afbb30b60ac8d6fe992507fd4363bc3
skamril <1954121+skamril@users.noreply.github.com>
2022-06-15 17:12:35 +0200

    feat(config): create General configuration for studies
    #849

---
+++ webapp/src/components/singlestudy/explore/Configuration/General/styles.ts
@@ -0,0 +1,17 @@
+import { styled, experimental_sx as sx } from "@mui/material";
+import Fieldset from "../../../../common/Fieldset";
+
+export const StyledFieldset = styled(Fieldset)(
+  sx({
+    p: 0,
+    pb: 5,
+    ".MuiBox-root": {
+      display: "flex",
+      flexWrap: "wrap",
+      gap: 2,
+      ".MuiFormControl-root": {
+        width: 220,
+      },
+    },
+  })
+);
--- webapp/src/components/singlestudy/explore/Configuration/index.tsx
+++ webapp/src/components/singlestudy/explore/Configuration/index.tsx
@@ -1,8 +1,58 @@
-import UnderConstruction from "../../../common/page/UnderConstruction";
-import previewImage from "./preview.png";
+/* eslint-disable react/no-unstable-nested-components */
+import { Paper } from "@mui/material";
+import * as R from "ramda";
+import { useMemo, useState } from "react";
+import PropertiesView from "../../../common/PropertiesView";
+import SplitLayoutView from "../../../common/SplitLayoutView";
+import ListElement from "../common/ListElement";
+import AdvancedParameters from "./AdvancedParameters";
+import General from "./General";
+import OptimizationPreferences from "./OptimizationPreferences";
+import RegionalDistricts from "./RegionalDistricts";
+import TimeSeriesManagement from "./TimeSeriesManagement";
 
 function Configuration() {
-  return <UnderConstruction previewImage={previewImage} />;
+  const [currentElementIndex, setCurrentElementIndex] = useState(0);
+  // TODO i18n
+  const listElement = useMemo(
+    () => [
+      { name: "General" },
+      { name: "Time-series management" },
+      { name: "Regional districts" },
+      { name: "Optimization preferences" },
+      { name: "Advanced parameters" },
+    ],
+    []
+  );
+
+  return (
+    <SplitLayoutView
+      left={
+        <PropertiesView
+          mainContent={
+            <ListElement
+              list={listElement}
+              currentElement={listElement[currentElementIndex].name}
+              setSelectedItem={(_, index) => {
+                setCurrentElementIndex(index);
+              }}
+            />
+          }
+        />
+      }
+      right={
+        <Paper sx={{ width: 1, height: 1, padding: 2, overflow: "auto" }}>
+          {R.cond([
+            [R.equals(0), () => <General />],
+            [R.equals(1), () => <TimeSeriesManagement />],
+            [R.equals(2), () => <RegionalDistricts />],
+            [R.equals(3), () => <OptimizationPreferences />],
+            [R.equals(4), () => <AdvancedParameters />],
+          ])(currentElementIndex)}
+        </Paper>
+      }
+    />
+  );
 }
 
 export default Configuration;
