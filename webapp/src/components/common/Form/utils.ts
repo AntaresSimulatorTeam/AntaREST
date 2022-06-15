7a109739e3cf9a4399a1b9cbbf145aa31a2bf0e8
skamril <1954121+skamril@users.noreply.github.com>
2022-06-15 17:13:27 +0200

    feat(common): update Form component
    #849

---
+++ webapp/src/components/common/Form/utils.ts
@@ -0,0 +1,47 @@
+import * as RA from "ramda-adjunct";
+import { AutoSubmitConfig, FormProps } from ".";
+
+export function toAutoSubmitConfig(
+  value: FormProps["autoSubmit"]
+): Required<AutoSubmitConfig> {
+  return {
+    wait: 1000,
+    ...(RA.isPlainObj(value) ? value : { enable: !!value }),
+  };
+}
+
+type UnknownArrayOrObject = unknown[] | Record<string, unknown>;
+
+// From https://github.com/react-hook-form/react-hook-form/discussions/1991#discussioncomment-351784
+// With little TS fixes.
+export function getDirtyValues(
+  dirtyFields: UnknownArrayOrObject | true,
+  allValues: UnknownArrayOrObject
+): UnknownArrayOrObject {
+  // NOTE: Recursive function.
+
+  // If *any* item in an array was modified, the entire array must be submitted, because there's no
+  // way to indicate "placeholders" for unchanged elements. `dirtyFields` is `true` for leaves.
+  if (dirtyFields === true || Array.isArray(dirtyFields)) {
+    return allValues;
+  }
+
+  // Here, we have an object.
+  return Object.fromEntries(
+    Object.keys(dirtyFields).map((key) => [
+      key,
+      getDirtyValues(
+        dirtyFields[key] as UnknownArrayOrObject | true,
+        (allValues as Record<string, unknown>)[key] as UnknownArrayOrObject
+      ),
+    ])
+  );
+}
+
+// From https://github.com/react-hook-form/react-hook-form/blob/master/src/utils/stringToPath.ts
+export function stringToPath(input: string): string[] {
+  return input
+    .replace(/["|']|\]/g, "")
+    .split(/\.|\[/)
+    .filter(Boolean);
+}
