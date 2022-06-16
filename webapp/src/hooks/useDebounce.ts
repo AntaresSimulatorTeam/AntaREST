8de27c78cfa49266cc5f9db530ef14f0222d3500
skamril <1954121+skamril@users.noreply.github.com>
2022-06-16 18:18:59 +0200

    feat(hooks): update useDebounce

--- webapp/src/hooks/useDebounce.ts
+++ webapp/src/hooks/useDebounce.ts
@@ -1,12 +1,34 @@
-import { debounce, DebouncedFunc, DebounceSettings } from "lodash";
+import {
+  debounce,
+  DebouncedFunc,
+  DebouncedFuncLeading,
+  DebounceSettings,
+  DebounceSettingsLeading,
+} from "lodash";
+import * as R from "ramda";
+import * as RA from "ramda-adjunct";
 import { useEffect, useMemo, useRef } from "react";
 import { F } from "ts-toolbelt";
 
-function useDebounce<T extends F.Function>(
+export interface UseDebounceParams extends DebounceSettings {
+  wait?: number;
+}
+
+type WaitOrParams = number | UseDebounceParams;
+
+const toParams = R.cond<[WaitOrParams | undefined], UseDebounceParams>([
+  [RA.isPlainObj, R.identity as () => UseDebounceParams],
+  [RA.isNumber, R.objOf("wait") as () => UseDebounceParams],
+  [R.T, RA.stubObj],
+]);
+
+function useDebounce<T extends F.Function, U extends WaitOrParams>(
   fn: T,
-  wait?: number,
-  options?: DebounceSettings
-): DebouncedFunc<T> {
+  params?: U
+): U extends DebounceSettingsLeading
+  ? DebouncedFuncLeading<T>
+  : DebouncedFunc<T> {
+  const { wait, ...options } = toParams(params);
   const fnRef = useRef(fn);
 
   useEffect(() => {
