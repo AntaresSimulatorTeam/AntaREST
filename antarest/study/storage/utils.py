b16bcd647d0812996f272e996882dfddd24bf184
Hyralc <charly6203@live.fr>
2022-06-22 10:43:40 +0200

    fix for windows

--- antarest/study/storage/utils.py
+++ antarest/study/storage/utils.py
@@ -350,8 +350,10 @@ def get_start_date(
 def extract_file_to_tmp_dir(
     zip_path: Path, inside_zip_path: Path
 ) -> Tuple[Path, Any]:
+    str_inside_zip_path = str(inside_zip_path)
+    str_inside_zip_path.replace("\\", "/")
     tmp_dir = tempfile.TemporaryDirectory()
     with ZipFile(zip_path) as zip_obj:
-        zip_obj.extract(str(inside_zip_path), tmp_dir.name)
+        zip_obj.extract(str_inside_zip_path, tmp_dir.name)
     path = Path(tmp_dir.name) / inside_zip_path
     return path, tmp_dir
