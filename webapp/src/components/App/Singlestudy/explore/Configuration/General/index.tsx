e78c51180a22f4fa8626a76310f3d6c4be3589d3
Samir Kamal <1954121+skamril@users.noreply.github.com>
2022-06-27 14:58:35 +0200

    Revert "Revert "Issue 850 Configuration General params Thematic trimming (#967)""
    This reverts commit df336c4fefb8816888578ecbd75cb8ce46736135.

--- webapp/src/components/App/Singlestudy/explore/Configuration/General/index.tsx
+++ webapp/src/components/App/Singlestudy/explore/Configuration/General/index.tsx
@@ -1,28 +1,58 @@
 import { useOutletContext } from "react-router";
+import * as R from "ramda";
+import { useState } from "react";
 import { StudyMetadata } from "../../../../../../common/types";
 import usePromiseWithSnackbarError from "../../../../../../hooks/usePromiseWithSnackbarError";
 import { getFormValues } from "./utils";
 import Form from "../../../../../common/Form";
 import Fields from "./Fields";
 import SimpleLoader from "../../../../../common/loaders/SimpleLoader";
-import UsePromiseCond from "../../../../../common/utils/UsePromiseCond";
+import ThematicTrimmingDialog from "./dialogs/ThematicTrimmingDialog";
 
 function GeneralParameters() {
   const { study } = useOutletContext<{ study: StudyMetadata }>();
+  const [dialog, setDialog] = useState<"thematicTrimming" | "">("");
 
   const { data, status, error } = usePromiseWithSnackbarError(
     () => getFormValues(study.id),
     { errorMessage: "Cannot get study data", deps: [study.id] } // TODO i18n
   );
 
-  return (
-    <UsePromiseCond
-      status={status}
-      ifPending={<SimpleLoader />}
-      ifRejected={<div>{error}</div>}
-      ifResolved={
+  ////////////////////////////////////////////////////////////////
+  // Event Handlers
+  ////////////////////////////////////////////////////////////////
+
+  const handleCloseDialog = () => {
+    setDialog("");
+  };
+
+  ////////////////////////////////////////////////////////////////
+  // JSX
+  ////////////////////////////////////////////////////////////////
+
+  return R.cond([
+    [
+      R.either(R.equals(PromiseStatus.Idle), R.equals(PromiseStatus.Pending)),
+      () => <SimpleLoader />,
+    ],
+    [R.equals(PromiseStatus.Rejected), () => <div>{error}</div>],
+    [
+      R.equals(PromiseStatus.Resolved),
+      () => (
         <Form autoSubmit config={{ defaultValues: data }}>
-          <Fields study={study} />
+          <Fields study={study} setDialog={setDialog} />
+          {R.cond([
+            [
+              R.equals("thematicTrimming"),
+              () => (
+                <ThematicTrimmingDialog
+                  open
+                  study={study}
+                  onClose={handleCloseDialog}
+                />
+              ),
+            ],
+          ])(dialog)}
         </Form>
       }
     />
