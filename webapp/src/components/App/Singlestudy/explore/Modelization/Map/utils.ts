93dd500354d7b9eacfdc686ccbbf3703d69e2dfa
skamril <1954121+skamril@users.noreply.github.com>
2022-07-06 10:54:46 +0200

    feat(singlestudy): remove unnecessary code of linkStyle function

--- webapp/src/components/App/Singlestudy/explore/Modelization/Map/utils.ts
+++ webapp/src/components/App/Singlestudy/explore/Modelization/Map/utils.ts
@@ -1,38 +1,10 @@
 import * as R from "ramda";
 
-export const linkStyle = (linkStyle: string): Array<Array<number> | string> => {
-  const linkCond = R.cond([
-    [
-      R.equals("dot"),
-      (): Array<Array<number> | string> => {
-        return [[1, 5], "round"];
-      },
-    ],
-    [
-      R.equals("dash"),
-      (): Array<Array<number> | string> => {
-        return [[16, 8], "square"];
-      },
-    ],
-    [
-      R.equals("dotdash"),
-      (): Array<Array<number> | string> => {
-        return [[10, 6, 1, 6], "square"];
-      },
-    ],
-    [
-      (_: string): boolean => true,
-      (): Array<Array<number> | string> => {
-        return [[0], "butt"];
-      },
-    ],
-  ]);
+type LinkStyleReturn = [number[], string];
 
-  const values = linkCond(linkStyle);
-  const style = values[0] as Array<number>;
-  const linecap = values[1] as string;
-
-  return [style, linecap];
-};
-
-export default {};
+export const linkStyle = R.cond<[string], LinkStyleReturn>([
+  [R.equals("dot"), (): LinkStyleReturn => [[1, 5], "round"]],
+  [R.equals("dash"), (): LinkStyleReturn => [[16, 8], "square"]],
+  [R.equals("dotdash"), (): LinkStyleReturn => [[10, 6, 1, 6], "square"]],
+  [R.T, (): LinkStyleReturn => [[0], "butt"]],
+]);
