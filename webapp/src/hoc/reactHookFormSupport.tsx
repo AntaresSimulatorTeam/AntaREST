3efe76c4ddbefb73ace95c4b64121895711d0a3c
skamril <1954121+skamril@users.noreply.github.com>
2022-07-21 15:43:29 +0200

    feat(hoc): update reactHookFormSupport

--- webapp/src/hoc/reactHookFormSupport.tsx
+++ webapp/src/hoc/reactHookFormSupport.tsx
@@ -1,14 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import hoistNonReactStatics from "hoist-non-react-statics";
-import React from "react";
-import { Controller, FieldPath, FieldValues } from "react-hook-form";
+import React, { useMemo } from "react";
+import {
+  Controller,
+  FieldPath,
+  FieldPathValue,
+  FieldValues,
+  Validate,
+} from "react-hook-form";
 import * as R from "ramda";
+import * as RA from "ramda-adjunct";
 import { ControlPlus, RegisterOptionsPlus } from "../components/common/Form";
 import { getComponentDisplayName } from "../utils/reactUtils";
 
 interface ReactHookFormSupport<TValue> {
   defaultValue?: NonNullable<TValue>;
   setValueAs?: (value: any) => any;
+  preValidate?: (value: any) => boolean;
 }
 
 // `...args: any` allows to be compatible with all field editors
@@ -32,7 +40,13 @@ type ReactHookFormSupportProps<
       rules?: Omit<
         RegisterOptionsPlus<TFieldValues, TFieldName>,
         // cf. UseControllerProps#rules
-        "valueAsNumber" | "valueAsDate" | "setValueAs" | "disabled"
+        | "valueAsNumber"
+        | "valueAsDate"
+        | "setValueAs"
+        | "disabled"
+        // Not necessary
+        | "onChange"
+        | "onBlur"
       >;
       shouldUnregister?: boolean;
       name: TFieldName;
@@ -46,7 +60,7 @@ type ReactHookFormSupportProps<
 function reactHookFormSupport<TValue>(
   options: ReactHookFormSupport<TValue> = {}
 ) {
-  const { defaultValue, setValueAs = R.identity } = options;
+  const { preValidate, setValueAs = R.identity } = options;
 
   function wrapWithReactHookFormSupport<
     TProps extends FieldEditorProps<TValue>
@@ -59,35 +73,54 @@ function reactHookFormSupport<TValue>(
       props: ReactHookFormSupportProps<TFieldValues, TFieldName, TContext> &
         TProps
     ) {
-      const { control, rules, shouldUnregister, ...fieldEditorProps } = props;
-      const { name, onChange, onBlur } = fieldEditorProps;
+      const { control, rules = {}, shouldUnregister, ...feProps } = props;
+      const { validate } = rules;
 
-      if (control && name) {
+      const validateWrapper = useMemo<
+        RegisterOptionsPlus<TFieldValues, TFieldName>["validate"]
+      >(() => {
+        if (preValidate) {
+          if (RA.isFunction(validate)) {
+            return (v) => preValidate?.(v) && validate(v);
+          }
+
+          if (RA.isPlainObj(validate)) {
+            return Object.keys(validate).reduce((acc, key) => {
+              acc[key] = (v) => preValidate?.(v) && validate[key](v);
+              return acc;
+            }, {} as Record<string, Validate<FieldPathValue<TFieldValues, TFieldName>>>);
+          }
+
+          return preValidate;
+        }
+        return validate;
+      }, [validate]);
+
+      if (control && feProps.name) {
         return (
           <Controller
             control={control}
-            name={name as TFieldName}
-            defaultValue={defaultValue as any} // useForm's defaultValues take precedence
+            name={feProps.name as TFieldName}
+            // useForm's defaultValues take precedence
+            defaultValue={(feProps.defaultValue ?? options.defaultValue) as any}
             rules={{
               ...rules,
-              onChange: (event) => {
-                onChange?.(event);
-                rules?.onChange?.(event);
-              },
-              onBlur: (event) => {
-                onBlur?.(event);
-                rules?.onBlur?.(event);
-              },
+              validate: validateWrapper,
             }}
             shouldUnregister={shouldUnregister}
             render={({
-              field: { ref, onChange, ...fieldProps },
+              field: { ref, onChange, onBlur, ...fieldProps },
               fieldState: { error },
             }) => (
               <FieldEditor
-                {...(fieldEditorProps as TProps)}
+                {...(feProps as TProps)}
                 {...fieldProps}
                 onChange={(event) => {
+                  // Called here instead of Controller's rules, to keep original event
+                  feProps.onChange?.(event);
+
+                  // https://github.com/react-hook-form/react-hook-form/discussions/8068#discussioncomment-2415789
+                  // Data send back to hook form
                   onChange(
                     setValueAs(
                       event.target.type === "checkbox"
@@ -95,6 +128,13 @@ function reactHookFormSupport<TValue>(
                         : event.target.value
                     )
                   );
+                }}
+                onBlur={(event) => {
+                  // Called here instead of Controller's rules, to keep original event
+                  feProps.onBlur?.(event);
+
+                  // Report input has been interacted (focus and blur)
+                  onBlur();
                 }}
                 inputRef={ref}
                 error={!!error}
@@ -105,7 +145,7 @@ function reactHookFormSupport<TValue>(
         );
       }
 
-      return <FieldEditor {...(fieldEditorProps as TProps)} />;
+      return <FieldEditor {...(feProps as TProps)} />;
     }
 
     ReactHookFormSupport.displayName = `ReactHookFormSupport(${getComponentDisplayName(
