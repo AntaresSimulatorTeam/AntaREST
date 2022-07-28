3c808b323e602fa474a82117940376fb0e6b8b6f
Alexis <alexis.martinet@rte-france.com>
2022-07-28 15:45:32 +0200

    fix review

--- webapp/src/components/common/EditableMatrix/MatrixGraphView.tsx
+++ webapp/src/components/common/EditableMatrix/MatrixGraphView.tsx
@@ -117,7 +117,7 @@ export default function MatrixGraphView(props: PropTypes) {
           label={t("matrix.monotonicView")}
         />
       </Box>
-      <Box sx={{ display: "block", width: "100%", height: "100%", m: 1 }}>
+      <Box sx={{ display: "block", width: 1, height: 1, m: 1 }}>
         <AutoSizer>
           {({ height, width }) => (
             <Plot
