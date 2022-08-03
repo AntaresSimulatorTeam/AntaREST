eb11e4f08169256b9ce0e767eb496fedf71546d1
skamril <1954121+skamril@users.noreply.github.com>
2022-08-03 11:14:38 +0200

    feat(login): update LoginWrapper

--- webapp/src/components/wrappers/LoginWrapper.tsx
+++ webapp/src/components/wrappers/LoginWrapper.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { useState } from "react";
 import { Box, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { LoadingButton } from "@mui/lab";
@@ -16,6 +16,7 @@ import storage, { StorageKey } from "../../services/utils/localStorage";
 import Form, { SubmitHandlerPlus } from "../common/Form";
 import StringFE from "../common/fieldEditors/StringFE";
 import PasswordFE from "../common/fieldEditors/PasswordFE";
+import UsePromiseCond from "../common/utils/UsePromiseCond";
 
 interface FormValues {
   username: string;
@@ -23,7 +24,7 @@ interface FormValues {
 }
 
 interface Props {
-  children: ReactNode;
+  children: React.ReactNode;
 }
 
 function LoginWrapper(props: Props) {
@@ -33,11 +34,7 @@ function LoginWrapper(props: Props) {
   const user = useAppSelector(getAuthUser);
   const dispatch = useAppDispatch();
 
-  const {
-    data: canDisplayApp,
-    isLoading,
-    isRejected,
-  } = usePromiseWithSnackbarError(
+  const res = usePromiseWithSnackbarError(
     async () => {
       if (user) {
         return true;
@@ -82,120 +79,127 @@ function LoginWrapper(props: Props) {
   // JSX
   ////////////////////////////////////////////////////////////////
 
-  if (isLoading) {
-    return <AppLoader />;
-  }
-
-  if (isRejected) {
-    return <GlobalPageLoadingError />;
-  }
-
-  if (canDisplayApp) {
-    // eslint-disable-next-line react/jsx-no-useless-fragment
-    return <>{children}</>;
-  }
-
   return (
-    <Box
-      display="flex"
-      height="100vh"
-      sx={{
-        background:
-          "radial-gradient(ellipse at top right, #190520 0%, #190520 30%, #222333 100%)",
-      }}
-    >
-      <Box
-        position="absolute"
-        top="0px"
-        right="0px"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-        flexWrap="nowrap"
-        boxSizing="border-box"
-      >
-        <img src={topRightBackground} alt="logo" style={{ height: "auto" }} />
-      </Box>
-      <Box
-        flexGrow={1}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        zIndex={999}
-      >
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="column"
-          flexWrap="nowrap"
-          boxSizing="border-box"
-        >
+    <UsePromiseCond
+      response={res}
+      ifPending={() => <AppLoader />}
+      ifRejected={() => <GlobalPageLoadingError />}
+      ifResolved={(canDisplayApp) =>
+        canDisplayApp ? (
+          children
+        ) : (
           <Box
             display="flex"
-            width="70%"
-            height="100%"
-            justifyContent="center"
-            alignItems="center"
-            flexDirection="column"
-            flexWrap="nowrap"
-            boxSizing="border-box"
+            height="100vh"
+            sx={{
+              background:
+                "radial-gradient(ellipse at top right, #190520 0%, #190520 30%, #222333 100%)",
+            }}
           >
-            <img src={logo} alt="logo" style={{ height: "96px" }} />
-            <Typography variant="h4" component="h4" color="primary" my={2}>
-              Antares Web
-            </Typography>
+            <Box
+              position="absolute"
+              top="0px"
+              right="0px"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+              flexWrap="nowrap"
+              boxSizing="border-box"
+            >
+              <img
+                src={topRightBackground}
+                alt="logo"
+                style={{ height: "auto" }}
+              />
+            </Box>
+            <Box
+              flexGrow={1}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              zIndex={999}
+            >
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                flexDirection="column"
+                flexWrap="nowrap"
+                boxSizing="border-box"
+              >
+                <Box
+                  display="flex"
+                  width="70%"
+                  height="100%"
+                  justifyContent="center"
+                  alignItems="center"
+                  flexDirection="column"
+                  flexWrap="nowrap"
+                  boxSizing="border-box"
+                >
+                  <img src={logo} alt="logo" style={{ height: "96px" }} />
+                  <Typography
+                    variant="h4"
+                    component="h4"
+                    color="primary"
+                    my={2}
+                  >
+                    Antares Web
+                  </Typography>
+                </Box>
+                <Box width="70%" my={2}>
+                  <Form onSubmit={handleSubmit} hideSubmitButton disableLoader>
+                    {({ control, formState: { isDirty, isSubmitting } }) => (
+                      <>
+                        <StringFE
+                          name="username"
+                          sx={{ my: 3 }}
+                          label="NNI"
+                          variant="filled"
+                          fullWidth
+                          control={control}
+                          rules={{ required: t("form.field.required") }}
+                        />
+                        <PasswordFE
+                          name="password"
+                          variant="filled"
+                          label={t("global.password")}
+                          inputProps={{ autoComplete: "current-password" }}
+                          fullWidth
+                          control={control}
+                          rules={{ required: t("form.field.required") }}
+                        />
+                        {loginError && (
+                          <Box
+                            mt={2}
+                            color="error.main"
+                            mb={4}
+                            sx={{ fontSize: "0.9rem" }}
+                          >
+                            {loginError}
+                          </Box>
+                        )}
+                        <Box display="flex" justifyContent="center" mt={6}>
+                          <LoadingButton
+                            type="submit"
+                            variant="contained"
+                            loading={isSubmitting}
+                            disabled={!isDirty || isSubmitting}
+                          >
+                            {t("global.connexion")}
+                          </LoadingButton>
+                        </Box>
+                      </>
+                    )}
+                  </Form>
+                </Box>
+              </Box>
+            </Box>
           </Box>
-          <Box width="70%" my={2}>
-            <Form onSubmit={handleSubmit} hideSubmitButton>
-              {({ control, formState: { isDirty, isSubmitting } }) => (
-                <>
-                  <StringFE
-                    name="username"
-                    sx={{ my: 3 }}
-                    label="NNI"
-                    variant="filled"
-                    fullWidth
-                    control={control}
-                    rules={{ required: t("form.field.required") }}
-                  />
-                  <PasswordFE
-                    name="password"
-                    variant="filled"
-                    label={t("global.password")}
-                    inputProps={{ autoComplete: "current-password" }}
-                    fullWidth
-                    control={control}
-                    rules={{ required: t("form.field.required") }}
-                  />
-                  {loginError && (
-                    <Box
-                      mt={2}
-                      color="error.main"
-                      mb={4}
-                      sx={{ fontSize: "0.9rem" }}
-                    >
-                      {loginError}
-                    </Box>
-                  )}
-                  <Box display="flex" justifyContent="center" mt={6}>
-                    <LoadingButton
-                      type="submit"
-                      variant="contained"
-                      loading={isSubmitting}
-                      disabled={!isDirty || isSubmitting}
-                    >
-                      {t("global.connexion")}
-                    </LoadingButton>
-                  </Box>
-                </>
-              )}
-            </Form>
-          </Box>
-        </Box>
-      </Box>
-    </Box>
+        )
+      }
+    />
   );
 }
 
