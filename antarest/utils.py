4b628cc4d318521117b5cbb71455e149d064805c
Paul Bui-Quang <paul.buiquang@rte-france.com>
2022-08-22 13:09:25 +0200

    Fix db config
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- antarest/utils.py
+++ antarest/utils.py
@@ -82,18 +82,19 @@ def init_db(
         connect_args["connect_timeout"] = config.db.db_connect_timeout
 
     extra = {}
-    if config.db.pool_recycle:
-        extra["pool_recycle"] = config.db.pool_recycle
-    if config.db.pool_pre_ping:
-        extra["pool_pre_ping"] = True
     if config.db.pool_use_null:
-        extra["pool_class"] = NullPool
-    if config.db.pool_max_overflow:
-        extra["max_overflow"] = config.db.pool_max_overflow
-    if config.db.pool_size:
-        extra["pool_size"] = config.db.pool_size
-    if config.db.pool_use_lifo:
-        extra["pool_use_lifo"] = config.db.pool_use_lifo
+        extra["poolclass"] = NullPool
+    else:
+        if config.db.pool_pre_ping:
+            extra["pool_pre_ping"] = True
+        if config.db.pool_recycle:
+            extra["pool_recycle"] = config.db.pool_recycle
+        if config.db.pool_max_overflow:
+            extra["max_overflow"] = config.db.pool_max_overflow
+        if config.db.pool_size:
+            extra["pool_size"] = config.db.pool_size
+        if config.db.pool_use_lifo:
+            extra["pool_use_lifo"] = config.db.pool_use_lifo
 
     engine = create_engine(
         config.db.db_url, echo=config.debug, connect_args=connect_args, **extra
