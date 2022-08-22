9fd2183681d28740f6db125c38f2935e5be1f838
skamril <1954121+skamril@users.noreply.github.com>
2022-08-22 13:04:26 +0200

    fix: code review

--- antarest/core/utils/string.py
+++ antarest/core/utils/string.py
@@ -4,4 +4,4 @@ def to_pascal_case(value: str) -> str:
 
 def to_camel_case(value: str) -> str:
     v = to_pascal_case(value)
-    return v[0].lower() + v[1:]
+    return v[0].lower() + v[1:] if len(v) > 0 else ""
