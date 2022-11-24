ad2fb3b19e08fc38d547015e12e99b0c92bdd4dd
hatim dinia <diniahatim@gmail.com>
2022-11-24 17:20:59 +0100

    refactor(ui-map): update map components

---
+++ webapp/src/components/App/Singlestudy/explore/Modelization/Map/Areas/AreaLink.tsx
@@ -0,0 +1,61 @@
+import { useTranslation } from "react-i18next";
+import {
+  setSelectedLink,
+  setSelectedNode,
+} from "../../../../../../../redux/ducks/studyDataSynthesis";
+import useAppDispatch from "../../../../../../../redux/hooks/useAppDispatch";
+import useAppSelector from "../../../../../../../redux/hooks/useAppSelector";
+import {
+  getMapNodes,
+  getSelectedLink,
+} from "../../../../../../../redux/selectors";
+import {
+  AreaLinkContainer,
+  AreaLinkContent,
+  AreaLinkRoot,
+  AreaLinkTitle,
+  AreaLinkLabel,
+} from "../style";
+
+function AreaLink() {
+  const [t] = useTranslation();
+  const dispatch = useAppDispatch();
+  const mapNodes = useAppSelector(getMapNodes);
+  const selectedLink = useAppSelector(getSelectedLink);
+
+  ////////////////////////////////////////////////////////////////
+  // Event Handlers
+  ////////////////////////////////////////////////////////////////
+
+  const handleLinkClick = (link: string | undefined) => {
+    if (mapNodes) {
+      const selectedNode = mapNodes.find((node) => node.id === link);
+      dispatch(setSelectedLink(undefined));
+      dispatch(setSelectedNode(selectedNode));
+    }
+  };
+
+  ////////////////////////////////////////////////////////////////
+  // JSX
+  ////////////////////////////////////////////////////////////////
+
+  return (
+    <AreaLinkRoot>
+      <AreaLinkTitle>{t("study.links")}</AreaLinkTitle>
+      <AreaLinkContainer>
+        <AreaLinkLabel>{t("study.area1")}</AreaLinkLabel>
+        <AreaLinkContent onClick={() => handleLinkClick(selectedLink?.source)}>
+          {selectedLink?.source}
+        </AreaLinkContent>
+      </AreaLinkContainer>
+      <AreaLinkContainer>
+        <AreaLinkLabel>{t("study.area2")}</AreaLinkLabel>
+        <AreaLinkContent onClick={() => handleLinkClick(selectedLink?.target)}>
+          {selectedLink?.target}
+        </AreaLinkContent>
+      </AreaLinkContainer>
+    </AreaLinkRoot>
+  );
+}
+
+export default AreaLink;
---
+++ webapp/src/components/App/Singlestudy/explore/Modelization/Map/Areas/DeleteAreaDialog.tsx
@@ -0,0 +1,114 @@
+import { Box, Button, Typography } from "@mui/material";
+import { AxiosError } from "axios";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useOutletContext } from "react-router-dom";
+import { StudyMetadata } from "../../../../../../../common/types";
+import useEnqueueErrorSnackbar from "../../../../../../../hooks/useEnqueueErrorSnackbar";
+import {
+  setSelectedNode,
+  setSelectedLink,
+  deleteMapNode,
+  deleteMapLink,
+} from "../../../../../../../redux/ducks/studyDataSynthesis";
+import useAppDispatch from "../../../../../../../redux/hooks/useAppDispatch";
+import useAppSelector from "../../../../../../../redux/hooks/useAppSelector";
+import {
+  getSelectedLink,
+  getSelectedNode,
+} from "../../../../../../../redux/selectors";
+import ConfirmationDialog from "../../../../../../common/dialogs/ConfirmationDialog";
+import { AreaDeleteIcon } from "../style";
+
+function DeleteAreaDialog() {
+  const { study } = useOutletContext<{ study: StudyMetadata }>();
+  const [t] = useTranslation();
+  const dispatch = useAppDispatch();
+  const enqueueErrorSnackbar = useEnqueueErrorSnackbar();
+  const [openConfirmationModal, setOpenConfirmationModal] =
+    useState<boolean>(false);
+  const selectedNode = useAppSelector(getSelectedNode);
+  const selectedLink = useAppSelector(getSelectedLink);
+
+  ////////////////////////////////////////////////////////////////
+  // Event Handlers
+  ////////////////////////////////////////////////////////////////
+
+  const handleDelete = async () => {
+    // Delete node
+    if (selectedNode) {
+      try {
+        await dispatch(
+          deleteMapNode({ studyId: study.id, source: selectedNode.id })
+        );
+      } catch (e) {
+        enqueueErrorSnackbar(
+          t("study.error.deleteAreaOrLink"),
+          e as AxiosError
+        );
+      }
+    }
+    // Delete link
+    if (selectedLink) {
+      try {
+        await dispatch(
+          deleteMapLink({
+            studyId: study.id,
+            source: selectedLink.source,
+            target: selectedLink.target,
+          })
+        );
+      } catch (e) {
+        enqueueErrorSnackbar(
+          t("study.error.deleteAreaOrLink"),
+          e as AxiosError
+        );
+      }
+    }
+    setOpenConfirmationModal(false);
+  };
+
+  ////////////////////////////////////////////////////////////////
+  // JSX
+  ////////////////////////////////////////////////////////////////
+
+  return (
+    <>
+      <Box
+        sx={{
+          width: "90%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          color="primary"
+          size="small"
+          onClick={() => {
+            dispatch(setSelectedNode(undefined));
+            dispatch(setSelectedLink(undefined));
+          }}
+        >
+          {t("button.back")}
+        </Button>
+        <AreaDeleteIcon onClick={() => setOpenConfirmationModal(true)} />
+      </Box>
+      {openConfirmationModal && (
+        <ConfirmationDialog
+          onCancel={() => setOpenConfirmationModal(false)}
+          onConfirm={handleDelete}
+          alert="warning"
+          open
+        >
+          <Typography sx={{ p: 3 }}>
+            {selectedNode && t("study.question.deleteArea")}
+            {selectedLink && t("study.question.deleteLink")}
+          </Typography>
+        </ConfirmationDialog>
+      )}
+    </>
+  );
+}
+
+export default DeleteAreaDialog;
