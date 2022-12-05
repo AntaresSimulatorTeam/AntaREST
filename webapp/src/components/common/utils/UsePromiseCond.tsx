a8571eb3a138786809de34fe0c249223a9409b72
Samir Kamal <1954121+skamril@users.noreply.github.com>
2022-12-05 17:47:51 +0100

    feat(hook): update UsePromiseCond
    * change 'response' prop
    * add mergeResponses util

--- webapp/src/components/common/utils/UsePromiseCond.tsx
+++ webapp/src/components/common/utils/UsePromiseCond.tsx
@@ -3,11 +3,41 @@ import { PromiseStatus, UsePromiseResponse } from "../../../hooks/usePromise";
 import SimpleLoader from "../loaders/SimpleLoader";
 import SimpleContent from "../page/SimpleContent";
 
-export interface UsePromiseCondProps<
-  T,
-  Response extends UsePromiseResponse<T> = UsePromiseResponse<T>
-> {
-  response: Response;
+export type Response<T = unknown> = Pick<
+  UsePromiseResponse<T>,
+  "data" | "status" | "error"
+>;
+
+export function mergeResponses<T1, T2>(
+  res1: Response<T1>,
+  res2: Response<T2>
+): Response<[T1, T2]> {
+  function getMergedStatus() {
+    const preResolvedStatus = [
+      PromiseStatus.Idle,
+      PromiseStatus.Pending,
+      PromiseStatus.Rejected,
+    ].find((status) => res1.status === status || res2.status === status);
+
+    return preResolvedStatus || PromiseStatus.Resolved;
+  }
+
+  const status = getMergedStatus();
+
+  return {
+    ...res1,
+    ...res2,
+    status,
+    data:
+      status === PromiseStatus.Resolved
+        ? [res1.data as T1, res2.data as T2]
+        : undefined,
+    error: res1.error || res2.error,
+  };
+}
+
+export interface UsePromiseCondProps<T> {
+  response: Response<T>;
   ifPending?: () => React.ReactNode;
   ifRejected?: (error: Response["error"]) => React.ReactNode;
   ifResolved?: (data: T) => React.ReactNode;
