ac9bbf820a4e2b5d152ec1cf7dea552ceb7090b4
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-02-23 19:01:13 +0100

    fix: correct the way parameters are parsed in archive worker application

--- antarest/worker/archive_worker_service.py
+++ antarest/worker/archive_worker_service.py
@@ -1,6 +1,6 @@
 import argparse
-import sys
 from pathlib import Path
+from typing import Sequence, Optional
 
 from antarest.core.config import Config
 from antarest.core.logging.utils import configure_logger
@@ -8,22 +8,24 @@ from antarest.core.utils.utils import get_local_path
 from antarest import __version__
 from antarest.utils import create_archive_worker
 
+ArgsType = Optional[Sequence[str]]
 
-def parse_arguments() -> argparse.Namespace:
+
+def parse_arguments(args: ArgsType = None) -> argparse.Namespace:
+    version = f'%(prog)s {__version__}'
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="Display worker version and exit",
+        action="version",
+        version=version,
+    )
     parser.add_argument(
         "-c",
         "--config",
         dest="config_file",
         help="path to the config file",
-    )
-    parser.add_argument(
-        "-v",
-        "--version",
-        dest="version",
-        help="Worker version",
-        action="store_true",
-        required=False,
     )
     parser.add_argument(
         "-w",
@@ -34,24 +36,26 @@ def parse_arguments() -> argparse.Namespace:
     )
     parser.add_argument(
         "-l",
+        "--local-root",
         "--local_root",
         dest="local_root",
         help="Define the local root path",
         required=False,
     )
-    return parser.parse_args()
+    return parser.parse_args(args)
 
 
-if __name__ == "__main__":
+def run_archive_worker(args: ArgsType = None) -> None:
     res = get_local_path() / "resources"
-    args = parse_arguments()
-    if args.version:
-        print(__version__)
-        sys.exit()
-    config_file = Path(args.config_file)
-    local_root = Path(args.local_root or "/")
-    workspace = args.workspace
+    namespace = parse_arguments(args)
+    config_file = Path(namespace.config_file)
+    local_root = Path(namespace.local_root or "/")
+    workspace = namespace.workspace
     config = Config.from_yaml_file(res=res, file=config_file)
     configure_logger(config)
     worker = create_archive_worker(config, workspace, Path(local_root))
     worker.start(threaded=False)
+
+
+if __name__ == "__main__":
+    run_archive_worker()
