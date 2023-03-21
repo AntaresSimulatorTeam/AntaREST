aa5e3e87d95b8b5061030025e89443e1fc71823d
Samir Kamal <1954121+skamril@users.noreply.github.com>
2023-03-21 15:49:27 +0100

    feat(ui): add @total-typescript/ts-reset lib and tsUtils (#1408)

---
+++ webapp/src/types/reset.d.ts
@@ -0,0 +1,7 @@
+import "@total-typescript/ts-reset/dist/array-includes";
+import "@total-typescript/ts-reset/dist/array-index-of";
+import "@total-typescript/ts-reset/dist/filter-boolean";
+// TODO After the resolution of this bug https://github.com/total-typescript/ts-reset/pull/56
+// import "@total-typescript/ts-reset/dist/is-array";
+import "@total-typescript/ts-reset/dist/set-has";
+import "@total-typescript/ts-reset/dist/map-has";
