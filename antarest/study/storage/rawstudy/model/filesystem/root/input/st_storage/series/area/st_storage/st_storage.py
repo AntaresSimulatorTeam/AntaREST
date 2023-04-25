6ad37285312aaeaeb8fc2b523ecef8c687c9429e
belthlemar <martin.belthle@rte-france.com>
2023-04-25 14:43:15 +0200

    feat(model): checkstyle + add default values for st-storage series

--- antarest/study/storage/rawstudy/model/filesystem/root/input/st_storage/series/area/st_storage/st_storage.py
+++ antarest/study/storage/rawstudy/model/filesystem/root/input/st_storage/series/area/st_storage/st_storage.py
@@ -2,11 +2,11 @@ from antarest.study.storage.rawstudy.model.filesystem.folder_node import (
     FolderNode,
 )
 from antarest.study.storage.rawstudy.model.filesystem.inode import TREE
-from antarest.study.storage.rawstudy.model.filesystem.matrix.constants import (
-    default_scenario_hourly,
-)
 from antarest.study.storage.rawstudy.model.filesystem.matrix.input_series_matrix import (
     InputSeriesMatrix,
+)
+from antarest.study.storage.variantstudy.business.matrix_constants.st_storage import (
+    series,
 )
 
 
@@ -14,19 +14,29 @@ class InputShortTermStorageAreaStorage(FolderNode):
     def build(self) -> TREE:
         children: TREE = {
             "PMAX-injection": InputSeriesMatrix(
-                self.context, self.config.next_file("PMAX-injection.txt")
+                self.context,
+                self.config.next_file("PMAX-injection.txt"),
+                default_empty=series.pmax_injection,
             ),
             "PMAX-withdrawal": InputSeriesMatrix(
-                self.context, self.config.next_file("PMAX-withdrawal.txt")
+                self.context,
+                self.config.next_file("PMAX-withdrawal.txt"),
+                default_empty=series.pmax_withdrawal,
             ),
             "inflow": InputSeriesMatrix(
-                self.context, self.config.next_file("inflow.txt")
+                self.context,
+                self.config.next_file("inflow.txt"),
+                default_empty=series.inflow,
             ),
             "lower-rule-curve": InputSeriesMatrix(
-                self.context, self.config.next_file("lower-rule-curve.txt")
+                self.context,
+                self.config.next_file("lower-rule-curve.txt"),
+                default_empty=series.lower_rule_curve,
             ),
             "upper-rule-curve": InputSeriesMatrix(
-                self.context, self.config.next_file("upper-rule-curve.txt")
+                self.context,
+                self.config.next_file("upper-rule-curve.txt"),
+                default_empty=series.upper_rule_curve,
             ),
         }
         return children
---
+++ antarest/study/storage/variantstudy/business/matrix_constants/st_storage/series.py
@@ -0,0 +1,5 @@
+pmax_injection = [[1.0]] * 8760
+pmax_withdrawal = [[1.0]] * 8760
+inflow = [[0.0]] * 8760
+lower_rule_curve = [[0.0]] * 8760
+upper_rule_curve = [[1.0]] * 8760
