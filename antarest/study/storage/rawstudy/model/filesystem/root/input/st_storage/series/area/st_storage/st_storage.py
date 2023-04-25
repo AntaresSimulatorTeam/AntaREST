8940b4bc37a4543a269e71402e55078c7502e0f0
belthlemar <martin.belthle@rte-france.com>
2023-04-25 14:15:17 +0200

    add 5 matrices to st-series

--- antarest/study/storage/rawstudy/model/filesystem/root/input/st_storage/series/area/st_storage/st_storage.py
+++ antarest/study/storage/rawstudy/model/filesystem/root/input/st_storage/series/area/st_storage/st_storage.py
@@ -13,10 +13,20 @@ from antarest.study.storage.rawstudy.model.filesystem.matrix.input_series_matrix
 class InputShortTermStorageAreaStorage(FolderNode):
     def build(self) -> TREE:
         children: TREE = {
-            "series": InputSeriesMatrix(
-                self.context,
-                self.config.next_file("series.txt"),
-                default_empty=default_scenario_hourly,
+            "PMAX-injection": InputSeriesMatrix(
+                self.context, self.config.next_file("PMAX-injection.txt")
+            ),
+            "PMAX-withdrawal": InputSeriesMatrix(
+                self.context, self.config.next_file("PMAX-withdrawal.txt")
+            ),
+            "inflow": InputSeriesMatrix(
+                self.context, self.config.next_file("inflow.txt")
+            ),
+            "lower-rule-curve": InputSeriesMatrix(
+                self.context, self.config.next_file("lower-rule-curve.txt")
+            ),
+            "upper-rule-curve": InputSeriesMatrix(
+                self.context, self.config.next_file("upper-rule-curve.txt")
             ),
         }
         return children
