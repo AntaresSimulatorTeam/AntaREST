8ed3661b4c2c419da63a4e63a2fd58dfed3f1630
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-05-02 16:22:25 +0200

    test(api): unit tests refactoring

--- tests/integration/test_core_blueprint.py
+++ tests/integration/test_core_blueprint.py
@@ -1,48 +1,37 @@
 import re
-import subprocess
 from unittest import mock
 
 from fastapi import FastAPI
-from http import HTTPStatus
 from starlette.testclient import TestClient
 
-from antarest.core import version_info
-from antarest.core.version_info import get_dependencies, get_commit_id
+
+class RegEx:
+    """A helper object that compares equal to a regex."""
+
+    def __init__(self, regex):
+        self.regex = regex
+        self.match = re.compile(self.regex).fullmatch
+
+    def __eq__(self, other):
+        return isinstance(other, str) and self.match(other)
+
+    def __ne__(self, other):
+        return not isinstance(other, str) or not self.match(other)
+
+    def __repr__(self):
+        return f"<RegEx({self.regex!r})>"
 
 
 class TestVersionInfo:
     def test_version_info(self, app: FastAPI):
         client = TestClient(app, raise_server_exceptions=False)
         res = client.get("/version")
-        assert res.status_code == HTTPStatus.OK
+        res.raise_for_status()
         actual = res.json()
         expected = {
             "name": "AntaREST",
-            "version": mock.ANY,
-            "gitcommit": mock.ANY,
+            "version": RegEx(r"\d+(?:\.\d+)+"),
+            "gitcommit": RegEx(r"^[0-9a-fA-F]{40}$"),
             "dependencies": mock.ANY,
         }
         assert actual == expected
-
-    def test_get_dependencies(self):
-        dependencies = get_dependencies()
-        assert isinstance(dependencies, dict)
-        assert "fastapi" in dependencies
-        assert re.compile(r"\d+\.\d+(?:\.\d+)?").match(dependencies["fastapi"])
-
-    def test_get_commit_id(self, tmp_path):
-        path_commit_id = tmp_path.joinpath("commit_id")
-        with open(path_commit_id, "w") as f:
-            f.write("fake_commit")
-        assert get_commit_id(tmp_path) == "fake_commit"
-        path_commit_id.unlink()
-        assert (
-            get_commit_id(tmp_path)
-            == subprocess.check_output(
-                "git log -1 HEAD --format=%H", encoding="utf-8", shell=True
-            ).strip()
-        )
-        version_info.get_last_commit_from_git = mock.MagicMock(
-            return_value="mock commit"
-        )
-        assert get_commit_id(tmp_path) == "mock commit"
