530b760c85a29ab609756240403708940c918c2b
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-05-09 09:37:43 +0200

    build: prepare next minor release v2.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AntaREST",
-    version="2.13.2",
+    version="2.14.0",
     description="Antares Server",
     long_description=long_description,
     long_description_content_type="text/markdown",
