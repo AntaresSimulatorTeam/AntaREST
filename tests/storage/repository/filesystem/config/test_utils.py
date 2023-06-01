24b10595cdf7c054815834181a82d65f653f0270
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-06-01 09:29:18 +0200

    refactor(api-model): improve `transform_name_to_id` implementation and add UT

--- tests/storage/repository/filesystem/config/test_utils.py
+++ tests/storage/repository/filesystem/config/test_utils.py
@@ -1,8 +1,61 @@
+import string
+
+import pytest
+
 from antarest.study.storage.rawstudy.model.filesystem.config.model import (
     transform_name_to_id,
 )
 
+VALID_CHARS = "azAZ09_-(),&"
 
-def test_id_transform():
-    assert transform_name_to_id("@é'rFf") == "rff"
-    assert transform_name_to_id("t@é'rFf") == "t rff"
+
+@pytest.mark.parametrize(
+    "name, expected",
+    [("France", "france"), ("@é'rFf", "rff"), ("t@é'rFf", "t rff")],
+)
+def test_transform_name_to_id__nominal_case(name, expected):
+    assert transform_name_to_id(name) == expected
+
+
+@pytest.mark.parametrize("name", VALID_CHARS)
+def test_transform_name_to_id__valid_chars(name):
+    assert transform_name_to_id(name, lower=True) == name.lower()
+    assert transform_name_to_id(name, lower=False) == name
+
+
+@pytest.mark.parametrize("name", set(string.punctuation) - set(VALID_CHARS))
+def test_transform_name_to_id__punctuation(name):
+    assert not transform_name_to_id(name)
+
+
+@pytest.mark.parametrize("name", set(string.whitespace) - set(VALID_CHARS))
+def test_transform_name_to_id__whitespace(name):
+    assert not transform_name_to_id(name)
+
+
+@pytest.mark.parametrize(
+    "name, expected",
+    [
+        ("$$foo", "foo"),
+        ("bar$$", "bar"),
+        # inner invalid chars are replaced by one space
+        ("foo$$$bar", "foo bar"),
+        ("$$foo$$$bar$$", "foo bar"),
+    ],
+)
+def test_transform_name_to_id__strip_invalid_duplicates(name, expected):
+    assert transform_name_to_id(name) == expected
+
+
+@pytest.mark.parametrize(
+    "name, expected",
+    [
+        ("  foo", "foo"),
+        ("bar  ", "bar"),
+        # inner whitespaces are preserved
+        ("foo   bar", "foo   bar"),
+        ("  foo   bar  ", "foo   bar"),
+    ],
+)
+def test_transform_name_to_id__strip_whitespaces(name, expected):
+    assert transform_name_to_id(name) == expected
