af7441a21af09aa9b01e1c2a7726f94340f16bab
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-06-27 23:21:31 +0200

    build: new bugfix release v2.14.4 (2023-06-28)

--- antarest/__init__.py
+++ antarest/__init__.py
@@ -7,9 +7,9 @@ from pathlib import Path
 
 # Standard project metadata
 
-__version__ = "2.14.3"
+__version__ = "2.14.4"
 __author__ = "RTE, Antares Web Team"
-__date__ = "2023-06-20"
+__date__ = "2023-06-28"
 # noinspection SpellCheckingInspection
 __credits__ = "(c) Réseau de Transport de l’Électricité (RTE)"
 
