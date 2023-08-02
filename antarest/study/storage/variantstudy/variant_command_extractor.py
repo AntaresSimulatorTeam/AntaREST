bb08750435ccec2ee5c76ad9aba68f7daef678ad
Laurent LAPORTE <43534797+laurent-laporte-pro@users.noreply.github.com>
2023-08-02 22:26:19 +0200

    fix(variant): enhance `VariantCommandsExtractor` for Renewable Clusters and Short-Term Storage (#1688)

--- antarest/study/storage/variantstudy/variant_command_extractor.py
+++ antarest/study/storage/variantstudy/variant_command_extractor.py
@@ -178,6 +178,8 @@ class VariantCommandsExtractor:
             elif command_obj.command_name in [
                 CommandName.REMOVE_LINK,
                 CommandName.REMOVE_THERMAL_CLUSTER,
+                CommandName.REMOVE_RENEWABLES_CLUSTER,
+                CommandName.REMOVE_ST_STORAGE,
             ]:
                 command_list = first_commands
                 priority = 1
@@ -192,6 +194,8 @@ class VariantCommandsExtractor:
                 command_list = last_commands
                 priority = 3
             elif command_obj.command_name in [
+                CommandName.CREATE_ST_STORAGE,
+                CommandName.CREATE_RENEWABLES_CLUSTER,
                 CommandName.CREATE_THERMAL_CLUSTER,
                 CommandName.CREATE_LINK,
             ]:
