b00e7556a60fae4777ad0589d99be6bcc167d7cc
MartinBelthle <102529366+MartinBelthle@users.noreply.github.com>
2023-08-02 22:01:28 +0200

    fix(log-parser): simplify parsing and make it accurate (#1682)
    Co-authored-by: Laurent LAPORTE <laurent.laporte.pro@gmail.com>

--- tests/launcher/test_log_parser.py
+++ tests/launcher/test_log_parser.py
@@ -1,49 +1,109 @@
-import pytest
+import json
+import sys
+from typing import List, Union
 
-from antarest.launcher.adapters.log_parser import LaunchProgressDTO, LogParser
+import pytest
+from antarest.launcher.adapters.log_parser import LaunchProgressDTO
+from tests.launcher.assets import ASSETS_DIR
 
 
 @pytest.mark.parametrize(
-    "launch_progress_dto,line,expected_progression,expected_output",
+    "launch_progress_dto, line, expected_progress, expected_result",
     [
         (
-            LaunchProgressDTO(N_K=100, N_ANNUAL_RESULT=10),
-            "[infos] MC-Years : [1 .. 11], total: 11",
-            0,
+            LaunchProgressDTO(total_mc_years=100),
+            "[infos] Loading the list of areas...",
+            1.0,
             True,
         ),
         (
-            LaunchProgressDTO(N_K=100, N_ANNUAL_RESULT=10),
+            LaunchProgressDTO(total_mc_years=100),
+            "[infos] MC-Years : [1 .. 11], total: 11",
+            2.0,
+            True,
+        ),
+        (
+            LaunchProgressDTO(total_mc_years=10),
             "this is a test",
-            0,
+            0.0,
             False,
         ),
         (
-            LaunchProgressDTO(N_K=100, N_ANNUAL_RESULT=10),
+            LaunchProgressDTO(total_mc_years=100),
             "[solver][infos] parallel batch size : 10",
-            9 * 0.8,
-            True,
+            0.0,
+            False,
         ),
         (
-            LaunchProgressDTO(N_K=100, N_ANNUAL_RESULT=10),
+            LaunchProgressDTO(total_mc_years=10),
             "[solver][infos] Exporting the annual results",
-            0.8 * 9 * 1 / 10,
+            9.6,
             True,
         ),
         (
-            LaunchProgressDTO(N_K=100, N_ANNUAL_RESULT=10),
+            LaunchProgressDTO(total_mc_years=10),
             "[solver][infos] Exporting the survey results",
-            99 * 0.8,
+            99.0,
+            True,
+        ),
+        (
+            LaunchProgressDTO(total_mc_years=10),
+            "[infos] [UI] Quitting the solver gracefully",
+            100.0,
             True,
         ),
     ],
 )
-def test_update_progress(
+def test_parse_log_lines(
     launch_progress_dto: LaunchProgressDTO,
     line: str,
-    expected_progression: float,
-    expected_output: bool,
-):
-    output = LogParser.update_progress(line, launch_progress_dto)
-    assert launch_progress_dto.progress == expected_progression
-    assert output == expected_output
+    expected_progress: float,
+    expected_result: bool,
+) -> None:
+    output = launch_progress_dto.parse_log_lines([line])
+    assert launch_progress_dto.progress == expected_progress
+    assert output == expected_result
+
+
+HistoryType = List[List[Union[str, float]]]
+
+
+class MyLaunchProgressDTO(LaunchProgressDTO):
+    history: HistoryType = []
+
+    def _update_progress(self, line: str) -> bool:
+        update = super()._update_progress(line)
+        if update:
+            self.history.append([line, self.progress])
+        return update
+
+
+@pytest.mark.parametrize(
+    "filename",
+    [
+        "simulation.error.log",
+        "simulation.error-big.log",
+        "simulation.fatal.log",
+        "simulation.nominal.log",
+        "simulation.nominal-big.log",
+    ],
+)
+def test_parse_log_lines__non_regression_test(filename: str) -> None:
+    log_path = ASSETS_DIR / "log_parser" / filename
+    lines = log_path.read_text(encoding="utf-8").splitlines()
+    dto = MyLaunchProgressDTO()
+    updated = dto.parse_log_lines(lines)
+    assert updated
+    # check history
+    history_path = log_path.with_suffix(".history.json")
+    if history_path.exists():
+        expected = json.loads(history_path.read_text(encoding="utf-8"))
+        assert dto.history == expected
+    else:
+        with history_path.open(mode="w", encoding="utf-8") as fd:
+            fd.write(json.dumps(dto.history, ensure_ascii=False, indent=True))
+        print(
+            f"\nWARNING: check the result in '{history_path.relative_to(ASSETS_DIR)}'"
+            f" and commit the file in Git",
+            file=sys.stderr,
+        )
