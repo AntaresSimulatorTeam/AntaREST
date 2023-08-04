77eacb02188fd9b7f1f1124a0283ef86e675f6f2
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-08-04 13:14:33 +0200

    test: add fixtures in `conftest.py` to create services

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,9 @@ import pytest
 # noinspection PyUnresolvedReferences
 from tests.conftest_db import *
 
+# noinspection PyUnresolvedReferences
+from tests.conftest_services import *
+
 # fmt: off
 HERE = Path(__file__).parent.resolve()
 PROJECT_DIR = next(iter(p for p in HERE.parents if p.joinpath("antarest").exists()))
