c8c051a9664225e5676b8a24fd42c6fa4586111c
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-08-08 18:05:01 +0200

    style: reindent unit test source code

--- tests/variantstudy/model/command/test_create_area.py
+++ tests/variantstudy/model/command/test_create_area.py
@@ -1,4 +1,5 @@
 import configparser
+from unittest.mock import Mock
 
 from antarest.study.storage.rawstudy.model.filesystem.config.model import (
     transform_name_to_id,
@@ -20,11 +21,10 @@ from antarest.study.storage.variantstudy.model.command_context import (
 
 
 class TestCreateArea:
-    def test_validation(self, empty_study: FileStudy):
-        pass
-
     def test_apply(
-        self, empty_study: FileStudy, command_context: CommandContext
+        self,
+        empty_study: FileStudy,
+        command_context: CommandContext,
     ):
         version = empty_study.config.version
         study_path = empty_study.config.study_path
@@ -37,10 +37,9 @@ class TestCreateArea:
                 "command_context": command_context,
             }
         )
-        output = create_area_command.apply(
-            study_data=empty_study,
-        )
+        output = create_area_command.apply(study_data=empty_study)
 
+        # fmt: off
         # Areas
         assert area_id in empty_study.config.areas
 
@@ -49,9 +48,7 @@ class TestCreateArea:
         assert area_name in area_list
 
         assert (study_path / "input" / "areas" / area_id).is_dir()
-        assert (
-            study_path / "input" / "areas" / area_id / "optimization.ini"
-        ).exists()
+        assert (study_path / "input" / "areas" / area_id / "optimization.ini").exists()
         assert (study_path / "input" / "areas" / area_id / "ui.ini").exists()
 
         # Hydro
@@ -60,13 +57,14 @@ class TestCreateArea:
         assert int(hydro["inter-daily-breakdown"][area_id]) == 1
         assert int(hydro["intra-daily-modulation"][area_id]) == 24
         assert int(hydro["inter-monthly-breakdown"][area_id]) == 1
+
+        # sourcery skip: no-conditionals-in-tests
         if version > 650:
             assert int(hydro["initialize reservoir date"][area_id]) == 0
             assert int(hydro["leeway low"][area_id]) == 1
             assert int(hydro["leeway up"][area_id]) == 1
             assert int(hydro["pumping efficiency"][area_id]) == 1
 
-        # fmt: off
         correlation = configparser.ConfigParser()
         correlation.read(study_path / "input" / "hydro" / "prepro" / "correlation.ini")
         correlation_dict = {k: v for k, v in correlation.items() if k != "DEFAULT"}
@@ -74,263 +72,70 @@ class TestCreateArea:
             "general": {"mode": "annual"},
             "annual": {},
         }
-        # fmt: on
 
         # Allocation
-        assert (
-            study_path / "input" / "hydro" / "allocation" / f"{area_id}.ini"
-        ).exists()
+        assert (study_path / "input" / "hydro" / "allocation" / f"{area_id}.ini").exists()
         allocation = configparser.ConfigParser()
-        allocation.read(
-            study_path / "input" / "hydro" / "allocation" / f"{area_id}.ini"
-        )
+        allocation.read(study_path / "input" / "hydro" / "allocation" / f"{area_id}.ini")
         assert int(allocation["[allocation"][area_id]) == 1
 
         # Capacity
-        assert (
-            study_path
-            / "input"
-            / "hydro"
-            / "common"
-            / "capacity"
-            / f"maxpower_{area_id}.txt.link"
-        ).exists()
-        assert (
-            study_path
-            / "input"
-            / "hydro"
-            / "common"
-            / "capacity"
-            / f"reservoir_{area_id}.txt.link"
-        ).exists()
+        assert (study_path / "input" / "hydro" / "common" / "capacity" / f"maxpower_{area_id}.txt.link").exists()
+        assert (study_path / "input" / "hydro" / "common" / "capacity" / f"reservoir_{area_id}.txt.link").exists()
+
+        # sourcery skip: no-conditionals-in-tests
         if version > 650:
             assert (
-                study_path
-                / "input"
-                / "hydro"
-                / "common"
-                / "capacity"
-                / f"creditmodulations_{area_id}.txt.link"
+                study_path / "input" / "hydro" / "common" / "capacity" / f"creditmodulations_{area_id}.txt.link"
             ).exists()
             assert (
-                study_path
-                / "input"
-                / "hydro"
-                / "common"
-                / "capacity"
-                / f"inflowPattern_{area_id}.txt.link"
+                study_path / "input" / "hydro" / "common" / "capacity" / f"inflowPattern_{area_id}.txt.link"
             ).exists()
-            assert (
-                study_path
-                / "input"
-                / "hydro"
-                / "common"
-                / "capacity"
-                / f"waterValues_{area_id}.txt.link"
-            ).exists()
+            assert (study_path / "input" / "hydro" / "common" / "capacity" / f"waterValues_{area_id}.txt.link").exists()
 
         # Prepro
         assert (study_path / "input" / "hydro" / "prepro" / area_id).is_dir()
-        assert (
-            study_path
-            / "input"
-            / "hydro"
-            / "prepro"
-            / area_id
-            / "energy.txt.link"
-        ).exists()
+        assert (study_path / "input" / "hydro" / "prepro" / area_id / "energy.txt.link").exists()
 
         allocation = configparser.ConfigParser()
-        allocation.read(
-            study_path / "input" / "hydro" / "prepro" / area_id / "prepro.ini"
-        )
+        allocation.read(study_path / "input" / "hydro" / "prepro" / area_id / "prepro.ini")
         assert float(allocation["prepro"]["intermonthly-correlation"]) == 0.5
 
         # Series
         assert (study_path / "input" / "hydro" / "series" / area_id).is_dir()
-        assert (
-            study_path
-            / "input"
-            / "hydro"
-            / "series"
-            / area_id
-            / "mod.txt.link"
-        ).exists()
-        assert (
-            study_path
-            / "input"
-            / "hydro"
-            / "series"
-            / area_id
-            / "ror.txt.link"
-        ).exists()
+        assert (study_path / "input" / "hydro" / "series" / area_id / "mod.txt.link").exists()
+        assert (study_path / "input" / "hydro" / "series" / area_id / "ror.txt.link").exists()
 
         # Links
         assert (study_path / "input" / "links" / area_id).is_dir()
-        assert (
-            study_path / "input" / "links" / area_id / "properties.ini"
-        ).exists()
+        assert (study_path / "input" / "links" / area_id / "properties.ini").exists()
 
-        # Load
-        # Prepro
-        assert (study_path / "input" / "load" / "prepro" / area_id).is_dir()
-        assert (
-            study_path
-            / "input"
-            / "load"
-            / "prepro"
-            / area_id
-            / "conversion.txt.link"
-        ).exists()
-        assert (
-            study_path
-            / "input"
-            / "load"
-            / "prepro"
-            / area_id
-            / "data.txt.link"
-        ).exists()
-        assert (
-            study_path / "input" / "load" / "prepro" / area_id / "k.txt.link"
-        ).exists()
-        assert (
-            study_path / "input" / "load" / "prepro" / area_id / "settings.ini"
-        ).exists()
-        assert (
-            study_path
-            / "input"
-            / "load"
-            / "prepro"
-            / area_id
-            / "translation.txt.link"
-        ).exists()
-
-        # Series
-        assert (
-            study_path
-            / "input"
-            / "load"
-            / "series"
-            / f"load_{area_id}.txt.link"
-        ).exists()
+        # Load / Solar / Wind
+        # sourcery skip: no-loop-in-tests
+        for gen_type in ("load", "solar", "wind"):
+            # Prepro
+            assert (study_path / "input" / gen_type / "prepro" / area_id).is_dir()
+            assert (study_path / "input" / gen_type / "prepro" / area_id / "conversion.txt.link").exists()
+            assert (study_path / "input" / gen_type / "prepro" / area_id / "data.txt.link").exists()
+            assert (study_path / "input" / gen_type / "prepro" / area_id / "k.txt.link").exists()
+            assert (study_path / "input" / gen_type / "prepro" / area_id / "settings.ini").exists()
+            assert (study_path / "input" / gen_type / "prepro" / area_id / "translation.txt.link").exists()
+            # Series
+            assert (study_path / "input" / gen_type / "series" / f"{gen_type}_{area_id}.txt.link").exists()
 
         # Misc-gen
-        assert (
-            study_path / "input" / "misc-gen" / f"miscgen-{area_id}.txt.link"
-        ).exists()
+        assert (study_path / "input" / "misc-gen" / f"miscgen-{area_id}.txt.link").exists()
 
         # Reserves
-        assert (
-            study_path / "input" / "reserves" / f"{area_id}.txt.link"
-        ).exists()
+        assert (study_path / "input" / "reserves" / f"{area_id}.txt.link").exists()
 
-        # Solar
-        # Prepro
-        assert (study_path / "input" / "solar" / "prepro" / area_id).is_dir()
-        assert (
-            study_path
-            / "input"
-            / "solar"
-            / "prepro"
-            / area_id
-            / "conversion.txt.link"
-        ).exists()
-        assert (
-            study_path
-            / "input"
-            / "solar"
-            / "prepro"
-            / area_id
-            / "data.txt.link"
-        ).exists()
-        assert (
-            study_path / "input" / "solar" / "prepro" / area_id / "k.txt.link"
-        ).exists()
-        assert (
-            study_path
-            / "input"
-            / "solar"
-            / "prepro"
-            / area_id
-            / "settings.ini"
-        ).exists()
-        assert (
-            study_path
-            / "input"
-            / "solar"
-            / "prepro"
-            / area_id
-            / "translation.txt.link"
-        ).exists()
-
-        # Series
-        assert (
-            study_path
-            / "input"
-            / "solar"
-            / "series"
-            / f"solar_{area_id}.txt.link"
-        ).exists()
-
-        # Thermal
-        assert (
-            study_path / "input" / "thermal" / "clusters" / area_id
-        ).is_dir()
-
-        assert (
-            study_path
-            / "input"
-            / "thermal"
-            / "clusters"
-            / area_id
-            / "list.ini"
-        ).exists()
+        # Thermal Clusters
+        assert (study_path / "input" / "thermal" / "clusters" / area_id).is_dir()
+        assert (study_path / "input" / "thermal" / "clusters" / area_id / "list.ini").exists()
 
         # thermal/areas ini file
         assert (study_path / "input" / "thermal" / "areas.ini").exists()
-
-        # Wind
-        # Prepro
-        assert (study_path / "input" / "wind" / "prepro" / area_id).is_dir()
-        assert (
-            study_path
-            / "input"
-            / "wind"
-            / "prepro"
-            / area_id
-            / "conversion.txt.link"
-        ).exists()
-        assert (
-            study_path
-            / "input"
-            / "wind"
-            / "prepro"
-            / area_id
-            / "data.txt.link"
-        ).exists()
-        assert (
-            study_path / "input" / "wind" / "prepro" / area_id / "k.txt.link"
-        ).exists()
-        assert (
-            study_path / "input" / "wind" / "prepro" / area_id / "settings.ini"
-        ).exists()
-        assert (
-            study_path
-            / "input"
-            / "wind"
-            / "prepro"
-            / area_id
-            / "translation.txt.link"
-        ).exists()
-
-        # Series
-        assert (
-            study_path
-            / "input"
-            / "wind"
-            / "series"
-            / f"wind_{area_id}.txt.link"
-        ).exists()
+        # fmt: on
 
         assert output.status
 
@@ -346,12 +151,12 @@ class TestCreateArea:
 
 
 def test_match(command_context: CommandContext):
+    # fmt: off
     base = CreateArea(area_name="foo", command_context=command_context)
     other_match = CreateArea(area_name="foo", command_context=command_context)
-    other_not_match = CreateArea(
-        area_name="bar", command_context=command_context
-    )
+    other_not_match = CreateArea(area_name="bar", command_context=command_context)
     other_other = RemoveArea(id="id", command_context=command_context)
+    # fmt: on
     assert base.match(other_match)
     assert not base.match(other_not_match)
     assert not base.match(other_other)
@@ -361,9 +166,8 @@ def test_match(command_context: CommandContext):
 
 def test_revert(command_context: CommandContext):
     base = CreateArea(area_name="foo", command_context=command_context)
-    assert CommandReverter().revert(base, [], None) == [
-        RemoveArea(id="foo", command_context=command_context)
-    ]
+    actual = CommandReverter().revert(base, [], Mock(spec=FileStudy))
+    assert actual == [RemoveArea(id="foo", command_context=command_context)]
 
 
 def test_create_diff(command_context: CommandContext):
