67dd82e4fee683db9d66d95242b15789f872669c
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-10-11 18:21:18 +0200

    build: prepare hotfix v2.15.3 (unreleased)

--- antarest/__init__.py
+++ antarest/__init__.py
@@ -7,9 +7,9 @@ from pathlib import Path
 
 # Standard project metadata
 
-__version__ = "2.15.2"
+__version__ = "2.15.3"
 __author__ = "RTE, Antares Web Team"
-__date__ = "2023-10-11"
+__date__ = "unreleased"
 # noinspection SpellCheckingInspection
 __credits__ = "(c) Réseau de Transport de l’Électricité (RTE)"
 
