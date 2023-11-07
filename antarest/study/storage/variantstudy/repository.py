cd15eec62302844aed3e740cb3679f8f92da3846
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-11-07 01:19:55 +0100

    refactor(variant-study-repo): optionally use a user-defined SqlAlchemy session instead of the global `db.session` and improve the documentation

--- antarest/study/storage/variantstudy/repository.py
+++ antarest/study/storage/variantstudy/repository.py
@@ -1,29 +1,65 @@
-from typing import List
+import typing as t
+
+from sqlalchemy.orm import Session  # type: ignore
 
 from antarest.core.interfaces.cache import ICache
 from antarest.core.utils.fastapi_sqlalchemy import db
+from antarest.study.model import Study
 from antarest.study.repository import StudyMetadataRepository
 from antarest.study.storage.variantstudy.model.dbmodel import CommandBlock, VariantStudy
 
 
 class VariantStudyRepository(StudyMetadataRepository):
     """
-    Variant  study repository
+    Variant study repository
     """
 
-    def __init__(self, cache_service: ICache):
-        super().__init__(cache_service)
+    def __init__(self, cache_service: ICache, session: t.Optional[Session] = None):
+        """
+        Initialize the variant study repository.
 
-    def get_children(self, parent_id: str) -> List[VariantStudy]:
-        studies: List[VariantStudy] = db.session.query(VariantStudy).filter(VariantStudy.parent_id == parent_id).all()
+        Args:
+            cache_service: Cache service for the repository.
+            session: Optional SQLAlchemy session to be used.
+        """
+        super().__init__(cache_service)
+        self._session = session
+
+    @property
+    def session(self) -> Session:
+        """
+        Get the SQLAlchemy session for the repository.
+
+        Returns:
+            SQLAlchemy session.
+        """
+        if self._session is None:
+            # Get or create the session from a context variable (thread local variable)
+            return db.session
+        # Get the user-defined session
+        return self._session
+
+    def get_children(self, parent_id: str) -> t.List[VariantStudy]:
+        """
+        Get the children of a variant study.
+
+        Args:
+            parent_id: Identifier of the parent study.
+
+        Returns:
+            List of `VariantStudy` objects.
+        """
+        studies: t.List[VariantStudy] = (
+            self.session.query(VariantStudy).filter(VariantStudy.parent_id == parent_id).all()
+        )
         return studies
 
-    def get_all_commandblocks(self) -> List[CommandBlock]:
-        outputs = db.session.query(CommandBlock).all()
+    def get_all_command_blocks(self) -> t.List[CommandBlock]:
+        """
+        Get all command blocks.
 
-        # for mypy
-        assert isinstance(outputs, list)
-        for output in outputs:
-            assert isinstance(output, CommandBlock)
-
-        return outputs
+        Returns:
+            List of `CommandBlock` objects.
+        """
+        cmd_blocks: t.List[CommandBlock] = self.session.query(CommandBlock).all()
+        return cmd_blocks
