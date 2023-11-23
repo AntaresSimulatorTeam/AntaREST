191fb98c0723299d6c6c531cf2a61140c3fd4c1a
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-11-23 08:15:03 +0100

    refactor(raw-study): the `create_from_fs` method is changed to return a `NamedTuple` (perf)

--- antarest/study/storage/rawstudy/model/filesystem/factory.py
+++ antarest/study/storage/rawstudy/model/filesystem/factory.py
@@ -1,8 +1,7 @@
 import logging
 import time
-from dataclasses import dataclass
+import typing as t
 from pathlib import Path
-from typing import Optional
 
 from antarest.core.interfaces.cache import CacheConstants, ICache
 from antarest.matrixstore.service import ISimpleMatrixService
@@ -15,8 +14,15 @@ from antarest.study.storage.rawstudy.model.filesystem.root.filestudytree import 
 logger = logging.getLogger(__name__)
 
 
-@dataclass
-class FileStudy:
+class FileStudy(t.NamedTuple):
+    """
+    Antares study stored on the disk.
+
+    Attributes:
+        config: Root object to handle all study parameters which impact tree structure.
+        tree: Top level node of antares tree structure.
+    """
+
     config: FileStudyTreeConfig
     tree: FileStudyTree
 
@@ -39,7 +45,7 @@ class StudyFactory:
         self,
         path: Path,
         study_id: str,
-        output_path: Optional[Path] = None,
+        output_path: t.Optional[Path] = None,
         use_cache: bool = True,
     ) -> FileStudy:
         cache_id = f"{CacheConstants.STUDY_FACTORY}/{study_id}"
@@ -55,11 +61,11 @@ class StudyFactory:
         logger.info(f"Study {study_id} config built in {duration}s")
         result = FileStudy(config, FileStudyTree(self.context, config))
         if study_id and use_cache:
+            logger.info(f"Cache new entry from StudyFactory (studyID: {study_id})")
             self.cache.put(
                 cache_id,
                 FileStudyTreeConfigDTO.from_build_config(config).dict(),
             )
-        logger.info(f"Cache new entry from StudyFactory (studyID: {study_id})")
         return result
 
     def create_from_config(self, config: FileStudyTreeConfig) -> FileStudyTree:
