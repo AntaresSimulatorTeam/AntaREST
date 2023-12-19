1359923a73887830d91a465c77ea484d06a2903f
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-12-19 20:24:35 +0100

    docs(bc): add docstring to `BindingConstraintFrequency` enumeration

--- antarest/study/storage/rawstudy/model/filesystem/config/binding_constraint.py
+++ antarest/study/storage/rawstudy/model/filesystem/config/binding_constraint.py
@@ -1,10 +1,30 @@
+import typing as t
 from enum import Enum
-from typing import Set
 
 from pydantic import BaseModel
 
 
 class BindingConstraintFrequency(str, Enum):
+    """
+    Frequency of binding constraint
+
+    - HOURLY: hourly time series with 8784 lines
+    - DAILY: daily time series with 366 lines
+    - WEEKLY: weekly time series with 366 lines (same as daily)
+
+    Usage example:
+
+    >>> bcf = BindingConstraintFrequency.HOURLY
+    >>> bcf == "hourly"
+    True
+    >>> bcf = BindingConstraintFrequency.DAILY
+    >>> "daily" == bcf
+    True
+    >>> bcf = BindingConstraintFrequency.WEEKLY
+    >>> bcf != "daily"
+    True
+    """
+
     HOURLY = "hourly"
     DAILY = "daily"
     WEEKLY = "weekly"
@@ -12,6 +32,6 @@ class BindingConstraintFrequency(str, Enum):
 
 class BindingConstraintDTO(BaseModel):
     id: str
-    areas: Set[str]
-    clusters: Set[str]
+    areas: t.Set[str]
+    clusters: t.Set[str]
     time_step: BindingConstraintFrequency
