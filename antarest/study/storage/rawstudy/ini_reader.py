02130401f298c401a9114f843a6d766e88d14791
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-12-29 11:22:31 +0100

    perf(raw-study): improve INI file reading performance
    The `IniReader.read` method has been optimized to read INI files faster. We now use a specific implementation which uses strings methods instead of `configparser`.

--- antarest/study/storage/rawstudy/ini_reader.py
+++ antarest/study/storage/rawstudy/ini_reader.py
@@ -1,11 +1,36 @@
-import configparser
-import contextlib
-import re
 import typing as t
 from abc import ABC, abstractmethod
 from pathlib import Path
 
 from antarest.core.model import JSON, SUB_JSON
+
+
+def convert_value(value: str) -> t.Union[str, int, float, bool]:
+    """Convert value to the appropriate type for JSON."""
+
+    try:
+        # Infinity values are not supported by JSON, so we use a string instead.
+        mapping = {"true": True, "false": False, "+inf": "+Inf", "-inf": "-Inf", "inf": "+Inf"}
+        return t.cast(t.Union[str, int, float, bool], mapping[value.lower()])
+    except KeyError:
+        try:
+            return int(value)
+        except ValueError:
+            try:
+                return float(value)
+            except ValueError:
+                return value
+
+
+def convert_obj(item: t.Any) -> SUB_JSON:
+    """Convert object to the appropriate type for JSON (scalar, dictionary or list)."""
+
+    if isinstance(item, dict):
+        return {key: convert_obj(value) for key, value in item.items()}
+    elif isinstance(item, list):
+        return [convert_obj(value) for value in item]
+    else:
+        return convert_value(item)
 
 
 class IReader(ABC):
@@ -16,151 +41,162 @@ class IReader(ABC):
     @abstractmethod
     def read(self, path: t.Any) -> JSON:
         """
-        Parse .ini file to json
+        Parse `.ini` file to json object.
+
         Args:
-            path: .ini file
+            path: Path to `.ini` file or file-like object.
 
-        Returns: json content
-
+        Returns:
+            Dictionary of parsed `.ini` file which can be converted to JSON.
         """
         raise NotImplementedError()
 
 
 class IniReader(IReader):
     """
-    Standard .ini file reader. Use for general purpose.
+    Common `.ini` file reader. Use for general purpose.
     """
 
-    @staticmethod
-    def _parse_bool(value: str) -> t.Optional[bool]:
-        return {"true": True, "false": False}.get(value.lower())
-
-    @staticmethod
-    def _parse_int(value: str) -> t.Optional[int]:
-        try:
-            return int(value)
-        except ValueError:
-            return None
-
-    @staticmethod
-    def _parse_float(value: str) -> t.Optional[float]:
-        try:
-            return float(value)
-        except ValueError:
-            return None
-
-    @staticmethod
-    def parse_value(value: str) -> t.Union[bool, int, float, str]:
-        def strict_bool(v: str) -> bool:
-            return {"true": True, "false": False}[v.lower()]
-
-        for parser in [strict_bool, int, float]:
-            with contextlib.suppress(KeyError, ValueError):
-                return parser(value)  # type: ignore
-        return value
-
-    @staticmethod
-    def _parse_json(json: configparser.SectionProxy) -> JSON:
-        return {key: IniReader.parse_value(value) for key, value in json.items()}
-
-    def read(self, path: t.Any) -> JSON:
-        config = IniConfigParser()
-        if isinstance(path, Path):
-            config.read(path)
-        else:
-            config.read_file(path)
-        return {key: IniReader._parse_json(config[key]) for key in config if key != "DEFAULT"}
-
-
-class SimpleKeyValueReader(IReader):
-    """
-    Standard .ini file reader. Use for general purpose.
-    """
-
-    @staticmethod
-    def _parse_inf(value: str) -> t.Optional[str]:
-        try:
-            return "+Inf" if float(value) == float("inf") else None
-        except ValueError:
-            return None
-
-    # noinspection PyProtectedMember
-    @staticmethod
-    def parse_value(value: str) -> SUB_JSON:
-        parsed: t.Union[str, int, float, bool, None] = SimpleKeyValueReader._parse_inf(value)
-        parsed = parsed if parsed is not None else IniReader._parse_bool(value)
-        parsed = parsed if parsed is not None else IniReader._parse_int(value)
-        parsed = parsed if parsed is not None else IniReader._parse_float(value)
-        return parsed if parsed is not None else value
-
-    @staticmethod
-    def _parse_json(json: JSON) -> JSON:
-        return {key: SimpleKeyValueReader.parse_value(value) for key, value in json.items()}
-
-    def read(self, path: t.Any) -> JSON:
-        json = {}
-        ini_file = path.open(mode="r", encoding="utf-8") if isinstance(path, Path) else path
-        with ini_file:
-            for line in ini_file:
-                line = line.strip()
-                if line and not line.startswith("#"):
-                    key, value = line.split("=")
-                    json[key.strip()] = value.strip()
-
-        return self._parse_json(json)
-
-
-# noinspection SpellCheckingInspection
-class IniConfigParser(configparser.RawConfigParser):
-    # The regex to match a section is modified so that it can parse
-    # a section name in square brackets, such as "[[allocation]]"
-    _SECT_TMPL = r"\[ (?P<header>.+) \]"
-    SECTCRE = re.compile(_SECT_TMPL, re.VERBOSE)
-
-    # noinspection SpellCheckingInspection
-    def optionxform(self, optionstr: str) -> str:
-        return optionstr
-
-
-class MultipleSameKeysIniReader(IReader):
-    """
-    Custom .ini reader for inputs/sets.ini file.
-    This file has format :
-    ```python
-    [chap]
-    + = areaA
-    + = areaB
-    ```
-
-    multikey is not compatible with standard .ini readers
-    """
-
-    def __init__(self, special_keys: t.Optional[t.List[str]] = None) -> None:
-        self.special_keys = special_keys or []
+    def __init__(self, special_keys: t.Sequence[str] = (), section_name: str = "settings") -> None:
         super().__init__()
 
-    def read(self, path: t.Any) -> JSON:
-        data: JSON = {}
-        section = ""
-        ini_file = path.open(mode="r", encoding="utf-8") if isinstance(path, Path) else path
-        with ini_file:
-            for line in ini_file:
-                line = line.strip()
-                if match := re.fullmatch(r"\[(.*)]", line):
-                    section = match[1]
-                    data[section] = {}
-                elif "=" in line:
-                    key, arg = map(str.strip, line.split("=", 1))
-                    value = IniReader.parse_value(arg)
-                    group = data[section]
-                    if key in group:
-                        if isinstance(group[key], list):
-                            group[key].append(value)
-                        else:
-                            group[key] = [group[key], value]
-                    elif key in self.special_keys:
-                        group[key] = [value]
-                    else:
-                        group[key] = value
+        # Default section name to use if `.ini` file has no section.
+        self._special_keys = set(special_keys)
 
-        return data
+        # List of keys which should be parsed as list.
+        self._section_name = section_name
+
+    def __repr__(self) -> str:  # pragma: no cover
+        """Return a string representation of the object."""
+        cls = self.__class__.__name__
+        # use getattr() to make sure that the attributes are defined
+        special_keys = tuple(getattr(self, "_special_keys", ()))
+        section_name = getattr(self, "_section_name", "settings")
+        return f"{cls}(special_keys={special_keys!r}, section_name={section_name!r})"
+
+    def read(self, path: t.Any) -> JSON:
+        if isinstance(path, (Path, str)):
+            try:
+                with open(path, mode="r", encoding="utf-8") as f:
+                    sections = self._parse_ini_file(f)
+            except FileNotFoundError:
+                # If the file is missing, an empty dictionary is returned.
+                # This is required tp mimic the behavior of `configparser.ConfigParser`.
+                return {}
+
+        elif hasattr(path, "read"):
+            with path:
+                sections = self._parse_ini_file(path)
+
+        else:  # pragma: no cover
+            raise TypeError(repr(type(path)))
+
+        return t.cast(JSON, convert_obj(sections))
+
+    def _parse_ini_file(self, ini_file: t.TextIO) -> JSON:
+        """
+        Parse `.ini` file to JSON object.
+
+        The following parsing rules are applied:
+
+        - If the file has no section, then the default section name is used.
+          This case is required to parse Xpansion `user/expansion/settings.ini` files
+          (using `SimpleKeyValueReader` subclass).
+
+        - If the file has duplicate sections, then the values are merged.
+          This case is required when the end-user produced an ill-formed `.ini` file.
+          This ensures the parsing is robust even if some values may be lost.
+
+        - If a section has duplicate keys, then the values are merged.
+          This case is required, for instance, to parse `settings/generaldata.ini` files which
+          has duplicate keys like "playlist_year_weight", "playlist_year +", "playlist_year -",
+          "select_var -", "select_var +", in the `[playlist]` section.
+          In this case, duplicate keys must be declared in the `special_keys` argument,
+          to parse them as list.
+
+        - If a section has no key, then an empty dictionary is returned.
+          This case is required to parse `input/hydro/prepro/correlation.ini` files.
+
+        - If a section name has square brackets, then they are preserved.
+          This case is required to parse `input/hydro/allocation/{area-id}.ini` files.
+
+        Args:
+            ini_file: file or file-like object.
+
+        Returns:
+            Dictionary of parsed `.ini` file which can be converted to JSON.
+        """
+        # NOTE: This algorithm is 1.93x faster than configparser.ConfigParser
+        sections: t.Dict[str, t.Dict[str, t.Any]] = {}
+        section_name = self._section_name
+
+        for line in ini_file:
+            line = line.strip()
+            if not line or line.startswith(";") or line.startswith("#"):
+                continue
+            elif line.startswith("["):
+                section_name = line[1:-1]
+                sections.setdefault(section_name, {})
+            elif "=" in line:
+                key, value = map(str.strip, line.split("=", 1))
+                section = sections.setdefault(section_name, {})
+                if key in self._special_keys:
+                    section.setdefault(key, []).append(value)
+                else:
+                    section[key] = value
+            else:
+                raise ValueError(f"☠☠☠ Invalid line: {line!r}")
+
+        return sections
+
+
+class SimpleKeyValueReader(IniReader):
+    """
+    Simple INI reader for "settings.ini" file which has no section.
+    """
+
+    def read(self, path: t.Any) -> JSON:
+        """
+        Parse `.ini` file which has no section to JSON object.
+
+        This class is required to parse Xpansion `user/expansion/settings.ini` files.
+
+        Args:
+            path: Path to `.ini` file or file-like object.
+
+        Returns:
+            Dictionary of parsed key/value pairs.
+        """
+        sections = super().read(path)
+        obj = t.cast(t.Mapping[str, JSON], sections)
+        return obj[self._section_name]
+
+
+class MultipleSameKeysIniReader(IniReader):
+    """
+    Custom `.ini` reader for `.ini` files which have duplicate keys in a section.
+
+    This class is required, to parse `settings/generaldata.ini` files which
+    has duplicate keys like "playlist_year_weight", "playlist_year +", "playlist_year -",
+    "select_var -", "select_var +", in the `[playlist]` section.
+
+    For instance::
+
+        [playlist]
+        playlist_reset = false
+        playlist_year + = 6
+        playlist_year + = 8
+        playlist_year + = 13
+
+    It is also required to parse `input/areas/sets.ini` files which have keys like "+" or "-".
+
+    For instance::
+
+        [all areas]
+        caption = All areas
+        comments = Spatial aggregates on all areas
+        + = east
+        + = west
+
+    This class is not compatible with standard `.ini` readers.
+    """
--- tests/storage/repository/antares_io/reader/test_ini_reader.py
+++ tests/storage/repository/antares_io/reader/test_ini_reader.py
@@ -1,14 +1,12 @@
+import io
 import textwrap
 from pathlib import Path
 
-import pytest
-
-from antarest.study.storage.rawstudy.ini_reader import IniReader, MultipleSameKeysIniReader
+from antarest.study.storage.rawstudy.ini_reader import IniReader, MultipleSameKeysIniReader, SimpleKeyValueReader
 
 
 class TestIniReader:
-    @pytest.mark.unit_test
-    def test_read(self, tmp_path: Path) -> None:
+    def test_read__nominal_case(self, tmp_path: Path) -> None:
         path = Path(tmp_path) / "test.ini"
         path.write_text(
             textwrap.dedent(
@@ -16,14 +14,17 @@ class TestIniReader:
                 [part1]
                 key_int = 1
                 key_float = 2.1
+                key_big_float = 1e16
+                key_small_float = -1e-16
+                key_inf = +Inf
                 key_str = value1
-            
+                key_empty =
+
                 [part2]
                 key_bool = True
-                key_bool2 = False
-                
-                [[allocation]]
-                my_area = 2.718
+                key_bool2 = false
+                # key_foo = False
+                ; key_bar = 3.14
                 """
             )
         )
@@ -32,9 +33,19 @@ class TestIniReader:
         actual = reader.read(path)
 
         expected = {
-            "part1": {"key_int": 1, "key_str": "value1", "key_float": 2.1},
-            "part2": {"key_bool": True, "key_bool2": False},
-            "[allocation]": {"my_area": 2.718},
+            "part1": {
+                "key_int": 1,
+                "key_str": "value1",
+                "key_float": 2.1,
+                "key_big_float": 1e16,
+                "key_small_float": -1e-16,
+                "key_inf": "+Inf",  # note: `+Inf` is not supported by JSON
+                "key_empty": "",
+            },
+            "part2": {
+                "key_bool": True,
+                "key_bool2": False,
+            },
         }
         assert actual == expected
 
@@ -42,64 +53,224 @@ class TestIniReader:
             actual_from_bytes = reader.read(f)
             assert actual_from_bytes == expected
 
+    def test_read__without_section(self) -> None:
+        """
+        If the file has no section, then the default section name is used.
+        This case is required to parse Xpansion `user/expansion/settings.ini` files
+        (using `SimpleKeyValueReader` subclass).
+        """
+        reader = IniReader(section_name="config")
+        actual = reader.read(
+            io.StringIO(
+                """
+                key_int = 1
+                key_float = 2.1
+                key_str = value1
+                key_inf = +Inf
+                """
+            )
+        )
+        expected = {
+            "config": {
+                "key_int": 1,
+                "key_str": "value1",
+                "key_float": 2.1,
+                "key_inf": "+Inf",  # note: `+Inf` is not supported by JSON
+            },
+        }
+        assert actual == expected
 
-class TestMultipleSameKeysIniReader:
-    def test_read_sets_init(self, tmp_path: Path) -> None:
-        path = Path(tmp_path) / "test.ini"
-        path.write_text(
-            textwrap.dedent(
+    def test_read__duplicate_sections(self) -> None:
+        """
+        If the file has duplicate sections, then the values are merged.
+        This case is required when the end-user produced an ill-formed `.ini` file.
+        """
+        reader = IniReader()
+        actual = reader.read(
+            io.StringIO(
                 """
                 [part1]
                 key_int = 1
                 key_float = 2.1
                 key_str = value1
-    
-                [part2]
-                key_bool = true
-                key_bool = false
 
+                [part1]
+                key_str = value2
+                key_bool = True
+                """
+            )
+        )
+        expected = {
+            "part1": {
+                "key_int": 1,
+                "key_str": "value2",
+                "key_float": 2.1,
+                "key_bool": True,
+            },
+        }
+        assert actual == expected
+
+    def test_read__duplicate_keys(self) -> None:
+        """
+        If a section has duplicate keys, then the values are merged.
+        This case is required, for instance, to parse `settings/generaldata.ini` files which
+        has duplicate keys like "playlist_year_weight", "playlist_year +", "playlist_year -",
+        "select_var -", "select_var +", in the `[playlist]` section.
+        In this case, duplicate keys must be declared in the `special_keys` argument,
+        to parse them as list.
+        """
+        reader = IniReader(
+            special_keys=[
+                "playlist_year_weight",
+                "playlist_year +",
+                "playlist_year -",
+                "select_var -",
+                "select_var +",
+            ]
+        )
+        actual = reader.read(
+            io.StringIO(
+                """
+                [part1]
+                key_int = 1
+                key_int = 2
+                
+                [playlist]
+                playlist_reset = false
+                playlist_year_weight = 1
+                playlist_year + = 2015
+                playlist_year + = 2016
+                playlist_year + = 2017
+                playlist_year - = 2018
+                playlist_year - = 2019
+                playlist_year - = 2020
+                select_var - = 1
+                select_var + = 2
+                """
+            )
+        )
+        expected = {
+            "part1": {"key_int": 2},  # last value is kept
+            "playlist": {
+                "playlist_reset": False,
+                "playlist_year_weight": [1],
+                "playlist_year +": [2015, 2016, 2017],
+                "playlist_year -": [2018, 2019, 2020],
+                "select_var -": [1],
+                "select_var +": [2],
+            },
+        }
+        assert actual == expected
+
+    def test_read__no_key(self) -> None:
+        """
+        If a section has no key, then an empty dictionary is returned.
+        This case is required to parse `input/hydro/prepro/correlation.ini` files.
+        """
+        reader = IniReader()
+        actual = reader.read(
+            io.StringIO(
+                """
+                [part1]
+                key_int = 1
+                
+                [part2]
+                """
+            )
+        )
+        expected = {
+            "part1": {"key_int": 1},
+            "part2": {},
+        }
+        assert actual == expected
+
+    def test_read__with_square_brackets(self) -> None:
+        """
+        If a section name has square brackets, then they are preserved.
+        This case is required to parse `input/hydro/allocation/{area-id}.ini` files.
+        """
+        reader = IniReader()
+        actual = reader.read(
+            io.StringIO(
+                """
+                [part1]
+                key_int = 1
+                
                 [[allocation]]
                 my_area = 2.718
                 """
             )
         )
-
-        reader = MultipleSameKeysIniReader()
-        actual = reader.read(path)
-
         expected = {
-            "part1": {"key_int": 1, "key_str": "value1", "key_float": 2.1},
-            "part2": {"key_bool": [True, False]},
+            "part1": {"key_int": 1},
             "[allocation]": {"my_area": 2.718},
         }
-
         assert actual == expected
 
-        with path.open() as f:
-            actual_from_bytes = reader.read(f)
-            assert actual_from_bytes == expected
 
-    def test_read__with_special_keys(self, tmp_path: Path) -> None:
-        path = Path(tmp_path) / "test.ini"
-        path.write_text(
-            textwrap.dedent(
+class TestSimpleKeyValueReader:
+    def test_read(self) -> None:
+        # sample extracted from `user/expansion/settings.ini`
+        settings = textwrap.dedent(
+            """
+            master=relaxed
+            uc_type=expansion_fast
+            optimality_gap=10001.0
+            relative_gap=1e-06
+            relaxed_optimality_gap=1e-05
+            max_iteration=20
+            solver=Xpress
+            log_level=3
+            separation_parameter=0.66
+            batch_size=102
+            yearly-weights=
+            additional-constraints=constraintsFile.txt
+            timelimit=1000000000000
+            """
+        )
+        ini_reader = SimpleKeyValueReader(section_name="dummy")
+        actual = ini_reader.read(io.StringIO(settings))
+
+        expected = {
+            "master": "relaxed",
+            "uc_type": "expansion_fast",
+            "optimality_gap": 10001.0,
+            "relative_gap": 1e-06,
+            "relaxed_optimality_gap": 1e-05,
+            "max_iteration": 20,
+            "solver": "Xpress",
+            "log_level": 3,
+            "separation_parameter": 0.66,
+            "batch_size": 102,
+            "yearly-weights": "",
+            "additional-constraints": "constraintsFile.txt",
+            "timelimit": 1000000000000,
+        }
+        assert actual == expected
+
+
+class TestMultipleSameKeysIniReader:
+    def test_read__sets(self) -> None:
+        """
+        It is also required to parse `input/areas/sets.ini` files which have keys like "+" or "-".
+        """
+        reader = MultipleSameKeysIniReader(["+", "-"])
+        actual = reader.read(
+            io.StringIO(
                 """
-                [chap]
-                + = areaA
-                + = areaB
+                [all areas]
+                caption = All areas
+                comments = Spatial aggregates on all areas
+                + = east
+                + = west
                 """
             )
         )
-
-        reader = MultipleSameKeysIniReader(special_keys=["+"])
-        actual = reader.read(path)
-
         expected = {
-            "chap": {"+": ["areaA", "areaB"]},
+            "all areas": {
+                "caption": "All areas",
+                "comments": "Spatial aggregates on all areas",
+                "+": ["east", "west"],
+            },
         }
-
         assert actual == expected
-
-        with path.open() as f:
-            actual_from_bytes = reader.read(f)
-            assert actual_from_bytes == expected
