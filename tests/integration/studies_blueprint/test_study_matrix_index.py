0df4640ddbe341da832cde56aa114e6ddf09cf33
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2024-01-04 19:33:19 +0100

    fix(thermals): correct TS Generator matrix index for thermal clusters
    - The modulation matrix is an hourly time series,
    - The data matrix is a daily time series.

---
+++ tests/integration/studies_blueprint/test_study_matrix_index.py
@@ -0,0 +1,100 @@
+from starlette.testclient import TestClient
+
+
+class TestStudyMatrixIndex:
+    """
+    The goal of this test is to check that the API allows to retrieve
+    information about the data matrices of a study.
+
+    The values are used byt the frontend to display the time series
+    with the right time column.
+    """
+
+    def test_get_study_matrix_index(
+        self,
+        client: TestClient,
+        user_access_token: str,
+        study_id: str,
+    ) -> None:
+        user_access_token = {"Authorization": f"Bearer {user_access_token}"}
+
+        # Check the matrix index for Thermal clusters
+        # ===========================================
+
+        # Check the Common matrix index
+        res = client.get(
+            f"/v1/studies/{study_id}/matrixindex",
+            headers=user_access_token,
+            params={"path": "input/thermal/prepro/fr/01_solar/modulation"},
+        )
+        assert res.status_code == 200, res.json()
+        actual = res.json()
+        # We expect to have an "hourly" time series with 8760 hours
+        expected = {
+            "first_week_size": 7,
+            "level": "hourly",
+            "start_date": "2001-01-01 00:00:00",
+            "steps": 8760,
+        }
+        assert actual == expected
+
+        # Check the TS Generator matrix index
+        res = client.get(
+            f"/v1/studies/{study_id}/matrixindex",
+            headers=user_access_token,
+            params={"path": "input/thermal/prepro/fr/01_solar/data"},
+        )
+        assert res.status_code == 200, res.json()
+        actual = res.json()
+        # We expect to have a "daily" time series with 365 days
+        expected = {
+            "first_week_size": 7,
+            "level": "daily",
+            "start_date": "2001-01-01 00:00:00",
+            "steps": 365,
+        }
+        assert actual == expected
+
+        # Check the time series
+        res = client.get(
+            f"/v1/studies/{study_id}/matrixindex",
+            headers=user_access_token,
+            params={"path": "input/thermal/series/fr/01_solar/series"},
+        )
+        assert res.status_code == 200, res.json()
+        actual = res.json()
+        # We expect to have an "hourly" time series with 8760 hours
+        expected = {
+            "first_week_size": 7,
+            "level": "hourly",
+            "start_date": "2001-01-01 00:00:00",
+            "steps": 8760,
+        }
+        assert actual == expected
+
+        # Check the default matrix index
+        # ==============================
+
+        res = client.get(f"/v1/studies/{study_id}/matrixindex", headers=user_access_token)
+        assert res.status_code == 200
+        actual = res.json()
+        expected = {
+            "first_week_size": 7,
+            "start_date": "2001-01-01 00:00:00",
+            "steps": 8760,
+            "level": "hourly",
+        }
+        assert actual == expected
+
+        # Check the matrix index of a daily time series stored in the output folder
+        # =========================================================================
+
+        res = client.get(
+            f"/v1/studies/{study_id}/matrixindex",
+            headers=user_access_token,
+            params={"path": "output/20201014-1427eco/economy/mc-all/areas/es/details-daily"},
+        )
+        assert res.status_code == 200
+        actual = res.json()
+        expected = {"first_week_size": 7, "start_date": "2001-01-01 00:00:00", "steps": 7, "level": "daily"}
+        assert actual == expected
