64542678777bf8d8bf464d16166bdba2c73f261a
Samir Kamal <1954121+skamril@users.noreply.github.com>
2024-01-09 16:27:35 +0100

    feat(ui-adq): add new table form

--- webapp/src/components/App/Singlestudy/explore/Configuration/AdequacyPatch/index.tsx
+++ webapp/src/components/App/Singlestudy/explore/Configuration/AdequacyPatch/index.tsx
@@ -1,4 +1,5 @@
 import { useOutletContext } from "react-router";
+import { useTranslation } from "react-i18next";
 import { StudyMetadata } from "../../../../../../common/types";
 import Form from "../../../../../common/Form";
 import { SubmitHandlerPlus } from "../../../../../common/Form/types";
@@ -8,9 +9,12 @@ import {
   getAdequacyPatchFormFields,
   setAdequacyPatchFormFields,
 } from "./utils";
+import TableMode from "../../../../../common/TableMode";
+import TabsView from "../../../../../common/TabsView";
 
 function AdequacyPatch() {
   const { study } = useOutletContext<{ study: StudyMetadata }>();
+  const { t } = useTranslation();
 
   ////////////////////////////////////////////////////////////////
   // Event Handlers
@@ -27,16 +31,36 @@ function AdequacyPatch() {
   ////////////////////////////////////////////////////////////////
 
   return (
-    <Form
-      key={study.id}
-      config={{
-        defaultValues: () => getAdequacyPatchFormFields(study.id),
-      }}
-      onSubmit={handleSubmit}
-      enableUndoRedo
-    >
-      <Fields />
-    </Form>
+    <TabsView
+      items={[
+        {
+          label: t("study.configuration.adequacyPatch.tab.general"),
+          content: (
+            <Form
+              key={study.id}
+              config={{
+                defaultValues: () => getAdequacyPatchFormFields(study.id),
+              }}
+              onSubmit={handleSubmit}
+              enableUndoRedo
+            >
+              <Fields />
+            </Form>
+          ),
+        },
+        {
+          label: t("study.configuration.adequacyPatch.tab.perimeter"),
+          content: (
+            <TableMode
+              studyId={study.id}
+              type="area"
+              columns={["adequacyPatchMode"]}
+            />
+          ),
+        },
+      ]}
+      TabListProps={{ sx: { mt: -2 } }}
+    />
   );
 }
 
