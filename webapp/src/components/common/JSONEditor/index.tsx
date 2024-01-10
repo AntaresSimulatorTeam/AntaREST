6fb410ad6350281b7ce219d881d3560c439cb73a
Samir Kamal <1954121+skamril@users.noreply.github.com>
2024-01-10 11:36:26 +0100

    feat(ui-common): add JSONEditor

---
+++ webapp/src/components/common/JSONEditor/index.tsx
@@ -0,0 +1,46 @@
+import JSONEditorLib, { JSONEditorOptions } from "jsoneditor";
+import { useEffect, useRef } from "react";
+import { useDeepCompareEffect, useMount } from "react-use";
+import "jsoneditor/dist/jsoneditor.min.css";
+import "./dark-theme.css";
+
+interface JSONEditorProps extends JSONEditorOptions {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  json: any;
+}
+
+function JSONEditor({ json, ...options }: JSONEditorProps) {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const editorRef = useRef<JSONEditorLib>();
+
+  useEffect(() => {
+    if (editorRef.current) {
+      editorRef.current.expandAll();
+    }
+  }, []);
+
+  useMount(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    const editor = new JSONEditorLib(ref.current, options);
+    editor.set(json);
+    editorRef.current = editor;
+
+    return () => editor.destroy();
+  });
+
+  useDeepCompareEffect(() => {
+    if (!editorRef.current) {
+      return;
+    }
+
+    editorRef.current.set(json);
+    editorRef.current.expandAll();
+  }, [json]);
+
+  return <div ref={ref} />;
+}
+
+export default JSONEditor;
