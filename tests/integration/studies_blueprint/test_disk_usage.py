dee9c8e0f2f91575e9c36445da6d539655af8982
belthlemar <martin.belthle@rte-france.com>
2024-01-30 18:14:31 +0100

    remove useless import

--- tests/integration/studies_blueprint/test_disk_usage.py
+++ tests/integration/studies_blueprint/test_disk_usage.py
@@ -1,14 +1,10 @@
 from starlette.testclient import TestClient
 
-from antarest.core.tasks.model import TaskStatus
-from tests.integration.utils import wait_task_completion
-
 
 class TestDiskUsage:
     def test_disk_usage_endpoint(
         self,
         client: TestClient,
-        admin_access_token: str,
         user_access_token: str,
         study_id: str,
     ) -> None:
@@ -18,7 +14,7 @@ class TestDiskUsage:
         - Ensure a successful response is received.
         - Confirm that the JSON response is an integer which represent a (big enough) directory size.
         """
-        admin_headers = {"Authorization": f"Bearer {admin_access_token}"}
+        user_headers = {"Authorization": f"Bearer {user_access_token}"}
         res = client.get(
             f"/v1/studies/{study_id}/disk-usage",
             headers={"Authorization": f"Bearer {user_access_token}"},
@@ -30,7 +26,7 @@ class TestDiskUsage:
         # Study copy
         copied = client.post(
             f"/v1/studies/{study_id}/copy?dest=copied&use_task=false",
-            headers=admin_headers,
+            headers=user_headers,
         )
         assert copied.status_code == 201
         parent_id = copied.json()
@@ -38,7 +34,7 @@ class TestDiskUsage:
         # Create variant
         res = client.post(
             f"/v1/studies/{parent_id}/variants",
-            headers=admin_headers,
+            headers=user_headers,
             params={"name": "Variant Test"},
         )
         assert res.status_code == 200
@@ -46,7 +42,7 @@ class TestDiskUsage:
 
         res = client.get(
             f"/v1/studies/{variant_id}/disk-usage",
-            headers=admin_headers,
+            headers=user_headers,
         )
         assert res.status_code == 200
         assert res.json() == 0
