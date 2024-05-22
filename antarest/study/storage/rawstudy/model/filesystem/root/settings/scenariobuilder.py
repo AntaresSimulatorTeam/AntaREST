658a6a3e342294b25b79b6f257683af1e9433e07
hatim dinia <diniahatim@gmail.com>
2024-05-22 09:48:25 +0200

    refactor(api): update scenario builder model

--- antarest/study/storage/rawstudy/model/filesystem/root/settings/scenariobuilder.py
+++ antarest/study/storage/rawstudy/model/filesystem/root/settings/scenariobuilder.py
@@ -1,17 +1,43 @@
 import typing as t
+from typing import Dict, Type, Optional, List
 
 from antarest.study.storage.rawstudy.model.filesystem.config.model import FileStudyTreeConfig
 from antarest.study.storage.rawstudy.model.filesystem.context import ContextServer
 from antarest.study.storage.rawstudy.model.filesystem.ini_file_node import IniFileNode
 
+scenario_symbols = {
+    "load": "l",
+    "thermal": "t",
+    "hydro": "h",
+    "wind": "w",
+    "solar": "s",
+    "ntc": "ntc",
+    "renewable": "r",
+    "bindingConstraints": "bc",
+}
+
+
+def parse_line(line: str):
+    line = line.strip()
+    if line.startswith('[') and line.endswith(']'):
+        # This is a section header
+        return line[1:-1], None  # Returns section name and None for value
+    if '=' in line:
+        key, value = line.split('=', 1)
+        key = key.strip()
+        try:
+            value = int(value.strip())
+            return key, value
+        except ValueError:
+            print(f"Invalid configuration value detected for key '{key}': '{value}' is not a valid integer.")
+    return None, None
+
 
 class ScenarioBuilder(IniFileNode):
     """
     Node representing the `settings/scenariobuilder.dat` file in an Antares study.
-
     This ".dat" file is a kind of ".ini"" file, where sections are rulesets.
     Each ruleset is a set of rules defined for each kind of generator or link.
-
     | Label                  | Symbol | Format                                     | Availability |
     |------------------------|:------:|--------------------------------------------|:------------:|
     | load                   |   l    | `l,<area>,<year> = <TS number>`            |              |
@@ -25,9 +51,7 @@ class ScenarioBuilder(IniFileNode):
     | hydro initial levels   |   hl   | `hl,<area>,<year> = <Level>`               |     8.0      |
     | hydro final levels     |  hfl   | `hfl,<area>,<year> = <Level>`              |     9.2      |
     | hydro generation power |  hgp   | `hgp,<area>,<year> = <TS number>`          |     9.1      |
-
     Legend:
-
     - `<area>`: The area ID (in lower case).
     - `<area1>`, `<area2>`: The area IDs of the two connected areas (source and target).
     - `<year>`: The year (0-based index) of the time series.
@@ -38,42 +62,52 @@ class ScenarioBuilder(IniFileNode):
     """
 
     def __init__(self, context: ContextServer, config: FileStudyTreeConfig):
-        self.config = config
-
-        study_version = int(config.version)
 
         rules: t.Dict[str, t.Union[t.Type[int], t.Type[float]]] = {}
 
-        # Add area-specific rules
-        for area in self.config.areas:
-            # load, hydro, wind, solar generators
-            symbols: t.Tuple[str, ...] = ("l", "h", "w", "s")
-            if study_version >= 920:
-                symbols += ("hgp",)  # Hydro generation power
-            rules.update({f"{symbol},{area},0": int for symbol in symbols})
-
-            # Hydro levels
-            if study_version >= 800:
-                rules[f"hl,{area},0"] = float
-            if study_version >= 920:
-                rules[f"hfl,{area},0"] = float
-
-            # thermal clusters
-            cluster_ids = (_id.lower() for _id in self.config.get_thermal_ids(area))
-            rules.update({f"t,{area},0,{cluster_id}": int for cluster_id in cluster_ids})
-
-            if study_version >= 810:
-                # renewable clusters
-                cluster_ids = (_id.lower() for _id in self.config.get_renewable_ids(area))
-                rules.update({f"r,{area},0,{cluster_id}": int for cluster_id in cluster_ids})
-
-            # NTC (links)
-            area2_ids = config.get_links(area)
-            rules.update({f"ntc,{area},{area2},0": int for area2 in area2_ids})
-
-            # Binding constraints
-            if study_version >= 870:
-                bc_groups = config.get_binding_constraint_groups()
-                rules.update({f"bc,{grp},0": int for grp in bc_groups})
-
         super().__init__(context=context, config=config, types={"Default Ruleset": rules})
+
+    def _get(self, url: Optional[List[str]] = None, depth: int = -1, expanded: bool = False, get_node: bool = False):
+
+        file_path = self.path
+        current_section = None
+        data = {}
+
+        matched_symbol = False  # Flag to track the first occurrence of the scenario type.
+
+        if url:
+            # The URL list includes essential filtering criteria: [active_ruleset, scenario_type].
+            # active_ruleset: Defines which configuration ruleset to apply.
+            # scenario_type: Specifies the type of scenario to filter configuration data by, if applicable.
+            active_ruleset = url[0]  # Retrieve the active ruleset identifier from the URL.
+            # Convert 'None' or empty string to None for scenario_type to ensure proper filtering.
+            scenario_type = None if not url[1] or url[1] == 'None' else url[1]
+        else:
+            # Defaults are used if no URL parameters are provided.
+            active_ruleset = 'Default Ruleset'  # Default ruleset if none specified.
+            scenario_type = None  # No scenario filtering if not specified.
+
+        symbol = None if not scenario_type else scenario_symbols[scenario_type]
+
+        with open(file_path, mode='r', encoding='utf-8') as file:
+            for line in file:
+                key, value = parse_line(line)
+                if key and value is None:
+                    # Detect new configuration sections (e.g., [Ruleset B]).
+                    current_section = key
+                    if current_section == active_ruleset:
+                        data[current_section] = {}
+                elif key and value is not None and current_section == active_ruleset:
+                    # Check if the key matches the active ruleset and scenario type.
+                    if symbol:
+                        if key.startswith(symbol):
+                            if not matched_symbol:
+                                matched_symbol = True  # Mark the first match of the scenario type.
+                            data[current_section][key] = value
+                        elif matched_symbol:
+                            # If a different scenario type is encountered after the first match, stop processing.
+                            break
+                    else:
+                        # If no specific scenario type is provided, all entries under the current section are included.
+                        data[current_section][key] = value
+        return data
