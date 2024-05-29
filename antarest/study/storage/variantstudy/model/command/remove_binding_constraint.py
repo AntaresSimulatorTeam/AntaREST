1c5eb47aaa5ef05b2fd01157920ea455a3fc8585
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2024-05-29 22:10:50 +0200

    fix(commands): correct `remove_binding_constraint` command to remove Scenario Builder data

--- antarest/study/storage/variantstudy/model/command/remove_binding_constraint.py
+++ antarest/study/storage/variantstudy/model/command/remove_binding_constraint.py
@@ -1,9 +1,11 @@
+import typing
 from typing import Any, Dict, List, Tuple
 
 from antarest.core.model import JSON
 from antarest.study.storage.rawstudy.model.filesystem.config.model import FileStudyTreeConfig
 from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
 from antarest.study.storage.variantstudy.model.command.common import CommandName, CommandOutput
+from antarest.study.storage.variantstudy.model.command.create_binding_constraint import DEFAULT_GROUP
 from antarest.study.storage.variantstudy.model.command.icommand import MATCH_SIGNATURE_SEPARATOR, ICommand
 from antarest.study.storage.variantstudy.model.model import CommandDTO
 
@@ -28,6 +30,24 @@ class RemoveBindingConstraint(ICommand):
         study_data.bindings.remove(next(iter([bind for bind in study_data.bindings if bind.id == self.id])))
         return CommandOutput(status=True), {}
 
+    # noinspection PyMethodMayBeStatic
+    def _remove_bc_from_scenario_builder(self, study_data: FileStudy, removed_groups: typing.Set[str]) -> None:
+        """
+        Update the scenario builder by removing the rows that correspond to the BC groups to remove.
+
+        NOTE: this update can be very long if the scenario builder configuration is large.
+        """
+        rulesets = study_data.tree.get(["settings", "scenariobuilder"])
+
+        for ruleset in rulesets.values():
+            for key in list(ruleset):
+                # The key is in the form "symbol,group,year"
+                symbol, *parts = key.split(",")
+                if symbol == "bc" and parts[0] in removed_groups:
+                    del ruleset[key]
+
+        study_data.tree.save(rulesets, ["settings", "scenariobuilder"])
+
     def _apply(self, study_data: FileStudy) -> CommandOutput:
         if self.id not in [bind.id for bind in study_data.config.bindings]:
             return CommandOutput(status=False, message=f"Binding constraint not found: '{self.id}'")
@@ -48,8 +68,14 @@ class RemoveBindingConstraint(ICommand):
         else:
             for term in ["lt", "gt", "eq"]:
                 study_data.tree.delete(["input", "bindingconstraints", f"{self.id}_{term}"])
-        output, _ = self._apply_config(study_data.config)
-        return output
+
+            # When all BC of a given group are removed, the group should be removed from the scenario builder
+            old_groups = {bd.get("group", DEFAULT_GROUP).lower() for bd in binding_constraints.values()}
+            new_groups = {bd.get("group", DEFAULT_GROUP).lower() for bd in new_binding_constraints.values()}
+            removed_groups = old_groups - new_groups
+            self._remove_bc_from_scenario_builder(study_data, removed_groups)
+
+        return self._apply_config(study_data.config)[0]
 
     def to_dto(self) -> CommandDTO:
         return CommandDTO(
