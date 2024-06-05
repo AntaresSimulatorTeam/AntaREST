1283d806f03a35f3542cb0f5733e081400312d23
Laurent LAPORTE <43534797+laurent-laporte-pro@users.noreply.github.com>
2024-06-05 14:53:40 +0200

    fix(variants): display variants in chronological order in the variants tree (#2049)
    This PR fixes
    [ANT-1797](https://gopro-tickets.rte-france.com/browse/ANT-1797)
    
    The database query was changed to query the children in chronological
    order.

---
+++ tests/storage/business/test_repository.py
@@ -0,0 +1,70 @@
+import datetime
+from unittest.mock import Mock
+
+from sqlalchemy.orm import Session  # type: ignore
+
+from antarest.core.interfaces.cache import ICache
+from antarest.study.model import RawStudy
+from antarest.study.storage.variantstudy.model.dbmodel import VariantStudy
+from antarest.study.storage.variantstudy.repository import VariantStudyRepository
+
+
+class TestVariantStudyRepository:
+    def test_get_children(self, db_session: Session) -> None:
+        """
+        Given a root study with children and a grandchild
+        When getting the children of the root study
+        Then the children are returned in chronological order
+        """
+        repository = VariantStudyRepository(cache_service=Mock(spec=ICache), session=db_session)
+
+        # Create a root study
+        raw_study = RawStudy(name="My Root")
+        db_session.add(raw_study)
+        db_session.commit()
+
+        # Ensure the root study has no children
+        children = repository.get_children(parent_id=raw_study.id)
+        assert children == []
+
+        # Prepare some dates for the children
+        day1 = datetime.datetime(2023, 1, 1)
+        day2 = datetime.datetime(2023, 1, 2)
+        day3 = datetime.datetime(2023, 1, 3)
+        day4 = datetime.datetime(2023, 1, 4)
+
+        # Create two variant studies of the same parent
+        variant1 = VariantStudy(name="My Variant 1", parent_id=raw_study.id, created_at=day1)
+        variant2 = VariantStudy(name="My Variant 2", parent_id=raw_study.id, created_at=day3)
+        db_session.add_all([variant1, variant2])
+        db_session.commit()
+
+        # Ensure the root study has 2 children
+        children = repository.get_children(parent_id=raw_study.id)
+        assert children == [variant1, variant2]
+        assert children[0].created_at < children[1].created_at
+
+        # Ensure variants have no children
+        children = repository.get_children(parent_id=variant1.id)
+        assert children == []
+        children = repository.get_children(parent_id=variant2.id)
+        assert children == []
+
+        # Add a variant study between the two existing ones (in chronological order)
+        variant3 = VariantStudy(name="My Variant 3", parent_id=raw_study.id, created_at=day2)
+        db_session.add(variant3)
+        db_session.commit()
+
+        # Ensure the root study has 3 children in chronological order
+        children = repository.get_children(parent_id=raw_study.id)
+        assert children == [variant1, variant3, variant2]
+        assert children[0].created_at < children[1].created_at < children[2].created_at
+
+        # Add a variant of a variant
+        variant3a = VariantStudy(name="My Variant 3a", parent_id=variant3.id, created_at=day4)
+        db_session.add(variant3a)
+        db_session.commit()
+
+        # Ensure the root study has the 3 same children
+        children = repository.get_children(parent_id=raw_study.id)
+        assert children == [variant1, variant3, variant2]
