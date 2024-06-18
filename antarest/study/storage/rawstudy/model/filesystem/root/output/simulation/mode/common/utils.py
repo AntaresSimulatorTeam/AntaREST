72600ce65133181de6fd607bcdcad4d44a2cc843
belthlemar <martin.belthle@rte-france.com>
2024-06-18 10:51:30 +0200

    refactor(filesystem): resolve one comment

--- antarest/study/storage/rawstudy/model/filesystem/root/output/simulation/mode/common/utils.py
+++ antarest/study/storage/rawstudy/model/filesystem/root/output/simulation/mode/common/utils.py
@@ -1,5 +1,3 @@
-import typing as t
-
 from antarest.study.storage.rawstudy.model.filesystem.folder_node import FolderNode
 from antarest.study.storage.rawstudy.model.filesystem.inode import TREE
 from antarest.study.storage.rawstudy.model.filesystem.root.output.simulation.mode.common.areas import (
@@ -15,15 +13,7 @@ from antarest.study.storage.rawstudy.model.filesystem.root.output.simulation.mod
     OutputSimulationModeMcAllGrid,
 )
 
-
-class OutputMappingType(t.TypedDict):
-    areas: t.Type[OutputSimulationAreas]
-    grid: t.Type[OutputSimulationModeMcAllGrid]
-    links: t.Type[OutputSimulationLinks]
-    binding_constraints: t.Type[OutputSimulationBindingConstraintItem]
-
-
-OUTPUT_MAPPING: OutputMappingType = {
+OUTPUT_MAPPING = {
     "areas": OutputSimulationAreas,
     "grid": OutputSimulationModeMcAllGrid,
     "links": OutputSimulationLinks,
@@ -38,5 +28,5 @@ class OutputSimulationModeCommon(FolderNode):
         children: TREE = {}
         for key, simulation_class in OUTPUT_MAPPING.items():
             if (self.config.path / key).exists():
-                children[key] = simulation_class(self.context, self.config.next_file(key))  # type: ignore
+                children[key] = simulation_class(self.context, self.config.next_file(key))
         return children
