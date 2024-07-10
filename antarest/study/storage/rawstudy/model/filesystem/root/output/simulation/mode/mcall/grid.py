1dda02a0ab9f95c900d5212b0a9ab3974021005e
belthlemar <martin.belthle@rte-france.com>
2024-07-10 14:44:30 +0200

    continue work

--- antarest/study/storage/rawstudy/model/filesystem/root/output/simulation/mode/mcall/grid.py
+++ antarest/study/storage/rawstudy/model/filesystem/root/output/simulation/mode/mcall/grid.py
@@ -15,13 +15,22 @@ class OutputSimulationModeMcAllGrid(FolderNode):
         files = [d.stem for d in self.config.path.iterdir()]
         children: TREE = {}
         for file in files:
-            children[file] = OutputSynthesis(self.context, self.config.next_file(f"{file}.txt"))
+            synthesis_class = DigestSynthesis if file == "digest" else OutputSynthesis
+            children[file] = synthesis_class(self.context, self.config.next_file(f"{file}.txt"))
         return children
 
 
 class OutputSynthesis(LazyNode[JSON, bytes, bytes]):
     def __init__(self, context: ContextServer, config: FileStudyTreeConfig):
         super().__init__(context, config)
+
+    def get_lazy_content(
+        self,
+        url: t.Optional[t.List[str]] = None,
+        depth: int = -1,
+        expanded: bool = False,
+    ) -> str:
+        return f"matrix://{self.config.path.name}"
 
     def load(
         self,
@@ -32,7 +41,9 @@ class OutputSynthesis(LazyNode[JSON, bytes, bytes]):
     ) -> JSON:
         file_path = self.config.path
         df = pd.read_csv(file_path, sep="\t")
-        return t.cast(JSON, df.to_dict(orient="split"))
+        output = df.to_dict(orient="split")
+        del output["index"]
+        return t.cast(JSON, output)
 
     def dump(self, data: bytes, url: t.Optional[t.List[str]] = None) -> None:
         self.config.path.parent.mkdir(exist_ok=True, parents=True)
@@ -51,3 +62,28 @@ class OutputSynthesis(LazyNode[JSON, bytes, bytes]):
 
     def denormalize(self) -> None:
         pass  # no external store in this node
+
+
+class DigestSynthesis(OutputSynthesis):
+    def __init__(self, context: ContextServer, config: FileStudyTreeConfig):
+        super().__init__(context, config)
+
+    def load(
+        self,
+        url: t.Optional[t.List[str]] = None,
+        depth: int = -1,
+        expanded: bool = False,
+        formatted: bool = True,
+    ) -> JSON:
+        file_path = self.config.path
+        df = pd.read_csv(
+            file_path,
+            sep="\t",
+            skiprows=4,
+            header=[0, 1, 2],
+            na_values="N/A",
+            float_precision="legacy",
+        )
+        output = df.to_dict(orient="split")
+        del output["index"]
+        return t.cast(JSON, output)
