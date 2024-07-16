57964013e6c399b1050163a4689d02f97d01135e
Mohamed Abdel Wedoud <mohammed.youssouf@gmail.com>
2024-07-16 21:09:27 +0200

    test(aggregation-api): add tests for the new `economy/mc-all` endpoints

--- tests/integration/raw_studies_blueprint/test_aggregate_raw_data.py
+++ tests/integration/raw_studies_blueprint/test_aggregate_raw_data.py
@@ -10,7 +10,8 @@ from antarest.study.storage.df_download import TableExportFormat
 from antarest.study.storage.rawstudy.model.filesystem.matrix.matrix import MatrixFrequency
 from tests.integration.raw_studies_blueprint.assets import ASSETS_DIR
 
-AREAS_REQUESTS = [
+# define the requests parameters for the `economy/mc-ind` outputs aggregation
+AREAS_REQUESTS__IND = [
     (
         {
             "output_id": "20201014-1425eco-goodbye",
@@ -68,7 +69,7 @@ AREAS_REQUESTS = [
     ),
 ]
 
-LINKS_REQUESTS = [
+LINKS_REQUESTS__IND = [
     (
         {
             "output_id": "20201014-1425eco-goodbye",
@@ -111,7 +112,7 @@ LINKS_REQUESTS = [
     ),
 ]
 
-SAME_REQUEST_DIFFERENT_FORMATS = [
+SAME_REQUEST_DIFFERENT_FORMATS__IND = [
     (
         {
             "output_id": "20201014-1425eco-goodbye",
@@ -142,7 +143,7 @@ SAME_REQUEST_DIFFERENT_FORMATS = [
 ]
 
 
-INCOHERENT_REQUESTS_BODIES = [
+INCOHERENT_REQUESTS_BODIES__IND = [
     {
         "output_id": "20201014-1425eco-goodbye",
         "query_file": AreasQueryFile.VALUES,
@@ -163,7 +164,7 @@ INCOHERENT_REQUESTS_BODIES = [
     },
 ]
 
-WRONGLY_TYPED_REQUESTS = [
+WRONGLY_TYPED_REQUESTS__IND = [
     {
         "output_id": "20201014-1425eco-goodbye",
         "query_file": "fake_query_file",
@@ -178,6 +179,169 @@ WRONGLY_TYPED_REQUESTS = [
         "output_id": "20201014-1425eco-goodbye",
         "query_file": AreasQueryFile.VALUES,
         "frequency": MatrixFrequency.HOURLY,
+        "format": "fake_format",
+    },
+]
+
+# define the requests parameters for the `economy/mc-all` outputs aggregation
+AREAS_REQUESTS__ALL = [
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": AreasQueryFile.VALUES,
+            "frequency": MatrixFrequency.DAILY,
+            "areas_ids": "",
+            "columns_names": "",
+        },
+        "test-01-all.result.tsv",
+    ),
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": AreasQueryFile.DETAILS,
+            "frequency": MatrixFrequency.MONTHLY,
+            "areas_ids": "de,fr,it",
+            "columns_names": "",
+        },
+        "test-02-all.result.tsv",
+    ),
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": AreasQueryFile.VALUES,
+            "frequency": MatrixFrequency.DAILY,
+            "areas_ids": "",
+            "columns_names": "",
+        },
+        "test-03-all.result.tsv",
+    ),
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": AreasQueryFile.VALUES,
+            "frequency": MatrixFrequency.DAILY,
+            "areas_ids": "es,fr,de",
+            "columns_names": "",
+        },
+        "test-04-all.result.tsv",
+    ),
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": AreasQueryFile.VALUES,
+            "frequency": MatrixFrequency.MONTHLY,
+            "areas_ids": "",
+            "columns_names": "",
+        },
+        "test-05-all.result.tsv",
+    ),
+]
+
+LINKS_REQUESTS__ALL = [
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": LinksQueryFile.VALUES,
+            "frequency": MatrixFrequency.DAILY,
+            "columns_names": "",
+        },
+        "test-01-all.result.tsv",
+    ),
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": LinksQueryFile.VALUES,
+            "frequency": MatrixFrequency.MONTHLY,
+            "columns_names": "",
+        },
+        "test-02-all.result.tsv",
+    ),
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": LinksQueryFile.VALUES,
+            "frequency": MatrixFrequency.DAILY,
+            "columns_names": "",
+        },
+        "test-03-all.result.tsv",
+    ),
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": LinksQueryFile.VALUES,
+            "frequency": MatrixFrequency.MONTHLY,
+            "links_ids": "de - fr",
+        },
+        "test-04-all.result.tsv",
+    ),
+]
+
+SAME_REQUEST_DIFFERENT_FORMATS__ALL = [
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": LinksQueryFile.VALUES,
+            "frequency": MatrixFrequency.DAILY,
+            "format": "csv",
+        },
+        "test-01-all.result.tsv",
+    ),
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": LinksQueryFile.VALUES,
+            "frequency": MatrixFrequency.MONTHLY,
+            "format": "tsv",
+        },
+        "test-01-all.result.tsv",
+    ),
+    (
+        {
+            "output_id": "20201014-1427eco",
+            "query_file": LinksQueryFile.VALUES,
+            "frequency": MatrixFrequency.MONTHLY,
+            "format": "xlsx",
+        },
+        "test-01-all.result.tsv",
+    ),
+]
+
+
+INCOHERENT_REQUESTS_BODIES__ALL = [
+    {
+        "output_id": "20201014-1427eco",
+        "query_file": AreasQueryFile.VALUES,
+        "frequency": MatrixFrequency.DAILY,
+    },
+    {
+        "output_id": "20201014-1427eco",
+        "query_file": LinksQueryFile.VALUES,
+        "frequency": MatrixFrequency.DAILY,
+        "columns_names": "fake_col",
+    },
+    {
+        "output_id": "20201014-1427eco",
+        "query_file": AreasQueryFile.VALUES,
+        "frequency": MatrixFrequency.MONTHLY,
+        "links_ids": "fake_id",
+    },
+]
+
+WRONGLY_TYPED_REQUESTS__ALL = [
+    {
+        "output_id": "20201014-1427eco",
+        "query_file": "fake_query_file",
+        "frequency": MatrixFrequency.MONTHLY,
+    },
+    {
+        "output_id": "20201014-1427eco",
+        "query_file": AreasQueryFile.VALUES,
+        "frequency": "fake_frequency",
+    },
+    {
+        "output_id": "20201014-1427eco",
+        "query_file": AreasQueryFile.VALUES,
+        "frequency": MatrixFrequency.DAILY,
         "format": "fake_format",
     },
 ]
@@ -200,7 +364,7 @@ class TestRawDataAggregationMCInd:
         """
         client.headers = {"Authorization": f"Bearer {user_access_token}"}
 
-        for params, expected_result_filename in AREAS_REQUESTS:
+        for params, expected_result_filename in AREAS_REQUESTS__IND:
             output_id = params.pop("output_id")
             res = client.get(f"/v1/studies/{internal_study_id}/areas/aggregate/mc-ind/{output_id}", params=params)
             assert res.status_code == 200, res.json()
@@ -229,7 +393,7 @@ class TestRawDataAggregationMCInd:
         """
         client.headers = {"Authorization": f"Bearer {user_access_token}"}
 
-        for params, expected_result_filename in LINKS_REQUESTS:
+        for params, expected_result_filename in LINKS_REQUESTS__IND:
             output_id = params.pop("output_id")
             res = client.get(f"/v1/studies/{internal_study_id}/links/aggregate/mc-ind/{output_id}", params=params)
             assert res.status_code == 200, res.json()
@@ -258,7 +422,7 @@ class TestRawDataAggregationMCInd:
         """
         client.headers = {"Authorization": f"Bearer {user_access_token}"}
 
-        for params, expected_result_filename in SAME_REQUEST_DIFFERENT_FORMATS:
+        for params, expected_result_filename in SAME_REQUEST_DIFFERENT_FORMATS__IND:
             output_id = params.pop("output_id")
             res = client.get(f"/v1/studies/{internal_study_id}/links/aggregate/mc-ind/{output_id}", params=params)
             assert res.status_code == 200, res.json()
@@ -289,7 +453,7 @@ class TestRawDataAggregationMCInd:
         Asserts that requests with incoherent bodies don't crash but send empty dataframes
         """
         client.headers = {"Authorization": f"Bearer {user_access_token}"}
-        for params in INCOHERENT_REQUESTS_BODIES:
+        for params in INCOHERENT_REQUESTS_BODIES__IND:
             output_id = params.pop("output_id")
             res = client.get(f"/v1/studies/{internal_study_id}/links/aggregate/mc-ind/{output_id}", params=params)
             assert res.status_code == 200, res.json()
@@ -302,7 +466,7 @@ class TestRawDataAggregationMCInd:
         Asserts that wrongly typed requests send an HTTP 422 Exception
         """
         client.headers = {"Authorization": f"Bearer {user_access_token}"}
-        for params in WRONGLY_TYPED_REQUESTS:
+        for params in WRONGLY_TYPED_REQUESTS__IND:
             output_id = params.pop("output_id")
             res = client.get(f"/v1/studies/{internal_study_id}/links/aggregate/mc-ind/{output_id}", params=params)
             assert res.status_code == 422
@@ -329,6 +493,164 @@ class TestRawDataAggregationMCInd:
         # test for links
         res = client.get(
             f"/v1/studies/{internal_study_id}/links/aggregate/mc-ind/unknown_id",
+            params={
+                "query_file": LinksQueryFile.VALUES,
+                "frequency": MatrixFrequency.HOURLY,
+            },
+        )
+        assert res.status_code == 404, res.json()
+        assert res.json()["exception"] == "OutputNotFound"
+        assert "unknown_id" in res.json()["description"], "The output_id should be in the message"
+
+
+@pytest.mark.integration_test
+class TestRawDataAggregationMCAll:
+    """
+    Check the aggregation of Raw Data from studies outputs in `economy/mc-all`
+    """
+
+    def test_area_aggregation(
+        self,
+        client: TestClient,
+        user_access_token: str,
+        internal_study_id: str,
+    ):
+        """
+        Test the aggregation of areas data
+        """
+        client.headers = {"Authorization": f"Bearer {user_access_token}"}
+
+        for params, expected_result_filename in AREAS_REQUESTS__ALL:
+            output_id = params.pop("output_id")
+            res = client.get(f"/v1/studies/{internal_study_id}/areas/aggregate/mc-all/{output_id}", params=params)
+            assert res.status_code == 200, res.json()
+            content = io.BytesIO(res.content)
+            df = pd.read_csv(content, index_col=0, sep=",")
+            resource_file = ASSETS_DIR.joinpath(f"aggregate_areas_raw_data/{expected_result_filename}")
+            resource_file.parent.mkdir(exist_ok=True, parents=True)
+            if not resource_file.exists():
+                # create the resource to add it to non-regression tests
+                df.to_csv(resource_file, sep="\t", index=False)
+            expected_df = pd.read_csv(resource_file, sep="\t", header=0)
+            expected_df = expected_df.replace({np.nan: None})
+            # cast types of expected_df to match df
+            for col in expected_df.columns:
+                expected_df[col] = expected_df[col].astype(df[col].dtype)
+            pd.testing.assert_frame_equal(df, expected_df)
+
+    @pytest.mark.skip(reason="Links aggregation data for `economies/mc-all` is not available yet.")
+    def test_links_aggregation(
+        self,
+        client: TestClient,
+        user_access_token: str,
+        internal_study_id: str,
+    ):
+        """
+        Test the aggregation of links data
+        """
+        client.headers = {"Authorization": f"Bearer {user_access_token}"}
+
+        for params, expected_result_filename in LINKS_REQUESTS__ALL:
+            output_id = params.pop("output_id")
+            res = client.get(f"/v1/studies/{internal_study_id}/links/aggregate/mc-all/{output_id}", params=params)
+            assert res.status_code == 200, res.json()
+            content = io.BytesIO(res.content)
+            df = pd.read_csv(content, index_col=0, sep=",")
+            resource_file = ASSETS_DIR.joinpath(f"aggregate_links_raw_data/{expected_result_filename}")
+            resource_file.parent.mkdir(exist_ok=True, parents=True)
+            if not resource_file.exists():
+                # create the resource to add it to non-regression tests
+                df.to_csv(resource_file, sep="\t", index=False)
+            expected_df = pd.read_csv(resource_file, sep="\t", header=0)
+            expected_df = expected_df.replace({np.nan: None})
+            # cast types of expected_df to match df
+            for col in expected_df.columns:
+                expected_df[col] = expected_df[col].astype(df[col].dtype)
+            pd.testing.assert_frame_equal(df, expected_df)
+
+    @pytest.mark.skip(reason="Links aggregation data for `economies/mc-all` is not available yet.")
+    def test_different_formats(
+        self,
+        client: TestClient,
+        user_access_token: str,
+        internal_study_id: str,
+    ):
+        """
+        Tests that all formats work and produce the same result
+        """
+        client.headers = {"Authorization": f"Bearer {user_access_token}"}
+
+        for params, expected_result_filename in SAME_REQUEST_DIFFERENT_FORMATS__ALL:
+            output_id = params.pop("output_id")
+            res = client.get(f"/v1/studies/{internal_study_id}/links/aggregate/mc-all/{output_id}", params=params)
+            assert res.status_code == 200, res.json()
+            content = io.BytesIO(res.content)
+            export_format = params["format"]
+            if export_format == TableExportFormat.CSV.value:
+                df = pd.read_csv(content, index_col=0, sep=",")
+            elif export_format == TableExportFormat.TSV.value:
+                df = pd.read_csv(content, index_col=0, sep="\t")
+            else:
+                df = pd.read_excel(content, index_col=0)  # type: ignore
+            resource_file = ASSETS_DIR.joinpath(f"aggregate_links_raw_data/{expected_result_filename}")
+            resource_file.parent.mkdir(exist_ok=True, parents=True)
+            if not resource_file.exists():
+                # create the resource to add it to non-regression tests
+                df.to_csv(resource_file, sep="\t", index=False)
+            expected_df = pd.read_csv(resource_file, sep="\t", header=0)
+            expected_df = expected_df.replace({np.nan: None})
+            # cast types of expected_df to match df
+            for col in expected_df.columns:
+                expected_df[col] = expected_df[col].astype(df[col].dtype)
+            pd.testing.assert_frame_equal(df, expected_df)
+
+    def test_aggregation_with_incoherent_bodies(
+        self, client: TestClient, user_access_token: str, internal_study_id: str
+    ):
+        """
+        Asserts that requests with incoherent bodies don't crash but send empty dataframes
+        """
+        client.headers = {"Authorization": f"Bearer {user_access_token}"}
+        for params in INCOHERENT_REQUESTS_BODIES__ALL:
+            output_id = params.pop("output_id")
+            res = client.get(f"/v1/studies/{internal_study_id}/links/aggregate/mc-all/{output_id}", params=params)
+            assert res.status_code == 200, res.json()
+            content = io.BytesIO(res.content)
+            df = pd.read_csv(content, index_col=0, sep=",")
+            assert df.empty
+
+    def test_wrongly_typed_request(self, client: TestClient, user_access_token: str, internal_study_id: str):
+        """
+        Asserts that wrongly typed requests send an HTTP 422 Exception
+        """
+        client.headers = {"Authorization": f"Bearer {user_access_token}"}
+        for params in WRONGLY_TYPED_REQUESTS__ALL:
+            output_id = params.pop("output_id")
+            res = client.get(f"/v1/studies/{internal_study_id}/links/aggregate/mc-all/{output_id}", params=params)
+            assert res.status_code == 422
+            assert res.json()["exception"] == "RequestValidationError"
+
+    def test_aggregation_with_wrong_output(self, client: TestClient, user_access_token: str, internal_study_id: str):
+        """
+        Asserts that requests with wrong output send an HTTP 422 Exception
+        """
+        client.headers = {"Authorization": f"Bearer {user_access_token}"}
+
+        # test for areas
+        res = client.get(
+            f"/v1/studies/{internal_study_id}/areas/aggregate/mc-all/unknown_id",
+            params={
+                "query_file": AreasQueryFile.VALUES,
+                "frequency": MatrixFrequency.HOURLY,
+            },
+        )
+        assert res.status_code == 404, res.json()
+        assert res.json()["exception"] == "OutputNotFound"
+        assert "unknown_id" in res.json()["description"], "The output_id should be in the message"
+
+        # test for links
+        res = client.get(
+            f"/v1/studies/{internal_study_id}/links/aggregate/mc-all/unknown_id",
             params={
                 "query_file": LinksQueryFile.VALUES,
                 "frequency": MatrixFrequency.HOURLY,
