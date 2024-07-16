624d07df93da43b21df5f35884acfeec7103a90d
Samir Kamal <1954121+skamril@users.noreply.github.com>
2024-07-16 10:18:58 +0200

    feat(ui-debug): update the view
    * install and use @mui/x-tree-view instead of the beta version of TreeView
    * update style
    * use SplitView
    * fix data type
    * fix issue with `list.txt` file displayed like a folder

--- webapp/src/components/App/Singlestudy/explore/Debug/DebugContext.ts
+++ webapp/src/components/App/Singlestudy/explore/Debug/DebugContext.ts
@@ -1,15 +1,12 @@
-import { createContext, useContext } from "react";
-import type { File } from "./utils";
+import { createContext } from "react";
+import type { FileInfo } from "./utils";
 import { voidFn } from "../../../../../utils/fnUtils";
 
 const initialDebugContextValue = {
-  onFileSelect: voidFn<[File]>,
+  onFileSelect: voidFn<[FileInfo]>,
   reloadTreeData: voidFn,
 };
 
 const DebugContext = createContext(initialDebugContextValue);
-
-export const useDebugContext = (): typeof initialDebugContextValue =>
-  useContext(DebugContext);
 
 export default DebugContext;
--- webapp/src/components/App/Singlestudy/explore/Debug/utils.ts
+++ webapp/src/components/App/Singlestudy/explore/Debug/utils.ts
@@ -3,27 +3,33 @@ import TextSnippetIcon from "@mui/icons-material/TextSnippet";
 import FolderIcon from "@mui/icons-material/Folder";
 import DatasetIcon from "@mui/icons-material/Dataset";
 import { SvgIconComponent } from "@mui/icons-material";
+import * as RA from "ramda-adjunct";
 
 ////////////////////////////////////////////////////////////////
 // Types
 ////////////////////////////////////////////////////////////////
 
-export type FileType = "json" | "file" | "matrix";
+export type FileType = "json" | "matrix" | "file" | "folder";
 
-export interface File {
+export interface FileInfo {
   fileType: FileType;
   filePath: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type TreeData = Record<string, any> | string;
+export type TreeFile = string | string[];
+
+export interface TreeFolder {
+  [key: string]: TreeFile | TreeFolder;
+}
+
+export type TreeData = TreeFolder | TreeFile;
 
 ////////////////////////////////////////////////////////////////
-// Utils
+// File Info
 ////////////////////////////////////////////////////////////////
 
-//Maps file types and folder to their corresponding icon components.
-const iconByFileType: Record<FileType | "folder", SvgIconComponent> = {
+// Maps file types to their corresponding icon components.
+const iconByFileType: Record<FileType, SvgIconComponent> = {
   matrix: DatasetIcon,
   json: DataObjectIcon,
   folder: FolderIcon,
@@ -31,22 +37,26 @@ const iconByFileType: Record<FileType | "folder", SvgIconComponent> = {
 } as const;
 
 /**
- * Gets the icon component for a given file type or folder.
+ * Gets the icon component for a given file type.
  *
- * @param type - The type of the file or "folder".
+ * @param type - The type of the file.
  * @returns The corresponding icon component.
  */
-export const getFileIcon = (type: FileType | "folder"): SvgIconComponent => {
-  return iconByFileType[type] || TextSnippetIcon;
-};
+export function getFileIcon(type: FileType): SvgIconComponent {
+  return iconByFileType[type];
+}
+
+export function isFolder(treeData: TreeData): treeData is TreeFolder {
+  return RA.isPlainObj(treeData);
+}
 
 /**
- * Determines the file type based on the tree data.
+ * Gets the file type based on the tree data.
  *
  * @param treeData - The data of the tree item.
- * @returns The determined file type or "folder".
+ * @returns The corresponding file type.
  */
-export const determineFileType = (treeData: TreeData): FileType | "folder" => {
+export function getFileType(treeData: TreeData): FileType {
   if (typeof treeData === "string") {
     if (
       treeData.startsWith("matrix://") ||
@@ -58,19 +68,5 @@ export const determineFileType = (treeData: TreeData): FileType | "folder" => {
       return "json";
     }
   }
-  return typeof treeData === "object" ? "folder" : "file";
-};
-
-/**
- * Filters out specific keys from the tree data.
- *
- * @param data - The original tree data.
- * @returns The filtered tree data.
- */
-export const filterTreeData = (data: TreeData): TreeData => {
-  const excludedKeys = new Set(["Desktop", "study", "logs"]);
-
-  return Object.fromEntries(
-    Object.entries(data).filter(([key]) => !excludedKeys.has(key)),
-  );
-};
+  return isFolder(treeData) ? "folder" : "file";
+}
