0a7ccf3bdcbac4cd67e306f50ab4a0848e6ef1b2
belthlemar <martin.belthle@rte-france.com>
2024-07-26 18:16:47 +0200

    fix lint

--- antarest/study/storage/rawstudy/model/filesystem/lazy_node.py
+++ antarest/study/storage/rawstudy/model/filesystem/lazy_node.py
@@ -133,7 +133,7 @@ class LazyNode(INode, ABC, t.Generic[G, S, V]):  # type: ignore
         self._assert_not_in_zipped_file()
         self._assert_url_end(url)
 
-        if isinstance(data, str) and self.context.resolver.resolve(data, format='json'):
+        if isinstance(data, str) and self.context.resolver.resolve(data, format="json"):
             self.get_link_path().write_text(data)
             if self.config.path.exists():
                 self.config.path.unlink()
