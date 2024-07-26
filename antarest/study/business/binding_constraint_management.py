beb23fc89b954d6e7736e8b1c4b1beec8645456e
belthlemar <martin.belthle@rte-france.com>
2024-07-26 18:13:15 +0200

    resolve issue with dev

--- antarest/study/business/binding_constraint_management.py
+++ antarest/study/business/binding_constraint_management.py
@@ -337,10 +337,17 @@ def _get_references_by_widths(
         The height of the matrices may vary depending on the time step,
         but the width should be consistent within a group of binding constraints.
     """
+    operator_matrix_file_map = {
+        BindingConstraintOperator.EQUAL: ["{bc_id}_eq"],
+        BindingConstraintOperator.GREATER: ["{bc_id}_gt"],
+        BindingConstraintOperator.LESS: ["{bc_id}_lt"],
+        BindingConstraintOperator.BOTH: ["{bc_id}_lt", "{bc_id}_gt"],
+    }
+
     references_by_width: t.Dict[int, t.List[t.Tuple[str, str]]] = {}
     _total = len(bcs)
     for _index, bc in enumerate(bcs):
-        matrices_name = OPERATOR_CONFLICT_MAP[bc.operator] if file_study.config.version >= 870 else ["{bc_id}"]
+        matrices_name = operator_matrix_file_map[bc.operator] if file_study.config.version >= 870 else ["{bc_id}"]
         for matrix_name in matrices_name:
             matrix_id = matrix_name.format(bc_id=bc.id)
             logger.info(f"⏲ Validating BC '{bc.id}': {matrix_id=} [{_index+1}/{_total}]")
