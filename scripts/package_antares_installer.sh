4cb782c0f6a642229880313fec02b500131c0ccd
Maurane GLAUDE <mauranne.glaude_externe@rte-france.com>
2024-08-23 10:24:24 +0200

    ci(workflow): fix deploy workflow

--- scripts/package_antares_installer.sh
+++ scripts/package_antares_installer.sh
@@ -7,16 +7,34 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 PROJECT_DIR=$(dirname -- "${SCRIPT_DIR}")
-INSTALLER_DIR="${PROJECT_DIR}/installer/src/antares_web_installer/"
+INSTALLER_DIR="${PROJECT_DIR}/installer"
+INSTALLER_SRC_DIR="${INSTALLER_DIR}/src/antares_web_installer"
 DIST_DIR="${PROJECT_DIR}/dist/package"
 
+echo "INFO: Initializing the virtual environment .installer"
+if [[ "$OSTYPE" == "msys"* ]]; then
+# initialize environment on windows
+  python -m venv .installer
+  .installer/bin/Activate.ps1
+  python -m pip install -r ${INSTALLER_DIR}/requirements.txt
+else
+# initialize environment on linux
+  python -m venv .installer
+  source .installer/bin/activate
+  python -m pip install -r ${INSTALLER_DIR}/requirements.txt
+fi
+
+# build package
 echo "INFO: Generating the Installer for the Desktop application..."
 if [[ "$OSTYPE" == "msys"* ]]; then
     pushd ${PROJECT_DIR}
-    pyinstaller --onefile "${INSTALLER_DIR}gui/__main__.py" --distpath "${DIST_DIR}" --hidden-import antares_web_installer.shortcuts._linux_shell --hidden-import antares_web_installer.shortcuts._win32_shell --noconsole --name AntaresWebInstaller
+    pyinstaller --onefile "${INSTALLER_SRC_DIR}/gui/__main__.py" --distpath "${DIST_DIR}" --hidden-import antares_web_installer.shortcuts._linux_shell --hidden-import antares_web_installer.shortcuts._win32_shell --noconsole --name AntaresWebInstaller
     popd
 else
     pushd ${PROJECT_DIR}
-    pyinstaller --onefile "${INSTALLER_DIR}cli/__main__.py" --distpath "${DIST_DIR}" --hidden-import antares_web_installer.shortcuts._linux_shell --hidden-import antares_web_installer.shortcuts._win32_shell --noconsole --name AntaresWebInstallerCLI
+    pyinstaller --onefile "${INSTALLER_SRC_DIR}/cli/__main__.py" --distpath "${DIST_DIR}" --hidden-import antares_web_installer.shortcuts._linux_shell --hidden-import antares_web_installer.shortcuts._win32_shell --noconsole --name AntaresWebInstallerCLI
     popd
 fi
+
+deactivate
+rm -r .installer
