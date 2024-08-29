88e3d3282f184b0323312e0fb94bbe74d243e46c
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2024-08-29 10:14:40 +0200

    Try to use hatch as in installer repo
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- scripts/package_antares_installer.sh
+++ scripts/package_antares_installer.sh
@@ -3,36 +3,24 @@
 # Antares Desktop Installer
 #
 # This script must be run by  ̀.github/worflows/deploy.yml`.
-# It builds the installer application and store it in the package directory.
+# It builds the installer application and stores it in the package directory.
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 PROJECT_DIR=$(dirname -- "${SCRIPT_DIR}")
 INSTALLER_DIR="${PROJECT_DIR}/installer"
-INSTALLER_SRC_DIR="${INSTALLER_DIR}/src/antares_web_installer"
 DIST_DIR="${PROJECT_DIR}/dist/package"
-
-echo "INFO: Initializing the virtual environment .installer"
-python -m venv .installer
-if [[ "$OSTYPE" == "msys"* ]]; then
-# initialize environment on windows
-  source .installer/Scripts/activate
-else
-# initialize environment on linux
-  source .installer/bin/activate
-fi
-python -m pip install -r ${INSTALLER_DIR}/requirements.txt
 
 # build package
 echo "INFO: Generating the Installer..."
-if [[ "$OSTYPE" == "msys"* ]]; then
-    pushd ${PROJECT_DIR}
-    pyinstaller --onefile "${INSTALLER_SRC_DIR}/gui/__main__.py" --distpath "${DIST_DIR}" --hidden-import antares_web_installer.shortcuts._linux_shell --hidden-import antares_web_installer.shortcuts._win32_shell --noconsole --name AntaresWebInstaller
-    popd
-else
-    pushd ${PROJECT_DIR}
-    pyinstaller --onefile "${INSTALLER_SRC_DIR}/cli/__main__.py" --distpath "${DIST_DIR}" --hidden-import antares_web_installer.shortcuts._linux_shell --hidden-import antares_web_installer.shortcuts._win32_shell --noconsole --name AntaresWebInstallerCLI
-    popd
-fi
 
-deactivate
-rm -rf .installer
+pushd ${INSTALLER_DIR}
+if [[ "$OSTYPE" == "msys"* ]]; then
+    # For windows we build the GUI version
+    hatch run pyinstaller:build_cli AntaresWebInstaller
+    mv dist/AntaresWebInstaller ${DIST_DIR}
+else
+    # For linux we build the command line version
+    hatch run pyinstaller:build_cli AntaresWebInstallerCLI
+    mv dist/AntaresWebInstallerCLI ${DIST_DIR}
+fi
+popd
