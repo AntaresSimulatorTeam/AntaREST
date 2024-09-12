81694f6c4225da86ee2ae754ceb7e6089e3ed867
hatim dinia <diniahatim@gmail.com>
2024-09-12 08:50:33 +0200

    refactor(ui): update `Wind` matrix

--- webapp/src/components/App/Singlestudy/explore/Modelization/Areas/Wind.tsx
+++ webapp/src/components/App/Singlestudy/explore/Modelization/Areas/Wind.tsx
@@ -15,12 +15,9 @@
 import { useOutletContext } from "react-router";
 import useAppSelector from "../../../../../../redux/hooks/useAppSelector";
 import { getCurrentAreaId } from "../../../../../../redux/selectors";
-import { MatrixStats, StudyMetadata } from "../../../../../../common/types";
-import MatrixInput from "../../../../../common/MatrixInput";
-import { Root } from "./style";
+import Matrix from "../../../../../common/MatrixGrid/Matrix";
 
 function Wind() {
-  const { study } = useOutletContext<{ study: StudyMetadata }>();
   const currentArea = useAppSelector(getCurrentAreaId);
   const url = `input/wind/series/wind_${currentArea}`;
 
@@ -28,11 +25,7 @@ function Wind() {
   // JSX
   ////////////////////////////////////////////////////////////////
 
-  return (
-    <Root>
-      <MatrixInput study={study} url={url} computStats={MatrixStats.STATS} />
-    </Root>
-  );
+  return <Matrix url={url} />;
 }
 
 export default Wind;
