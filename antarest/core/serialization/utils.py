fca691ad2792bd636dc3d6f9503bb406c52ba0ae
belthlemar <martin.belthle@rte-france.com>
2024-09-17 14:38:59 +0200

    add little comment to explain utility functions

--- antarest/core/serialization/utils.py
+++ antarest/core/serialization/utils.py
@@ -15,7 +15,11 @@ import pydantic
 
 ADAPTER: pydantic.TypeAdapter[t.Any] = pydantic.TypeAdapter(
     type=t.Any, config=pydantic.config.ConfigDict(ser_json_inf_nan="constants")
-)
+)  # ser_json_inf_nan="constants" means infinity and NaN values will be serialized as `Infinity` and `NaN`.
+
+
+# These utility functions allow to serialize with pydantic instead of using the built-in python "json" library.
+# Since pydantic v2 is written in RUST it's way faster.
 
 
 def from_json(data: t.Union[str, bytes, bytearray]) -> t.Dict[str, t.Any]:
