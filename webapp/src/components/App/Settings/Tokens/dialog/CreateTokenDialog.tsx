fa1661978a8b27ee3e7ca3c14df41d83167a3d10
MartinBelthle <martin.belthle@rte-france.com>
2024-09-18 15:45:33 +0200

    chore(front-end): add license headers inside front-end (#2145)
    Fix [ANT-1916] for the front-end part

--- webapp/src/components/App/Settings/Tokens/dialog/CreateTokenDialog.tsx
+++ webapp/src/components/App/Settings/Tokens/dialog/CreateTokenDialog.tsx
@@ -1,3 +1,16 @@
+/** Copyright (c) 2024, RTE (https://www.rte-france.com)
+ *
+ * See AUTHORS.txt
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This file is part of the Antares project.
+ */
+
 import TokenIcon from "@mui/icons-material/Token";
 import { IconButton, Paper, Tooltip } from "@mui/material";
 import { useSnackbar } from "notistack";
