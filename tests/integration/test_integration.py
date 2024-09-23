a0be48e211fcc1e771ff0ddcafa822d99b320d6f
belthlemar <martin.belthle@rte-france.com>
2024-09-23 14:38:36 +0200

    fix(playlist): change response model to accept optional answers

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -109,28 +109,6 @@ def test_main(client: TestClient, admin_access_token: str) -> None:
     )
     assert res.status_code == 200, res.json()
 
-    # playlist
-    res = client.post(
-        f"/v1/studies/{study_id}/raw?path=settings/generaldata/general/nbyears",
-        headers={"Authorization": f'Bearer {george_credentials["access_token"]}'},
-        json=5,
-    )
-    assert res.status_code == 204
-
-    res = client.put(
-        f"/v1/studies/{study_id}/config/playlist",
-        headers={"Authorization": f'Bearer {george_credentials["access_token"]}'},
-        json={"playlist": [1, 2], "weights": {1: 8.0, 3: 9.0}},
-    )
-    assert res.status_code == 200
-
-    res = client.get(
-        f"/v1/studies/{study_id}/config/playlist",
-        headers={"Authorization": f'Bearer {george_credentials["access_token"]}'},
-    )
-    assert res.status_code == 200
-    assert res.json() == {"1": 8.0, "2": 1.0}
-
     # Update the active ruleset
     active_ruleset_name = "ruleset test"
     res = client.post(
