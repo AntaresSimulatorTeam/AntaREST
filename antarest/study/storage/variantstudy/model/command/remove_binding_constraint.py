bf08ccedf04d08c8cc30e294ba7bc157c2efb3dc
belthlemar <martin.belthle@rte-france.com>
2024-09-30 15:40:03 +0200

    only open sc builder when needed

--- antarest/study/storage/variantstudy/model/command/remove_binding_constraint.py
+++ antarest/study/storage/variantstudy/model/command/remove_binding_constraint.py
@@ -70,7 +70,8 @@ class RemoveBindingConstraint(ICommand):
             old_groups = {bd.get("group", DEFAULT_GROUP).lower() for bd in binding_constraints.values()}
             new_groups = {bd.get("group", DEFAULT_GROUP).lower() for bd in new_binding_constraints.values()}
             removed_groups = old_groups - new_groups
-            remove_bc_from_scenario_builder(study_data, removed_groups)
+            if removed_groups:
+                remove_bc_from_scenario_builder(study_data, removed_groups)
 
         return self._apply_config(study_data.config)[0]
 
