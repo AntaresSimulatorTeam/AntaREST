61d18c68ce3a36a98d46a8a651da0b9c6001ebae
Mohamed Abdel Wedoud <mohammed.youssouf@gmail.com>
2024-10-07 18:17:32 +0200

    refactor(scripts): add the possibility of using `Gunicorn` when specified

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,18 +11,31 @@ min() {
 
 workers=$(min 30 ${NB_WORKERS_ANTARES:-17}) # default to 17 and max is 30
 
+# Check for --no-gunicorn or --multiple-ports argument
+use_gunicorn=true
+for arg in "$@"
+do
+    if [[ $arg == "--no-gunicorn" || $arg == "--multiple-ports" ]]; then
+        use_gunicorn=false
+        break
+    fi
+done
+
 if [ -z "$1" ] ; then
   sh $CUR_DIR/pre-start.sh
-  pids=() # Initialize empty array to store background process IDs
-  for ((i=0; i<workers; i++))
-  do
-    uvicorn antarest.wsgi:app --host 0.0.0.0 --port $((5000 + $i)) --log-level info --timeout-keep-alive 600 &
-    pids+=($!) # Store background process IDs
-  done
-  for pid in ${pids[*]};
-  do
-    wait $pid # Wait for each background process to finish
-  done
+  if [ "$use_gunicorn" = true ]; then
+    gunicorn --config $BASE_DIR/conf/gunicorn.py --worker-class=uvicorn.workers.UvicornWorker antarest.wsgi:app
+  else
+    pids=() # Initialize empty array to store background process IDs
+    for ((i=0; i<workers; i++))
+    do
+      uvicorn antarest.wsgi:app --host 0.0.0.0 --port $((5000 + $i)) --log-level info --timeout-keep-alive 600 &
+      pids+=($!) # Store background process IDs
+    done
+    for pid in ${pids[*]};
+    do
+      wait $pid # Wait for each background process to finish
+    done
 else
   export PYTHONPATH=$BASE_DIR
   python3 $BASE_DIR/antarest/main.py -c $ANTAREST_CONF --module "$1"
