cbc6b397e8e06b0173be0912275acf2716199c24
Mohamed Abdel Wedoud <mohammed.youssouf@gmail.com>
2024-10-07 11:38:17 +0200

    perf(scripts): improve load balancing, make the main process wait for background processes

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,9 +5,24 @@ set -e
 CUR_DIR=$(cd "$(dirname "$0")" && pwd)
 BASE_DIR=$(dirname "$CUR_DIR")
 
+min() {
+    echo $(( $1 < $2 ? $1 : $2 ))
+}
+
+workers=$(min 30 ${NB_WORKERS_ANTARES:-17}) # default to 17 and max is 30
+
 if [ -z "$1" ] ; then
   sh $CUR_DIR/pre-start.sh
-  gunicorn --config $BASE_DIR/conf/gunicorn.py --worker-class=uvicorn.workers.UvicornWorker antarest.wsgi:app
+  pids=() # Initialize empty array to store background process IDs
+  for ((i=0; i<workers; i++))
+  do
+    uvicorn antarest.wsgi:app --host 0.0.0.0 --port $((5000 + $i)) --log-level info --timeout-keep-alive 600 &
+    pids+=($!) # Store background process IDs
+  done
+  for pid in ${pids[*]};
+  do
+    wait $pid # Wait for each background process to finish
+  done
 else
   export PYTHONPATH=$BASE_DIR
   python3 $BASE_DIR/antarest/main.py -c $ANTAREST_CONF --module "$1"
