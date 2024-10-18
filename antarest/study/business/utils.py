0495128b7d1d97c51aef61a7cb915d211a6f9ba1
belthlemar <martin.belthle@rte-france.com>
2024-10-18 13:44:19 +0200

    put listener even with raw studies

--- antarest/study/business/utils.py
+++ antarest/study/business/utils.py
@@ -25,6 +25,7 @@ from antarest.study.storage.storage_service import StudyStorageService
 from antarest.study.storage.utils import is_managed
 from antarest.study.storage.variantstudy.business.utils import transform_command_to_dto
 from antarest.study.storage.variantstudy.model.command.icommand import ICommand
+from antarest.study.storage.variantstudy.model.command_listener.command_listener import ICommandListener
 
 # noinspection SpellCheckingInspection
 GENERAL_DATA_PATH = "settings/generaldata"
@@ -35,11 +36,12 @@ def execute_or_add_commands(
     file_study: FileStudy,
     commands: t.Sequence[ICommand],
     storage_service: StudyStorageService,
+    listener: t.Optional[ICommandListener] = None,
 ) -> None:
     if isinstance(study, RawStudy):
         executed_commands: t.MutableSequence[ICommand] = []
         for command in commands:
-            result = command.apply(file_study)
+            result = command.apply(file_study, listener)
             if not result.status:
                 raise CommandApplicationError(result.message)
             executed_commands.append(command)
