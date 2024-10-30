1b6dc2bcd010a9ec2dcdb12f27973f15ebb76c82
Theo Pascoli <theo.pascoli@rte-france.com>
2024-10-30 11:32:48 +0100

    feat: add support for pyarrow in load management

--- antarest/study/business/load_management.py
+++ antarest/study/business/load_management.py
@@ -9,23 +9,22 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-import typing as t
+import io
+from typing import cast
+
+import pandas as pd
 
 from antarest.core.model import JSON
 from antarest.core.serialization import AntaresBaseModel
-from antarest.study.business.all_optional_meta import camel_case_model
 from antarest.study.model import MatrixFormat, Study
 from antarest.study.storage.rawstudy.model.filesystem.matrix.input_series_matrix import InputSeriesMatrix
 from antarest.study.storage.storage_service import StudyStorageService
 
 LOAD_PATH = "input/load/series/load_{area_id}"
 
-@camel_case_model
 class LoadInfoDTO(AntaresBaseModel):
     matrix: JSON | bytes
 
-class LoadCreationInfo(LoadInfoDTO):
-    pass
 
 class LoadManager:
     def __init__(self, storage_service: StudyStorageService) -> None:
@@ -35,11 +34,15 @@ class LoadManager:
         load_path = LOAD_PATH.replace("{area_id}", area_id).split("/")
         file_study = self.storage_service.get_storage(study).get_raw(study)
 
-        if matrix_format == MatrixFormat.JSON:
-            node = file_study.tree.get_node(load_path)
-            if isinstance(node, InputSeriesMatrix):
-                matrix = InputSeriesMatrix.parse(node)
-                return LoadInfoDTO(matrix=matrix)
-        elif matrix_format == MatrixFormat.ARROW:
-            pass
+        node = file_study.tree.get_node(load_path)
 
+        if isinstance(node, InputSeriesMatrix):
+            if matrix_format == MatrixFormat.JSON:
+                matrix_json = cast(JSON, InputSeriesMatrix.parse(node))
+                return LoadInfoDTO(matrix = matrix_json)
+            elif matrix_format == MatrixFormat.ARROW:
+                matrix_df: pd.DataFrame = cast(pd.DataFrame, InputSeriesMatrix.parse(node, return_dataframe=True))
+                with io.BytesIO() as buffer:
+                    matrix_df.columns = matrix_df.columns.map(str)
+                    matrix_df.to_feather(buffer, compression="uncompressed")
+                    return LoadInfoDTO(matrix = buffer.getvalue())
