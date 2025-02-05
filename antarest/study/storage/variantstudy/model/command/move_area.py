457d9103cbf859adb9b66da6e9d3691c4f89bad5
Theo Pascoli <theo.pascoli@rte-france.com>
2025-02-05 09:35:24 +0100

    fix(area): remove deleted commands method

--- antarest/study/storage/variantstudy/model/command/move_area.py
+++ antarest/study/storage/variantstudy/model/command/move_area.py
@@ -73,19 +73,5 @@ class MoveArea(ICommand):
         )
 
     @override
-    def match_signature(self) -> str:
-        return str(self.command_name.value + MATCH_SIGNATURE_SEPARATOR + self.area_id)
-
-    @override
-    def match(self, other: ICommand, equal: bool = False) -> bool:
-        if not isinstance(other, MoveArea):
-            return False
-        return self.area_id == other.area_id
-
-    @override
-    def _create_diff(self, other: "ICommand") -> t.List["ICommand"]:
-        return []
-
-    @override
     def get_inner_matrices(self) -> t.List[str]:
         return []
