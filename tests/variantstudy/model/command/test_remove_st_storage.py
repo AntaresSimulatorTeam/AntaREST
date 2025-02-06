d05956a7f9dc391bcf35f2303e4d469c56ef8d3b
Theo Pascoli <theo.pascoli@rte-france.com>
2025-02-06 09:44:39 +0100

    feat(all): upgrade pydantic, black, mypy and isort

--- tests/variantstudy/model/command/test_remove_st_storage.py
+++ tests/variantstudy/model/command/test_remove_st_storage.py
@@ -81,16 +81,19 @@ class TestRemoveSTStorage:
                 storage_id="?%$$",  # bad name
                 study_version=STUDY_VERSION_8_8,
             )
-        assert ctx.value.errors() == [
+        expected_error = [
             {
                 "ctx": {"pattern": "[a-z0-9_(),& -]+"},
                 "input": "?%$$",
                 "loc": ("storage_id",),
                 "msg": "String should match pattern '[a-z0-9_(),& -]+'",
                 "type": "string_pattern_mismatch",
-                "url": "https://errors.pydantic.dev/2.8/v/string_pattern_mismatch",
             }
         ]
+        actual_errors = ctx.value.errors()
+        for error in actual_errors:
+            error.pop("url")
+        assert actual_errors == expected_error
 
     def test_apply_config__invalid_version(self, empty_study: FileStudy, command_context: CommandContext):
         # Given an old study in version 720
