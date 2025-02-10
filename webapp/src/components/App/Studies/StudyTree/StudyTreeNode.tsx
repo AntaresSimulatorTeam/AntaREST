56dd8c4276f2e00f04408b044ba718236bbad7e8
Anis SMAIL <anis.smail_externe@rte-france.com>
2025-02-10 17:17:53 +0100

    fix build

--- webapp/src/components/App/Studies/StudyTree/StudyTreeNode.tsx
+++ webapp/src/components/App/Studies/StudyTree/StudyTreeNode.tsx
@@ -12,7 +12,7 @@
  * This file is part of the Antares project.
  */
 
-import { memo, useMemo } from "react";
+import { useMemo } from "react";
 import * as R from "ramda";
 import type { StudyTreeNodeProps } from "./types";
 import TreeItemEnhanced from "@/components/common/TreeItemEnhanced";
