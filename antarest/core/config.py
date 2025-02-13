916365f7e53639247a27c16b8090d6b74bc7e1f5
Anis SMAIL <anis.smail_externe@rte-france.com>
2025-02-13 01:23:23 +0100

    ad check for overlapping workspaces

--- antarest/core/config.py
+++ antarest/core/config.py
@@ -165,6 +165,8 @@ class StorageConfig:
             if "workspaces" in data
             else defaults.workspaces
         )
+
+        cls._validate_workspaces(data, workspaces)
         return cls(
             matrixstore=Path(data["matrixstore"]) if "matrixstore" in data else defaults.matrixstore,
             archive_dir=Path(data["archive_dir"]) if "archive_dir" in data else defaults.archive_dir,
@@ -190,6 +192,19 @@ class StorageConfig:
                 defaults.snapshot_retention_days,
             ),
         )
+
+    @classmethod
+    def _validate_workspaces(cls, config_as_json: JSON, workspaces: Dict[str, WorkspaceConfig]) -> None:
+        """
+        Validate that no two workspaces have overlapping paths.
+        """
+        workspace_name_by_path = [(config.path, name) for name, config in workspaces.items()]
+        for path, name in workspace_name_by_path:
+            for path2, name2 in workspace_name_by_path:
+                if name != name2 and path.is_relative_to(path2):
+                    raise ValueError(
+                        f"Overlapping workspace paths found: '{name}' and '{name2}' '{path}' is relative to '{path2}' "
+                    )
 
 
 @dataclass(frozen=True)
