22ae4e414d8467bb46323c5c085e60dcddd2cc2b
belthlemar <martin.belthle@rte-france.com>
2025-02-21 16:30:58 +0100

    remove loggers

--- antarest/study/storage/variantstudy/model/command/create_xpansion_candidate.py
+++ antarest/study/storage/variantstudy/model/command/create_xpansion_candidate.py
@@ -9,7 +9,6 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-import logging
 from typing import Any, Dict, List, Optional, Tuple
 
 from typing_extensions import override
@@ -25,8 +24,6 @@ from antarest.study.storage.variantstudy.model.command.icommand import ICommand
 from antarest.study.storage.variantstudy.model.command.xpansion_common import assert_candidate_is_correct
 from antarest.study.storage.variantstudy.model.command_listener.command_listener import ICommandListener
 from antarest.study.storage.variantstudy.model.model import CommandDTO
-
-logger = logging.getLogger(__name__)
 
 
 class CreateXpansionCandidate(ICommand):
--- antarest/study/storage/variantstudy/model/command/update_xpansion_candidate.py
+++ antarest/study/storage/variantstudy/model/command/update_xpansion_candidate.py
@@ -9,7 +9,6 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-import logging
 from typing import Any, Dict, List, Optional, Tuple
 
 from typing_extensions import override
@@ -28,8 +27,6 @@ from antarest.study.storage.variantstudy.model.command.xpansion_common import (
 )
 from antarest.study.storage.variantstudy.model.command_listener.command_listener import ICommandListener
 from antarest.study.storage.variantstudy.model.model import CommandDTO
-
-logger = logging.getLogger(__name__)
 
 
 class UpdateXpansionCandidate(ICommand):
