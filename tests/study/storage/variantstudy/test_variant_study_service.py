771626fe3886855d80850c1bde09ecb765272800
MartinBelthle <martin.belthle@rte-france.com>
2025-02-21 14:48:46 +0100

    chore(patch)!: remove patch service (#2359)

--- tests/study/storage/variantstudy/test_variant_study_service.py
+++ tests/study/storage/variantstudy/test_variant_study_service.py
@@ -29,7 +29,6 @@ from antarest.login.utils import current_user_context
 from antarest.matrixstore.service import SimpleMatrixService
 from antarest.study.business.utils import execute_or_add_commands
 from antarest.study.model import RawStudy, StudyAdditionalData
-from antarest.study.storage.patch_service import PatchService
 from antarest.study.storage.rawstudy.model.filesystem.config.st_storage import (
     STStorageGroup,
     STStorageProperties,
@@ -130,7 +129,6 @@ class TestVariantStudyService:
         raw_study_service: RawStudyService,
         simple_matrix_service: SimpleMatrixService,
         generator_matrix_constants: GeneratorMatrixConstants,
-        patch_service: PatchService,
         study_storage_service: StudyStorageService,
         # pytest parameters
         denormalize: bool,
@@ -189,7 +187,6 @@ class TestVariantStudyService:
         command_context = CommandContext(
             generator_matrix_constants=generator_matrix_constants,
             matrix_service=simple_matrix_service,
-            patch_service=patch_service,
         )
 
         create_area_fr = CreateArea(command_context=command_context, area_name="fr", study_version=study_version)
