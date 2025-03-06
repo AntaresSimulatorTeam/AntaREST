ae1ef80800f6f29c75f18670bf1e789450d4c95e
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2025-03-06 17:05:29 +0100

    chore(hydro): adapt test for backport
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- tests/study/business/areas/test_hydro_management.py
+++ tests/study/business/areas/test_hydro_management.py
@@ -9,21 +9,29 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
+import datetime
+import os
+import uuid
 from pathlib import Path
+from typing import cast
 from unittest.mock import Mock
 
 import pytest
+from sqlalchemy.orm import Session
 
-from antarest.matrixstore.service import SimpleMatrixService
+from antarest.core.model import PublicMode
+from antarest.login.model import Group, User
 from antarest.study.business.areas.hydro_management import HydroManager, ManagementOptionsFormFields
-from antarest.study.business.study_interface import FileStudyInterface
+from antarest.study.model import RawStudy, Study, StudyContentStatus
 from antarest.study.storage.rawstudy.model.filesystem.config.files import build
 from antarest.study.storage.rawstudy.model.filesystem.context import ContextServer
 from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
 from antarest.study.storage.rawstudy.model.filesystem.root.filestudytree import FileStudyTree
 from antarest.study.storage.rawstudy.raw_study_service import RawStudyService
-from antarest.study.storage.variantstudy.business.matrix_constants_generator import GeneratorMatrixConstants
+from antarest.study.storage.storage_service import StudyStorageService
+from antarest.study.storage.variantstudy.command_factory import CommandFactory
 from antarest.study.storage.variantstudy.model.command_context import CommandContext
+from antarest.study.storage.variantstudy.variant_study_service import VariantStudyService
 
 hydro_ini_content = {
     "input": {
@@ -46,37 +54,66 @@ hydro_ini_content = {
 }
 
 
-@pytest.fixture(name="hydro_manager")
-def hydro_manager_fixture(
-    raw_study_service: RawStudyService,
-    generator_matrix_constants: GeneratorMatrixConstants,
-    simple_matrix_service: SimpleMatrixService,
-) -> HydroManager:
-    hydro_manager = HydroManager(
-        command_context=CommandContext(
-            generator_matrix_constants=generator_matrix_constants,
-            matrix_service=simple_matrix_service,
-        )
+@pytest.fixture(name="study_storage_service")
+def study_storage_service() -> StudyStorageService:
+    """Return a mocked StudyStorageService."""
+    return Mock(
+        spec=StudyStorageService,
+        variant_study_service=Mock(
+            spec=VariantStudyService,
+            command_factory=Mock(
+                spec=CommandFactory,
+                command_context=Mock(spec=CommandContext),
+            ),
+        ),
+        get_storage=Mock(return_value=Mock(spec=RawStudyService, get_raw=Mock(spec=FileStudy))),
     )
-    return hydro_manager
 
 
-@pytest.fixture(name="study")
-def study_interface_fixture(tmp_path: Path) -> FileStudyInterface:
-    study_id = "study_test"
-    study_path = tmp_path.joinpath(f"tmp/{study_id}")
-    config = build(study_path, study_id)
+# noinspection PyArgumentList
+@pytest.fixture(name="study_uuid")
+def study_uuid_fixture(db_session: Session) -> str:
+    user = User(id=0, name="admin")
+    group = Group(id="my-group", name="group")
+    raw_study = RawStudy(
+        id=str(uuid.uuid4()),
+        name="Dummy",
+        version="860",  # version 860 is required for the storage feature
+        author="John Smith",
+        created_at=datetime.datetime.now(datetime.timezone.utc),
+        updated_at=datetime.datetime.now(datetime.timezone.utc),
+        public_mode=PublicMode.FULL,
+        owner=user,
+        groups=[group],
+        workspace="default",
+        path="/path/to/study",
+        content_status=StudyContentStatus.WARNING,
+    )
+    db_session.add(raw_study)
+    db_session.commit()
+    return cast(str, raw_study.id)
+
+
+@pytest.fixture
+def study_tree(tmp_path: Path, study_uuid: str) -> FileStudyTree:
+    study_path = tmp_path.joinpath(f"tmp/{study_uuid}")
+    study_path.mkdir(parents=True, exist_ok=True)
+    config = build(study_path, study_uuid)
     tree = FileStudyTree(Mock(spec=ContextServer), config)
     tree.save(hydro_ini_content)
-
-    file_study_interface = FileStudyInterface(file_study=FileStudy(config=config, tree=tree))
-
-    return file_study_interface
+    return tree
 
 
 class TestHydroManagement:
+
     @pytest.mark.unit_test
-    def test_get_field_values(self, hydro_manager: HydroManager, study: FileStudyInterface) -> None:
+    def test_get_field_values(
+        self,
+        db_session: Session,
+        study_storage_service: StudyStorageService,
+        study_uuid: str,
+        study_tree: FileStudyTree,
+    ) -> None:
         """
         Set up:
             Retrieve a study service and a study interface
@@ -84,6 +121,16 @@ class TestHydroManagement:
         Test:
             Check if `get_field_values` returns the right values
         """
+        study: RawStudy = db_session.query(Study).get(study_uuid)
+
+        # Prepare the mocks
+        storage = study_storage_service.get_storage(study)
+        file_study = storage.get_raw(study)
+        file_study.tree = study_tree
+
+        # Given the following arguments
+        hydro_manager = HydroManager(study_storage_service)
+
         # add som areas
         areas = ["AreaTest1", "AREATEST2", "area_test_3"]
 
@@ -103,7 +150,14 @@ class TestHydroManagement:
             assert data_area_raw == data_area_upper
 
     @pytest.mark.unit_test
-    def test_set_field_values(self, tmp_path: Path, hydro_manager: HydroManager, study: FileStudyInterface) -> None:
+    def test_set_field_values(
+        self,
+        tmp_path: Path,
+        db_session: Session,
+        study_storage_service: StudyStorageService,
+        study_uuid: str,
+        study_tree: FileStudyTree,
+    ) -> None:
         """
         Set up:
             Retrieve a study service and a study interface
@@ -114,6 +168,16 @@ class TestHydroManagement:
             Simulate a regular change
             Check if the field was successfully edited for each area without duplicates
         """
+        study: RawStudy = db_session.query(Study).get(study_uuid)
+
+        # Prepare the mocks
+        storage = study_storage_service.get_storage(study)
+        file_study = storage.get_raw(study)
+        file_study.tree = study_tree
+
+        # Given the following arguments
+        hydro_manager = HydroManager(study_storage_service)
+
         # store the area ids
         areas = ["AreaTest1", "AREATEST2", "area_test_3"]
 
