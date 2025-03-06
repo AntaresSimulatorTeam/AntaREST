fdb0c641887707b8c12abd3880ca1278f0f7ff48
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2025-03-06 11:38:53 +0100

    fix(variants): remove second regeneration of study config
    This is a quick fix, the inputs of class VariantCommandGenerator
    should probably be reconsidered more in depth.
    
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- antarest/study/storage/variantstudy/variant_command_generator.py
+++ antarest/study/storage/variantstudy/variant_command_generator.py
@@ -17,18 +17,31 @@ from pathlib import Path
 from typing import Callable, List, Optional, Tuple, Union, cast
 
 from antarest.core.utils.utils import StopWatch
-from antarest.study.storage.rawstudy.model.filesystem.config.model import FileStudyTreeConfig
-from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy, StudyFactory
+from antarest.study.storage.rawstudy.model.filesystem.config.model import (
+    FileStudyTreeConfig,
+)
+from antarest.study.storage.rawstudy.model.filesystem.factory import (
+    FileStudy,
+    StudyFactory,
+)
 from antarest.study.storage.utils import update_antares_info
 from antarest.study.storage.variantstudy.model.command.common import CommandOutput
 from antarest.study.storage.variantstudy.model.command.icommand import ICommand
-from antarest.study.storage.variantstudy.model.command_listener.command_listener import ICommandListener
+from antarest.study.storage.variantstudy.model.command_listener.command_listener import (
+    ICommandListener,
+)
 from antarest.study.storage.variantstudy.model.dbmodel import VariantStudy
-from antarest.study.storage.variantstudy.model.model import GenerationResultInfoDTO, NewDetailsDTO
+from antarest.study.storage.variantstudy.model.model import (
+    GenerationResultInfoDTO,
+    NewDetailsDTO,
+)
 
 logger = logging.getLogger(__name__)
 
-APPLY_CALLBACK = Callable[[ICommand, Union[FileStudyTreeConfig, FileStudy], Optional[ICommandListener]], CommandOutput]
+APPLY_CALLBACK = Callable[
+    [ICommand, Union[FileStudyTreeConfig, FileStudy], Optional[ICommandListener]],
+    CommandOutput,
+]
 
 
 class CmdNotifier:
@@ -117,7 +130,9 @@ class VariantCommandGenerator:
     ) -> GenerationResultInfoDTO:
         # Build file study
         logger.info("Building study tree")
-        study = self.study_factory.create_from_fs(dest_path, "", use_cache=False)
+        study = self.study_factory.create_from_fs(
+            dest_path, metadata.id if metadata else "", use_cache=metadata is not None
+        )
         if metadata:
             update_antares_info(metadata, study.tree, update_author=True)
 
