da61f44f5b9dc3710a5f98e728d18b79d5efa9c3
belthlemar <martin.belthle@rte-france.com>
2025-03-20 14:50:06 +0100

    add tests

--- antarest/study/storage/variantstudy/model/command/update_renewables_clusters.py
+++ antarest/study/storage/variantstudy/model/command/update_renewables_clusters.py
@@ -15,9 +15,11 @@ from typing_extensions import override
 
 from antarest.core.exceptions import ChildNotFoundError
 from antarest.study.business.model.renewable_cluster_model import RenewableClusterUpdates
+from antarest.study.storage.rawstudy.model.filesystem.config.identifier import transform_name_to_id
 from antarest.study.storage.rawstudy.model.filesystem.config.model import FileStudyTreeConfig
 from antarest.study.storage.rawstudy.model.filesystem.config.renewable import (
     RenewableConfig,
+    RenewablePropertiesType,
     create_renewable_properties,
 )
 from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
@@ -50,7 +52,7 @@ class UpdateRenewablesClusters(ICommand):
 
             renewable_mapping: dict[str, tuple[int, RenewableConfig]] = {}
             for index, renewable in enumerate(study_data.areas[area_id].renewables):
-                renewable_mapping[renewable.id] = (index, renewable)
+                renewable_mapping[transform_name_to_id(renewable.id)] = (index, renewable)
 
             for cluster_id in value:
                 if cluster_id not in renewable_mapping:
@@ -64,7 +66,7 @@ class UpdateRenewablesClusters(ICommand):
                 index, renewable = renewable_mapping[cluster_id]
                 current_properties = renewable.model_dump(mode="json")
                 current_properties.update(
-                    self.cluster_properties[area_id][renewable.id].model_dump(
+                    self.cluster_properties[area_id][cluster_id].model_dump(
                         mode="json", exclude_unset=True, exclude_none=True
                     )
                 )
@@ -82,19 +84,25 @@ class UpdateRenewablesClusters(ICommand):
             except ChildNotFoundError:
                 return CommandOutput(status=False, message=f"The area '{area_id}' is not found.")
 
+            # Validates the Ini file
+            clusters_by_id: dict[str, tuple[str, RenewablePropertiesType]] = {
+                transform_name_to_id(k): (k, create_renewable_properties(self.study_version, v))
+                for k, v in all_clusters_for_area.items()
+            }
+
             for cluster_id, properties in value.items():
-                if cluster_id not in all_clusters_for_area:
+                if cluster_id not in clusters_by_id:
                     return CommandOutput(
                         status=False,
                         message=f"The renewable cluster '{cluster_id}' in the area '{area_id}' is not found.",
                     )
                 # Performs the update
                 new_properties_dict = properties.model_dump(mode="json", by_alias=False, exclude_unset=True)
-                current_properties_obj = create_renewable_properties(
-                    self.study_version, all_clusters_for_area[cluster_id]
-                )
+                current_properties_obj = clusters_by_id[cluster_id][1]
                 updated_obj = current_properties_obj.model_copy(update=new_properties_dict)
-                all_clusters_for_area[cluster_id] = updated_obj.model_dump(mode="json", by_alias=True)
+                all_clusters_for_area[clusters_by_id[cluster_id][0]] = updated_obj.model_dump(
+                    mode="json", by_alias=True
+                )
 
             study_data.tree.save(data=all_clusters_for_area, url=ini_path)
 
--- tests/variantstudy/model/command/test_update_renewable_cluster.py
+++ tests/variantstudy/model/command/test_update_renewable_cluster.py
@@ -10,6 +10,7 @@
 #
 # This file is part of the Antares project.
 import pytest
+from core.serde.ini_reader import IniReader
 from study.storage.rawstudy.model.filesystem.factory import FileStudy
 from study.storage.variantstudy.model.command.create_area import CreateArea
 from study.storage.variantstudy.model.command.create_renewables_cluster import CreateRenewablesCluster
@@ -21,6 +22,7 @@ from study.storage.variantstudy.model.command_context import CommandContext
 class TestUpdateRenewableCluster:
     def _set_up(self, study: FileStudy, command_context: CommandContext):
         CreateArea(area_name="FR", command_context=command_context, study_version=study.config.version).apply(study)
+        CreateArea(area_name="de", command_context=command_context, study_version=study.config.version).apply(study)
         # Modify the general data to put the sutdy in mode clusters
         UpdateConfig(
             target="settings/generaldata/other preferences/renewable-generation-modelling",
@@ -30,7 +32,19 @@ class TestUpdateRenewableCluster:
         ).apply(study)
         CreateRenewablesCluster(
             area_id="fr",
-            parameters={"name": "CLUSTER"},
+            parameters={"name": "CLUSTER_1"},
+            command_context=command_context,
+            study_version=study.config.version,
+        ).apply(study)
+        CreateRenewablesCluster(
+            area_id="fr",
+            parameters={"name": "cluster_2"},
+            command_context=command_context,
+            study_version=study.config.version,
+        ).apply(study)
+        CreateRenewablesCluster(
+            area_id="de",
+            parameters={"name": "Cluster_3"},
             command_context=command_context,
             study_version=study.config.version,
         ).apply(study)
@@ -38,6 +52,63 @@ class TestUpdateRenewableCluster:
     @pytest.mark.parametrize("empty_study", ["empty_study_870.zip"], indirect=True)
     def test_nominal_case(self, empty_study: FileStudy, command_context: CommandContext):
         self._set_up(empty_study, command_context)
+        study_version = empty_study.config.version
+        study_path = empty_study.config.study_path
+
+        # Check existing properties
+        fr_ini = study_path / "input" / "renewables" / "clusters" / "fr" / "list.ini"
+        de_ini = study_path / "input" / "renewables" / "clusters" / "de" / "list.ini"
+        fr_content = IniReader().read(fr_ini)
+        de_content = IniReader().read(de_ini)
+        expected_fr_content = {
+            "CLUSTER_1": {
+                "name": "CLUSTER_1",
+                "enabled": True,
+                "unitcount": 1,
+                "nominalcapacity": 0.0,
+                "group": "other res 1",
+                "ts-interpretation": "power-generation",
+            },
+            "cluster_2": {
+                "name": "cluster_2",
+                "enabled": True,
+                "unitcount": 1,
+                "nominalcapacity": 0.0,
+                "group": "other res 1",
+                "ts-interpretation": "power-generation",
+            },
+        }
+        assert fr_content == expected_fr_content
+        expected_de_content = {
+            "Cluster_3": {
+                "name": "Cluster_3",
+                "enabled": True,
+                "unitcount": 1,
+                "nominalcapacity": 0.0,
+                "group": "other res 1",
+                "ts-interpretation": "power-generation",
+            }
+        }
+        assert de_content == expected_de_content
+
+        # Update several properties
+        new_properties = {"fr": {"cluster_1": {"enabled": False}}, "DE": {"CLUSTER_3": {"unit_count": 4}}}
+        cmd = UpdateRenewablesClusters(
+            cluster_properties=new_properties,
+            command_context=command_context,
+            study_version=study_version,
+        )
+        output = cmd.apply(empty_study)
+        assert output.status is True
+        assert output.message == "The renewable clusters were successfully updated."
+
+        # Checks updated properties
+        fr_content = IniReader().read(fr_ini)
+        de_content = IniReader().read(de_ini)
+        expected_fr_content["CLUSTER_1"]["enabled"] = False
+        expected_de_content["Cluster_3"]["unitcount"] = 4
+        assert fr_content == expected_fr_content
+        assert de_content == expected_de_content
 
     @pytest.mark.parametrize("empty_study", ["empty_study_870.zip"], indirect=True)
     def test_error_cases(self, empty_study: FileStudy, command_context: CommandContext):
