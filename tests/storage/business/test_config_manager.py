84bf1d741e2a1816a3bac4f3ebde4816d46f95cf
belthlemar <martin.belthle@rte-france.com>
2025-03-21 18:20:00 +0100

    use old tests

--- tests/storage/business/test_config_manager.py
+++ tests/storage/business/test_config_manager.py
@@ -13,14 +13,25 @@
 from pathlib import Path
 from unittest.mock import Mock
 
-from antares.study.version import StudyVersion
-
 from antarest.study.business.study_interface import StudyInterface
-from antarest.study.business.thematic_trimming_field_infos import FIELDS_INFO
+from antarest.study.business.thematic_trimming_field_infos import (
+    FIELDS_INFO,
+    ThematicTrimmingFormFields860,
+    ThematicTrimmingFormFields910,
+    ThematicTrimmingFormFieldsBase,
+)
 from antarest.study.business.thematic_trimming_management import (
-    ThematicTrimmingFormFields,
     ThematicTrimmingManager,
     get_fields_info,
+)
+from antarest.study.model import (
+    STUDY_VERSION_7_0,
+    STUDY_VERSION_8,
+    STUDY_VERSION_8_2,
+    STUDY_VERSION_8_3,
+    STUDY_VERSION_8_4,
+    STUDY_VERSION_8_6,
+    STUDY_VERSION_9_1,
 )
 from antarest.study.storage.rawstudy.model.filesystem.config.model import FileStudyTreeConfig
 from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
@@ -54,48 +65,52 @@ def test_thematic_trimming_config(command_context: CommandContext) -> None:
         {"variables selection": {"selected_vars_reset": True, "select_var -": ["DENS", "Profit by plant"]}},
         # For study version >= 840:
         {"variables selection": {"selected_vars_reset": False, "select_var +": ["CONG. FEE (ALG.)"]}},
+        # For study version >= 860:
+        {"variables selection": {"selected_vars_reset": True, "select_var -": ["STS inj by plant"]}},
+        # For study version >= 910:
+        {"variables selection": {"selected_vars_reset": True, "select_var -": ["STS by group"]}},
     ]
 
     study = Mock(StudyInterface)
     study.get_files.return_value = FileStudy(config=config, tree=file_tree_mock)
 
-    study.version = config.version = 700
+    study.version = config.version = STUDY_VERSION_7_0
     actual = thematic_trimming_manager.get_field_values(study)
-    fields_info = get_fields_info(StudyVersion.parse(study.version))
-    expected = ThematicTrimmingFormFields(**dict.fromkeys(fields_info, True))
+    fields_info = get_fields_info(study.version)
+    expected = ThematicTrimmingFormFieldsBase.model_validate(dict.fromkeys(fields_info, True))
     assert actual == expected
 
-    study.version = config.version = 800
+    study.version = config.version = STUDY_VERSION_8
     actual = thematic_trimming_manager.get_field_values(study)
-    fields_info = get_fields_info(StudyVersion.parse(study.version))
-    expected = ThematicTrimmingFormFields(**dict.fromkeys(fields_info, True))
+    fields_info = get_fields_info(study.version)
+    expected = ThematicTrimmingFormFieldsBase.model_validate(dict.fromkeys(fields_info, True))
     expected.avl_dtg = False
     assert actual == expected
 
-    study.version = config.version = 820
+    study.version = config.version = STUDY_VERSION_8_2
     actual = thematic_trimming_manager.get_field_values(study)
-    fields_info = get_fields_info(StudyVersion.parse(study.version))
-    expected = ThematicTrimmingFormFields(**dict.fromkeys(fields_info, True))
+    fields_info = get_fields_info(study.version)
+    expected = ThematicTrimmingFormFieldsBase.model_validate(dict.fromkeys(fields_info, True))
     expected.avl_dtg = False
     assert actual == expected
 
-    study.version = config.version = 830
+    study.version = config.version = STUDY_VERSION_8_3
     actual = thematic_trimming_manager.get_field_values(study)
-    fields_info = get_fields_info(StudyVersion.parse(study.version))
-    expected = ThematicTrimmingFormFields(**dict.fromkeys(fields_info, True))
+    fields_info = get_fields_info(study.version)
+    expected = ThematicTrimmingFormFieldsBase.model_validate(dict.fromkeys(fields_info, True))
     expected.dens = False
     expected.profit_by_plant = False
     assert actual == expected
 
-    study.version = config.version = 840
-    study_version = StudyVersion.parse(study.version)
+    study.version = config.version = STUDY_VERSION_8_4
+    study_version = study.version
     actual = thematic_trimming_manager.get_field_values(study)
     fields_info = get_fields_info(study_version)
-    expected = ThematicTrimmingFormFields(**dict.fromkeys(fields_info, False))
+    expected = ThematicTrimmingFormFieldsBase.model_validate(dict.fromkeys(fields_info, False))
     expected.cong_fee_alg = True
     assert actual == expected
 
-    new_config = ThematicTrimmingFormFields(**dict.fromkeys(fields_info, True))
+    new_config = ThematicTrimmingFormFieldsBase.model_validate(dict.fromkeys(fields_info, True))
     new_config.coal = False
     thematic_trimming_manager.set_field_values(study, new_config)
     assert study.add_commands.called_with(
@@ -107,7 +122,7 @@ def test_thematic_trimming_config(command_context: CommandContext) -> None:
         )
     )
 
-    new_config = ThematicTrimmingFormFields(**dict.fromkeys(fields_info, False))
+    new_config = ThematicTrimmingFormFieldsBase.model_validate(dict.fromkeys(fields_info, False))
     new_config.renw_1 = True
     thematic_trimming_manager.set_field_values(study, new_config)
     assert study.add_commands.called_with(
@@ -122,4 +137,18 @@ def test_thematic_trimming_config(command_context: CommandContext) -> None:
         )
     )
 
-    assert len(FIELDS_INFO) == 94
+    assert len(FIELDS_INFO) == 95
+
+    study.version = config.version = STUDY_VERSION_8_6
+    actual = thematic_trimming_manager.get_field_values(study)
+    fields_info = get_fields_info(study.version)
+    expected = ThematicTrimmingFormFields860.model_validate(dict.fromkeys(fields_info, True))
+    expected.sts_inj_by_plant = False
+    assert actual == expected
+
+    study.version = config.version = STUDY_VERSION_9_1
+    actual = thematic_trimming_manager.get_field_values(study)
+    fields_info = get_fields_info(study.version)
+    expected = ThematicTrimmingFormFields910.model_validate(dict.fromkeys(fields_info, True))
+    expected.sts_by_group = False
+    assert actual == expected
