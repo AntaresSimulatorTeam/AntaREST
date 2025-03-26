acd39167af569ecce686aff4ae364dc35aa459ad
belthlemar <martin.belthle@rte-france.com>
2025-03-26 17:20:58 +0100

    resolve comment

--- antarest/study/storage/variantstudy/model/command/update_st_storages.py
+++ antarest/study/storage/variantstudy/model/command/update_st_storages.py
@@ -19,12 +19,11 @@ from antarest.study.storage.rawstudy.model.filesystem.config.identifier import t
 from antarest.study.storage.rawstudy.model.filesystem.config.model import FileStudyTreeConfig
 from antarest.study.storage.rawstudy.model.filesystem.config.st_storage import (
     STStorageConfigType,
-    STStoragePropertiesType,
     create_st_storage_config,
     create_st_storage_properties,
 )
 from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
-from antarest.study.storage.variantstudy.model.command.common import CommandName, CommandOutput
+from antarest.study.storage.variantstudy.model.command.common import CommandName, CommandOutput, IdMapping
 from antarest.study.storage.variantstudy.model.command.icommand import ICommand, OutputTuple
 from antarest.study.storage.variantstudy.model.command_listener.command_listener import ICommandListener
 from antarest.study.storage.variantstudy.model.model import CommandDTO
@@ -80,24 +79,19 @@ class UpdateSTStorages(ICommand):
                 return CommandOutput(status=False, message=f"The area '{area_id}' is not found.")
 
             # Validates the Ini file
-            storages_by_id: dict[str, tuple[str, STStoragePropertiesType]] = {
-                transform_name_to_id(k): (k, create_st_storage_properties(self.study_version, v))
-                for k, v in all_clusters_for_area.items()
-            }
+            id_mapping = IdMapping(create_st_storage_properties, all_clusters_for_area, self.study_version)
 
             for storage_id, properties in value.items():
-                if storage_id not in storages_by_id:
+                if not id_mapping.asserts_id_exists(storage_id):
                     return CommandOutput(
                         status=False,
                         message=f"The short-term storage '{storage_id}' in the area '{area_id}' is not found.",
                     )
                 # Performs the update
                 new_properties_dict = properties.model_dump(mode="json", by_alias=False, exclude_unset=True)
-                current_properties_obj = storages_by_id[storage_id][1]
+                storage_key, current_properties_obj = id_mapping.get_key_and_properties(storage_id)
                 updated_obj = current_properties_obj.model_copy(update=new_properties_dict)
-                all_clusters_for_area[storages_by_id[storage_id][0]] = updated_obj.model_dump(
-                    mode="json", by_alias=True
-                )
+                all_clusters_for_area[storage_key] = updated_obj.model_dump(mode="json", by_alias=True)
 
             study_data.tree.save(data=all_clusters_for_area, url=ini_path)
 
