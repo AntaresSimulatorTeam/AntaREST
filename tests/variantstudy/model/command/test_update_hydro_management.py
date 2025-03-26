10361e235bea506e020320c5289267a3d6fda536
belthlemar <martin.belthle@rte-france.com>
2025-03-26 11:57:54 +0100

    continue test

--- tests/variantstudy/model/command/test_update_hydro_management.py
+++ tests/variantstudy/model/command/test_update_hydro_management.py
@@ -12,20 +12,31 @@
 import pytest
 from study.storage.rawstudy.model.filesystem.factory import FileStudy
 from study.storage.variantstudy.model.command.create_area import CreateArea
-from study.storage.variantstudy.model.command.create_st_storage import CreateSTStorage
 from study.storage.variantstudy.model.command_context import CommandContext
+
+from antarest.core.serde.ini_reader import read_ini
 
 
 class TestUpdateHydroManagement:
     def _set_up(self, study: FileStudy, command_context: CommandContext):
         CreateArea(area_name="FR", command_context=command_context, study_version=study.config.version).apply(study)
-        CreateSTStorage(
-            area_id="fr",
-            parameters={"name": "STORAGE_1"},
-            command_context=command_context,
-            study_version=study.config.version,
-        ).apply(study)
+        CreateArea(area_name="be", command_context=command_context, study_version=study.config.version).apply(study)
 
     @pytest.mark.parametrize("empty_study", ["empty_study_870.zip"], indirect=True)
     def test_nominal_case(self, empty_study: FileStudy, command_context: CommandContext):
-        pass
+        self._set_up(empty_study, command_context)
+        # study_version = empty_study.config.version
+        study_path = empty_study.config.study_path
+
+        # Check existing file
+        hydro_ini = study_path / "input" / "hydro" / "hydro.ini"
+        ini_content = read_ini(hydro_ini)
+        assert ini_content == {
+            "inter-daily-breakdown": {"fr": 1, "be": 1},
+            "intra-daily-modulation": {"fr": 24, "be": 24},
+            "inter-monthly-breakdown": {"fr": 1, "be": 1},
+            "initialize reservoir date": {"fr": 0, "be": 0},
+            "leeway low": {"fr": 1, "be": 1},
+            "leeway up": {"fr": 1, "be": 1},
+            "pumping efficiency": {"fr": 1, "be": 1},
+        }
