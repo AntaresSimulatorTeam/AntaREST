37d1a5485ef55d9b1f3cff4c70b70dd1a99a3ea3
belthlemar <martin.belthle@rte-france.com>
2025-03-28 16:54:46 +0100

    add license

--- antarest/study/storage/rawstudy/model/filesystem/config/thematic_trimming.py
+++ antarest/study/storage/rawstudy/model/filesystem/config/thematic_trimming.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2025, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+
+
 from typing import Any, Self, TypeAlias
 
 from antares.study.version import StudyVersion
