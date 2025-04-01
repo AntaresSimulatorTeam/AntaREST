d8909bb4c73c7808812c8cda70900f37536c07b4
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2025-04-01 15:25:30 +0200

    refactor(thermal): improve classes design for thermal clusters (#2430)
    The goal of that work is to propose a new schema for business domain classes,
    starting with thermal clusters.
    
    The main points are:
    
    - separate business models for ThermalCluster themselves, their update, and their creation
      creation and update become plain python functions that can be re-used elsewhere
    - no more version-related classes: however business classes can be validated on demand against
      a given study version
    - separate "model" for serialization to INI file. Serialization and parsing depend on business model,
      not the other way around
    
    A change in serialization too:
    
    - in "file tree config", we don't use anymore the INI-like serialization, but camel case serialization,
      like any other think exposed   in our API
    - in commands, we don't use anymore the INI-like serialization either, which should be specific 
      to the INI format either
    
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- antarest/study/storage/rawstudy/model/filesystem/config/thermal.py
+++ antarest/study/storage/rawstudy/model/filesystem/config/thermal.py
@@ -9,487 +9,87 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-
-from typing import Annotated, Any, Dict, Optional, Type, TypeAlias, cast
+from typing import Any, Optional
 
 from antares.study.version import StudyVersion
-from pydantic import BeforeValidator, Field, TypeAdapter, field_validator
-from pydantic_core.core_schema import ValidationInfo
-from typing_extensions import override
+from pydantic import ConfigDict, Field
 
-from antarest.study.business.enum_ignore_case import EnumIgnoreCase
-from antarest.study.storage.rawstudy.model.filesystem.config.cluster import ClusterProperties
-from antarest.study.storage.rawstudy.model.filesystem.config.identifier import (
-    IgnoreCaseIdentifier,
-    transform_name_to_id,
+from antarest.core.serde import AntaresBaseModel
+from antarest.core.utils.string import to_kebab_case
+from antarest.study.business.model.thermal_cluster_model import (
+    LawOption,
+    LocalTSGenerationBehavior,
+    ThermalCluster,
+    ThermalClusterGroup,
+    ThermalCostGeneration,
+    initialize_thermal_cluster,
+    validate_thermal_cluster_against_version,
 )
-from antarest.study.storage.rawstudy.model.filesystem.config.validation import extract_version, study_version_context
 
 
-class LocalTSGenerationBehavior(EnumIgnoreCase):
+class ThermalClusterFileData(AntaresBaseModel):
     """
-    Options related to time series generation.
-    The option `USE_GLOBAL` is used by default.
-
-    Attributes:
-        USE_GLOBAL: Use the global time series parameters.
-        FORCE_NO_GENERATION: Do not generate time series.
-        FORCE_GENERATION: Force the generation of time series.
+    Thermal cluster data parsed from INI file.
     """
 
-    USE_GLOBAL = "use global"
-    FORCE_NO_GENERATION = "force no generation"
-    FORCE_GENERATION = "force generation"
+    model_config = ConfigDict(alias_generator=to_kebab_case, extra="forbid", populate_by_name=True)
 
-    @override
-    def __repr__(self) -> str:  # pragma: no cover
-        return f"{self.__class__.__name__}.{self.name}"
+    name: str
+    unit_count: Optional[int] = Field(default=None, alias="unitcount")
+    nominal_capacity: Optional[float] = Field(default=None, alias="nominalcapacity")
+    enabled: Optional[bool] = None
+    group: Optional[ThermalClusterGroup] = None
+    gen_ts: Optional[LocalTSGenerationBehavior] = None
+    min_stable_power: Optional[float] = None
+    min_up_time: Optional[int] = None
+    min_down_time: Optional[int] = None
+    must_run: Optional[bool] = None
+    spinning: Optional[float] = None
+    volatility_forced: Optional[float] = Field(default=None, alias="volatility.forced")
+    volatility_planned: Optional[float] = Field(default=None, alias="volatility.planned")
+    law_forced: Optional[LawOption] = Field(default=None, alias="law.forced")
+    law_planned: Optional[LawOption] = Field(default=None, alias="law.planned")
+    marginal_cost: Optional[float] = None
+    spread_cost: Optional[float] = None
+    fixed_cost: Optional[float] = None
+    startup_cost: Optional[float] = None
+    market_bid_cost: Optional[float] = None
+    co2: Optional[float] = None
 
+    # Added in 8.6
+    nh3: Optional[float] = None
+    so2: Optional[float] = None
+    nox: Optional[float] = None
+    pm2_5: Optional[float] = Field(default=None, alias="pm2_5")
+    pm5: Optional[float] = None
+    pm10: Optional[float] = None
+    nmvoc: Optional[float] = None
+    op1: Optional[float] = None
+    op2: Optional[float] = None
+    op3: Optional[float] = None
+    op4: Optional[float] = None
+    op5: Optional[float] = None
 
-class LawOption(EnumIgnoreCase):
-    """
-    Law options used for series generation.
-    The UNIFORM `law` is used by default.
-    """
+    # Added in 8.7
+    cost_generation: Optional[ThermalCostGeneration] = Field(default=None, alias="costgeneration")
+    efficiency: Optional[float] = None
+    variable_o_m_cost: Optional[float] = Field(default=None, alias="variableomcost")
 
-    UNIFORM = "uniform"
-    GEOMETRIC = "geometric"
-
-    @override
-    def __repr__(self) -> str:  # pragma: no cover
-        return f"{self.__class__.__name__}.{self.name}"
-
-
-class ThermalClusterGroup(EnumIgnoreCase):
-    """
-    Thermal cluster groups.
-    The group `OTHER1` is used by default.
-    """
-
-    NUCLEAR = "nuclear"
-    LIGNITE = "lignite"
-    HARD_COAL = "hard coal"
-    GAS = "gas"
-    OIL = "oil"
-    MIXED_FUEL = "mixed fuel"
-    OTHER1 = "other 1"
-    OTHER2 = "other 2"
-    OTHER3 = "other 3"
-    OTHER4 = "other 4"
-
-    @override
-    def __repr__(self) -> str:  # pragma: no cover
-        return f"{self.__class__.__name__}.{self.name}"
+    def to_model(self) -> ThermalCluster:
+        return ThermalCluster.model_validate(self.model_dump(exclude_none=True))
 
     @classmethod
-    @override
-    def _missing_(cls, value: object) -> Optional["ThermalClusterGroup"]:
-        """
-        Retrieves the default group or the matched group when an unknown value is encountered.
-        """
-        if isinstance(value, str):
-            # Check if any group value matches the input value ignoring case sensitivity.
-            # noinspection PyUnresolvedReferences
-            if any(value.lower() == group.value for group in cls):
-                return cast(ThermalClusterGroup, super()._missing_(value))
-            return cls.OTHER1
-        return cast(Optional["ThermalClusterGroup"], super()._missing_(value))
+    def from_model(cls, cluster: ThermalCluster) -> "ThermalClusterFileData":
+        return cls.model_validate(cluster.model_dump(exclude={"id"}))
 
 
-class ThermalCostGeneration(EnumIgnoreCase):
-    """
-    Specifies how to generate thermal cluster cost.
-    The value `SetManually` is used by default.
-    """
-
-    SET_MANUALLY = "SetManually"
-    USE_COST_TIME_SERIES = "useCostTimeseries"
+def parse_thermal_cluster(study_version: StudyVersion, data: Any) -> ThermalCluster:
+    cluster = ThermalClusterFileData.model_validate(data).to_model()
+    validate_thermal_cluster_against_version(study_version, cluster)
+    initialize_thermal_cluster(cluster, study_version)
+    return cluster
 
 
-class ThermalProperties(ClusterProperties):
-    """
-    Thermal cluster configuration model.
-    This model describes the configuration parameters for a thermal cluster.
-    """
-
-    def get_id(self) -> str:
-        return transform_name_to_id(self.name, lower=False)
-
-    group: ThermalClusterGroup = Field(
-        default=ThermalClusterGroup.OTHER1,
-        description="Thermal Cluster Group",
-        title="Thermal Cluster Group",
-    )
-
-    gen_ts: LocalTSGenerationBehavior = Field(
-        default=LocalTSGenerationBehavior.USE_GLOBAL,
-        description="Time Series Generation Option",
-        alias="gen-ts",
-        title="Time Series Generation",
-    )
-    min_stable_power: float = Field(
-        default=0.0,
-        description="Min. Stable Power (MW)",
-        alias="min-stable-power",
-        title="Min. Stable Power",
-    )
-    min_up_time: int = Field(
-        default=1,
-        ge=1,
-        le=168,
-        description="Min. Up time (h)",
-        alias="min-up-time",
-        title="Min. Up Time",
-    )
-    min_down_time: int = Field(
-        default=1,
-        ge=1,
-        le=168,
-        description="Min. Down time (h)",
-        alias="min-down-time",
-        title="Min. Down Time",
-    )
-    must_run: bool = Field(
-        default=False,
-        description="Must run flag",
-        alias="must-run",
-        title="Must Run",
-    )
-    spinning: float = Field(
-        default=0.0,
-        ge=0,
-        le=100,
-        description="Spinning (%)",
-        title="Spinning",
-    )
-    volatility_forced: float = Field(
-        default=0.0,
-        ge=0,
-        le=1,
-        description="Forced Volatility",
-        alias="volatility.forced",
-        title="Forced Volatility",
-    )
-    volatility_planned: float = Field(
-        default=0.0,
-        ge=0,
-        le=1,
-        description="Planned volatility",
-        alias="volatility.planned",
-        title="Planned Volatility",
-    )
-    law_forced: LawOption = Field(
-        default=LawOption.UNIFORM,
-        description="Forced Law (ts-generator)",
-        alias="law.forced",
-        title="Forced Law",
-    )
-    law_planned: LawOption = Field(
-        default=LawOption.UNIFORM,
-        description="Planned Law (ts-generator)",
-        alias="law.planned",
-        title="Planned Law",
-    )
-    marginal_cost: float = Field(
-        default=0.0,
-        ge=0,
-        description="Marginal cost (euros/MWh)",
-        alias="marginal-cost",
-        title="Marginal Cost",
-    )
-    spread_cost: float = Field(
-        default=0.0,
-        ge=0,
-        description="Spread (euros/MWh)",
-        alias="spread-cost",
-        title="Spread Cost",
-    )
-    fixed_cost: float = Field(
-        default=0.0,
-        ge=0,
-        description="Fixed cost (euros/hour)",
-        alias="fixed-cost",
-        title="Fixed Cost",
-    )
-    startup_cost: float = Field(
-        default=0.0,
-        ge=0,
-        description="Startup cost (euros/startup)",
-        alias="startup-cost",
-        title="Startup Cost",
-    )
-    market_bid_cost: float = Field(
-        default=0.0,
-        ge=0,
-        description="Market bid cost (euros/MWh)",
-        alias="market-bid-cost",
-        title="Market Bid Cost",
-    )
-    co2: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of CO2 (t/MWh)",
-        title="Emission rate of CO2",
-    )
-
-    @field_validator("min_down_time", "min_up_time", mode="before")
-    def _validate_min_up_and_down_time(cls, v: int) -> int:
-        return 1 if v < 1 else 168 if v > 168 else v
-
-
-class Thermal860Properties(ThermalProperties):
-    """
-    Thermal cluster configuration model for 860 study.
-    """
-
-    nh3: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of NH3 (t/MWh)",
-        title="Emission rate of NH3",
-    )
-    so2: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of SO2 (t/MWh)",
-        title="Emission rate of SO2",
-    )
-    nox: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of NOX (t/MWh)",
-        title="Emission rate of NOX",
-    )
-    pm2_5: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of PM 2.5 (t/MWh)",
-        title="Emission rate of PM 2.5",
-        alias="pm2_5",
-    )
-    pm5: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of PM 5 (t/MWh)",
-        title="Emission rate of PM 5",
-    )
-    pm10: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of PM 10 (t/MWh)",
-        title="Emission rate of PM 10",
-    )
-    nmvoc: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of NMVOC (t/MWh)",
-        title="Emission rate of NMVOC",
-    )
-    op1: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of pollutant 1 (t/MWh)",
-        title="Emission rate of pollutant 1",
-    )
-    op2: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of pollutant 2 (t/MWh)",
-        title="Emission rate of pollutant 2",
-    )
-    op3: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of pollutant 3 (t/MWh)",
-        title="Emission rate of pollutant 3",
-    )
-    op4: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of pollutant 4 (t/MWh)",
-        title="Emission rate of pollutant 4",
-    )
-    op5: float = Field(
-        default=0.0,
-        ge=0,
-        description="Emission rate of pollutant 5 (t/MWh)",
-        title="Emission rate of pollutant 5",
-    )
-
-
-# noinspection SpellCheckingInspection
-class Thermal870Properties(Thermal860Properties):
-    """
-    Thermal cluster configuration model for study in version 8.7 or above.
-    """
-
-    cost_generation: ThermalCostGeneration = Field(
-        default=ThermalCostGeneration.SET_MANUALLY,
-        alias="costgeneration",
-        description="Cost generation option",
-        title="Cost Generation",
-    )
-    efficiency: float = Field(
-        default=100.0,
-        ge=0,
-        le=100,
-        description="Efficiency (%)",
-        title="Efficiency",
-    )
-    # Even if `variableomcost` is a cost it could be negative.
-    variable_o_m_cost: float = Field(
-        default=0.0,
-        description="Operating and Maintenance Cost (€/MWh)",
-        alias="variableomcost",
-        title="Variable O&M Cost",
-    )
-
-
-class ThermalConfig(ThermalProperties, IgnoreCaseIdentifier):
-    """
-    Thermal properties with section ID.
-
-    Usage:
-
-    >>> from antarest.study.storage.rawstudy.model.filesystem.config.thermal import ThermalConfig
-
-    >>> cl = ThermalConfig(name="cluster 01!", group="Nuclear", co2=123)
-    >>> cl.id
-    'cluster 01'
-    >>> cl.group == ThermalClusterGroup.NUCLEAR
-    True
-    >>> cl.co2
-    123.0
-    >>> cl.nh3
-    Traceback (most recent call last):
-      ...
-    AttributeError: 'ThermalConfig' object has no attribute 'nh3'"""
-
-
-class Thermal860Config(Thermal860Properties, IgnoreCaseIdentifier):
-    """
-    Thermal properties for study in version 860
-
-    Usage:
-
-    >>> from antarest.study.storage.rawstudy.model.filesystem.config.thermal import Thermal860Config
-
-    >>> cl = Thermal860Config(name="cluster 01!", group="Nuclear", co2=123, nh3=456)
-    >>> cl.id
-    'cluster 01'
-    >>> cl.group == ThermalClusterGroup.NUCLEAR
-    True
-    >>> cl.co2
-    123.0
-    >>> cl.nh3
-    456.0
-    >>> cl.op1
-    0.0
-    """
-
-
-class Thermal870Config(Thermal870Properties, IgnoreCaseIdentifier):
-    """
-    Thermal properties for study in version 8.7 or above.
-
-    Usage:
-
-    >>> from antarest.study.storage.rawstudy.model.filesystem.config.thermal import Thermal870Config
-
-    >>> cl = Thermal870Config(name="cluster 01!", group="Nuclear", co2=123, nh3=456, efficiency=97)
-    >>> cl.id
-    'cluster 01'
-    >>> cl.group == ThermalClusterGroup.NUCLEAR
-    True
-    >>> cl.co2
-    123.0
-    >>> cl.nh3
-    456.0
-    >>> cl.op1
-    0.0
-    >>> cl.efficiency
-    97.0
-    >>> cl.variable_o_m_cost
-    0.0
-    >>> cl.cost_generation == ThermalCostGeneration.SET_MANUALLY
-    True
-    """
-
-
-def _validate_thermal_config(data: Any, info: ValidationInfo) -> Any:
-    """
-    When instantiating thermal cluster data from a dictionary, we need the study version
-    to choose which version of the config we need to create.
-    """
-    if not isinstance(data, dict):
-        return data
-    return get_thermal_config_cls(extract_version(info)).model_validate(data)
-
-
-def _validate_thermal_properties(data: Any, info: ValidationInfo) -> Any:
-    """
-    When instantiating thermal cluster data from a dictionary, we need the study version
-    to choose which version of the config we need to create.
-    """
-    if not isinstance(data, dict):
-        return data
-    study_version = extract_version(info)
-    if study_version >= 870:
-        return Thermal870Properties.model_validate(data)
-    elif study_version == 860:
-        return Thermal860Properties.model_validate(data)
-    else:
-        return ThermalProperties.model_validate(data)
-
-
-ThermalConfigType: TypeAlias = Annotated[
-    Thermal870Config | Thermal860Config | ThermalConfig, BeforeValidator(_validate_thermal_config)
-]
-ThermalPropertiesType: TypeAlias = Annotated[
-    Thermal870Properties | Thermal860Properties | ThermalProperties, BeforeValidator(_validate_thermal_properties)
-]
-
-_CONFIG_ADAPTER: TypeAdapter[ThermalConfigType] = TypeAdapter(ThermalConfigType)
-_PROPERTIES_ADAPTER: TypeAdapter[ThermalPropertiesType] = TypeAdapter(ThermalPropertiesType)
-
-
-def get_thermal_config_cls(study_version: StudyVersion) -> Type[ThermalConfigType]:
-    """
-    Retrieves the thermal configuration class based on the study version.
-
-    Args:
-        study_version: The version of the study.
-
-    Returns:
-        The thermal configuration class.
-    """
-    if study_version >= 870:
-        return Thermal870Config
-    elif study_version == 860:
-        return Thermal860Config
-    else:
-        return ThermalConfig
-
-
-def create_thermal_properties(study_version: StudyVersion, data: Dict[str, Any]) -> ThermalPropertiesType:
-    """
-    Factory method to create thermal properties.
-    """
-    return _PROPERTIES_ADAPTER.validate_strings(data, context=study_version_context(study_version))
-
-
-def create_thermal_config(study_version: StudyVersion, **kwargs: Any) -> ThermalConfigType:
-    """
-    Factory method to create a thermal configuration model.
-
-    Args:
-        study_version: The version of the study.
-        **kwargs: The properties to be used to initialize the model.
-
-    Returns:
-        The thermal configuration model.
-
-    Raises:
-        ValueError: If the study version is not supported.
-    """
-    return _CONFIG_ADAPTER.validate_strings(kwargs, context=study_version_context(study_version))
+def serialize_thermal_cluster(study_version: StudyVersion, cluster: ThermalCluster) -> dict[str, Any]:
+    validate_thermal_cluster_against_version(study_version, cluster)
+    return ThermalClusterFileData.from_model(cluster).model_dump(by_alias=True, exclude_none=True)
