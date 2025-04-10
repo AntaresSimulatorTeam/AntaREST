baac034cdba2c868cf190f493387afc23316ea0f
Maurane GLAUDE <mauranne.glaude_externe@rte-france.com>
2025-04-10 10:30:14 +0200

    fix(aggregator_management): adapt to new aggregator_manager object, rename functions

--- tests/study/business/test_aggregator_manager.py
+++ tests/study/business/test_aggregator_manager.py
@@ -8,8 +8,8 @@ from antarest.study.business.aggregator_management import AggregatorManager, MCA
 from antarest.study.storage.rawstudy.model.filesystem.matrix.matrix import MatrixFrequency
 
 
-@pytest.fixture(name="set_up_studies")
-def studies_to_import_fixture(tmp_path: Path) -> Path:
+@pytest.fixture(name="get_output_path")
+def get_output_path_fixture(tmp_path: Path) -> Path:
     # Prepare the directories used by the repos
     assets_dir = Path(__file__).parent.joinpath("areas/assets/aggregator_manager")
 
@@ -18,11 +18,11 @@ def studies_to_import_fixture(tmp_path: Path) -> Path:
     with zipfile.ZipFile(study_path) as zip_output:
         zip_output.extractall(path=tmp_path)
 
-    return tmp_path.joinpath("viz_2")
+    return tmp_path.joinpath("viz_2/output")
 
 
 @pytest.mark.unit_test
-def test_storage_different_max_size_value(tmp_path: Path, set_up_studies: Path):
+def test_storage_different_max_size_value(tmp_path: Path, get_output_path: Path):
     """
     SetUp: This test unzip the viz_2 study that contains a great number of mc values located
     in the example directory.
@@ -32,12 +32,11 @@ def test_storage_different_max_size_value(tmp_path: Path, set_up_studies: Path):
     Test: The call on aggregate_output_data of the first aggregator management should pass
     and the second should fail.
     """
-    output_id = set_up_studies.joinpath("20230912-1354eco").name
+    output_path = get_output_path.joinpath("20230912-1354eco")
 
     # aggregation_results_max_size equals to 400Mo
     aggregator_manager_high_bound = AggregatorManager(
-        study_path=set_up_studies,
-        output_id=output_id,
+        output_path=output_path,
         query_file=MCAllAreasQueryFile.VALUES,
         frequency=MatrixFrequency.HOURLY,
         ids_to_consider=[],
@@ -47,8 +46,7 @@ def test_storage_different_max_size_value(tmp_path: Path, set_up_studies: Path):
 
     # aggregation_results_max_size equals to 200Mo
     aggregator_manager_low_bound = AggregatorManager(
-        study_path=set_up_studies,
-        output_id=output_id,
+        output_path=output_path,
         query_file=MCAllAreasQueryFile.VALUES,
         frequency=MatrixFrequency.HOURLY,
         ids_to_consider=[],
