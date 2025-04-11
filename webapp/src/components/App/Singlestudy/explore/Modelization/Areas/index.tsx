ef56d13d3640d650e2ff7aa3e6d237fd74332849
Samir Kamal <1954121+skamril@users.noreply.github.com>
2025-04-11 11:59:02 +0100

    fix(ui-clusters): matrice error when area change

--- webapp/src/components/App/Singlestudy/explore/Modelization/Areas/index.tsx
+++ webapp/src/components/App/Singlestudy/explore/Modelization/Areas/index.tsx
@@ -12,19 +12,19 @@
  * This file is part of the Antares project.
  */
 
-import { useOutletContext } from "react-router";
+import { useLocation, useNavigate, useOutletContext } from "react-router";
+import { setCurrentArea } from "../../../../../../redux/ducks/studySyntheses";
+import useAppDispatch from "../../../../../../redux/hooks/useAppDispatch";
+import useAppSelector from "../../../../../../redux/hooks/useAppSelector";
+import useStudySynthesis from "../../../../../../redux/hooks/useStudySynthesis";
+import { getCurrentArea, getStudySynthesis } from "../../../../../../redux/selectors";
 import type { StudyMetadata } from "../../../../../../types/types";
+import SplitView from "../../../../../common/SplitView";
 import EmptyView from "../../../../../common/page/EmptyView";
+import ViewWrapper from "../../../../../common/page/ViewWrapper";
+import UsePromiseCond from "../../../../../common/utils/UsePromiseCond";
 import AreaPropsView from "./AreaPropsView";
 import AreasTab from "./AreasTab";
-import useStudySynthesis from "../../../../../../redux/hooks/useStudySynthesis";
-import { getStudySynthesis, getCurrentArea } from "../../../../../../redux/selectors";
-import useAppDispatch from "../../../../../../redux/hooks/useAppDispatch";
-import { setCurrentArea } from "../../../../../../redux/ducks/studySyntheses";
-import useAppSelector from "../../../../../../redux/hooks/useAppSelector";
-import UsePromiseCond from "../../../../../common/utils/UsePromiseCond";
-import SplitView from "../../../../../common/SplitView";
-import ViewWrapper from "../../../../../common/page/ViewWrapper";
 
 function Areas() {
   const { study } = useOutletContext<{ study: StudyMetadata }>();
@@ -34,6 +34,32 @@ function Areas() {
   });
   const currentArea = useAppSelector(getCurrentArea);
   const dispatch = useAppDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  ////////////////////////////////////////////////////////////////
+  // Utils
+  ////////////////////////////////////////////////////////////////
+
+  const updateUrl = (newAreaId: string) => {
+    let pathSegments = location.pathname.split("/");
+    const areaIndex = pathSegments.findIndex((segment) => segment === "area");
+    const areaIdIndex = areaIndex + 1;
+
+    if (areaIndex !== -1 && newAreaId !== pathSegments[areaIdIndex]) {
+      const nextPageIndex = areaIdIndex + 1;
+
+      // Set the new area ID
+      pathSegments[areaIdIndex] = newAreaId;
+
+      // If the we are in a cluster page, we don't keep the cluster ID
+      if (["thermal", "storages", "renewables"].includes(pathSegments[nextPageIndex])) {
+        pathSegments = pathSegments.slice(0, nextPageIndex + 1);
+      }
+
+      navigate(pathSegments.join("/"), { replace: true });
+    }
+  };
 
   ////////////////////////////////////////////////////////////////
   // Event Handlers
@@ -41,6 +67,7 @@ function Areas() {
 
   const handleAreaClick = (areaId: string): void => {
     dispatch(setCurrentArea(areaId));
+    updateUrl(areaId);
   };
 
   ////////////////////////////////////////////////////////////////
