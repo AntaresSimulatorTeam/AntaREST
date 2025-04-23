83d17480ea5a11e330285877163feba2ba7da8aa
belthlemar <martin.belthle@rte-france.com>
2025-04-23 14:35:44 +0200

    continue

--- tests/storage/integration/test_STA_mini.py
+++ tests/storage/integration/test_STA_mini.py
@@ -25,9 +25,8 @@ from fastapi import FastAPI
 from starlette.testclient import TestClient
 
 from antarest.core.application import create_app_ctxt
-from antarest.core.jwt import DEFAULT_ADMIN_USER, JWTGroup, JWTUser
+from antarest.core.jwt import JWTGroup, JWTUser
 from antarest.core.model import JSON
-from antarest.core.requests import RequestParameters
 from antarest.core.roles import RoleType
 from antarest.matrixstore.service import MatrixService
 from antarest.study.main import build_study_service
@@ -78,8 +77,7 @@ def assert_with_errors(
 ) -> None:
     url = url[len("/v1/studies/") :]
     uuid, url = url.split("/raw?path=")
-    params = RequestParameters(user=ADMIN)
-    output = storage_service.get(uuid=uuid, url=url, depth=3, formatted=formatted, params=params)
+    output = storage_service.get(uuid=uuid, url=url, depth=3, formatted=formatted)
     assert_study(
         output,
         expected_output,
@@ -492,9 +490,8 @@ def test_sta_mini_copy(storage_service) -> None:
     assert result.status_code == HTTPStatus.CREATED.value
     uuid = result.json()
 
-    parameters = RequestParameters(user=ADMIN)
-    data_source = storage_service.get(source_study_name, "/", -1, True, parameters)
-    data_destination = storage_service.get(uuid, "/", -1, True, parameters)
+    data_source = storage_service.get(source_study_name, "/", -1, True)
+    data_destination = storage_service.get(uuid, "/", -1, True)
 
     link_url_source = data_source["input"]["links"]["de"]["fr"]
     assert "matrixfile://fr.txt" == link_url_source
@@ -564,8 +561,7 @@ def notest_sta_mini_with_wrong_output_folder(storage_service: StudyService, sta_
 
 @pytest.mark.integration_test
 def test_sta_mini_import(tmp_path: Path, storage_service) -> None:
-    params = RequestParameters(user=ADMIN)
-    path_study = storage_service.get_study_path(UUID, params)
+    path_study = storage_service.get_study_path(UUID)
     sta_mini_zip_filepath = shutil.make_archive(tmp_path, "zip", path_study)
     sta_mini_zip_path = Path(sta_mini_zip_filepath)
 
@@ -579,9 +575,7 @@ def test_sta_mini_import(tmp_path: Path, storage_service) -> None:
 
 @pytest.mark.integration_test
 def test_sta_mini_import_output(tmp_path: Path, storage_service) -> None:
-    params = RequestParameters(user=ADMIN)
-
-    path_study_output = storage_service.get_study_path(UUID, params) / "output" / "20201014-1422eco-hello"
+    path_study_output = storage_service.get_study_path(UUID) / "output" / "20201014-1422eco-hello"
     sta_mini_output_zip_filepath = shutil.make_archive(tmp_path, "zip", path_study_output)
 
     shutil.rmtree(path_study_output)
@@ -630,11 +624,7 @@ def test_sta_mini_filter(storage_service, url: str, expected_output: dict):
 
 
 def test_sta_mini_output_variables_nominal_case(output_service):
-    variables = output_service.output_variables_information(
-        UUID,
-        "20201014-1422eco-hello",
-        RequestParameters(user=DEFAULT_ADMIN_USER),
-    )
+    variables = output_service.output_variables_information(UUID, "20201014-1422eco-hello")
     assert variables["area"] == [
         "OV. COST",
         "OP. COST",
@@ -688,22 +678,14 @@ def test_sta_mini_output_variables_nominal_case(output_service):
 
 def test_sta_mini_output_variables_no_mc_ind(output_service):
     with pytest.raises(BadOutputFormat, match=r"Not a year by year simulation"):
-        output_service.output_variables_information(
-            UUID,
-            "20201014-1427eco",
-            RequestParameters(user=DEFAULT_ADMIN_USER),
-        )
+        output_service.output_variables_information(UUID, "20201014-1427eco")
 
 
 def test_sta_mini_output_variables_no_links(output_service):
     study_path = Path(output_service._study_service.get_study(UUID).path)
     links_folder = study_path / "output" / "20201014-1422eco-hello" / "economy" / "mc-ind" / "00001" / "links"
     shutil.rmtree(links_folder)
-    variables = output_service.output_variables_information(
-        UUID,
-        "20201014-1422eco-hello",
-        RequestParameters(user=DEFAULT_ADMIN_USER),
-    )
+    variables = output_service.output_variables_information(UUID, "20201014-1422eco-hello")
     # When there's no links folder, asserts the endpoint doesn't fail and simply return an empty list
     assert variables["link"] == []
 
@@ -714,10 +696,6 @@ def test_sta_mini_output_variables_no_areas(output_service):
     areas_mc_all_folder = study_path / "output" / "20201014-1422eco-hello" / "economy" / "mc-all" / "areas"
     shutil.rmtree(areas_mc_ind_folder)
     shutil.rmtree(areas_mc_all_folder)
-    variables = output_service.output_variables_information(
-        UUID,
-        "20201014-1422eco-hello",
-        RequestParameters(user=DEFAULT_ADMIN_USER),
-    )
+    variables = output_service.output_variables_information(UUID, "20201014-1422eco-hello")
     # When there's no areas folder, asserts the endpoint doesn't fail and simply return an empty list
     assert variables["area"] == []
