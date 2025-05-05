a6fd6280d1487a3f17a1f3ed1e18c2685ab82b73
Anis SMAIL <anis.smail_externe@rte-france.com>
2025-05-06 01:11:48 +0200

    remove commented command

--- antarest/study/storage/utils.py
+++ antarest/study/storage/utils.py
@@ -517,6 +517,3 @@ def is_non_study_folder(path: Path, filter_in: List[str], filter_out: List[str])
     if should_ignore_folder_for_scan(path, filter_in, filter_out):
         return False
     return True
-
-
-# dzdo mount -t cifs smb://antrsprdsa008vm/per ~/my_smb_mount -o username=smailani,vers=3.0
