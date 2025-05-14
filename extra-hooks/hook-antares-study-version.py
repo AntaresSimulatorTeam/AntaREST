da69ebe4a148ca3dd10b548d939cacba286c2540
belthlemar <martin.belthle@rte-france.com>
2025-05-14 18:38:31 +0200

    remove potential useless hook

--- extra-hooks/hook-antares-study-version.py
+++
@@ -1,4 +0,0 @@
-from PyInstaller.utils.hooks import collect_submodules
-
-# We need to import the zip files to create empty studies inside the Desktop app
-hiddenimports = collect_submodules("antares.study.version")
