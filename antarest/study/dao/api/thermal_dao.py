11fd2ce8c79ace2570466d8ec4e91146cce25185
belthlemar <martin.belthle@rte-france.com>
2025-05-22 10:25:14 +0200

    wip

---
+++ antarest/study/dao/api/thermal_dao.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2025, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+
+from abc import ABC, abstractmethod
+from typing import Sequence
+
+from antarest.study.business.model.link_model import Link
+from antarest.study.business.model.thermal_cluster_model import ThermalCluster
+
+
+class ReadOnlyThermalDao(ABC):
+    @abstractmethod
+    def get_thermals(self) -> Sequence[ThermalCluster]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_thermal(self, area_id: str, cluster_id: str) -> ThermalCluster:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def thermal_exists(self, area_id: str, thermal_id: str) -> bool:
+        raise NotImplementedError()
+
+
+class ThermalDao(ReadOnlyThermalDao):
+    @abstractmethod
+    def save_thermal(self, thermal: ThermalCluster) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def save_link_indirect_capacities(self, area_from: str, area_to: str, series_id: str) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def save_link_direct_capacities(self, area_from: str, area_to: str, series_id: str) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def save_thermal_series(self, area_from: str, area_to: str, series_id: str) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def delete_thermal(self, thermal: ThermalCluster) -> None:
+        raise NotImplementedError()
