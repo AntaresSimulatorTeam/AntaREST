2bc3dbb14559f80f1e3628b5efd38dee4368aee8
Anis SMAIL <anis.smail_externe@rte-france.com>
2025-05-28 09:52:19 +0200

    fix typos

--- antarest/study/storage/utils.py
+++ antarest/study/storage/utils.py
@@ -420,7 +420,7 @@ def is_folder_safe(workspace: WorkspaceConfig, folder: str) -> bool:
     requested_path = workspace.path / folder
     requested_path = requested_path.resolve()
     safe_dir = workspace.path.resolve()
-    # check wether the requested path is a subdirectory of the workspace
+    # check whether the requested path is a subdirectory of the workspace
     return requested_path.is_relative_to(safe_dir)
 
 
@@ -492,7 +492,7 @@ def has_non_study_folder(
             if is_non_study_folder(sub_path, filter_in, filter_out) and show:
                 return True
         except (PermissionError, OSError):
-            logger.warning("tried to run is_non_study_folder on {sub_path} but no permission")
+            logger.warning(f"tried to run is_non_study_folder on {sub_path} but no permission")
     return False
 
 
