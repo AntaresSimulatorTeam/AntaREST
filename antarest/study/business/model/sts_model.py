8d6f03c1ef7bcf3936abd191d8056baf50e83fe3
belthlemar <martin.belthle@rte-france.com>
2025-06-02 13:06:08 +0200

    ongoing work

--- antarest/study/business/model/sts_model.py
+++ antarest/study/business/model/sts_model.py
@@ -9,72 +9,201 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-from typing import Optional
+from typing import Annotated, Any, Optional, TypeAlias
 
 from antares.study.version import StudyVersion
-from pydantic import field_validator
+from pydantic import ConfigDict, Field, model_validator
+from pydantic.alias_generators import to_camel
 
-from antarest.core.model import LowerCaseId
-from antarest.study.business.all_optional_meta import all_optional_model, camel_case_model
-from antarest.study.model import STUDY_VERSION_8_8, STUDY_VERSION_9_2
-from antarest.study.storage.rawstudy.model.filesystem.config.st_storage import (
-    STStorage920Config,
-    STStorage920Properties,
-    STStoragePropertiesType,
-    create_st_storage_properties,
-)
+from antarest.core.exceptions import InvalidFieldForVersionError
+from antarest.core.serde import AntaresBaseModel
+from antarest.study.business.enum_ignore_case import EnumIgnoreCase
+from antarest.study.model import STUDY_VERSION_8_6
+from antarest.study.storage.rawstudy.model.filesystem.config.identifier import transform_name_to_id
+from antarest.study.storage.rawstudy.model.filesystem.config.validation import ItemName
 
 
-@all_optional_model
-@camel_case_model
-class STStorageUpdate(STStorage920Properties):
+class STStorageGroup(EnumIgnoreCase):
     """
-    Model representing the form used to EDIT an existing short-term storage.
-    """
+    This class defines the specific energy storage systems.
 
-    class Config:
-        populate_by_name = True
+    Enum values:
 
-    def validate_model_against_version(self, study_version: StudyVersion) -> None:
-        if study_version < STUDY_VERSION_9_2:
-            wrong_fields = []
-            for field in ["efficiency_withdrawal", "penalize_variation_withdrawal", "penalize_variation_injection"]:
-                if getattr(self, field) is not None:
-                    wrong_fields.append(field)
-            if wrong_fields:
-                raise ValueError(
-                    f"You provided v9.2 field(s): `{', '.join(wrong_fields)}` but your study is in version {study_version}"
-                )
-
-        if study_version < STUDY_VERSION_8_8 and self.enabled is not None:
-            raise ValueError(f"You provided a v8.8 field: `enabled` but your study is in version {study_version}")
-
-
-STStorageUpdates = dict[LowerCaseId, dict[LowerCaseId, STStorageUpdate]]
-
-
-class STStorageCreation(STStorageUpdate):
-    """
-    Model representing the form used to CREATE a new short-term storage.
+        - PSP_OPEN: Represents an open pumped storage plant.
+        - PSP_CLOSED: Represents a closed pumped storage plant.
+        - PONDAGE: Represents a pondage storage system (reservoir storage system).
+        - BATTERY: Represents a battery storage system.
+        - OTHER1...OTHER5: Represents other energy storage systems.
     """
 
-    # noinspection Pydantic
-    @field_validator("name", mode="before")
+    PSP_OPEN = "psp_open"
+    PSP_CLOSED = "psp_closed"
+    PONDAGE = "pondage"
+    BATTERY = "battery"
+    OTHER1 = "other1"
+    OTHER2 = "other2"
+    OTHER3 = "other3"
+    OTHER4 = "other4"
+    OTHER5 = "other5"
+
+
+# Validation helpers
+Capacity: TypeAlias = Annotated[float, Field(ge=0)]
+Efficiency: TypeAlias = Annotated[float, Field(ge=0, le=1)]
+InitialLevel: TypeAlias = Annotated[float, Field(ge=0, le=1)]
+
+
+class STStorage(AntaresBaseModel):
+    """
+    Short-term storage model.
+    """
+
+    model_config = ConfigDict(alias_generator=to_camel, extra="forbid", populate_by_name=True)
+
+    name: ItemName
+
+    @model_validator(mode="before")
     @classmethod
-    def validate_name(cls, name: Optional[str]) -> str:
-        """
-        Validator to check if the name is not empty.
-        """
-        if not name:
-            raise ValueError("'name' must not be empty")
-        return name
+    def set_id(cls, data: Any) -> Any:
+        if isinstance(data, dict) and "id" not in data and "name" in data:
+            data["id"] = transform_name_to_id(data["name"])
+        return data
 
-    def to_properties(self, version: StudyVersion) -> STStoragePropertiesType:
-        return create_st_storage_properties(study_version=version, data=self.model_dump(mode="json", exclude_none=True))
+    injection_nominal_capacity: Capacity = 0
+    withdrawal_nominal_capacity: Capacity = 0
+    reservoir_capacity: Capacity = 0
+    efficiency: Efficiency = 1
+    initial_level: InitialLevel = 0.5
+    initial_level_optim: bool = False
+
+    # Added in 8.8
+    enabled: Optional[bool] = None
+
+    # Added in 9.2
+    group: Optional[str] = None
+    efficiency_withdrawal: Optional[Efficiency] = None
+    penalize_variation_injection: Optional[bool] = None
+    penalize_variation_withdrawal: Optional[bool] = None
 
 
-@camel_case_model
-class STStorageOutput(STStorage920Config):
+class STStorageCreation(AntaresBaseModel):
     """
-    Model representing the form used to display the details of a short-term storage entry.
+    Represents a creation request for a short-term storage.
+
+    Most fields are optional: at creation time, default values of the short-term storage
+    model will be used.
     """
+
+    model_config = ConfigDict(alias_generator=to_camel, extra="forbid", populate_by_name=True)
+
+    name: ItemName
+
+    injection_nominal_capacity: Optional[Capacity] = None
+    withdrawal_nominal_capacity: Optional[Capacity] = None
+    reservoir_capacity: Optional[Capacity] = None
+    efficiency: Optional[Efficiency] = None
+    initial_level: Optional[InitialLevel] = None
+    initial_level_optim: Optional[bool] = None
+    enabled: Optional[bool] = None
+    group: Optional[str] = None
+    efficiency_withdrawal: Optional[Efficiency] = None
+    penalize_variation_injection: Optional[bool] = None
+    penalize_variation_withdrawal: Optional[bool] = None
+
+
+class STStorageUpdate(AntaresBaseModel):
+    """
+    Represents an update of a short-term storage.
+
+    Only not-None fields will be used to update the short-term storage.
+    """
+
+    model_config = ConfigDict(alias_generator=to_camel, extra="forbid", populate_by_name=True)
+
+    @model_validator(mode="before")
+    @classmethod
+    def _ignore_name(cls, data: Any) -> Any:
+        """
+        Renaming is not currently supported, but name needs to be accepted
+        for backwards compatibility. We can restore that property when
+        proper renaming is implemented.
+        """
+        if isinstance(data, dict) and "name" in data:
+            del data["name"]
+        return data
+
+    injection_nominal_capacity: Optional[Capacity] = None
+    withdrawal_nominal_capacity: Optional[Capacity] = None
+    reservoir_capacity: Optional[Capacity] = None
+    efficiency: Optional[Efficiency] = None
+    initial_level: Optional[InitialLevel] = None
+    initial_level_optim: Optional[bool] = None
+    enabled: Optional[bool] = None
+    group: Optional[str] = None
+    efficiency_withdrawal: Optional[Efficiency] = None
+    penalize_variation_injection: Optional[bool] = None
+    penalize_variation_withdrawal: Optional[bool] = None
+
+
+
+def _check_min_version(data: Any, field: str, version: StudyVersion) -> None:
+    if getattr(data, field) is not None:
+        raise InvalidFieldForVersionError(f"Field {field} is not a valid field for study version {version}")
+
+
+def validate_st_storage_against_version(
+    version: StudyVersion,
+    storage_data: STStorage | STStorageCreation | STStorageUpdate,
+) -> None:
+    """
+    Validates input short-term storage data against the provided study versions
+
+    Will raise an InvalidFieldForVersionError if a field is not valid for the given study version.
+    """
+    if version < STUDY_VERSION_8_6:
+        raise InvalidFieldForVersionError(f"Short-term storages only exist since v8.8 and your study is in {version}")
+
+
+        for field in ["nh3", "so2", "nox", "pm2_5", "pm5", "pm10", "nmvoc", "op1", "op2", "op3", "op4", "op5"]:
+            _check_min_version(cluster_data, field, version)
+
+    if version < STUDY_VERSION_8_7:
+        for field in ["cost_generation", "efficiency", "variable_o_m_cost"]:
+            _check_min_version(cluster_data, field, version)
+
+
+def _initialize_field_default(cluster: ThermalCluster, field: str, default_value: Any) -> None:
+    if getattr(cluster, field) is None:
+        setattr(cluster, field, default_value)
+
+
+def initialize_thermal_cluster(cluster: ThermalCluster, version: StudyVersion) -> None:
+    """
+    Set undefined version-specific fields to default values.
+    """
+    if version >= STUDY_VERSION_8_6:
+        for field in ["nh3", "so2", "nox", "pm2_5", "pm5", "pm10", "nmvoc", "op1", "op2", "op3", "op4", "op5"]:
+            _initialize_field_default(cluster, field, 0)
+
+    if version >= STUDY_VERSION_8_7:
+        _initialize_field_default(cluster, "cost_generation", ThermalCostGeneration.SET_MANUALLY)
+        _initialize_field_default(cluster, "efficiency", 100.0)
+        _initialize_field_default(cluster, "variable_o_m_cost", 0.0)
+
+
+def create_thermal_cluster(cluster_data: ThermalClusterCreation, version: StudyVersion) -> ThermalCluster:
+    """
+    Creates a thermal cluster from a creation request, checking and initializing it against the specified study version.
+    """
+    cluster = ThermalCluster.model_validate(cluster_data.model_dump(exclude_none=True))
+    validate_thermal_cluster_against_version(version, cluster_data)
+    initialize_thermal_cluster(cluster, version)
+    return cluster
+
+
+def update_thermal_cluster(cluster: ThermalCluster, data: ThermalClusterUpdate) -> ThermalCluster:
+    """
+    Updates a cluster according to the provided update data.
+    """
+    return cluster.model_copy(update=data.model_dump(exclude_none=True))
+
--- antarest/study/storage/rawstudy/model/filesystem/config/st_storage.py
+++ antarest/study/storage/rawstudy/model/filesystem/config/st_storage.py
@@ -9,266 +9,60 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-
-from typing import Annotated, Any, Dict, Type, TypeAlias
+from typing import Any, Optional
 
 from antares.study.version import StudyVersion
-from pydantic import BeforeValidator, Field, TypeAdapter
-from pydantic_core.core_schema import ValidationInfo
+from pydantic import ConfigDict, Field
 
-from antarest.core.model import LowerCaseStr
-from antarest.study.business.enum_ignore_case import EnumIgnoreCase
-from antarest.study.model import STUDY_VERSION_8_6, STUDY_VERSION_8_8, STUDY_VERSION_9_2
-from antarest.study.storage.rawstudy.model.filesystem.config.cluster import ItemProperties
-from antarest.study.storage.rawstudy.model.filesystem.config.identifier import LowerCaseIdentifier, transform_name_to_id
-from antarest.study.storage.rawstudy.model.filesystem.config.validation import extract_version, study_version_context
+from antarest.core.serde import AntaresBaseModel
+from antarest.core.utils.string import to_kebab_case
+from antarest.study.business.model.sts_model import STStorage
+from antarest.study.business.model.thermal_cluster_model import (
+    initialize_thermal_cluster,
+    validate_thermal_cluster_against_version,
+)
 
 
-class STStorageGroup(EnumIgnoreCase):
+class STStorageFileData(AntaresBaseModel):
     """
-    This class defines the specific energy storage systems.
-
-    Enum values:
-
-        - PSP_OPEN: Represents an open pumped storage plant.
-        - PSP_CLOSED: Represents a closed pumped storage plant.
-        - PONDAGE: Represents a pondage storage system (reservoir storage system).
-        - BATTERY: Represents a battery storage system.
-        - OTHER1...OTHER5: Represents other energy storage systems.
+    Short-term storage data parsed from INI file.
     """
 
-    PSP_OPEN = "psp_open"
-    PSP_CLOSED = "psp_closed"
-    PONDAGE = "pondage"
-    BATTERY = "battery"
-    OTHER1 = "other1"
-    OTHER2 = "other2"
-    OTHER3 = "other3"
-    OTHER4 = "other4"
-    OTHER5 = "other5"
+    model_config = ConfigDict(extra="forbid", populate_by_name=True)
+
+    name: str
+    injection_nominal_capacity: Optional[float] = Field(default=None, alias="injectionnominalcapacity")
+    withdrawal_nominal_capacity: Optional[float] = Field(default=None, alias="withdrawalnominalcapacity")
+    reservoir_capacity: Optional[float] = Field(default=None, alias="reservoircapacity")
+    efficiency: Optional[float] = None
+    initial_level: Optional[float] = Field(default=None, alias="initiallevel")
+    initial_level_optim: Optional[float] = Field(default=None, alias="initialleveloptim")
+
+    # Added in 8.8
+    enabled: Optional[bool] = None
+
+    # Added in 9.2
+    group: Optional[str] = None
+
+    efficiency_withdrawal: Optional[float] = Field(default=None, alias="efficiencywithdrawal")
+    penalize_variation_injection: Optional[bool] = Field(default=None, alias="penalize-variation-injection")
+    penalize_variation_withdrawal: Optional[bool] = Field(default=None, alias="penalize-variation-withdrawal")
+
+    def to_model(self) -> STStorage:
+        return STStorage.model_validate(self.model_dump(exclude_none=True))
+
+    @classmethod
+    def from_model(cls, storage: STStorage) -> "STStorageFileData":
+        return cls.model_validate(storage.model_dump(exclude={"id"}))
 
 
-# noinspection SpellCheckingInspection
-class STStorageProperties(ItemProperties):
-    """
-    Properties of a short-term storage system read from the configuration files.
-
-    All aliases match the name of the corresponding field in the INI files.
-    """
-
-    def get_id(self) -> str:
-        return transform_name_to_id(self.name)
-
-    group: STStorageGroup = Field(
-        STStorageGroup.OTHER1,
-        description="Energy storage system group",
-        title="Short-Term Storage Group",
-    )
-    injection_nominal_capacity: float = Field(
-        0,
-        description="Injection nominal capacity (MW)",
-        ge=0,
-        alias="injectionnominalcapacity",
-        title="Injection Nominal Capacity",
-    )
-    withdrawal_nominal_capacity: float = Field(
-        0,
-        description="Withdrawal nominal capacity (MW)",
-        ge=0,
-        alias="withdrawalnominalcapacity",
-        title="Withdrawal Nominal Capacity",
-    )
-    reservoir_capacity: float = Field(
-        0,
-        description="Reservoir capacity (MWh)",
-        ge=0,
-        alias="reservoircapacity",
-        title="Reservoir Capacity",
-    )
-    efficiency: float = Field(
-        1,
-        description="Efficiency of the storage system (%)",
-        ge=0,
-        le=1,
-        title="Efficiency",
-    )
-    # The `initial_level` value must be between 0 and 1, but the default value is 0.5
-    initial_level: float = Field(
-        0.5,
-        description="Initial level of the storage system (%)",
-        ge=0,
-        le=1,
-        alias="initiallevel",
-        title="Initial Level",
-    )
-    initial_level_optim: bool = Field(
-        False,
-        description="Flag indicating if the initial level is optimized",
-        alias="initialleveloptim",
-        title="Initial Level Optimization",
-    )
+def parse_st_storage(study_version: StudyVersion, data: Any) -> STStorage:
+    storage = STStorageFileData.model_validate(data).to_model()
+    validate_thermal_cluster_against_version(study_version, storage)
+    initialize_thermal_cluster(storage, study_version)
+    return storage
 
 
-class STStorage880Properties(STStorageProperties):
-    """
-    Short term storage configuration model for 880 study.
-    """
-
-    # Activity status:
-    # - True: the plant may generate.
-    # - False: Ignored by the simulator.
-    enabled: bool = Field(default=True, description="Activity status")
-
-
-class STStorage920Properties(STStorage880Properties):
-    """
-    Short term storage configuration model for 920 study.
-    """
-
-    group: LowerCaseStr = Field(default="other1")  # type: ignore
-    efficiency_withdrawal: float = Field(default=1, ge=0, le=1, alias="efficiencywithdrawal")
-    penalize_variation_injection: bool = Field(default=False, alias="penalize-variation-injection")
-    penalize_variation_withdrawal: bool = Field(default=False, alias="penalize-variation-withdrawal")
-
-
-# noinspection SpellCheckingInspection
-class STStorageConfig(STStorageProperties, LowerCaseIdentifier):
-    """
-    Manage the configuration files in the context of Short-Term Storage.
-    It provides a convenient way to read and write configuration data from/to an INI file format.
-
-    Usage:
-
-    >>> from antarest.study.storage.rawstudy.model.filesystem.config.st_storage import STStorageConfig
-
-    >>> st = STStorageConfig(name="Storage 1", group="battery", injection_nominal_capacity=1500)
-    >>> st.id
-    'storage 1'
-    >>> st.group == STStorageGroup.BATTERY
-    True
-    >>> st.injection_nominal_capacity
-    1500.0
-    >>> st.injection_nominal_capacity = -897.32
-    Traceback (most recent call last):
-      ...
-    pydantic.error_wrappers.ValidationError: 1 validation error for STStorageConfig
-    injection_nominal_capacity
-      ensure this value is greater than or equal to 0 (type=value_error.number.not_ge; limit_value=0)
-    """
-
-
-class STStorage880Config(STStorage880Properties, LowerCaseIdentifier):
-    """
-    Short Term Storage config for study in version 8.8 or above.
-
-    Usage:
-
-    >>> from antarest.study.storage.rawstudy.model.filesystem.config.st_storage import STStorage880Config
-
-    >>> st = STStorage880Config(name="Storage 1", group="battery", enabled=False)
-    >>> st.id
-    'storage 1'
-    >>> st.group == STStorageGroup.BATTERY
-    True
-    >>> st.enabled
-    False
-    """
-
-
-class STStorage920Config(STStorage920Properties, LowerCaseIdentifier):
-    """
-    Short Term Storage config for study in version 9.2 or above.
-    """
-
-
-def _validate_st_storage_config(data: Any, info: ValidationInfo) -> Any:
-    """
-    When instantiating thermal cluster data from a dictionary, we need the study version
-    to choose which version of the config we need to create.
-    """
-    if not isinstance(data, dict):
-        return data
-    return get_st_storage_config_cls(extract_version(info)).model_validate(data)
-
-
-def _validate_st_storage_properties(data: Any, info: ValidationInfo) -> Any:
-    """
-    When instantiating thermal cluster data from a dictionary, we need the study version
-    to choose which version of the config we need to create.
-    """
-    if not isinstance(data, dict):
-        return data
-    study_version = extract_version(info)
-    if study_version >= STUDY_VERSION_9_2:
-        return STStorage920Properties.model_validate(data)
-    elif study_version >= STUDY_VERSION_8_8:
-        return STStorage880Properties.model_validate(data)
-    elif study_version >= STUDY_VERSION_8_6:
-        return STStorageProperties.model_validate(data)
-    else:
-        raise ValueError(f"Unsupported study version: {study_version}")
-
-
-STStorageConfigType: TypeAlias = Annotated[
-    STStorageConfig | STStorage880Config | STStorage920Config, BeforeValidator(_validate_st_storage_config)
-]
-STStoragePropertiesType: TypeAlias = Annotated[
-    STStorageProperties | STStorage880Properties | STStorage920Properties,
-    BeforeValidator(_validate_st_storage_properties),
-]
-
-_CONFIG_ADAPTER: TypeAdapter[STStorageConfigType] = TypeAdapter(STStorageConfigType)
-_PROPERTIES_ADAPTER: TypeAdapter[STStoragePropertiesType] = TypeAdapter(STStoragePropertiesType)
-
-
-def create_st_storage_properties(study_version: StudyVersion, data: Dict[str, Any]) -> STStoragePropertiesType:
-    """
-    Factory method to create st_storage properties.
-
-    Args:
-        study_version: The version of the study.
-        data: The dictionary of data to be used to initialize the model.
-
-    Returns:
-        The short term storage properties.
-
-    Raises:
-        ValueError: If the study version is not supported.
-    """
-    return _PROPERTIES_ADAPTER.validate_python(data, context=study_version_context(study_version))
-
-
-def get_st_storage_config_cls(study_version: StudyVersion) -> Type[STStorageConfigType]:
-    """
-    Retrieves the short-term storage configuration class based on the study version.
-
-    Args:
-        study_version: The version of the study.
-
-    Returns:
-        The short-term storage configuration class.
-    """
-    if study_version >= STUDY_VERSION_9_2:
-        return STStorage920Config
-    elif study_version >= STUDY_VERSION_8_8:
-        return STStorage880Config
-    elif study_version >= STUDY_VERSION_8_6:
-        return STStorageConfig
-    raise ValueError(f"Unsupported study version: {study_version}")
-
-
-def create_st_storage_config(study_version: StudyVersion, **kwargs: Any) -> STStorageConfigType:
-    """
-    Factory method to create a short-term storage configuration model.
-
-    Args:
-        study_version: The version of the study.
-        **kwargs: The properties to be used to initialize the model.
-
-    Returns:
-        The short-term storage configuration model.
-
-    Raises:
-        ValueError: If the study version is not supported.
-    """
-    return _CONFIG_ADAPTER.validate_strings(kwargs, context=study_version_context(study_version))
+def serialize_st_storage(study_version: StudyVersion, storage: STStorage) -> dict[str, Any]:
+    validate_thermal_cluster_against_version(study_version, storage)
+    return STStorageFileData.from_model(storage).model_dump(mode="json", by_alias=True, exclude_none=True)
