35f3fc5298c9bc0be2921e296427092c44b33028
belthlemar <martin.belthle@rte-france.com>
2025-06-02 17:27:46 +0200

    dfg

--- antarest/study/storage/variantstudy/model/command/remove_st_storage.py
+++ antarest/study/storage/variantstudy/model/command/remove_st_storage.py
@@ -15,10 +15,14 @@ from typing import List, Optional
 from pydantic import Field
 from typing_extensions import override
 
+from antarest.study.dao.api.study_dao import StudyDao
 from antarest.study.model import STUDY_VERSION_8_6
-from antarest.study.storage.rawstudy.model.filesystem.config.model import Area, FileStudyTreeConfig
-from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
-from antarest.study.storage.variantstudy.model.command.common import CommandName, CommandOutput
+from antarest.study.storage.variantstudy.model.command.common import (
+    CommandName,
+    CommandOutput,
+    command_failed,
+    command_succeeded,
+)
 from antarest.study.storage.variantstudy.model.command.icommand import ICommand
 from antarest.study.storage.variantstudy.model.command_listener.command_listener import ICommandListener
 from antarest.study.storage.variantstudy.model.model import CommandDTO
@@ -43,81 +47,15 @@ class RemoveSTStorage(ICommand):
     area_id: str = Field(description="Area ID", pattern=r"[a-z0-9_(),& -]+")
     storage_id: str = Field(description="Short term storage ID", pattern=r"[a-z0-9_(),& -]+")
 
-    def remove_from_config(self, study_data: FileStudyTreeConfig) -> CommandOutput:
-        """
-        Applies configuration changes to the study data: remove the storage from the storages list.
-
-        Args:
-            study_data: The study data configuration.
-
-        Returns:
-            A tuple containing the command output and a dictionary of extra data.
-            On success, the dictionary is empty.
-        """
-        # Check if the study version is above the minimum required version.
-        version = study_data.version
-        if version < REQUIRED_VERSION:
-            return CommandOutput(
-                status=False,
-                message=f"Invalid study version {version}, at least version {REQUIRED_VERSION} is required.",
-            )
-
-        # Search the Area in the configuration
-        if self.area_id not in study_data.areas:
-            return CommandOutput(
-                status=False,
-                message=f"Area '{self.area_id}' does not exist in the study configuration.",
-            )
-
-        area: Area = study_data.areas[self.area_id]
-
-        # Search the Short term storage in the area
-        for st_storage in area.st_storages:
-            if st_storage.id == self.storage_id:
-                break
-        else:
-            return CommandOutput(
-                status=False,
-                message=f"Short term storage '{self.storage_id}' does not exist in the area '{self.area_id}'.",
-            )
-
-        # Remove the Short term storage from the configuration
-        area.st_storages.remove(st_storage)
-
-        return CommandOutput(
-            status=True,
-            message=f"Short term storage '{self.storage_id}' removed from the area '{self.area_id}'.",
-        )
-
     @override
-    def _apply(self, study_data: FileStudy, listener: Optional[ICommandListener] = None) -> CommandOutput:
-        """
-        Applies the study data to update storage configurations and saves the changes:
-        remove the storage from the configuration and remove the attached time series.
+    def _apply_dao(self, study_data: StudyDao, listener: Optional[ICommandListener] = None) -> CommandOutput:
+        if not study_data.thermal_exists(self.area_id, self.storage_id):
+            return command_failed(f"Short-term storage '{self.storage_id}' in area '{self.area_id}' does not exist")
 
-        Args:
-            study_data: The study data to be applied.
+        storage = study_data.get_st_storage(self.area_id, self.storage_id)
+        study_data.delete_storage(self.area_id, storage)
 
-        Returns:
-            The output of the command execution.
-        """
-        # It is required to delete the files and folders that correspond to the short-term storage
-        # BEFORE updating the configuration, as we need the configuration to do so.
-        # Specifically, deleting the time series uses the list of short-term storages from the configuration.
-
-        paths = [
-            ["input", "st-storage", "clusters", self.area_id, "list", self.storage_id],
-            ["input", "st-storage", "series", self.area_id, self.storage_id],
-        ]
-        area: Area = study_data.config.areas[self.area_id]
-        if len(area.st_storages) == 1:
-            paths.append(["input", "st-storage", "series", self.area_id])
-
-        for path in paths:
-            study_data.tree.delete(path)
-        # Deleting the short-term storage in the configuration must be done AFTER
-        # deleting the files and folders.
-        return self.remove_from_config(study_data.config)
+        return command_succeeded(f"Short-term storage '{self.storage_id}' inside area '{self.area_id}' deleted")
 
     @override
     def to_dto(self) -> CommandDTO:
