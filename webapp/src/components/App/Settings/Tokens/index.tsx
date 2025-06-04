74579c0ec3705180e4e46017343c0bd49ac27740
Samir Kamal <1954121+skamril@users.noreply.github.com>
2025-06-04 15:30:55 +0200

    refactor(ui-tokens): optimize tokens view (#2508)
    - fix view when there is no user (desktop)
    - clean up code

--- webapp/src/components/App/Settings/Tokens/index.tsx
+++ webapp/src/components/App/Settings/Tokens/index.tsx
@@ -12,98 +12,34 @@
  * This file is part of the Antares project.
  */
 
-import { toError } from "@/utils/fnUtils";
-import DeleteIcon from "@mui/icons-material/Delete";
-import InfoIcon from "@mui/icons-material/Info";
-import TokenIcon from "@mui/icons-material/Token";
-import {
-  Box,
-  CircularProgress,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Skeleton,
-  Typography,
-} from "@mui/material";
-import { produce } from "immer";
-import { useSnackbar } from "notistack";
-import * as R from "ramda";
-import { useMemo, useReducer, useState } from "react";
+import UsePromiseCond from "@/components/common/utils/UsePromiseCond";
+import type { BotDetailsDTO } from "@/types/types";
+import { Box, List, ListItem, Skeleton } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { usePromise as usePromiseWrapper, useUpdateEffect } from "react-use";
-import type { Action } from "redux";
-import useEnqueueErrorSnackbar from "../../../../hooks/useEnqueueErrorSnackbar";
 import usePromiseWithSnackbarError from "../../../../hooks/usePromiseWithSnackbarError";
 import useAppSelector from "../../../../redux/hooks/useAppSelector";
 import { getAuthUser } from "../../../../redux/selectors";
-import { deleteBot, getBots, getUser, getUsers } from "../../../../services/api/user";
-import { isUserAdmin, sortByProp } from "../../../../services/utils";
-import type { BotDTO, BotDetailsDTO, UserDTO } from "../../../../types/types";
-import { isSearchMatching } from "../../../../utils/stringUtils";
-import ConfirmationDialog from "../../../common/dialogs/ConfirmationDialog";
-import ReadTokenDialog from "./dialog/ReadTokenDialog";
+import { getBots, getUser, getUsers } from "../../../../services/api/user";
+import { isUserAdmin } from "../../../../services/utils";
 import Header from "./Header";
-
-interface BotDetailsDtoWithUser extends BotDetailsDTO {
-  user: UserDTO;
-}
-
-enum TokenActionKind {
-  ADD = "ADD",
-  DELETE = "DELETE",
-  RESET = "RESET",
-}
-
-interface TokenAction extends Action<string> {
-  payload?: BotDTO["id"] | BotDTO | BotDTO[];
-}
-
-const reducer = produce<BotDetailsDtoWithUser[], [TokenAction]>((draft, action) => {
-  const { payload } = action;
-
-  switch (action.type) {
-    case TokenActionKind.ADD: {
-      draft.push(payload as BotDetailsDtoWithUser);
-      return;
-    }
-    case TokenActionKind.DELETE: {
-      const index = draft.findIndex((token) => token.id === payload);
-      if (index > -1) {
-        draft.splice(index, 1);
-      }
-      return;
-    }
-    case TokenActionKind.RESET:
-      return payload as BotDetailsDtoWithUser[];
-  }
-});
+import TokenList from "./TokenList";
+import type { BotDetailsDtoWithUser } from "./types";
 
 function Tokens() {
-  const [tokenToDisplayInfo, setTokenToDisplayInfo] = useState<BotDetailsDTO>();
-  const [tokenToDelete, setTokenToDelete] = useState<BotDetailsDTO>();
-  const [tokensInLoading, setTokensInLoading] = useState<BotDetailsDTO[]>([]);
-  const [tokens, dispatch] = useReducer(reducer, []);
-  const [searchValue, setSearchValue] = useState("");
-  const { enqueueSnackbar } = useSnackbar();
-  const enqueueErrorSnackbar = useEnqueueErrorSnackbar();
-  const mounted = usePromiseWrapper();
   const { t } = useTranslation();
+  const [searchValue, setSearchValue] = useState("");
+  const [tokensInLoading, setTokensInLoading] = useState<Array<BotDetailsDTO["id"]>>([]);
   const authUser = useAppSelector(getAuthUser);
 
-  const {
-    data: initialTokens,
-    isLoading,
-    reload: reloadFetchTokens,
-  } = usePromiseWithSnackbarError(
+  const res = usePromiseWithSnackbarError<BotDetailsDtoWithUser[]>(
     async () => {
       if (!authUser) {
         return [];
       }
 
       const isAuthUserAdmin = isUserAdmin(authUser);
+
       const bots = await getBots({
         verbose: 1,
         owner: isAuthUserAdmin ? undefined : authUser.id,
@@ -111,6 +47,7 @@ function Tokens() {
 
       if (isAuthUserAdmin) {
         const users = await getUsers();
+
         return bots.map((bot) => ({
           ...bot,
           user: users.find((user) => user.id === bot.owner),
@@ -118,63 +55,18 @@ function Tokens() {
       }
 
       const user = await getUser(authUser.id);
+
       return bots.map((bot) => ({ ...bot, user }));
     },
     { errorMessage: t("settings.error.tokensError"), deps: [authUser] },
   );
 
-  useUpdateEffect(() => {
-    setTokenToDelete(undefined);
-    setTokensInLoading([]);
-
-    dispatch({ type: TokenActionKind.RESET, payload: initialTokens || [] });
-  }, [initialTokens]);
-
-  const filteredAndSortedTokens = useMemo(() => {
-    let list = tokens;
-    if (searchValue) {
-      list = tokens?.filter(({ name, user }) => {
-        return isSearchMatching(searchValue, [name, user.name]);
-      });
+  useEffect(() => {
+    if (res.data) {
+      const tokenIds = res.data.map((token) => token.id);
+      setTokensInLoading((prev) => prev.filter((id) => tokenIds.includes(id)));
     }
-    return sortByProp((token) => token.user.name, list);
-  }, [searchValue, tokens]);
-
-  ////////////////////////////////////////////////////////////////
-  // Utils
-  ////////////////////////////////////////////////////////////////
-
-  const addToken = (token: BotDTO) => {
-    dispatch({ type: TokenActionKind.ADD, payload: token });
-  };
-
-  ////////////////////////////////////////////////////////////////
-  // Event Handlers
-  ////////////////////////////////////////////////////////////////
-
-  const handleDeleteToken = () => {
-    if (!tokenToDelete) {
-      return;
-    }
-
-    const token = tokenToDelete;
-    setTokensInLoading((prev) => [...prev, token]);
-    setTokenToDelete(undefined);
-
-    mounted(deleteBot(token.id))
-      .then(() => {
-        dispatch({ type: TokenActionKind.DELETE, payload: token.id });
-        enqueueSnackbar(t("settings.success.tokenDelete", { 0: token.name }), {
-          variant: "success",
-        });
-      })
-      .catch((err) => {
-        enqueueErrorSnackbar(t("settings.error.tokenDelete", { 0: token.name }), toError(err));
-      })
-      .finally(() => {
-        setTokensInLoading((prev) => prev.filter((u) => u !== token));
-      });
-  };
+  }, [res.data]);
 
   ////////////////////////////////////////////////////////////////
   // JSX
@@ -188,95 +80,28 @@ function Tokens() {
         height: 1,
       }}
     >
-      <Header
-        setSearchValue={setSearchValue}
-        addToken={addToken}
-        reloadFetchTokens={reloadFetchTokens}
-      />
+      <Header setSearchValue={setSearchValue} reloadFetchTokens={res.reload} />
       <List sx={{ overflow: "auto" }}>
-        {R.cond([
-          // Loading
-          [
-            () => isLoading,
-            () =>
-              Array.from({ length: 3 }, (v, k) => k).map((v) => (
-                <ListItem key={v} disablePadding>
-                  <Skeleton sx={{ width: 1, height: 50 }} />
-                </ListItem>
-              )) as React.ReactNode,
-          ],
-          // Token list
-          [
-            () => filteredAndSortedTokens.length > 0,
-            () =>
-              filteredAndSortedTokens.map((token) => (
-                <ListItem
-                  key={token.id}
-                  secondaryAction={
-                    tokensInLoading.includes(token) ? (
-                      <Box sx={{ display: "flex" }}>
-                        <CircularProgress color="inherit" size={25} />
-                      </Box>
-                    ) : (
-                      <>
-                        <IconButton onClick={() => setTokenToDisplayInfo(token)}>
-                          <InfoIcon />
-                        </IconButton>
-                        <IconButton edge="end" onClick={() => setTokenToDelete(token)}>
-                          <DeleteIcon />
-                        </IconButton>
-                      </>
-                    )
-                  }
-                  disablePadding
-                >
-                  <ListItemButton onClick={() => setTokenToDisplayInfo(token)}>
-                    <ListItemIcon>
-                      <TokenIcon />
-                    </ListItemIcon>
-                    <ListItemText
-                      primary={
-                        <Box sx={{ display: "flex" }}>
-                          {token.name}
-                          <Typography sx={{ ml: 2, opacity: 0.4, fontStyle: "italic" }}>
-                            {token.user.name}
-                          </Typography>
-                        </Box>
-                      }
-                    />
-                  </ListItemButton>
-                </ListItem>
-              )),
-          ],
-          // No token
-          [
-            R.T,
-            () => (
-              <Typography sx={{ m: 2 }} align="center">
-                {t("settings.noToken")}
-              </Typography>
-            ),
-          ],
-        ])()}
-      </List>
-      {tokenToDelete && (
-        <ConfirmationDialog
-          titleIcon={DeleteIcon}
-          onCancel={() => setTokenToDelete(undefined)}
-          onConfirm={handleDeleteToken}
-          alert="warning"
-          open
-        >
-          {t("settings.question.deleteToken", { 0: tokenToDelete.name })}
-        </ConfirmationDialog>
-      )}
-      {tokenToDisplayInfo && (
-        <ReadTokenDialog
-          open
-          onCancel={() => setTokenToDisplayInfo(undefined)}
-          token={tokenToDisplayInfo}
+        <UsePromiseCond
+          response={res}
+          ifPending={() =>
+            Array.from({ length: 3 }, (v, k) => k).map((v) => (
+              <ListItem key={v} disablePadding>
+                <Skeleton sx={{ width: 1, height: 50 }} />
+              </ListItem>
+            ))
+          }
+          ifFulfilled={(tokens) => (
+            <TokenList
+              tokens={tokens}
+              searchValue={searchValue}
+              setTokensInLoading={setTokensInLoading}
+              tokensInLoading={tokensInLoading}
+              reloadFetchTokens={res.reload}
+            />
+          )}
         />
-      )}
+      </List>
     </Box>
   );
 }
