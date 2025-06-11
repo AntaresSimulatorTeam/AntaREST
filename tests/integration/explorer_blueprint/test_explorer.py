333ed363e43a747a6f0a4ad1af0685aa7689e85d
Anis SMAIL <anis.smail_externe@rte-france.com>
2025-06-11 09:00:04 +0200

    fix test

--- tests/integration/explorer_blueprint/test_explorer.py
+++ tests/integration/explorer_blueprint/test_explorer.py
@@ -65,10 +65,10 @@ def test_explorer(client: TestClient, admin_access_token: str, study_tree: Path)
     )
     res.raise_for_status()
     directories_res = res.json()
-    directories_res = [FolderDTO(**d) for d in directories_res]
+    directories_res = list(sorted([FolderDTO(**d) for d in directories_res], key=lambda f: f.name))
     directorires_expected = [
-        FolderDTO(path=Path("folder/trash"), workspace="ext", name="trash", hasChildren=False, isStudyFolder=False),
         FolderDTO(path=Path("folder/studyC"), workspace="ext", name="studyC", hasChildren=False, isStudyFolder=True),
+        FolderDTO(path=Path("folder/trash"), workspace="ext", name="trash", hasChildren=False, isStudyFolder=False),
     ]
     assert directories_res == directorires_expected
 
