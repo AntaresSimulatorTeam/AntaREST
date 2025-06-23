d0092561d234c2d572a3c4d90fe201013a96dea0
wahadameh <mehdi.wahada@rte-france.com>
2025-06-23 15:56:17 +0200

    feat(matrixstore): added blank get_matrices in in_memory.py

--- antarest/matrixstore/in_memory.py
+++ antarest/matrixstore/in_memory.py
@@ -15,6 +15,7 @@ from typing import Dict
 import pandas as pd
 from typing_extensions import override
 
+from antarest.matrixstore.matrix_model import MatrixModel
 from antarest.matrixstore.repository import compute_hash
 from antarest.matrixstore.service import ISimpleMatrixService
 
@@ -44,3 +45,7 @@ class InMemorySimpleMatrixService(ISimpleMatrixService):
     @override
     def delete(self, matrix_id: str) -> None:
         del self._content[matrix_id]
+
+    @override
+    def get_matrices(self) -> list[MatrixModel]:
+        raise NotImplementedError()
