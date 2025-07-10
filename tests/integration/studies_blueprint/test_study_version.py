66ce9cf16a6cc62be73c4e99c2719ab32f73b42a
belthlemar <martin.belthle@rte-france.com>
2025-07-10 09:30:51 +0200

    fix one failing test

--- tests/integration/studies_blueprint/test_study_version.py
+++ tests/integration/studies_blueprint/test_study_version.py
@@ -15,9 +15,11 @@ import os
 import zipfile
 from pathlib import Path
 
+from antares.study.version.upgrade_app import UpgradeApp
 from starlette.testclient import TestClient
 
 from antarest.core.serde.ini_reader import read_ini
+from antarest.study.model import STUDY_VERSION_9_2
 from tests.integration.assets import ASSETS_DIR
 
 
@@ -26,33 +28,21 @@ class TestStudyVersions:
     This class contains tests related to the handling of different study versions
     """
 
-    def test_nominal_case(self, client: TestClient, user_access_token: str, tmp_path: str) -> None:
+    def test_nominal_case(self, client: TestClient, user_access_token: str, tmp_path: Path) -> None:
         # =============================
         #  SET UP
         # =============================
         client.headers = {"Authorization": f"Bearer {user_access_token}"}
 
-        data = """[antares]
-version = 9.0
-caption = test_version_90
-created = 1682506382.235618
-lastsave = 1682506382.23562
-author = Unknown
-"""
-        tmp_dir = Path(tmp_path)
         zip_path = ASSETS_DIR / "STA-mini.zip"
-        # Extract zip inside tmp_dir
-        new_zip_path = tmp_dir / "test_version_90"
+
+        # Creates a study in version 9.2 from the base study in version 7.0
+        new_zip_path = tmp_path / "test_version_92"
         with zipfile.ZipFile(zip_path, "r") as zip_ref:
             zip_ref.extractall(new_zip_path)
-
-        # Change file content
-        target_path = os.path.join(new_zip_path, "STA-mini", "study.antares")
-        with open(target_path, "w") as file:
-            file.write(data)
-
-        final_path = tmp_dir / "test_version_90.zip"
-        # Rezip it
+        app = UpgradeApp(new_zip_path / "STA-mini", version=STUDY_VERSION_9_2)
+        app()
+        final_path = tmp_path / "test_version_92.zip"
         with zipfile.ZipFile(final_path, "w", zipfile.ZIP_DEFLATED) as zipf:
             for root, dirs, files in os.walk(new_zip_path):
                 for file in files:
@@ -63,7 +53,7 @@ author = Unknown
         #  LIFECYCLE
         # =============================
 
-        for f in [zip_path, final_path]:
+        for f in [zip_path, final_path]:  # zip path is the study in version 7.0 and final path the one in version 9.2
             # Imports a study
             res = client.post("/v1/studies/_import", files={"study": io.BytesIO(f.read_bytes())})
             res.raise_for_status()
@@ -72,13 +62,13 @@ author = Unknown
             # Gets study information
             res = client.get(f"v1/studies/{study_id}")
             res.raise_for_status()
-            assert res.json()["version"] == 900 if f == final_path else 700
+            assert res.json()["version"] == 920 if f == final_path else 700
 
             # Reads `study.version` file
             res = client.get(f"v1/studies/{study_id}/raw?path=study")
             res.raise_for_status()
             version = str(res.json()["antares"]["version"])
-            assert version == "9.0" if f == final_path else "700"
+            assert version == "9.2" if f == final_path else "700"
 
             # Delete the study
             res = client.delete(f"v1/studies/{study_id}")
