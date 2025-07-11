95c1fe6387e29b757c453949bb8d03a84c421643
belthlemar <martin.belthle@rte-france.com>
2025-07-11 14:56:09 +0200

    finalize test

--- tests/integration/studies_blueprint/test_normalization.py
+++ tests/integration/studies_blueprint/test_normalization.py
@@ -10,6 +10,8 @@
 #
 # This file is part of the Antares project.
 import io
+import shutil
+import zipfile
 from pathlib import Path
 
 from starlette.testclient import TestClient
@@ -27,12 +29,35 @@ class TestNormalization:
         study_id = res.json()
 
         # Unzip the STA-mini study inside the tmp_path
+        denormalized_path = tmp_path / "denormalized_study"
+        with zipfile.ZipFile(sta_mini_zip_path, mode="r") as zipf:
+            zipf.extractall(denormalized_path)
         # Copy the files inside the managed study
         # This way we mimick the case of a raw study managed but not normalized
+        shutil.rmtree(denormalized_path / "STA-mini" / "output")
+        internal_path = tmp_path / "internal_workspace" / study_id
+        shutil.rmtree(internal_path)
+        shutil.copytree(denormalized_path / "STA-mini", internal_path)
 
         ##########################
         # Nominal cases
         ##########################
+
+        # Checks disk usage before
+        res = client.get(f"/v1/studies/{study_id}/disk-usage")
+        assert res.status_code == 200
+        disk_usage = res.json()
+        assert 6 * 1024 * 1024 < disk_usage < 8 * 1024 * 1024
+
+        # Normalize the study
+        res = client.put(f"/v1/studies/{study_id}/normalize")
+        assert res.status_code == 200
+
+        # Checks disk usage after. It should be way lower
+        res = client.get(f"/v1/studies/{study_id}/disk-usage")
+        assert res.status_code == 200
+        disk_usage = res.json()
+        assert disk_usage < 0.5 * 1024 * 1024
 
         ##########################
         # Error cases
