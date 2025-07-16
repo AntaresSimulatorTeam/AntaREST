3be65cee39f4946dd3c752f96fdbec509839ff81
Anis <smailio@users.noreply.github.com>
2025-07-16 09:55:15 +0200

    feat(desktop): ui improvement (#2575)
    [`ANT-3469`](https://gopro-tickets.rte-france.com/browse/ANT-3469)
    
    ---------
    
    Co-authored-by: Anis SMAIL <anis.smail_externe@rte-france.com>

--- antarest/study/storage/explorer_service.py
+++ antarest/study/storage/explorer_service.py
@@ -10,15 +10,18 @@
 #
 # This file is part of the Antares project.
 
+import ctypes
 import logging
+import os
+import sys
 from pathlib import PurePosixPath
-from typing import List
+from typing import List, Optional
 
 from antarest.core.config import Config
 from antarest.study.model import (
     DEFAULT_WORKSPACE_NAME,
     FolderDTO,
-    WorkspaceMetadata,
+    WorkspaceDTO,
 )
 from antarest.study.storage.utils import (
     get_folder_from_workspace,
@@ -78,14 +81,56 @@ class Explorer:
 
         return folders
 
-    def list_workspaces(
-        self,
-    ) -> List[WorkspaceMetadata]:
+    def list_workspaces(self) -> List[WorkspaceDTO]:
         """
-        Return the list of all configured workspace name, except the default one.
+        Return the list of all configured workspace names, except the default one.
+        On Windows, includes the disk name (volume label) in WorkspaceDTO
         """
-        return [
-            WorkspaceMetadata(name=workspace_name)
-            for workspace_name in self.config.storage.workspaces.keys()
-            if workspace_name != DEFAULT_WORKSPACE_NAME
-        ]
+        result = []
+        for workspace_name in self.config.storage.workspaces.keys():
+            if workspace_name == DEFAULT_WORKSPACE_NAME:
+                continue
+
+            if os.name == "nt":
+                # On Windows, we use Windows-specific APIs (via `get_volume_label`) to retrieve the disk name
+                # for each workspace.
+                disk_name = get_volume_label(workspace_name)
+                result.append(WorkspaceDTO(name=workspace_name, disk_name=disk_name))
+            else:
+                # On Linux systems, disks are mounted as directories under a common filesystem hierarchy
+                # (/mnt or /media), and there is no notion of a disk name in the same way.
+                result.append(WorkspaceDTO(name=workspace_name))
+
+        return result
+
+
+def get_volume_label(drive_letter: str) -> Optional[str]:
+    """
+    Returns the volume label like 'OS' for a given drive letter like 'C:\\'.
+    """
+    # assert we're on windows so mypy doesn't check windll on linux
+    if sys.platform == "win32":
+        volume_name_buffer = ctypes.create_unicode_buffer(1024)
+        file_system_name_buffer = ctypes.create_unicode_buffer(1024)
+        serial_number = ctypes.c_ulong()
+        max_component_length = ctypes.c_ulong()
+        file_system_flags = ctypes.c_ulong()
+
+        result = ctypes.windll.kernel32.GetVolumeInformationW(
+            ctypes.c_wchar_p(drive_letter),
+            volume_name_buffer,
+            ctypes.sizeof(volume_name_buffer),
+            ctypes.byref(serial_number),
+            ctypes.byref(max_component_length),
+            ctypes.byref(file_system_flags),
+            file_system_name_buffer,
+            ctypes.sizeof(file_system_name_buffer),
+        )
+
+        if result:
+            return volume_name_buffer.value
+
+        # in case the volume has no label we just return None
+        return None
+    else:
+        raise NotImplementedError("Function only available on windows")
