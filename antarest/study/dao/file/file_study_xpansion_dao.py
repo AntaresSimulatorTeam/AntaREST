71d1f657b589bdf1dfd4da5636fe0461f02ff77a
belthlemar <martin.belthle@rte-france.com>
2025-07-29 14:44:38 +0200

    finalize src code

--- antarest/study/dao/file/file_study_xpansion_dao.py
+++ antarest/study/dao/file/file_study_xpansion_dao.py
@@ -48,7 +48,18 @@ class FileStudyXpansionDao(XpansionDao, ABC):
 
     @override
     def save_xpansion_candidate(self, candidate: XpansionCandidate, old_id: Optional[str] = None) -> None:
-        raise NotImplementedError()
+        candidates = self._get_all_xpansion_candidates()
+        if old_id:
+            # We should remove the candidate corresponding to the `old_id`
+            for key, value in candidates.items():
+                if value["name"] == old_id:
+                    del candidates[key]
+                    break
+
+        new_id = str(len(candidates) + 1)  # The first candidate key is 1
+        candidates[new_id] = candidate.model_dump(mode="json", by_alias=True, exclude_none=True)
+        candidates_data = {"user": {"expansion": {"candidates": candidates}}}
+        self.get_file_study().tree.save(candidates_data)
 
     def _get_all_xpansion_candidates(self) -> dict[str, Any]:
         file_study = self.get_file_study()
