7d9fcd6d0e8bb221253bda050d7b0024bbeea138
belthlemar <martin.belthle@rte-france.com>
2025-08-01 15:58:32 +0200

    fixing 2 tests

--- tests/storage/repository/filesystem/matrix/test_matrix_node.py
+++ tests/storage/repository/filesystem/matrix/test_matrix_node.py
@@ -36,7 +36,7 @@ class MockMatrixNode(MatrixNode):
         return MOCK_MATRIX
 
     def write_dataframe(self, df: pd.DataFrame) -> None:
-        pass  # not used
+        df.to_csv(self.config.path, sep="\t", header=False, index=False)
 
     def check_errors(self, data: str, url: Optional[List[str]] = None, raising: bool = False) -> List[str]:
         pass  # not used
