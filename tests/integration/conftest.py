1d4581559acd9c77bb4705a8ef7d9af23cf831bc
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2025-08-22 16:58:04 +0200

    remove test code
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -9,14 +9,11 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-import cProfile
+
 import os
-import pstats
 import typing as t
 import zipfile
 from pathlib import Path
-from pstats import SortKey
-from typing import Iterable
 
 import jinja2
 import pytest
@@ -26,8 +23,6 @@ from starlette.testclient import TestClient
 
 from antarest.dbmodel import Base
 from antarest.main import fastapi_app
-from antarest.service_creator import Services
-from antarest.study.service import StudyService
 from tests.integration.assets import ASSETS_DIR
 
 HERE = Path(__file__).parent.resolve()
@@ -37,87 +32,64 @@ RESOURCES_DIR = PROJECT_DIR.joinpath("resources")
 RUN_ON_WINDOWS = os.name == "nt"
 
 
-@pytest.fixture
-def app_and_services(tmp_path: Path) -> Iterable[tuple[FastAPI, Services]]:
+@pytest.fixture(name="app")
+def app_fixture(tmp_path: Path) -> t.Generator[FastAPI, None, None]:
     # Currently, it is impossible to use a SQLite database in memory (with "sqlite:///:memory:")
     # because the database is created by the FastAPI application during each integration test,
     # which doesn't apply the migrations (migrations are done by Alembic).
     # An alternative is to use a SQLite database stored on disk, because migrations can be persisted.
+    db_path = tmp_path / "db.sqlite"
+    db_url = f"sqlite:///{db_path}"
 
-    with cProfile.Profile() as prof:
-        db_path = tmp_path / "db.sqlite"
-        db_url = f"sqlite:///{db_path}"
+    # ATTENTION: when setting up integration tests, be aware that creating the database
+    # tables requires a dedicated DB engine (the `engine` below).
+    # This is crucial as the FastAPI application initializes its own engine (a global object),
+    # and the DB engine used in integration tests is not the same.
+    engine = create_engine(db_url, echo=False)
+    Base.metadata.create_all(engine)
+    del engine  # This object won't be used anymore.
 
-        # ATTENTION: when setting up integration tests, be aware that creating the database
-        # tables requires a dedicated DB engine (the `engine` below).
-        # This is crucial as the FastAPI application initializes its own engine (a global object),
-        # and the DB engine used in integration tests is not the same.
-        engine = create_engine(db_url, echo=False)
-        Base.metadata.create_all(engine)
-        del engine  # This object won't be used anymore.
+    # Prepare the directories used by the repos
+    matrix_dir = tmp_path / "matrix_store"
+    archive_dir = tmp_path / "archive_dir"
+    tmp_dir = tmp_path / "tmp"
+    default_workspace = tmp_path / "internal_workspace"
+    ext_workspace_path = tmp_path / "ext_workspace"
 
-        # Prepare the directories used by the repos
-        matrix_dir = tmp_path / "matrix_store"
-        archive_dir = tmp_path / "archive_dir"
-        tmp_dir = tmp_path / "tmp"
-        default_workspace = tmp_path / "internal_workspace"
-        ext_workspace_path = tmp_path / "ext_workspace"
+    matrix_dir.mkdir()
+    archive_dir.mkdir()
+    tmp_dir.mkdir()
+    default_workspace.mkdir()
+    ext_workspace_path.mkdir()
 
-        matrix_dir.mkdir()
-        archive_dir.mkdir()
-        tmp_dir.mkdir()
-        default_workspace.mkdir()
-        ext_workspace_path.mkdir()
+    # Extract the sample study
+    sta_mini_zip_path = ASSETS_DIR.joinpath("STA-mini.zip")
+    with zipfile.ZipFile(sta_mini_zip_path) as zip_output:
+        zip_output.extractall(path=ext_workspace_path)
 
-        # Extract the sample study
-        sta_mini_zip_path = ASSETS_DIR.joinpath("STA-mini.zip")
-        with zipfile.ZipFile(sta_mini_zip_path) as zip_output:
-            zip_output.extractall(path=ext_workspace_path)
+    # Generate a "config.yml" file for the app
+    template_loader = jinja2.FileSystemLoader(searchpath=ASSETS_DIR)
+    template_env = jinja2.Environment(loader=template_loader)
+    template = template_env.get_template("config.template.yml")
 
-        # Generate a "config.yml" file for the app
-        template_loader = jinja2.FileSystemLoader(searchpath=ASSETS_DIR)
-        template_env = jinja2.Environment(loader=template_loader)
-        template = template_env.get_template("config.template.yml")
-
-        config_path = tmp_path / "config.yml"
-        launcher_name = "launcher_mock.bat" if RUN_ON_WINDOWS else "launcher_mock.sh"
-        with open(config_path, "w") as fh:
-            fh.write(
-                template.render(
-                    db_url=db_url,
-                    default_workspace_path=str(default_workspace),
-                    ext_workspace_path=str(ext_workspace_path),
-                    matrix_dir=str(matrix_dir),
-                    archive_dir=str(archive_dir),
-                    tmp_dir=str(tmp_dir),
-                    launcher_mock=ASSETS_DIR / launcher_name,
-                )
+    config_path = tmp_path / "config.yml"
+    launcher_name = "launcher_mock.bat" if RUN_ON_WINDOWS else "launcher_mock.sh"
+    with open(config_path, "w") as fh:
+        fh.write(
+            template.render(
+                db_url=db_url,
+                default_workspace_path=str(default_workspace),
+                ext_workspace_path=str(ext_workspace_path),
+                matrix_dir=str(matrix_dir),
+                archive_dir=str(archive_dir),
+                tmp_dir=str(tmp_dir),
+                launcher_mock=ASSETS_DIR / launcher_name,
             )
+        )
 
-        app, services = fastapi_app(config_path, RESOURCES_DIR, mount_front=False)
-
-        with open("/home/leclercsyl/tmp/integration-test.txt", "w") as fh:
-            stats = pstats.Stats(prof, stream=fh)
-            stats.sort_stats(SortKey.CUMULATIVE)
-            stats.print_stats()
-
-    yield app, services
+    app, services = fastapi_app(config_path, RESOURCES_DIR, mount_front=False)
+    yield app
     services.watcher.stop()
-
-
-@pytest.fixture(name="app")
-def app_fixture(app_and_services: tuple[FastAPI, Services]) -> FastAPI:
-    return app_and_services[0]
-
-
-@pytest.fixture
-def services(app_and_services: tuple[FastAPI, Services]) -> Services:
-    return app_and_services[1]
-
-
-@pytest.fixture
-def study_service(services: Services) -> StudyService:
-    return services.study
 
 
 @pytest.fixture(name="client")
@@ -136,13 +108,6 @@ def admin_access_token_fixture(client: TestClient) -> str:
     res.raise_for_status()
     credentials = res.json()
     return t.cast(str, credentials["access_token"])
-
-
-@pytest.fixture
-def admin_client(client: TestClient, admin_access_token: str) -> TestClient:
-    headers = {"Authorization": f"Bearer {admin_access_token}"}
-    client.headers.update(headers)
-    return client
 
 
 @pytest.fixture(name="user_access_token")
