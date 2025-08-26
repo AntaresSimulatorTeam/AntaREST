1eea66d3d7b77b376fb1c72f0afbfaadd3a21cdd
Theo Pascoli <48944759+TheoPascoli@users.noreply.github.com>
2025-08-26 17:09:51 +0200

    feat(layer): storage isolation (#2677)
    Moves layer related methods to a new layer_manager. This manager
    implements the DAO pattern.
    
    Adds 3 commands :
    - CreateLayer
    - RemoveLayer
    - UpdateLayerName
    
    There is still something to do regarding layers, it is the updates of
    all the areas associated with a given layer. However, this will be done
    when the Area has been refactored with the new models.

--- antarest/study/business/area_management.py
+++ antarest/study/business/area_management.py
@@ -11,17 +11,16 @@
 # This file is part of the Antares project.
 
 import logging
-import re
-from typing import Any, Dict, List, Mapping, Optional, Sequence
+from typing import Any, Dict, List, Mapping, Optional
 
-from antarest.core.exceptions import ConfigFileNotFound, DuplicateAreaName, LayerNotAllowedToBeDeleted, LayerNotFound
+from antarest.core.exceptions import ConfigFileNotFound, DuplicateAreaName, LayerNotFound
 from antarest.core.model import JSON
+from antarest.study.business.areas.area_utils import _get_area_layers, _get_ui_info_map
 from antarest.study.business.model.area_model import (
     AreaCreationDTO,
     AreaInfoDTO,
     AreaOutput,
     AreaType,
-    LayerInfoDTO,
     UpdateAreaUi,
 )
 from antarest.study.business.model.area_properties_model import (
@@ -32,7 +31,6 @@ from antarest.study.business.study_interface import StudyInterface
 from antarest.study.storage.rawstudy.model.filesystem.config.area import (
     AreaFolder,
     ThermalAreasProperties,
-    UIProperties,
 )
 from antarest.study.storage.rawstudy.model.filesystem.config.identifier import transform_name_to_id
 from antarest.study.storage.rawstudy.model.filesystem.config.model import Area, DistrictSet
@@ -51,47 +49,6 @@ logger = logging.getLogger(__name__)
 
 _ALL_AREAS_PATH = "input/areas"
 _THERMAL_AREAS_PATH = "input/thermal/areas"
-
-
-def _get_ui_info_map(file_study: FileStudy, area_ids: Sequence[str]) -> Dict[str, Any]:
-    """
-    Get the UI information (a JSON object) for each selected Area.
-
-    Args:
-        file_study: A file study from which the configuration can be read.
-        area_ids: List of selected area IDs.
-
-    Returns:
-        Dictionary where keys are IDs, and values are UI objects.
-
-    Raises:
-        ChildNotFoundError: if one of the Area IDs is not found in the configuration.
-    """
-    # If there is no ID, it is better to return an empty dictionary
-    # instead of raising an obscure exception.
-    if not area_ids:
-        return {}
-
-    ui_info_map = file_study.tree.get(["input", "areas", ",".join(area_ids), "ui"])
-
-    # If there is only one ID in the `area_ids`, the result returned from
-    # the `file_study.tree.get` call will be a single UI object.
-    # On the other hand, if there are multiple values in `area_ids`,
-    # the result will be a dictionary where the keys are the IDs,
-    # and the values are the corresponding UI objects.
-    if len(area_ids) == 1:
-        ui_info_map = {area_ids[0]: ui_info_map}
-
-    # Convert to UIProperties to ensure that the UI object is valid.
-    ui_info_map = {area_id: UIProperties(**ui_info).to_config() for area_id, ui_info in ui_info_map.items()}
-
-    return ui_info_map
-
-
-def _get_area_layers(area_uis: Dict[str, Any], area: str) -> List[str]:
-    if area in area_uis and "ui" in area_uis[area] and "layers" in area_uis[area]["ui"]:
-        return re.split(r"\s+", (str(area_uis[area]["ui"]["layers"]) or ""))
-    return []
 
 
 class AreaManager:
@@ -254,28 +211,6 @@ class AreaManager:
         area_ids = list(file_study.config.areas)
         return _get_ui_info_map(file_study, area_ids)
 
-    def get_layers(self, study: StudyInterface) -> List[LayerInfoDTO]:
-        file_study = study.get_files()
-        area_ids = list(file_study.config.areas)
-        ui_info_map = _get_ui_info_map(file_study, area_ids)
-        layers = file_study.tree.get(["layers", "layers", "layers"])
-        if not layers:
-            layers["0"] = "All"
-        return [
-            LayerInfoDTO(
-                id=str(layer),
-                name=layers[str(layer)],
-                areas=[
-                    area
-                    for area in ui_info_map
-                    if str(layer) in _get_area_layers(ui_info_map, area)
-                    # the layer 0 always display all areas
-                    or str(layer) == "0"
-                ],
-            )
-            for layer in layers
-        ]
-
     def update_layer_areas(self, study: StudyInterface, layer_id: str, areas: List[str]) -> None:
         logger.info(f"Updating layer {layer_id} with areas {areas}")
         file_study = study.get_files()
@@ -339,69 +274,6 @@ class AreaManager:
             commands.extend(create_update_commands(area))
 
         study.add_commands(commands)
-
-    def update_layer_name(self, study: StudyInterface, layer_id: str, layer_name: str) -> None:
-        logger.info(f"Updating layer {layer_id} with name {layer_name}")
-        file_study = study.get_files()
-        layers = file_study.tree.get(["layers", "layers", "layers"])
-        if layer_id not in [str(layer) for layer in list(layers.keys())]:
-            raise LayerNotFound
-        command = UpdateConfig(
-            target=f"layers/layers/layers/{layer_id}",
-            data=layer_name,
-            command_context=self._command_context,
-            study_version=study.version,
-        )
-        study.add_commands([command])
-
-    def create_layer(self, study: StudyInterface, layer_name: str) -> str:
-        file_study = study.get_files()
-        layers = file_study.tree.get(["layers", "layers", "layers"])
-        command_context = self._command_context
-        new_id = max((int(layer) for layer in layers), default=0) + 1
-        if new_id == 1:
-            command = UpdateConfig(
-                target="layers/layers/layers",
-                data={"0": "All", "1": layer_name},
-                command_context=command_context,
-                study_version=study.version,
-            )
-        else:
-            command = UpdateConfig(
-                target=f"layers/layers/layers/{new_id}",
-                data=layer_name,
-                command_context=command_context,
-                study_version=study.version,
-            )
-        study.add_commands([command])
-        return str(new_id)
-
-    def remove_layer(self, study: StudyInterface, layer_id: str) -> None:
-        """
-        Remove a layer from a study.
-
-        Raises:
-            LayerNotAllowedToBeDeleted: If the layer ID is "0".
-            LayerNotFound: If the layer ID is not found.
-        """
-        if layer_id == "0":
-            raise LayerNotAllowedToBeDeleted
-
-        file_study = study.get_files()
-        layers = file_study.tree.get(["layers", "layers", "layers"])
-
-        if layer_id not in layers:
-            raise LayerNotFound
-
-        del layers[layer_id]
-
-        command = UpdateConfig(
-            target="layers/layers/layers",
-            data=layers,
-            command_context=self._command_context,
-            study_version=study.version,
-        )
-        study.add_commands([command])
 
     def create_area(self, study: StudyInterface, area_creation_info: AreaCreationDTO) -> AreaInfoDTO:
         file_study = study.get_files()
