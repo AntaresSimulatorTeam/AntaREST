6e14a2e2f43568bc0a279d94937018612ef4771d
belthlemar <martin.belthle@rte-france.com>
2025-08-28 16:28:12 +0200

    use dao

--- antarest/study/dao/file/file_study_user_resources_dao.py
+++ antarest/study/dao/file/file_study_user_resources_dao.py
@@ -11,12 +11,19 @@
 # This file is part of the Antares project.
 from abc import ABC, abstractmethod
 from pathlib import PurePosixPath
+from typing import Any, cast
 
 from typing_extensions import override
 
+from antarest.core.exceptions import ChildNotFoundError, ResourceDeletionNotAllowed
 from antarest.study.business.model.user_model import ResourceType
 from antarest.study.dao.api.user_resources_dao import UserResourcesDao
 from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
+from antarest.study.storage.rawstudy.model.filesystem.root.user.user import User
+
+
+def is_url_writeable(user_node: User, url: list[str]) -> bool:
+    return url[0] not in [file.filename for file in user_node.registered_files]
 
 
 class FileStudyUserResourceDao(UserResourcesDao, ABC):
@@ -26,7 +33,22 @@ class FileStudyUserResourceDao(UserResourcesDao, ABC):
 
     @override
     def save_user_resource(self, resource_type: ResourceType, resource_path: PurePosixPath) -> None:
-        raise NotImplementedError
+        url = [item for item in resource_path.parts if item]
+        study_tree = self.get_file_study().tree
+        user_node = cast(User, study_tree.get_node(["user"]))
+        if not is_url_writeable(user_node, url):
+            raise ResourceDeletionNotAllowed(f"you are not allowed to create a resource here: {resource_path}")
+        try:
+            study_tree.get_node(["user"] + url)
+        except ChildNotFoundError:
+            # Creates the tree recursively to be able to create a resource inside a non-existing folder.
+            last_value = b"" if resource_type == ResourceType.FILE else {}
+            nested_dict: dict[str, Any] = {url[-1]: last_value}
+            for key in reversed(url[:-1]):
+                nested_dict = {key: nested_dict}
+            study_tree.save({"user": nested_dict})
+        else:
+            raise ResourceDeletionNotAllowed(f"the given resource already exists: {resource_path}")
 
     @override
     def delete_user_resource(self, resource_path: PurePosixPath) -> None:
--- antarest/study/storage/variantstudy/model/command/create_user_resource.py
+++ antarest/study/storage/variantstudy/model/command/create_user_resource.py
@@ -9,21 +9,17 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-from typing import List, Optional, cast
+from pathlib import PurePosixPath
+from typing import List, Optional
 
 from typing_extensions import override
 
-from antarest.core.exceptions import ChildNotFoundError
-from antarest.core.model import JSON
-from antarest.study.business.model.user_model import CreateUserResourceData, ResourceType
-from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
-from antarest.study.storage.rawstudy.model.filesystem.root.user.user import User
+from antarest.study.business.model.user_model import CreateUserResourceData
+from antarest.study.dao.api.study_dao import StudyDao
 from antarest.study.storage.variantstudy.model.command.common import (
     CommandName,
     CommandOutput,
-    command_failed,
     command_succeeded,
-    is_url_writeable,
 )
 from antarest.study.storage.variantstudy.model.command.icommand import ICommand
 from antarest.study.storage.variantstudy.model.command_listener.command_listener import ICommandListener
@@ -46,26 +42,11 @@ class CreateUserResource(ICommand):
     data: CreateUserResourceData
 
     @override
-    def _apply(self, study_data: FileStudy, listener: Optional[ICommandListener] = None) -> CommandOutput:
+    def _apply_dao(self, study_data: StudyDao, listener: Optional[ICommandListener] = None) -> CommandOutput:
         resource_type = self.data.resource_type
-        res_path = self.data.path
-        url = [item for item in res_path.split("/") if item]
-        study_tree = study_data.tree
-        user_node = cast(User, study_tree.get_node(["user"]))
-        if not is_url_writeable(user_node, url):
-            return command_failed(message=f"you are not allowed to create a resource here: {res_path}")
-        try:
-            study_tree.get_node(["user"] + url)
-        except ChildNotFoundError:
-            # Creates the tree recursively to be able to create a resource inside a non-existing folder.
-            last_value = b"" if resource_type == ResourceType.FILE else {}
-            nested_dict: JSON = {url[-1]: last_value}
-            for key in reversed(url[:-1]):
-                nested_dict = {key: nested_dict}
-            study_tree.save({"user": nested_dict})
-        else:
-            return command_failed(message=f"the given resource already exists: {res_path}")
-        return command_succeeded(message=f"{resource_type} {res_path} has been successfully created. ")
+        resource_path = PurePosixPath(self.data.path)
+        study_data.save_user_resource(resource_type, resource_path)
+        return command_succeeded(message=f"{resource_type} {resource_path} has been successfully created. ")
 
     @override
     def to_dto(self) -> CommandDTO:
