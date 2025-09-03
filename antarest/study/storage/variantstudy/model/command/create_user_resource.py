cf02162690610251feb1b7a76cf1ab7862003e14
MartinBelthle <martin.belthle@rte-france.com>
2025-09-03 09:25:04 +0200

    refactor(user): introduce dao for user commands (#2692)
    This PR also allows users to create a file and giving its content. This
    way we avoid using the command `UpdateRawFile`

--- antarest/study/storage/variantstudy/model/command/create_user_resource.py
+++ antarest/study/storage/variantstudy/model/command/create_user_resource.py
@@ -9,36 +9,20 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-from enum import StrEnum
-from typing import List, Optional, cast
+from typing import List, Optional
 
 from typing_extensions import override
 
-from antarest.core.exceptions import ChildNotFoundError
-from antarest.core.model import JSON
-from antarest.core.serde import AntaresBaseModel
-from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
-from antarest.study.storage.rawstudy.model.filesystem.root.user.user import User
+from antarest.study.business.model.user_model import UserResourceDataCreation
+from antarest.study.dao.api.study_dao import StudyDao
 from antarest.study.storage.variantstudy.model.command.common import (
     CommandName,
     CommandOutput,
-    command_failed,
     command_succeeded,
-    is_url_writeable,
 )
 from antarest.study.storage.variantstudy.model.command.icommand import ICommand
 from antarest.study.storage.variantstudy.model.command_listener.command_listener import ICommandListener
 from antarest.study.storage.variantstudy.model.model import CommandDTO
-
-
-class ResourceType(StrEnum):
-    FILE = "file"
-    FOLDER = "folder"
-
-
-class CreateUserResourceData(AntaresBaseModel):
-    path: str
-    resource_type: ResourceType
 
 
 class CreateUserResource(ICommand):
@@ -54,35 +38,18 @@ class CreateUserResource(ICommand):
     # Command parameters
     # ==================
 
-    data: CreateUserResourceData
+    data: UserResourceDataCreation
 
     @override
-    def _apply(self, study_data: FileStudy, listener: Optional[ICommandListener] = None) -> CommandOutput:
-        resource_type = self.data.resource_type
-        res_path = self.data.path
-        url = [item for item in res_path.split("/") if item]
-        study_tree = study_data.tree
-        user_node = cast(User, study_tree.get_node(["user"]))
-        if not is_url_writeable(user_node, url):
-            return command_failed(message=f"you are not allowed to create a resource here: {res_path}")
-        try:
-            study_tree.get_node(["user"] + url)
-        except ChildNotFoundError:
-            # Creates the tree recursively to be able to create a resource inside a non-existing folder.
-            last_value = b"" if resource_type == ResourceType.FILE else {}
-            nested_dict: JSON = {url[-1]: last_value}
-            for key in reversed(url[:-1]):
-                nested_dict = {key: nested_dict}
-            study_tree.save({"user": nested_dict})
-        else:
-            return command_failed(message=f"the given resource already exists: {res_path}")
-        return command_succeeded(message=f"{resource_type} {res_path} has been successfully created. ")
+    def _apply_dao(self, study_data: StudyDao, listener: Optional[ICommandListener] = None) -> CommandOutput:
+        study_data.save_user_resource(self.data)
+        return command_succeeded(message=f"{self.data.resource_type} {self.data.path} has been successfully created.")
 
     @override
     def to_dto(self) -> CommandDTO:
         return CommandDTO(
             action=self.command_name.value,
-            args={"data": self.data.model_dump(mode="json")},
+            args={"data": self.data.model_dump(mode="json", exclude_none=True)},
             study_version=self.study_version,
         )
 
