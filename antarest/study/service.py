64b0ac03c6890825cffb65e549fe1994eab7c5a0
TheoPascoli <theo.pascoli@rte-france.com>
2025-09-10 10:20:49 +0200

    Reapply "feat(matrix): add arrow format for input matrices inside GET `/raw` endpoint (#2624)"
    This reverts commit 78e02c47946afba5f533d7610282231ca1fa24a3.

--- antarest/study/service.py
+++ antarest/study/service.py
@@ -124,10 +124,11 @@ from antarest.study.repository import (
 from antarest.study.storage.matrix_profile import adjust_matrix_columns_index
 from antarest.study.storage.rawstudy.model.filesystem.config.model import FileStudyTreeConfigDTO
 from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
+from antarest.study.storage.rawstudy.model.filesystem.folder_node import FolderNode
 from antarest.study.storage.rawstudy.model.filesystem.ini_file_node import IniFileNode
 from antarest.study.storage.rawstudy.model.filesystem.inode import INode, OriginalFile
 from antarest.study.storage.rawstudy.model.filesystem.matrix.input_series_matrix import InputSeriesMatrix
-from antarest.study.storage.rawstudy.model.filesystem.matrix.matrix import imports_matrix_from_bytes
+from antarest.study.storage.rawstudy.model.filesystem.matrix.matrix import MatrixNode, imports_matrix_from_bytes
 from antarest.study.storage.rawstudy.model.filesystem.matrix.output_series_matrix import OutputSeriesMatrix
 from antarest.study.storage.rawstudy.model.filesystem.raw_file_node import RawFileNode
 from antarest.study.storage.rawstudy.raw_study_service import RawStudyService
@@ -2353,3 +2354,27 @@ class StudyService:
         It will put every matrix in the study in the matrix-store.
         """
         self.storage_service.get_storage(study).get_raw(study).tree.normalize()
+
+    def get_raw_content(self, uuid: str, path: str, depth: int, formatted: bool) -> Any:
+        """
+        Returns the content of a node based on the provided arguments.
+        """
+        study = self.get_study(uuid)
+        assert_permission(study, StudyPermissionType.READ)
+        file_study = self.get_file_study(study)
+        url = [item for item in path.split("/") if item]
+        node = file_study.tree.get_node(url)
+
+        if isinstance(node, MatrixNode):
+            return node.parse_as_dataframe()
+
+        # Minor optimization: in order to not restart the search from the root
+        # in the specific case of folders, we can assume there is no
+        # "remaining parts" in the URL.
+        # This is a technical debt because this is clearly an implicit contract.
+        # Should be solved by returning remaining parts after a search, for ex.
+        if isinstance(node, FolderNode):
+            return node.get(url=[], depth=depth)
+
+        # Else we have to restart from the root to guarantee the right behaviour
+        return file_study.tree.get(url, depth=depth, formatted=formatted)
