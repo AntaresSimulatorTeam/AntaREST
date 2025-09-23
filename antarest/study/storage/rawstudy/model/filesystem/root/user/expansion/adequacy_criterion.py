1c856a04d4a8f069022cc041d03c2c8184dd715b
belthlemar <martin.belthle@rte-france.com>
2025-09-23 10:35:22 +0200

    cleaning

--- antarest/study/storage/rawstudy/model/filesystem/root/user/expansion/adequacy_criterion.py
+++ antarest/study/storage/rawstudy/model/filesystem/root/user/expansion/adequacy_criterion.py
@@ -11,17 +11,12 @@
 # This file is part of the Antares project.
 from typing_extensions import override
 
-from antarest.matrixstore.matrix_uri_mapper import MatrixUriMapper
-from antarest.study.storage.rawstudy.model.filesystem.config.model import FileStudyTreeConfig
 from antarest.study.storage.rawstudy.model.filesystem.folder_node import FolderNode
 from antarest.study.storage.rawstudy.model.filesystem.inode import TREE
 from antarest.study.storage.rawstudy.model.filesystem.raw_file_node import RawFileNode
 
 
 class ExpansionAdequacyCriterion(FolderNode):
-    def __init__(self, matrix_mapper: MatrixUriMapper, config: FileStudyTreeConfig):
-        super().__init__(matrix_mapper, config)
-
     @override
     def build(self) -> TREE:
         return {"adequacy_criterion": RawFileNode(self.matrix_mapper, self.config.next_file("adequacy_criterion.yml"))}
