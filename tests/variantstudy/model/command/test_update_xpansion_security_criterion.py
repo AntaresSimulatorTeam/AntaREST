25d7081cddf1040859d346be588add11e6563343
belthlemar <martin.belthle@rte-france.com>
2025-09-23 15:16:52 +0200

    start writing an UT

---
+++ tests/variantstudy/model/command/test_update_xpansion_security_criterion.py
@@ -0,0 +1,154 @@
+# Copyright (c) 2025, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+
+
+from antarest.study.business.model.xpansion_model import (
+    XpansionSecurityCriterionUpdate,
+    XpansionSecurityPattern,
+    XpansionSettingsUpdate,
+)
+from antarest.study.model import STUDY_VERSION_8_7
+from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
+from antarest.study.storage.variantstudy.model.command.create_area import CreateArea
+from antarest.study.storage.variantstudy.model.command.update_xpansion_security_criterion import (
+    UpdateXpansionSecurityCriterion,
+)
+from antarest.study.storage.variantstudy.model.command.update_xpansion_settings import UpdateXpansionSettings
+from antarest.study.storage.variantstudy.model.command_context import CommandContext
+
+
+class TestUpdateXpansionSettings:
+    @staticmethod
+    def set_up(empty_study: FileStudy, command_context: CommandContext):
+        empty_study.tree.save(
+            {
+                "user": {
+                    "expansion": {
+                        "capa": {},
+                        "weights": {},
+                        "constraints": {},
+                        "settings": {},
+                        "candidates": {},
+                        "adequacy_criterion": {
+                            "adequacy_criterion": {"stopping_threshold": 3, "criterion_count_threshold": 1.2}
+                        },
+                    }
+                }
+            }
+        )
+        cmd1 = CreateArea(area_name="FR", command_context=command_context, study_version=empty_study.config.version)
+        cmd2 = CreateArea(area_name="de", command_context=command_context, study_version=empty_study.config.version)
+        cmd1.apply(empty_study)
+        cmd2.apply(empty_study)
+
+    def test_nominal_case(self, empty_study_870: FileStudy, command_context: CommandContext):
+        empty_study = empty_study_870
+        self.set_up(empty_study, command_context)
+
+        # Add some patterns
+        new_criterion = XpansionSecurityCriterionUpdate(
+            patterns=[
+                XpansionSecurityPattern(area="fr", criterion=2.3),
+                XpansionSecurityPattern(area="de", criterion=11),
+            ]
+        )
+        cmd = UpdateXpansionSecurityCriterion(
+            criterion=new_criterion,
+            command_context=command_context,
+            study_version=STUDY_VERSION_8_7,
+        )
+        output = cmd.apply(study_data=empty_study)
+        assert output.status, output.message
+
+        # Checks the file content
+        file_path = (
+            empty_study.config.study_path / "user" / "expansion" / "adequacy_criterion" / "adequacy_criterion.yml"
+        )
+        assert (
+            file_path.read_text()
+            == """criterion_count_threshold: 1.2
+patterns:
+- area: fr
+  criterion: 2.3
+- area: de
+  criterion: 11.0
+stopping_threshold: 3.0
+"""
+        )
+
+        # Modify the criterion_count_threshold only
+        cmd = UpdateXpansionSecurityCriterion(
+            criterion=XpansionSecurityCriterionUpdate(criterion_count_threshold=4),
+            command_context=command_context,
+            study_version=STUDY_VERSION_8_7,
+        )
+        output = cmd.apply(study_data=empty_study)
+        assert output.status, output.message
+
+        # Checks the file content
+        assert (
+            file_path.read_text()
+            == """criterion_count_threshold: 4.0
+patterns:
+- area: fr
+  criterion: 2.3
+- area: de
+  criterion: 11.0
+stopping_threshold: 3.0
+"""
+        )
+
+        # Removes a pattern
+        cmd = UpdateXpansionSecurityCriterion(
+            criterion=XpansionSecurityCriterionUpdate(patterns=[XpansionSecurityPattern(area="fr", criterion=6.1)]),
+            command_context=command_context,
+            study_version=STUDY_VERSION_8_7,
+        )
+        output = cmd.apply(study_data=empty_study)
+        assert output.status, output.message
+
+        # Checks the file content
+        assert (
+            file_path.read_text()
+            == """criterion_count_threshold: 4.0
+patterns:
+- area: fr
+  criterion: 6.1
+stopping_threshold: 3.0
+"""
+        )
+
+    def test_error_cases(self, empty_study_870: FileStudy, command_context: CommandContext):
+        empty_study = empty_study_870
+        self.set_up(empty_study, command_context)
+
+        # Try to update with a non-existing weight
+        new_constraint = XpansionSettingsUpdate.model_validate({"yearly_weights": "test"})
+        cmd = UpdateXpansionSettings(
+            settings=new_constraint,
+            command_context=command_context,
+            study_version=STUDY_VERSION_8_7,
+        )
+        output = cmd.apply(study_data=empty_study)
+        assert output.status is False
+        assert "Additional weights file 'test' does not exist" in output.message
+
+        # Try to update with a non-existing constraint
+        new_constraint = XpansionSettingsUpdate.model_validate({"additional_constraints": "test"})
+        cmd = UpdateXpansionSettings(
+            settings=new_constraint,
+            command_context=command_context,
+            study_version=STUDY_VERSION_8_7,
+        )
+        output = cmd.apply(study_data=empty_study)
+        assert output.status is False
+        assert "Additional constraints file 'test' does not exist" in output.message
