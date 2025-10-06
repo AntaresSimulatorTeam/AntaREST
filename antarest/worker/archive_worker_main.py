413cfd80cc25177fd095dc2f85cdf2ce306f8291
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2025-10-06 11:15:23 +0200

    add comment
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- antarest/worker/archive_worker_main.py
+++ antarest/worker/archive_worker_main.py
@@ -1,7 +1,8 @@
 import multiprocessing
 
 if __name__ == "__main__":
-    # Freeze support needed for pyinstaller
+    # Freeze support needed for pyinstaller. Important to run it before other imports,
+    # because some of them call multiprocessing functions.
     multiprocessing.freeze_support()
 
     from antarest.worker.archive_worker_service import run_archive_worker
