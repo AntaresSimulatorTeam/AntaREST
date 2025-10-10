640500e8d89a4448f70b7a3fcec46be9e2b5bdc8
Anis SMAIL <anis.smail_externe@rte-france.com>
2025-10-10 14:48:17 +0200

    chore(release): v2.25.0 (2025-10-14)

--- antarest/__init__.py
+++ antarest/__init__.py
@@ -20,9 +20,9 @@ from pathlib import Path
 
 # Standard project metadata
 
-__version__ = "2.24.0"
+__version__ = "2.25.0"
 __author__ = "RTE, Antares Web Team"
-__date__ = "2025-09-08"
+__date__ = "2025-10-14"
 # noinspection SpellCheckingInspection
 __credits__ = "(c) Réseau de Transport de l’Électricité (RTE)"
 
