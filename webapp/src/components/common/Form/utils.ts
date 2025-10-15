b1925f83c6e792185b2d9840e59f94200ba4a7ad
Samir Kamal <1954121+skamril@users.noreply.github.com>
2025-10-15 10:36:32 +0200

    refactor(ui): update RHF version and use the new feature to get dirty values (#2768)

--- webapp/src/components/common/Form/utils.ts
+++ webapp/src/components/common/Form/utils.ts
@@ -24,46 +24,6 @@ export function toAutoSubmitConfig(value: FormProps["autoSubmit"]): AutoSubmitCo
   };
 }
 
-type UnknownArrayOrObject = unknown[] | Record<string, unknown>;
-
-// From https://github.com/react-hook-form/react-hook-form/discussions/1991#discussioncomment-351784
-// With little TS fixes.
-export function getDirtyValues(
-  dirtyFields: UnknownArrayOrObject | true,
-  allValues: UnknownArrayOrObject,
-): UnknownArrayOrObject {
-  // NOTE: Recursive function.
-
-  // Object with index key is considered as array by react-hook-form
-  if (Array.isArray(dirtyFields) && RA.isPlainObj(allValues)) {
-    return dirtyFields.reduce((acc: Record<string, unknown>, v, index) => {
-      if (v === true) {
-        acc[index] = (allValues as Record<string, unknown>)[index];
-      }
-      return acc;
-    }, {});
-  }
-
-  // If *any* item in an array was modified, the entire array must be submitted, because there's no
-  // way to indicate "placeholders" for unchanged elements. `dirtyFields` is `true` for leaves.
-  if (dirtyFields === true || Array.isArray(dirtyFields)) {
-    return allValues;
-  }
-
-  // Here, we have an object.
-  return Object.fromEntries(
-    Object.keys(dirtyFields)
-      .filter((key) => dirtyFields[key] !== false)
-      .map((key) => [
-        key,
-        getDirtyValues(
-          dirtyFields[key] as UnknownArrayOrObject | true,
-          (allValues as Record<string, unknown>)[key] as UnknownArrayOrObject,
-        ),
-      ]),
-  );
-}
-
 // From https://github.com/react-hook-form/react-hook-form/blob/master/src/utils/stringToPath.ts
 export function stringToPath(input: string): string[] {
   return input
