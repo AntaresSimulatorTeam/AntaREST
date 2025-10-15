cf0d274f5dcfd63423e52a70c8c17bcaa1c35dc2
belthlemar <martin.belthle@rte-france.com>
2025-10-15 11:50:46 +0200

    add override

--- antarest/study/storage/variantstudy/model/command/create_user_resource.py
+++ antarest/study/storage/variantstudy/model/command/create_user_resource.py
@@ -57,5 +57,6 @@ class CreateUserResource(ICommand):
     def get_inner_matrices(self) -> List[str]:
         return []
 
+    @override
     def get_inner_blobs(self) -> List[str]:
         return [self.data.blob_id]
