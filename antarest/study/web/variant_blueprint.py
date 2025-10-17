1110d6d9888d6a077ef61aa39e50fd6dcf4963e2
belthlemar <martin.belthle@rte-france.com>
2025-10-17 09:27:33 +0200

    remove update config cmd

--- antarest/study/web/variant_blueprint.py
+++ antarest/study/web/variant_blueprint.py
@@ -22,9 +22,8 @@ from antarest.core.tasks.model import TaskDTO
 from antarest.core.utils.utils import sanitize_string, sanitize_uuid
 from antarest.core.utils.web import APITag
 from antarest.login.auth import Auth
-from antarest.study.model import StudyAdditionalData, StudyMetadataDTO
+from antarest.study.model import StudyMetadataDTO
 from antarest.study.service import StudyService
-from antarest.study.storage.variantstudy.model.command.update_config import UpdateConfig
 from antarest.study.storage.variantstudy.model.model import CommandDTOAPI, VariantTreeDTO
 
 logger = logging.getLogger(__name__)
@@ -68,32 +67,7 @@ def create_study_variant_routes(
         sanitized_uuid = sanitize_uuid(uuid)
         logger.info(f"Creating new variant '{name}' from study {uuid}")
         variant_study = variant_study_service.create_variant_study(uuid=sanitized_uuid, name=name)
-
-        author = study_service.get_user_name()
-        parent_author = (variant_study.additional_data or StudyAdditionalData()).author
-        if author != parent_author:
-            command_context = study_service.storage_service.variant_study_service.command_factory.command_context
-            study_service.apply_commands(
-                variant_study.id,
-                [
-                    UpdateConfig(
-                        target="study",
-                        data={
-                            "antares": {
-                                "version": variant_study.version,
-                                "caption": variant_study.name,
-                                "created": variant_study.get_created_at_timestamp(),
-                                "lastsave": variant_study.get_updated_at_timestamp(),
-                                "author": author,
-                                "editor": author,
-                            }
-                        },
-                        command_context=command_context,
-                        study_version=variant_study.version,
-                    ).to_dto()
-                ],
-            )
-        return str(variant_study.id)
+        return variant_study.id
 
     @bp.get(
         "/studies/{uuid}/variants",
