67d0e00d1f089f4b34eb465042f6092e594feb94
hdinia <33469289+hdinia@users.noreply.github.com>
2025-10-21 11:36:09 +0200

    form styles

--- webapp/src/components/App/Singlestudy/explore/Modelization/BindingConstraints/BindingConstView/ConstraintForm/ConstraintFields.tsx
+++ webapp/src/components/App/Singlestudy/explore/Modelization/BindingConstraints/BindingConstView/ConstraintForm/ConstraintFields.tsx
@@ -26,6 +26,7 @@ import {
   OUTPUT_FILTERS_OPTIONS,
   TIME_STEPS_OPTIONS,
 } from "../utils";
+import { Box } from "@mui/material";
 
 interface Props {
   study: StudyMetadata;
@@ -42,88 +43,91 @@ function ConstraintFields({ study }: Props) {
   ////////////////////////////////////////////////////////////////
 
   return (
-    <Fieldset>
-      <SwitchFE
-        name="enabled"
-        label={t("study.modelization.bindingConst.enabled")}
-        control={control}
-        sx={{
-          "& .MuiFormControlLabel-root": {
-            ml: 0,
-          },
-        }}
-      />
-      <StringFE
-        disabled
-        name="name"
-        label={t("global.name")}
-        control={control}
-        rules={{ validate: (v) => validateString(v) }}
-      />
-      {studyVersion >= 870 && (
-        <StringFE
-          name="group"
-          label={t("global.group")}
+    <Box sx={{ my: 1, width: 1 }}>
+      <Box sx={{ display: "flex", gap: 1, flexWrap: "wrap", alignItems: "center" }}>
+        <SwitchFE
+          name="enabled"
+          label={t("study.modelization.bindingConst.enabled")}
           control={control}
-          rules={{
-            validate: (v) => {
-              if (typeof v === "string") {
-                return validateString(v, {
-                  maxLength: 20,
-                  specialChars: "-",
-                });
-              }
+          sx={{
+            "& .MuiFormControlLabel-root": {
+              ml: 0,
             },
           }}
         />
-      )}
-      <SelectFE
-        name="timeStep"
-        label={t("study.modelization.bindingConst.type")}
-        variant="outlined"
-        options={TIME_STEPS_OPTIONS}
-        control={control}
-        sx={{ minWidth: 195 }}
-      />
-      <SelectFE
-        name="operator"
-        label={t("study.modelization.bindingConst.operator")}
-        variant="outlined"
-        options={OPERATOR_OPTIONS}
-        control={control}
-        sx={{ minWidth: 195 }}
-      />
-      {studyVersion >= 830 && (
-        <>
-          <Fieldset.Break />
-          <SelectFE
-            name="filterYearByYear"
-            label={t("study.outputFilters.filterByYear")}
-            variant="outlined"
-            options={OUTPUT_FILTERS_OPTIONS}
-            multiple
-            control={control}
-            sx={{ minWidth: 195 }}
-          />
-          <SelectFE
-            name="filterSynthesis"
-            label={t("study.outputFilters.filterSynthesis")}
-            variant="outlined"
-            options={OUTPUT_FILTERS_OPTIONS}
-            multiple
-            control={control}
-            sx={{ minWidth: 195 }}
-          />
+        <StringFE
+          disabled
+          name="name"
+          label={t("global.name")}
+          control={control}
+          rules={{ validate: (v) => validateString(v) }}
+        />
+        {studyVersion >= 870 && (
           <StringFE
-            name="comments"
-            label={t("study.modelization.bindingConst.comments")}
+            name="group"
+            label={t("global.group")}
             control={control}
-            required={false}
-            sx={{ minWidth: 195 }}
+            rules={{
+              validate: (v) => {
+                if (typeof v === "string") {
+                  return validateString(v, {
+                    maxLength: 20,
+                    specialChars: "-",
+                  });
+                }
+              },
+            }}
           />
-        </>
-      )}
-    </Fieldset>
+        )}
+        <SelectFE
+          name="timeStep"
+          label={t("study.modelization.bindingConst.type")}
+          variant="outlined"
+          options={TIME_STEPS_OPTIONS}
+          control={control}
+          sx={{ minWidth: 195 }}
+        />
+        <SelectFE
+          name="operator"
+          label={t("study.modelization.bindingConst.operator")}
+          variant="outlined"
+          options={OPERATOR_OPTIONS}
+          control={control}
+          sx={{ minWidth: 100 }}
+        />
+        <Box sx={{ display: "flex", gap: 1, mt: 1, width: 1 }}>
+          {studyVersion >= 830 && (
+            <>
+              <SelectFE
+                name="filterYearByYear"
+                label={t("study.outputFilters.filterByYear")}
+                variant="outlined"
+                options={OUTPUT_FILTERS_OPTIONS}
+                multiple
+                control={control}
+                sx={{ minWidth: 195 }}
+              />
+              <SelectFE
+                name="filterSynthesis"
+                label={t("study.outputFilters.filterSynthesis")}
+                variant="outlined"
+                options={OUTPUT_FILTERS_OPTIONS}
+                multiple
+                control={control}
+                sx={{ minWidth: 195 }}
+              />
+              <StringFE
+                name="comments"
+                label={t("study.modelization.bindingConst.comments")}
+                control={control}
+                required={false}
+                sx={{ minWidth: 195 }}
+              />
+            </>
+          )}
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
