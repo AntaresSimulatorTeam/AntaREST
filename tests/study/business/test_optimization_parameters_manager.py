e9336aa1d8cbd9c91dfb16233b5c5e88bf0b0a7e
MartinBelthle <martin.belthle@rte-france.com>
2025-10-27 14:14:21 +0100

    fix(generaldata): allow reading with missing sections (#2799)

---
+++ tests/study/business/test_optimization_parameters_manager.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2025, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+
+
+from antarest.study.business.model.config.optimization_config_model import OptimizationPreferences
+from antarest.study.business.optimization_management import OptimizationManager
+from antarest.study.business.study_interface import FileStudyInterface
+from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
+from antarest.study.storage.variantstudy.model.command_context import CommandContext
+
+
+def test_missing_section(empty_study_880: FileStudy, command_context: CommandContext) -> None:
+    # Remove the section from the generaldata file
+    study = empty_study_880
+    general_data_content = study.tree.get(["settings", "generaldata"])
+    del general_data_content["optimization"]
+    study.tree.save(general_data_content, ["settings", "generaldata"])
+    # Ensures we're still able to read the data.
+    manager = OptimizationManager(command_context)
+    params = manager.get_optimization_preferences(FileStudyInterface(study))
+    assert params == OptimizationPreferences()
