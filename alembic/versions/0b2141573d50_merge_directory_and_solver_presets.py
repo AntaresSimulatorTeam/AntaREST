ee5423d449779de460417550d68f7a5503233f55
Theo Pascoli <48944759+TheoPascoli@users.noreply.github.com>
2025-10-29 17:22:40 +0100

    fix(migrations): resolve alembic divergent heads (#2815)

---
+++ alembic/versions/0b2141573d50_merge_directory_and_solver_presets.py
@@ -0,0 +1,21 @@
+"""merge directory management and solver presets branches
+
+Revision ID: 0b2141573d50
+Revises: f1ff6904ff9f, 04a098a4a38e
+Create Date: 2025-10-29 00:00:00.000000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "0b2141573d50"
+down_revision = ("f1ff6904ff9f", "04a098a4a38e")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
