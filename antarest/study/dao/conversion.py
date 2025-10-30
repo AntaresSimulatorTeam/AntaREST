85f2370ab31a1832736232615598a51d453090d0
belthlemar <martin.belthle@rte-france.com>
2025-10-30 14:44:06 +0100

    c

--- antarest/study/dao/conversion.py
+++ antarest/study/dao/conversion.py
@@ -9,10 +9,14 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
+from typing import Sequence
+
 from antares.study.version import StudyVersion
 
 from antarest.core.exceptions import ChildNotFoundError
 from antarest.matrixstore.service import ISimpleMatrixService
+from antarest.study.business.model.renewable_cluster_model import RenewableCluster
+from antarest.study.business.model.sts_model import STStorage, STStorageAdditionalConstraint
 from antarest.study.business.model.thermal_cluster_model import ThermalCluster
 from antarest.study.dao.api.study_dao import ReadOnlyStudyDao, StudyDao
 from antarest.study.model import STUDY_VERSION_8_1, STUDY_VERSION_8_6, STUDY_VERSION_8_7, STUDY_VERSION_9_2
@@ -59,22 +63,15 @@ class StudyConverter:
             thermals = area_names_and_thermals[area_id].thermals or []
             self._convert_thermal_clusters(area_id, thermals)
 
-            # todo: matrices
             # Renewables
             if self._study_version >= STUDY_VERSION_8_1:
-                self._new_dao.save_renewables(area_id, list(renewable_clusters.get(area_id, {}).values()))
-                # todo: matrices
+                renewables = list(renewable_clusters.get(area_id, {}).values())
+                self._convert_renewable_clusters(area_id, renewables)
+
             # Short-term storages
             if self._study_version >= STUDY_VERSION_8_6:
                 storages = list(st_storages[area_id].values())
-                self._new_dao.save_st_storages(area_id, storages)
-                # todo: matrices
-                if self._study_version >= STUDY_VERSION_9_2:
-                    for storage in storages:
-                        self._new_dao.save_st_storage_additional_constraints(
-                            area_id, storage.id, st_storages_constraints[area_id][storage.id]
-                        )
-                        # todo: matrices
+                self._convert_short_term_storages(area_id, storages, st_storages_constraints.get(area_id, {}))
 
     def _convert_thermal_clusters(self, area_id: str, thermals: list[ThermalCluster]) -> None:
         self._new_dao.save_thermals(area_id, thermals)
@@ -95,3 +92,53 @@ class StudyConverter:
 
                 co2_cost_id = self._matrix_service.create(self._source_dao.get_thermal_co2_cost(area_id, thermal_id))
                 self._new_dao.save_thermal_co2_cost(area_id, thermal_id, co2_cost_id)
+
+    def _convert_renewable_clusters(self, area_id: str, renewables: Sequence[RenewableCluster]) -> None:
+        self._new_dao.save_renewables(area_id, renewables)
+        for renewable in renewables:
+            series_id = self._matrix_service.create(self._source_dao.get_renewable_series(area_id, renewable.id))
+            self._new_dao.save_renewable_series(area_id, renewable.id, series_id)
+
+    def _convert_short_term_storages(
+        self, area_id: str, storages: list[STStorage], constraints: dict[str, list[STStorageAdditionalConstraint]]
+    ) -> None:
+        self._new_dao.save_st_storages(area_id, storages)
+        for st_storage in storages:
+            sts_id = st_storage.id
+            p_max_inj_id = self._matrix_service.create(self._source_dao.get_st_storage_pmax_injection(area_id, sts_id))
+            self._new_dao.save_st_storage_pmax_injection(area_id, sts_id, p_max_inj_id)
+
+            p_max_wit_id = self._matrix_service.create(self._source_dao.get_st_storage_pmax_withdrawal(area_id, sts_id))
+            self._new_dao.save_st_storage_pmax_withdrawal(area_id, sts_id, p_max_wit_id)
+
+            low_id = self._matrix_service.create(self._source_dao.get_st_storage_lower_rule_curve(area_id, sts_id))
+            self._new_dao.save_st_storage_lower_rule_curve(area_id, sts_id, low_id)
+
+            upper_id = self._matrix_service.create(self._source_dao.get_st_storage_upper_rule_curve(area_id, sts_id))
+            self._new_dao.save_st_storage_upper_rule_curve(area_id, sts_id, upper_id)
+
+            inflows_id = self._matrix_service.create(self._source_dao.get_st_storage_inflows(area_id, sts_id))
+            self._new_dao.save_st_storage_inflows(area_id, sts_id, inflows_id)
+
+            if self._study_version >= STUDY_VERSION_9_2:
+                self._new_dao.save_st_storage_additional_constraints(area_id, sts_id, constraints[sts_id])
+
+                cost_injection = self._source_dao.get_st_storage_cost_injection(area_id, sts_id)
+                injection_id = self._matrix_service.create(cost_injection)
+                self._new_dao.save_st_storage_cost_injection(area_id, sts_id, injection_id)
+
+                cost_withdrawal = self._source_dao.get_st_storage_cost_withdrawal(area_id, sts_id)
+                withdrawal_id = self._matrix_service.create(cost_withdrawal)
+                self._new_dao.save_st_storage_cost_withdrawal(area_id, sts_id, withdrawal_id)
+
+                cost_level = self._source_dao.get_st_storage_cost_level(area_id, sts_id)
+                cost_level_id = self._matrix_service.create(cost_level)
+                self._new_dao.save_st_storage_cost_level(area_id, sts_id, cost_level_id)
+
+                cost_var_injection = self._source_dao.get_st_storage_cost_variation_injection(area_id, sts_id)
+                cost_var_injection_id = self._matrix_service.create(cost_var_injection)
+                self._new_dao.save_st_storage_cost_variation_injection(area_id, sts_id, cost_var_injection_id)
+
+                cost_var_withdrawal = self._source_dao.get_st_storage_cost_variation_withdrawal(area_id, sts_id)
+                cost_var_withdrawal_id = self._matrix_service.create(cost_var_withdrawal)
+                self._new_dao.save_st_storage_cost_variation_withdrawal(area_id, sts_id, cost_var_withdrawal_id)
