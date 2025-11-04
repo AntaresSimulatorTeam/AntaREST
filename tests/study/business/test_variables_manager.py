903dc87cebe8877dd3d36ec6f6b3d502ceaa5ef2
belthlemar <martin.belthle@rte-france.com>
2025-11-04 16:38:04 +0100

    add other error tests

--- tests/study/business/test_variables_manager.py
+++ tests/study/business/test_variables_manager.py
@@ -45,22 +45,29 @@ AVAILABLE_VARIABLES = OutputVariablesList(
         pytest.param(OutputVariablesType.AREA, "", "", "area_from_id", None, None, None, None, "You provided an link related id for areas", id="Area with `area_from_id`"),
         pytest.param(OutputVariablesType.AREA, "", "", None, "area_to_id", None, None, None, "You provided an link related id for areas", id="Area with `area_to_id`"),
         pytest.param(OutputVariablesType.AREA, "a", "fake_area", None, None, None, None, None, "The variable 'a' does not exist for area 'fake_area' and type 'area'", id="Area does not exist"),
+        pytest.param(OutputVariablesType.AREA, "fake_variable", "fr", None, None, None, None, None, "The variable 'fake_variable' does not exist for area 'fr' and type 'area'", id="Area variable does not exist"),
         # Thermal
         pytest.param(OutputVariablesType.THERMAL, "", "area", None, None, None, None, None, "You should provide `thermal_id` for thermal clusters", id="Thermal w/o `thermal_id`"),
         pytest.param(OutputVariablesType.THERMAL, "", "area", None, None, "th", "r", None, "You provided an storage/renewable id for thermal clusters", id="Thermal with `renewable_id`"),
         pytest.param(OutputVariablesType.THERMAL, "", "area", None, None, "th", None, "sts", "You provided an storage/renewable id for thermal clusters", id="Thermal with `st_storage_id`"),
+        pytest.param(OutputVariablesType.THERMAL, "", "fr", None, None, "fake_th", None, None, "The variable '' does not exist for area 'fr' and type 'thermal'", id="Thermal does not exist"),
+        pytest.param(OutputVariablesType.THERMAL, "fake_var", "fr", None, None, "01_solar", None, None, "The variable 'fake_var' does not exist for area 'fr' and type 'thermal'", id="Thermal variable does not exist"),
         # Renewable
         pytest.param(OutputVariablesType.RENEWABLE, "", "area", None, None, None, None, None, "You should provide `renewable_id` for renewable clusters", id="Renewable w/o `renewable_id`"),
         pytest.param(OutputVariablesType.RENEWABLE, "", "area", None, None, "th", "r", None, "You provided an storage/thermal id for renewable clusters", id="Renewable with `thermal_id`"),
         pytest.param(OutputVariablesType.RENEWABLE, "", "area", None, None, None, "r", "sts", "You provided an storage/thermal id for renewable clusters", id="Renewable with `st_storage_id`"),
+        pytest.param(OutputVariablesType.RENEWABLE, "", "fr", None, None, None, "fake_re", None, "The variable '' does not exist for area 'fr' and type 'renewable'", id="Renewable does not exist"),
+        pytest.param(OutputVariablesType.RENEWABLE, "fake_var", "fr", None, None, None, "01_renew", None, "The variable 'fake_var' does not exist for area 'fr' and type 'renewable'", id="Renewable variable does not exist"),
         # Short-term storage
         pytest.param(OutputVariablesType.SHORT_TERM_STORAGE, "", "area", None, None, None, None, None, "You should provide `st_storage_id` for short-term storages", id="STS w/o `st_storage_id`"),
         pytest.param(OutputVariablesType.SHORT_TERM_STORAGE, "", "area", None, None, None, "th", "sts", "You provided an renewable/thermal id for short-term storages", id="STS with `thermal_id`"),
         pytest.param(OutputVariablesType.SHORT_TERM_STORAGE, "", "area", None, None, "r", None, "sts", "You provided an renewable/thermal id for short-term storages", id="STS with `st_storage_id`"),
+        pytest.param(OutputVariablesType.SHORT_TERM_STORAGE, "", "fr", None, None, None, None, "fake_sts", "The variable '' does not exist for area 'fr' and type 'st_storage'", id="STS does not exist"),
+        pytest.param(OutputVariablesType.SHORT_TERM_STORAGE, "fake_var", "fr", None, None, None, None, "01_sts", "The variable 'fake_var' does not exist for area 'fr' and type 'st_storage'", id="STS variable does not exist"),
     ],
 )
 #fmt: on
-def test_get_variables_view_coherence(
+def test_get_variables_view_coherence_errors(
     variable_type: OutputVariablesType,
     variable_name: str,
     area_id: str | None,
@@ -82,5 +89,5 @@ def test_get_variables_view_coherence(
             area_to_id,
             thermal_id,
             renewable_id,
-            st_storage_id,
+            st_storage_id
         )
