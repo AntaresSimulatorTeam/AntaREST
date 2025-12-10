fec4268e6f3e21075991a77126979b0caae2fc0a
belthlemar <martin.belthle@rte-france.com>
2025-12-10 15:14:02 +0100

    fix relative import issue

--- tests/storage/business/test_export.py
+++ tests/storage/business/test_export.py
@@ -16,7 +16,6 @@ from zipfile import ZipFile
 
 import pytest
 from checksumdir import dirhash
-from db_statement_recorder import DBStatementRecorder
 from py7zr import SevenZipFile, py7zr
 
 from antarest.blobstore.service import BlobService
@@ -33,6 +32,7 @@ from antarest.study.storage.variantstudy.business.matrix_constants_generator imp
 from antarest.study.storage.variantstudy.model.command.create_area import CreateArea
 from antarest.study.storage.variantstudy.model.command_context import CommandContext
 from tests.conftest import empty_study_fixture
+from tests.db_statement_recorder import DBStatementRecorder
 from tests.helpers import create_raw_study, with_db_context
 
 
