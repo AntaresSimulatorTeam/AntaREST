893ed8d93b2fbd3d4def6720b180daf597c12e0b
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2025-12-11 15:26:54 +0100

    use new funxtion in old matrix garbage collector
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- antarest/matrixstore/matrix_garbage_collector.py
+++ antarest/matrixstore/matrix_garbage_collector.py
@@ -18,13 +18,11 @@ This service runs as a background thread and periodically cleans unused matrices
 
 import logging
 import time
-from typing import Set
 
 from typing_extensions import override
 
 from antarest.core.interfaces.service import IService
-from antarest.core.utils.fastapi_sqlalchemy import db
-from antarest.core.utils.utils import StopWatch, current_time
+from antarest.maintenance.tasks.gc_matrix import clean_matrices
 from antarest.matrixstore.service import MatrixService
 
 logger = logging.getLogger(__name__)
@@ -44,44 +42,11 @@ class MatrixGarbageCollector(IService):
         self.dry_run = dry_run
         self.retention_time = retention_time
 
-    def _delete_unused_saved_matrices(self, unused_matrices: Set[str]) -> None:
-        """Delete all files with the name in unused_matrices"""
-        logger.info("Deleting unused saved matrices:")
-        for unused_matrix_id in unused_matrices:
-            logger.info(f"Matrix {unused_matrix_id} is set to be deleted")
-            if not self.dry_run:
-                logger.info(f"Deleting {unused_matrix_id}")
-                self.matrix_service.delete(unused_matrix_id)
-
-    def clean_matrices(self) -> None:
-        """Delete all matrices that are not used anymore"""
-        stopwatch = StopWatch()
-        logger.info("Beginning of the cleaning process")
-        used_matrices = {matrix.matrix_id for matrix in self.matrix_service.get_used_matrices()}
-        all_existing_matrices = self.matrix_service.get_matrices()
-        saved_matrices = {matrix.id: matrix.created_at for matrix in all_existing_matrices}
-        unused_matrices = set(saved_matrices) - used_matrices
-
-        if unused_matrices:
-            # Compare for each matrix, its lifetime duration to the `retention_time` value.
-            # If it's more, remove the matrix. Otherwise, pass.
-            matrices_to_remove = set()
-            now = current_time()
-            for matrix in unused_matrices:
-                matrix_lifetime = (now - saved_matrices[matrix]).total_seconds()
-                if matrix_lifetime >= self.retention_time:
-                    matrices_to_remove.add(matrix)
-
-            self._delete_unused_saved_matrices(unused_matrices=matrices_to_remove)
-
-        stopwatch.log_elapsed(lambda x: logger.info(f"Finished cleaning matrices in {x}s"))
-
     @override
     def _loop(self) -> None:
         while True:
             try:
-                with db():
-                    self.clean_matrices()
+                clean_matrices(self.matrix_service, self.dry_run, self.retention_time)
             except Exception as e:
                 logger.error("Error while cleaning matrices", exc_info=e)
             logger.info(f"Sleeping for {self.sleeping_time}s")
