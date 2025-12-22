d8a20a525774b8c77796cca8d22d66fe631a3507
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2025-12-22 17:36:07 +0100

    mypy
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- antarest/study/output/parquet_output_storage.py
+++ antarest/study/output/parquet_output_storage.py
@@ -13,50 +13,62 @@ from pathlib import Path
 from typing import BinaryIO, Iterator, List, Optional, Sequence
 
 import pandas as pd
+from typing_extensions import override
 
-from antarest.study.model import MatrixIndex, StudySimResultDTO
+from antarest.study.model import MatrixFrequency, MatrixIndex, StudySimResultDTO
 from antarest.study.output.output_model import OutputVariablesList
 from antarest.study.output.output_storage import IOutputStorage, OutputStorageType
 from antarest.study.output.utils import QueryFileType
-from antarest.study.storage.rawstudy.model.filesystem.matrix.matrix import MatrixFrequency
 from antarest.study.storage.rawstudy.model.filesystem.root.output.simulation.mode.mcall.digest import DigestUI
 
 
 class ParquetOutputStorage(IOutputStorage):
     @property
+    @override
     def storage_type(self) -> OutputStorageType:
         return OutputStorageType.PARQUET
 
+    @override
     def import_output(self, study_id: str, output: BinaryIO | Path, output_name: Optional[str] = None) -> Optional[str]:
         raise NotImplementedError()
 
+    @override
     def get_study_sim_result(self, study_id: str) -> List[StudySimResultDTO]:
         raise NotImplementedError()
 
+    @override
     def delete_output(self, study_id: str, output_id: str) -> None:
         raise NotImplementedError()
 
+    @override
     def export_output(self, study_id: str, output_id: str, target: Path) -> None:
         raise NotImplementedError()
 
+    @override
     def output_exists(self, study_id: str, output_id: str) -> bool:
         raise NotImplementedError()
 
+    @override
     def is_output_archived(self, study_id: str, output_id: str) -> bool:
         raise NotImplementedError()
 
+    @override
     def archive_study_output(self, study_id: str, output_id: str) -> None:
         raise NotImplementedError()
 
+    @override
     def unarchive_study_output(self, study_id: str, output_id: str) -> None:
         raise NotImplementedError()
 
+    @override
     def get_digest(self, study_id: str, output_id: str) -> DigestUI:
         raise NotImplementedError()
 
+    @override
     def get_output_time_index(self, study_id: str, output_id: str, frequency: MatrixFrequency) -> MatrixIndex:
         raise NotImplementedError()
 
+    @override
     def aggregate_output_data(
         self,
         study_id: str,
@@ -70,5 +82,6 @@ class ParquetOutputStorage(IOutputStorage):
     ) -> Iterator[pd.DataFrame]:
         raise NotImplementedError()
 
+    @override
     def extract_variables_list(self, study_id: str, output_id: str) -> OutputVariablesList:
         raise NotImplementedError()
