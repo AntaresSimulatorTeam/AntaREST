89d0bba63acf4d659ac6af5e257858f1ccb195cd
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2025-12-23 15:11:00 +0100

    add unit test for LFS
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

---
+++ tests/study/output/lfs/test_flat_dir_large_file_storage.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2025, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+from pathlib import Path
+
+import pytest
+
+from antarest.study.output.lfs.flat_dir_large_file_storage import FlatDirLargeFileStorage
+
+
+def test_lfs(tmp_path: Path):
+    storage = FlatDirLargeFileStorage(tmp_path / "lfs")
+
+    assert not storage.file_exists("unknown")
+
+    src_file_path = tmp_path / "input.txt"
+    src_file_path.write_bytes("My content".encode("utf-8"))
+    storage.write_file("my-blob", src_file_path)
+
+    assert storage.file_exists("my-blob")
+
+    target_path = tmp_path / "output.txt"
+    storage.read_file("my-blob", target_path)
+    assert target_path.read_bytes().decode("utf-8") == "My content"
+
+    storage.delete_file("my-blob")
+    assert not storage.file_exists("my-blob")
+
+    target_path = tmp_path / "output-2.txt"
+    with pytest.raises(FileNotFoundError):
+        storage.read_file("my-blob", target_path)
