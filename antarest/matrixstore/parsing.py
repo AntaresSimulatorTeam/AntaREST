260512a47231c24f15496c4704ecdd1e1d39b87a
belthlemar <martin.belthle@rte-france.com>
2026-01-07 11:31:36 +0100

    a

--- antarest/matrixstore/parsing.py
+++ antarest/matrixstore/parsing.py
@@ -14,27 +14,36 @@ from typing import cast
 
 import numpy as np
 import pandas as pd
+import polars as pl
+from polars.exceptions import ComputeError, NoDataError
 
 from antarest.core.config import InternalMatrixFormat
 from antarest.core.serde.matrix_export import write_dataframe_in_tsv_format
 
 
-def load_matrix(matrix_format: InternalMatrixFormat, path: Path, matrix_version: int) -> pd.DataFrame:
+def load_matrix(matrix_format: InternalMatrixFormat, path: Path, matrix_version: int) -> pl.DataFrame:
     if matrix_format == InternalMatrixFormat.TSV:
         # Based on the matrix version, we assume its format
         if matrix_version == 1:
-            df = pd.DataFrame(data=np.loadtxt(path, delimiter="\t", dtype=np.float64, ndmin=2))
+            df = pl.DataFrame(data=np.loadtxt(path, delimiter="\t", dtype=np.float64, ndmin=2))
         else:
             try:
-                df = pd.read_csv(path, sep="\t", header=0)
-            except pd.errors.EmptyDataError:  # `read_csv` method can fail if the dataframe is empty
-                df = pd.DataFrame()
+                df = pl.read_csv(path, n_threads=1, separator="\t", has_header=False)
+            except ComputeError:
+                # Happens for file `conversion.txt` as polars infer the data as int64, but the value is too big.
+                # In such cases, we'll read the data as a string and convert it in float64 afterward
+                df = pl.read_csv(path, n_threads=1, separator="\t", has_header=False, infer_schema=False).with_columns(
+                    pl.all().cast(pl.Float64)
+                )
+            except NoDataError:  # if the dataframe is empty
+                df = pl.DataFrame()
     elif matrix_format == InternalMatrixFormat.HDF:
         df = cast(pd.DataFrame, pd.read_hdf(path))
+        df = pl.from_pandas(df)
     elif matrix_format == InternalMatrixFormat.PARQUET:
-        df = pd.read_parquet(path)
+        df = pl.read_parquet(path)
     elif matrix_format == InternalMatrixFormat.FEATHER:
-        df = pd.read_feather(path)
+        df = pl.read_ipc(path)
     else:
         raise NotImplementedError(f"Internal matrix format '{matrix_format}' is not implemented")
 
