b07fe447b7a41a3ee8ba1b58a3fd4afc45b077d9
belthlemar <martin.belthle@rte-france.com>
2026-01-12 09:46:21 +0100

    use polars inside digest

--- antarest/core/utils/polars.py
+++ antarest/core/utils/polars.py
@@ -9,13 +9,15 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
+from typing import Any
+
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
 import polars as pl
 
 
-def create_polars_dataframe(data: npt.NDArray[np.float64] | list[list[int | float]] | pd.DataFrame) -> pl.DataFrame:
+def create_polars_dataframe(data: npt.NDArray[np.float64] | list[list[Any]] | pd.DataFrame) -> pl.DataFrame:
     if isinstance(data, list):
         data = np.array(data)
     return pl.DataFrame(data, schema=[str(i) for i in range(data.shape[1])])
--- antarest/study/storage/rawstudy/model/filesystem/root/output/simulation/mode/mcall/digest.py
+++ antarest/study/storage/rawstudy/model/filesystem/root/output/simulation/mode/mcall/digest.py
@@ -12,12 +12,13 @@
 
 from typing import List, Optional, cast
 
-import pandas as pd
+import polars as pl
 from pydantic import Field
 from typing_extensions import override
 
 from antarest.core.model import JSON
 from antarest.core.serde import AntaresBaseModel
+from antarest.core.utils.polars import create_polars_dataframe
 from antarest.matrixstore.matrix_uri_mapper import MatrixUriMapper
 from antarest.study.storage.rawstudy.model.filesystem.config.model import FileStudyTreeConfig
 from antarest.study.storage.rawstudy.model.filesystem.root.output.simulation.mode.mcall.synthesis import OutputSynthesis
@@ -36,45 +37,45 @@ class DigestUI(AntaresBaseModel):
     flow_quadratic: DigestMatrixUI = Field(alias="flowQuadratic")
 
 
-def _get_flow_linear(df: pd.DataFrame) -> DigestMatrixUI:
+def _get_flow_linear(df: pl.DataFrame) -> DigestMatrixUI:
     return _get_flow(df, "Links (FLOW LIN.)")
 
 
-def _get_flow_quadratic(df: pd.DataFrame) -> DigestMatrixUI:
+def _get_flow_quadratic(df: pl.DataFrame) -> DigestMatrixUI:
     return _get_flow(df, "Links (FLOW QUAD.)")
 
 
-def _get_flow(df: pd.DataFrame, keyword: str) -> DigestMatrixUI:
-    first_column = df["1"].tolist()
+def _get_flow(df: pl.DataFrame, keyword: str) -> DigestMatrixUI:
+    first_column = df["1"]
     index = next((k for k, v in enumerate(first_column) if v == keyword), None)
     if not index:
         return DigestMatrixUI(columns=[], data=[], groupedColumns=False)
     index_start = index + 2
     df_col_start = 1
     df_size = next((k for k, v in enumerate(first_column[index_start:]) if v == ""), len(first_column) - index_start)
-    flow_df = df.iloc[index_start : index_start + df_size, df_col_start : df_col_start + df_size]
-    data = flow_df.iloc[1:, :].to_numpy().tolist()
-    cols = [""] + flow_df.iloc[0, 1:].tolist()
+    flow_df = df.slice(index_start, df_size).select(pl.all().slice(df_col_start, df_size))
+    data = flow_df.slice(1, None).to_numpy().tolist()
+    cols = [""] + list(flow_df.row(0)[1:])
     return DigestMatrixUI(columns=cols, data=data, groupedColumns=False)
 
 
-def _build_areas_and_districts(df: pd.DataFrame, first_row: int) -> DigestMatrixUI:
-    first_column = df["1"].tolist()
-    first_area_row = df.iloc[first_row, 2:].tolist()
+def _build_areas_and_districts(df: pl.DataFrame, first_row: int) -> DigestMatrixUI:
+    first_column = df["1"]
+    first_area_row = df.row(first_row)[2:]
     col_number = next((k for k, v in enumerate(first_area_row) if v == ""), df.shape[1])
-    final_index = first_column[first_row:].index("") + first_row
-    data = df.iloc[first_row:final_index, 1 : col_number + 1].to_numpy().tolist()
-    cols_raw = df.iloc[first_row - 3 : first_row, 2 : col_number + 1].to_numpy().tolist()
+    final_index = first_column.slice(first_row, None).to_list().index("") + first_row
+    data = df.slice(first_row, final_index - first_row).select(pl.all().slice(1, col_number)).to_numpy().tolist()
+    cols_raw = df.slice(first_row - 3, 3).select(pl.all().slice(2, col_number - 1)).to_numpy().tolist()
     columns = [[""]] + [[a, b, c] for a, b, c in zip(cols_raw[0], cols_raw[1], cols_raw[2])]
     return DigestMatrixUI(columns=columns, data=data, groupedColumns=True)
 
 
-def _get_area(df: pd.DataFrame) -> DigestMatrixUI:
+def _get_area(df: pl.DataFrame) -> DigestMatrixUI:
     return _build_areas_and_districts(df, 7)
 
 
-def _get_district(df: pd.DataFrame) -> DigestMatrixUI:
-    first_column = df["1"].tolist()
+def _get_district(df: pl.DataFrame) -> DigestMatrixUI:
+    first_column = df["1"]
     first_row = next((k for k, v in enumerate(first_column) if "@" in v), None)
     if not first_row:
         return DigestMatrixUI(columns=[], data=[], groupedColumns=False)
@@ -95,7 +96,7 @@ class DigestSynthesis(OutputSynthesis):
     ) -> JSON:
         df = self._parse_digest_file()
 
-        output = df.to_dict(orient="split")
+        output = df.to_pandas().to_dict(orient="split")
         del output["index"]
         return cast(JSON, output)
 
@@ -111,7 +112,7 @@ class DigestSynthesis(OutputSynthesis):
         districts = _get_district(df)
         return DigestUI(area=area, districts=districts, flowLinear=flow_linear, flowQuadratic=flow_quadratic)
 
-    def _parse_digest_file(self) -> pd.DataFrame:
+    def _parse_digest_file(self) -> pl.DataFrame:
         """
         Parse a digest file as a whole and return a single DataFrame.
 
@@ -129,6 +130,5 @@ class DigestSynthesis(OutputSynthesis):
             # Adjust the number of columns in each row
             data = [row + [""] * (max_cols - len(row)) for row in data]
 
-            # Returns a DataFrame from the data (do not convert values to float)
-            df = pd.DataFrame(data=data, columns=[str(i) for i in range(max_cols)], dtype=object)
-            return df
+            # Returns a DataFrame from the data
+            return create_polars_dataframe(data)
