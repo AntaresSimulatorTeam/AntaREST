cb6039477d4ec2fe10a881b0be6d7e826ce4248a
belthlemar <martin.belthle@rte-france.com>
2026-01-12 17:40:15 +0100

    remove

--- antarest/matrixstore/repository.py
+++ antarest/matrixstore/repository.py
@@ -194,7 +194,6 @@ def compute_hash(df: pl.DataFrame) -> str:
     """
     # Convert polars dataframe to pandas one for backward compatibility of the hashing value.
     pandas_df = df.to_pandas()
-    pandas_df = pandas_df.replace({None: np.nan})
     if df.columns == [str(i) for i in range(len(df.columns))]:
         pandas_df.columns = pd.RangeIndex(0, pandas_df.shape[1])  # type: ignore
 
