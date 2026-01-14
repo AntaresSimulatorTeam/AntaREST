7c6e824bab03233d04af3150d968880c972f7604
belthlemar <martin.belthle@rte-france.com>
2026-01-14 16:13:56 +0100

    remove useless init

--- antarest/study/storage/rawstudy/model/filesystem/root/output/simulation/mode/mcall/digest.py
+++ antarest/study/storage/rawstudy/model/filesystem/root/output/simulation/mode/mcall/digest.py
@@ -19,7 +19,6 @@ from typing_extensions import override
 from antarest.core.model import JSON
 from antarest.core.serde import AntaresBaseModel
 from antarest.core.utils.polars import create_polars_dataframe
-from antarest.study.storage.rawstudy.model.filesystem.config.model import FileStudyTreeConfig
 from antarest.study.storage.rawstudy.model.filesystem.root.output.simulation.mode.mcall.synthesis import OutputSynthesis
 
 
@@ -82,9 +81,6 @@ def _get_district(df: pl.DataFrame) -> DigestMatrixUI:
 
 
 class DigestSynthesis(OutputSynthesis):
-    def __init__(self, config: FileStudyTreeConfig):
-        super().__init__(config)
-
     @override
     def load(
         self,
