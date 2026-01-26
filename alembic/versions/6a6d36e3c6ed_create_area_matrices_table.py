a8378651fda6f2f11f652d5a65ed5fe08433fa71
belthlemar <martin.belthle@rte-france.com>
2026-01-26 10:58:22 +0100

    c

--- alembic/versions/6a6d36e3c6ed_create_area_matrices_table.py
+++ alembic/versions/6a6d36e3c6ed_create_area_matrices_table.py
@@ -20,64 +20,49 @@ def upgrade():
     study_id_col = sa.Column("study_id", sa.String(length=36), nullable=False)
     area_id_col = sa.Column("area_id", sa.String(length=255), nullable=False)
     matrix_id_col = sa.Column("matrix_id", sa.String(), nullable=False)
-
-    op.create_table(
-        "load",
-        study_id_col,  area_id_col, matrix_id_col,
-        sa.ForeignKeyConstraint(
+    foreign_key_constraint = sa.ForeignKeyConstraint(
             ["study_id", "area_id"],
             ["area.study_id", "area.area_id"],
-            name=op.f("fk_load_study_id_area_id_area"),
-            ondelete="CASCADE",
-        ),
+            ondelete="CASCADE"
+        )
+
+    # Load
+    foreign_key_constraint.name = op.f("fk_load_study_id_area_id_area")
+    op.create_table(
+        "load",
+        study_id_col,  area_id_col, matrix_id_col, foreign_key_constraint,
         sa.PrimaryKeyConstraint("study_id", "area_id", name=op.f("pk_load")),
     )
 
+    # Solar
+    foreign_key_constraint.name = op.f("fk_solar_study_id_area_id_area")
     op.create_table(
         "solar",
-        study_id_col,  area_id_col, matrix_id_col,
-        sa.ForeignKeyConstraint(
-            ["study_id", "area_id"],
-            ["area.study_id", "area.area_id"],
-            name=op.f("fk_solar_study_id_area_id_area"),
-            ondelete="CASCADE",
-        ),
+        study_id_col,  area_id_col, matrix_id_col, foreign_key_constraint,
         sa.PrimaryKeyConstraint("study_id", "area_id", name=op.f("pk_solar")),
     )
 
+    # Wind
+    foreign_key_constraint.name = op.f("fk_wind_study_id_area_id_area")
     op.create_table(
         "wind",
-        study_id_col,  area_id_col, matrix_id_col,
-        sa.ForeignKeyConstraint(
-            ["study_id", "area_id"],
-            ["area.study_id", "area.area_id"],
-            name=op.f("fk_wind_study_id_area_id_area"),
-            ondelete="CASCADE",
-        ),
+        study_id_col,  area_id_col, matrix_id_col, foreign_key_constraint,
         sa.PrimaryKeyConstraint("study_id", "area_id", name=op.f("pk_wind")),
     )
 
+    # Reserves
+    foreign_key_constraint.name = op.f("fk_reserves_study_id_area_id_area")
     op.create_table(
         "reserves",
-        study_id_col,  area_id_col, matrix_id_col,
-        sa.ForeignKeyConstraint(
-            ["study_id", "area_id"],
-            ["area.study_id", "area.area_id"],
-            name=op.f("fk_reserves_study_id_area_id_area"),
-            ondelete="CASCADE",
-        ),
+        study_id_col,  area_id_col, matrix_id_col, foreign_key_constraint,
         sa.PrimaryKeyConstraint("study_id", "area_id", name=op.f("pk_reserves")),
     )
 
+    # Misc-gen
+    foreign_key_constraint.name = op.f("fk_misc_gen_study_id_area_id_area")
     op.create_table(
         "misc_gen",
-        study_id_col,  area_id_col, matrix_id_col,
-        sa.ForeignKeyConstraint(
-            ["study_id", "area_id"],
-            ["area.study_id", "area.area_id"],
-            name=op.f("fk_misc_gen_study_id_area_id_area"),
-            ondelete="CASCADE",
-        ),
+        study_id_col,  area_id_col, matrix_id_col, foreign_key_constraint,
         sa.PrimaryKeyConstraint("study_id", "area_id", name=op.f("pk_misc_gen")),
     )
 
