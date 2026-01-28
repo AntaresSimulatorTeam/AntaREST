285a73521c53b9a4b005f44ea4e79dd13e3b2fbe
belthlemar <martin.belthle@rte-france.com>
2026-01-28 16:38:04 +0100

    add provider

--- tests/study/dao/test_database_area_matrices_dao.py
+++ tests/study/dao/test_database_area_matrices_dao.py
@@ -9,14 +9,21 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
+from pathlib import Path
+from unittest.mock import Mock
+
 import polars as pl
 import pytest
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from antarest.core.config import InternalMatrixFormat
 from antarest.core.exceptions import AreaNotFound
 from antarest.maintenance.tasks.common import BackGroundTaskStatus
 from antarest.maintenance.tasks.gc_matrix import clean_matrices
+from antarest.matrixstore.repository import MatrixContentRepository, MatrixDataSetRepository, MatrixRepository
+from antarest.matrixstore.service import MatrixService
+from antarest.study.dao.database.database_matrices_provider import StudyDatabaseMatrixUsageProvider
 from antarest.study.dao.database.database_study_dao import DatabaseStudyDao
 from antarest.study.dao.database.models import (
     LOAD_TABLE,
@@ -152,15 +159,31 @@ def test_misc_gen_lifecycle(db_session: Session, dao: DatabaseStudyDao) -> None:
         assert misc_gen_rows == []
 
 
-def test_garbage_collection(dao: DatabaseStudyDao) -> None:
-    matrix_service = dao._matrix_service
+def test_garbage_collection(dao: DatabaseStudyDao, db_session: Session, tmp_path: Path) -> None:
+    # Create a real matrix_service
+    bucket_dir = tmp_path / "matrix_store"
+    matrix_service = MatrixService(
+        repo=MatrixRepository(db_session),
+        repo_dataset=MatrixDataSetRepository(db_session),
+        matrix_content_repository=MatrixContentRepository(bucket_dir, InternalMatrixFormat.FEATHER),
+        file_transfer_manager=Mock(),
+        task_service=Mock(),
+        config=Mock(),
+        user_service=Mock(),
+    )
+    dao._matrix_service = matrix_service
+
+    # Register the DB provider
+    provider = StudyDatabaseMatrixUsageProvider(matrix_service)
+    matrix_service.register_usage_provider(provider)
+
+    # Create a matrix in the matrix-store
     dataframe = pl.DataFrame(data=[[1, 2.5], [3, 4.7]], orient="row")
     series_id = matrix_service.create(dataframe)
-    area_id = "paris"
-
-    dao.save_area(area_id)
 
     # Create `load`, `solar`, `wind`, `reserves` and `misc-gen` matrices in DB
+    area_id = "paris"
+    dao.save_area(area_id)
     dao.save_load(area_id, series_id)
     dao.save_solar(area_id, series_id)
     dao.save_wind(area_id, series_id)
