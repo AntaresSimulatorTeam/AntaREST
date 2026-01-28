e350ba1763dba8f3f392833e310c6442078e753f
belthlemar <martin.belthle@rte-france.com>
2026-01-28 18:41:49 +0100

    add tests

--- tests/study/dao/test_database_link_dao.py
+++ tests/study/dao/test_database_link_dao.py
@@ -9,13 +9,22 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-
+import pytest
+from db_statement_recorder import DBStatementRecorder
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from antarest.core.exceptions import LinkNotFound
 from antarest.study.business.model.link_model import DEFAULT_COLOR, AssetType, Link, LinkStyle, TransmissionCapacity
 from antarest.study.dao.database.database_study_dao import DatabaseStudyDao
 from antarest.study.dao.database.models import LINK_TABLE
+
+
+def _create_default_link(dao: DatabaseStudyDao) -> None:
+    dao.save_area("Paris")
+    dao.save_area("London")
+    link = Link(area1="paris", area2="london")
+    dao.save_link(link)
 
 
 def test_create_link_with_default_properties(db_session: Session, dao: DatabaseStudyDao) -> None:
@@ -51,3 +60,37 @@ def test_create_link_with_default_properties(db_session: Session, dao: DatabaseS
     # Check the return method
     created_link = dao.get_link("london", "paris")
     assert created_link == link
+
+
+def test_exists_method(dao: DatabaseStudyDao) -> None:
+    # Asserts at first the link does not exist
+    assert dao.link_exists("london", "paris") is False
+
+    # Create the link
+    _create_default_link(dao)
+
+    # Asserts now it returns `True`
+    assert dao.link_exists("london", "paris") is True
+
+
+def test_get_method(dao: DatabaseStudyDao) -> None:
+    with pytest.raises(LinkNotFound):
+        dao.get_link("london", "paris")
+
+    _create_default_link(dao)
+
+    # Should not raise this time
+    dao.get_link("london", "paris")
+
+
+def test_get_all_links(dao: DatabaseStudyDao, db_session: Session) -> None:
+    _create_default_link(dao)
+    dao.save_area("Berlin")
+    dao.save_link(Link(area1="paris", area2="berlin"))
+
+    # Ensures we do not perform N+1 requests
+    with DBStatementRecorder(db_session.bind) as db_recorder:
+        all_links = dao.get_links()
+        assert len(all_links) == 2
+
+    assert len(db_recorder.sql_statements) == 1, str(db_recorder)
