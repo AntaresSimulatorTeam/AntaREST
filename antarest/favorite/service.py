61c126455a757f4d6c2db7b77946971fa2f33ec0
mehdiwahada <mehdi.wahada@rte-france.com>
2026-02-02 16:58:34 +0100

    fix(favorite):  integer user_id in favorite studies table (#2968)
    What it does ? : 
    - It links users of the identities table to the favorite_study table 
    - Replace the get_user_id() method by the get_user_impersonator()
    
    Why ? : 
    - It makes the code in favorite easier to understand while resolving the
    token problem that could occur with the user_id

--- antarest/favorite/service.py
+++ antarest/favorite/service.py
@@ -12,7 +12,7 @@
 
 from antarest.favorite.model import FavoriteStudy, FavoriteStudyDTO
 from antarest.favorite.repository import FavoriteRepository
-from antarest.login.utils import get_user_id
+from antarest.login.utils import get_user_impersonator
 
 
 class FavoriteService:
@@ -22,11 +22,14 @@ class FavoriteService:
     def list_favorites(self) -> list[FavoriteStudyDTO]:
         favorites = self.favorite_repository.get_all()
         dto_favorites = [fav.to_dto() for fav in favorites]
+        dto_favorites = sorted(dto_favorites, key=lambda fav: fav.study_name)
         return dto_favorites
 
     def add_favorite(self, study_uuid: str) -> FavoriteStudyDTO:
-        favorite_study = self.favorite_repository.save(FavoriteStudy(user_id=get_user_id(), study_id=study_uuid))
+        favorite_study = self.favorite_repository.save(
+            FavoriteStudy(user_id=get_user_impersonator(), study_id=study_uuid)
+        )
         return favorite_study.to_dto()
 
     def delete_favorite(self, study_uuid: str) -> None:
-        self.favorite_repository.delete(get_user_id(), study_uuid)
+        self.favorite_repository.delete(study_uuid)
