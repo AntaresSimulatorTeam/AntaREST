5af387c388c7cddf144e103b5305104c148c5bf0
wahadameh <mehdi.wahada@rte-france.com>
2026-02-02 10:53:25 +0100

    feat(favorite_directories): corrected alembic migration problem

--- alembic/versions/y7x41d4a8yc_add_foreign_key_users_favorite_table.py
+++ alembic/versions/y7x41d4a8yc_add_foreign_key_users_favorite_table.py
@@ -11,24 +11,20 @@ from sqlalchemy import ForeignKey
 
 # revision identifiers, used by Alembic.
 revision = 'y7x41d4a8yc'
-down_revision = '7b406752d80'
+down_revision = '6a6d36e3c6ed'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    # dialect_name: str = op.get_context().dialect.name
-    #
-    # if dialect_name == "postgresql":
-    #     with op.batch_alter_table('favorite_study', schema=None) as batch_op:
-    #         batch_op.create_foreign_key("fk_user_id_favorite_study", "identities", "favorite_study", ["user_id"], ["id"], ondelete="CASCADE")
+
     op.drop_table('favorite_study')
     op.create_table(
         "favorite_study",
         sa.Column("user_id", sa.Integer(), nullable=False),
-        sa.Column("study_id", sa.String(), sa.ForeignKey("study.id", ondelete="CASCADE"), nullable=False),
+        sa.Column("study_id", sa.String(36), sa.ForeignKey("study.id", ondelete="CASCADE"), nullable=False),
         sa.PrimaryKeyConstraint("user_id", "study_id", name="pk_favorite_study"),
-        sa.ForeignKeyConstraint(["user_id"], ["identities.id"], name="", ondelete="CASCADE"),
+        sa.ForeignKeyConstraint(["user_id"], ["identities.id"], name="fk_user_id_favorite_study", ondelete="CASCADE"),
     )
 
 
