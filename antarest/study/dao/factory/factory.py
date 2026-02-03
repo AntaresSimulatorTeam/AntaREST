b16335efa8e4149240eaec9930abb0de1a090491
belthlemar <martin.belthle@rte-france.com>
2026-02-03 16:22:23 +0100

    c

--- antarest/study/dao/factory/factory.py
+++ antarest/study/dao/factory/factory.py
@@ -9,6 +9,10 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
+from pathlib import Path
+
+from antares.study.version import StudyVersion
+
 from antarest.core.utils.fastapi_sqlalchemy import db
 from antarest.matrixstore.service import ISimpleMatrixService
 from antarest.study.business.model.area_model import DEFAULT_LAYER_ID, DEFAULT_LAYER_NAME
@@ -18,6 +22,7 @@ from antarest.study.dao.database.database_study_dao import DatabaseStudyDao
 from antarest.study.dao.file.file_study_dao import FileStudyTreeDao
 from antarest.study.model import StorageMode, Study
 from antarest.study.storage.storage_service import StudyStorageService
+from antarest.study.storage.utils import create_new_empty_study, is_managed, update_antares_info
 from antarest.study.storage.variantstudy.model.command_context import CommandContext
 
 
@@ -36,14 +41,22 @@ class DaoFactory:
         self._matrix_service = matrix_service
         self._storage_service = storage_service
 
-    def create_study_dao(self, study: Study) -> StudyDao:
+    def create_study_dao(self, study: Study) -> tuple[StudyDao, Study]:
         if study.storage_mode == StorageMode.DATABASE:
             dao = DatabaseStudyDao(study.id, db.session, self._matrix_service)
             dao.save_layer(Layer(id=DEFAULT_LAYER_ID, name=DEFAULT_LAYER_NAME))
-            return dao
+            return dao, study
 
-        return FileStudyTreeDao(
-            self._storage_service.get_storage(study).get_raw(study),
-            self._command_context.generator_matrix_constants,
-            self._command_context.blob_service,
-        )
+        # Create the FileStudy
+        path_study = Path(study.path)
+
+        create_new_empty_study(version=StudyVersion.parse(study.version), path_study=path_study)
+
+        file_study = self.study_factory.create_from_fs(path_study, is_managed(study), study.id)
+        update_antares_info(study, file_study.tree, update_author=True)
+
+        study.path = str(path_study)
+
+        context = self._command_context
+        dao = FileStudyTreeDao(file_study, context.generator_matrix_constants, context.blob_service)
+        return dao, study
