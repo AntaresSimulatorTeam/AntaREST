36f689ec1cbed8d70f49f4ed3be1fd0443d9907e
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2026-02-04 14:42:44 +0100

    check in test that mutiple upsert is indeed less query intensive
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- tests/study/dao/test_sql_utils.py
+++ tests/study/dao/test_sql_utils.py
@@ -13,6 +13,7 @@ from pathlib import Path
 from typing import Callable
 
 import pytest
+from db_statement_recorder import DBStatementRecorder
 from sqlalchemy import Column, Integer, MetaData, String, Table, create_engine, select
 from sqlalchemy.orm import sessionmaker
 
@@ -30,8 +31,10 @@ TEST_TABLE = Table(
 )
 
 
-@pytest.mark.parametrize("upsert_method", [generic_upsert_multiple, upsert_multiple])
-def test_upsert_multiple(tmp_path: Path, upsert_method: Callable):
+@pytest.mark.parametrize(
+    "upsert_method, expected_queries_1, expected_queries_2", [(generic_upsert_multiple, 2, 3), (upsert_multiple, 1, 1)]
+)
+def test_upsert_multiple(tmp_path: Path, upsert_method: Callable, expected_queries_1: int, expected_queries_2: int):
     engine = create_engine(f"sqlite:///{tmp_path / 'test.db'}")
 
     METADATA.create_all(engine)
@@ -39,29 +42,30 @@ def test_upsert_multiple(tmp_path: Path, upsert_method: Callable):
     sessionfactory = sessionmaker(bind=engine)
     session = sessionfactory()
 
-    insertions = [
-        {"id": "1", "sub_id": "2", "str_value": "val1", "int_value": 12},
-        {"id": "1", "sub_id": "3", "str_value": "val2", "int_value": 52},
-        {"id": "2", "sub_id": "3", "str_value": "val3", "int_value": 66},
-    ]
-
-    # generic_upsert(session, TEST_TABLE, values=insertions, key_columns=[TEST_TABLE.c.id, TEST_TABLE.c.sub_id],
-    # update_columns=["str_value", "int_value"])
-    upsert_method(session, TEST_TABLE, values=insertions)
+    with DBStatementRecorder(engine) as db_recorder:
+        insertions = [
+            {"id": "1", "sub_id": "2", "str_value": "val1", "int_value": 12},
+            {"id": "1", "sub_id": "3", "str_value": "val2", "int_value": 52},
+            {"id": "2", "sub_id": "3", "str_value": "val3", "int_value": 66},
+        ]
+        upsert_method(session, TEST_TABLE, values=insertions)
+        assert len(db_recorder.sql_statements) == expected_queries_1
 
     rows = session.execute(select(TEST_TABLE)).fetchall()
     assert rows == [("1", "2", "val1", 12), ("1", "3", "val2", 52), ("2", "3", "val3", 66)]
 
     # Updates one existing row and inserts a new one
-    updates = [
-        {"id": "1", "sub_id": "2", "str_value": "val1_updated", "int_value": 72},
-        {"id": "3", "sub_id": "4", "str_value": "val4", "int_value": 0},
-    ]
-    upsert_method(
-        session,
-        TEST_TABLE,
-        values=updates,
-    )
+    with DBStatementRecorder(engine) as db_recorder:
+        updates = [
+            {"id": "1", "sub_id": "2", "str_value": "val1_updated", "int_value": 72},
+            {"id": "3", "sub_id": "4", "str_value": "val4", "int_value": 0},
+        ]
+        upsert_method(
+            session,
+            TEST_TABLE,
+            values=updates,
+        )
+        assert len(db_recorder.sql_statements) == expected_queries_2
 
     rows = session.execute(select(TEST_TABLE)).fetchall()
     assert rows == [
