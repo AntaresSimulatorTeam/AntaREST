4f81633a09f4b6f6aaade6ff77b2f16a896fa83e
wahadameh <mehdi.wahada@rte-france.com>
2026-02-05 09:27:45 +0100

    feat(favorite directory): merged conflicting alembic versions

---
+++ alembic/versions/9v8a86r0g4xy_merge_link_and_favorite.py
@@ -0,0 +1,24 @@
+"""merge_link_and_favorite
+
+Revision ID: 9v8a86r0g4xy
+Revises: 10318e5320f6
+Create Date: 2026-02-05 09:25:18.927783
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9v8a86r0g4xy'
+down_revision = ("10318e5320f6", "124274d80f2e", "u9t13x9i6kt")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
