92a8df020c58429d96e44a8cff97fcd4f91dacc1
belthlemar <martin.belthle@rte-france.com>
2026-02-10 13:03:38 +0100

    still one issue

--- tests/study/dao/test_database_initialize_study.py
+++ tests/study/dao/test_database_initialize_study.py
@@ -12,6 +12,7 @@
 from antarest.study.business.model.config.adequacy_patch_model import AdequacyPatchParameters
 from antarest.study.business.model.config.advanced_parameters_model import AdvancedParameters
 from antarest.study.business.model.config.general_model import GeneralConfig
+from antarest.study.business.model.config.optimization_config_model import OptimizationPreferences
 from antarest.study.business.model.config.playlist_model import Playlist
 from antarest.study.business.model.config.timeseries_config_model import TimeSeriesConfiguration
 from antarest.study.business.model.layer_model import Layer
@@ -22,7 +23,7 @@ def test_initialize_study(dao: DatabaseStudyDao) -> None:
     """Test that the study is initialized with the right default values"""
     assert dao.get_layers() == [Layer(id="0", name="All", areas=[])]
     assert dao.get_general_config() == GeneralConfig()
-    # assert dao.get_optimization_preferences() == OptimizationPreferences()
+    assert dao.get_optimization_preferences() == OptimizationPreferences()
     assert dao.get_advanced_parameters() == AdvancedParameters()
     assert dao.get_adequacy_patch_parameters() == AdequacyPatchParameters()
     assert dao.get_playlist_config() == Playlist()
