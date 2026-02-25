2107a273e312d4949a7253d87cec347a2fa1545d
belthlemar <martin.belthle@rte-france.com>
2026-02-25 16:23:23 +0100

    start mapping

--- antarest/study/storage/rawstudy/raw_path_to_matrix_mapper.py
+++ antarest/study/storage/rawstudy/raw_path_to_matrix_mapper.py
@@ -10,11 +10,13 @@
 #
 # This file is part of the Antares project.
 from pathlib import Path
+from typing import Callable
 
 import polars as pl
 
 from antarest.study.business.model.xpansion_model import XpansionResourceFileType
 from antarest.study.dao.api.study_dao import StudyDao
+from antarest.study.model import STUDY_VERSION_8_7
 
 
 class RawPathToMatrixMapper:
@@ -60,9 +62,64 @@ class RawPathToMatrixMapper:
         assert isinstance(matrix, pl.DataFrame)
         return matrix
 
-    def _get_matrix_inside_input_folder(self, parts: tuple[str, ...], error_msg: str) -> pl.DataFrame:
-        pass
-
     def _get_matrix_inside_output_folder(self, parts: tuple[str, ...], error_msg: str) -> pl.DataFrame:
         # todo
         raise NotImplementedError("We do not handle output files for the moment")
+
+    def _get_matrix_inside_input_folder(self, parts: tuple[str, ...], error_msg: str) -> pl.DataFrame:
+        study_version = self._dao.get_version()
+
+        if not parts:
+            raise ValueError(error_msg)
+
+        prefix, rest = parts[0], parts[1:]
+        if prefix == "wind":
+            if len(rest) != 2 and rest[0] != "series":
+                raise ValueError(error_msg)
+            return self._dao.get_wind(rest[1].removeprefix("wind_"))
+
+        elif prefix == "solar":
+            if len(rest) != 2 and rest[0] != "series":
+                raise ValueError(error_msg)
+            return self._dao.get_solar(rest[1].removeprefix("solar_"))
+
+        elif prefix == "load":
+            if len(rest) != 2 and rest[0] != "series":
+                raise ValueError(error_msg)
+            return self._dao.get_load(rest[1].removeprefix("load_"))
+
+        elif prefix == "reserves":
+            if len(rest) != 1:
+                raise ValueError(error_msg)
+            return self._dao.get_reserves(rest[0])
+
+        elif prefix == "misc-gen":
+            if len(rest) != 1:
+                raise ValueError(error_msg)
+            return self._dao.get_misc_gen(rest[0].removeprefix("miscgen-"))
+
+        elif prefix == "bindingconstraints":
+            if len(rest) != 1:
+                raise ValueError(error_msg)
+            if study_version < STUDY_VERSION_8_7:
+                return self._dao.get_constraint_values_matrix(rest[0])
+            # Based on the suffix we'll know which DAO method to use
+            mapping: dict[str, Callable[[str], pl.DataFrame]] = {
+                "lt": self._dao.get_constraint_less_term_matrix,
+                "gt": self._dao.get_constraint_greater_term_matrix,
+                "eq": self._dao.get_constraint_equal_term_matrix,
+            }
+            bc_id, suffix = rest[0][:-3], rest[0][-2:]
+            return mapping[suffix](bc_id)
+
+        """
+        children: TREE = {
+            "areas": InputAreas(self.matrix_mapper, config.next_file("areas")),
+            "hydro": InputHydro(self.matrix_mapper, config.next_file("hydro")),
+            "links": InputLink(self.matrix_mapper, config.next_file("links")),
+            "thermal": InputThermal(self.matrix_mapper, config.next_file("thermal")),
+        }
+            children["renewables"] = ClusteredRenewables(self.matrix_mapper, config.next_file("renewables"))
+            children["st-storage"] = InputSTStorage(self.matrix_mapper, config.next_file("st-storage"))
+        pass
+        """
