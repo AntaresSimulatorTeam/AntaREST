b7c81d1453c03572d408af437fc10ca8080b89a7
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2026-02-25 14:38:42 +0100

    add missing type
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

---
+++ antarest/core/api_types.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2026, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+from typing import Annotated, TypeAlias
+
+from pydantic import StringConstraints
+
+# A string without new lines. This ensures that it's safe to log it, see sonar issue python:S5145
+SanitizedStr: TypeAlias = Annotated[str, StringConstraints(pattern=r"^.*$")]
