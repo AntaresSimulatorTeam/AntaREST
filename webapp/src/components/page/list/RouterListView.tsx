5e7ca64fca2b847179863e2aa96b10f692c71989
Samir Kamal <1954121+skamril@users.noreply.github.com>
2026-02-26 09:02:03 +0000

    feat(ui-output): add districts list with areas (#3016)
    - LIstView: allow rendering content without TanStack routing
    - use ListView
    - refactor of some code
    - disable search for synthesis list
    
    ANT-4472

---
+++ webapp/src/components/page/list/RouterListView.tsx
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2026, RTE (https://www.rte-france.com)
+ *
+ * See AUTHORS.txt
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This file is part of the Antares project.
+ */
+
+import useCurrentRouteId from "@/hooks/router/useCurrentRouteId";
+import { Box } from "@mui/material";
+import { Outlet, type ToOptions } from "@tanstack/react-router";
+import ListPanel, { type ListPanelItem, type ListPanelProps } from "../../ListPanel";
+import RouterListItemButton from "../../router/RouterListItemButton";
+import SplitView, { type SplitViewProps } from "../SplitView";
+import { renderListItem } from "./utils";
+
+export interface RouterListViewItem extends ListPanelItem {
+  linkOptions: ToOptions;
+  disabled?: boolean;
+}
+
+interface RouterListViewProps {
+  list: RouterListViewItem[];
+  splitId: SplitViewProps["splitId"];
+  splitMinSize?: SplitViewProps["minSize"];
+  emptyListView?: React.ReactNode;
+  onAdd?: ListPanelProps["onAdd"];
+  actions?: ListPanelProps["actions"];
+  disableSearch?: ListPanelProps["disableSearch"];
+}
+
+function RouterListView({
+  list,
+  splitId,
+  splitMinSize,
+  emptyListView,
+  onAdd,
+  actions,
+  disableSearch,
+}: RouterListViewProps) {
+  // Get current route ID to force rebuilds of links when the route changes.
+  // Allows to update relative links (e.g. `to: "."`).
+  const currentRouteId = useCurrentRouteId();
+
+  return (
+    <SplitView splitId={splitId} minSize={splitMinSize}>
+      <ListPanel
+        list={list}
+        onAdd={onAdd}
+        actions={actions}
+        disableSearch={disableSearch}
+        renderItem={(item) => (
+          <RouterListItemButton key={currentRouteId} {...item.linkOptions} disabled={item.disabled}>
+            {renderListItem(item)}
+          </RouterListItemButton>
+        )}
+      />
+      <Box>{list.length > 0 ? <Outlet /> : emptyListView}</Box>
+    </SplitView>
+  );
+}
+
+export default RouterListView;
--- webapp/src/routes/_authenticated/studies/$studyId/explore/outputs/$outputId/-components/OutputMatrixViewer/OutputFilters/ClusterSelector.tsx
+++ webapp/src/routes/_authenticated/studies/$studyId/explore/outputs/$outputId/-components/OutputMatrixViewer/OutputFilters/ClusterSelector.tsx
@@ -17,7 +17,7 @@ import type { VariablesListDTO } from "@/services/api/studies/outputs/variableVi
 import { Autocomplete } from "@mui/material";
 import { useEffect, useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { getClusters, getFirstClusterId, type ClusterOption, type DataType } from "../../-utils";
+import { getClusters, getFirstClusterId, type ClusterOption, type DataType } from "../../../-utils";
 
 interface ClusterSelectorProps {
   variablesMetadata: VariablesListDTO | null;
@@ -39,7 +39,7 @@ function ClusterSelector({
   const { t } = useTranslation();
 
   const clusterOptions = useMemo(() => {
-    if (!variablesMetadata || !selectedItemId) {
+    if (!variablesMetadata) {
       return [];
     }
 
