81c4032cebb7c09348b0360779d4b4740f908d41
TheoPascoli <theo.pascoli@rte-france.com>
2026-03-04 10:10:38 +0100

    feat(comments): fix alembic head

--- alembic/versions/8a9a91f6a2bc_add_comments_column_to_study.py
+++ alembic/versions/8a9a91f6a2bc_add_comments_column_to_study.py
@@ -1,7 +1,7 @@
 """add_comments_column_to_study
 
 Revision ID: 8a9a91f6a2bc
-Revises: 562d4e1bd95d
+Revises: c11cfe5728b0
 Create Date: 2026-03-04 11:00:00.000000
 
 """
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "8a9a91f6a2bc"
-down_revision = "562d4e1bd95d"
+down_revision = "c11cfe5728b0"
 branch_labels = None
 depends_on = None
 
