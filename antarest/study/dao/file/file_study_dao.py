d3166380591447721de8d9d1a8d5346987ff60b0
belthlemar <martin.belthle@rte-france.com>
2026-03-31 16:40:19 +0200

    perf(db): introduce bulk fs dao method for future works

--- antarest/study/dao/file/file_study_dao.py
+++ antarest/study/dao/file/file_study_dao.py
@@ -41,6 +41,7 @@ from antarest.study.dao.file.file_study_user_resources_dao import FileStudyUserR
 from antarest.study.dao.file.file_study_xpansion_dao import FileStudyXpansionDao
 from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
 from antarest.study.storage.rawstudy.model.filesystem.matrix.input_series_matrix import InputSeriesMatrix
+from antarest.study.storage.rawstudy.model.filesystem.matrix.matrix import MatrixNode
 
 if t.TYPE_CHECKING:
     from antarest.blobstore.service import IBlobService
@@ -123,3 +124,67 @@ class FileStudyTreeDao(
         if isinstance(node, InputSeriesMatrix):
             return node.parse_as_dataframe()
         raise NotAMatrixError(url)
+
+    def get_matrices_ids(self, nodes: list[MatrixNode]) -> dict[MatrixNode, str]:
+        """
+        Get multiple matrices ids efficiently.
+        It performs at most 1 DB query for the whole list
+        """
+        result = {}
+
+        denormalized_nodes = []
+
+        for matrix_node in nodes:
+            if matrix_node.is_normalized:
+                # For the normalized nodes, we simply get the matrix id from the matrix mapper.
+                matrix_id = matrix_node.matrix_mapper.get_link_content(matrix_node)
+                assert isinstance(matrix_id, str)
+                result[matrix_node] = matrix_id
+            else:
+                denormalized_nodes.append(matrix_node)
+
+        ########## Denormalized nodes ##########
+        # The `create_batch` allows us to perform only 1 DB insert for all our matrix ids.
+        matrix_service = self._generator_matrix_constants.matrix_service
+        matrix_ids = matrix_service.create_batch(node.parse_content() for node in denormalized_nodes)
+        for k, node in enumerate(denormalized_nodes):
+            result[node] = matrix_ids[k]
+
+        return result
+
+    def save_matrices(self, nodes_and_matrix_ids: list[tuple[MatrixNode, str]]) -> None:
+        """
+        Saves multiple matrices in their corresponding nodes efficiently.
+        It performs at most 2 DB queries for the whole list
+        """
+        denormalized_matrices_mapping: dict[str, list[MatrixNode]] = {}
+        normalized_matrices_mapping: dict[str, list[MatrixNode]] = {}
+
+        # First, we separate_matrices in 2 groups, the normalized and the denormalized ones.
+        for matrix_node, matrix_id in nodes_and_matrix_ids:
+            if matrix_node.is_normalized:
+                normalized_matrices_mapping.setdefault(matrix_id, []).append(matrix_node)
+            else:
+                denormalized_matrices_mapping.setdefault(matrix_id, []).append(matrix_node)
+
+        matrix_service = self._generator_matrix_constants.matrix_service
+
+        ########## Normalized matrices ##########
+        # First, we check if all the matrices are already in the database in 1 single DB query.
+        if not matrix_service.all_exist(list(normalized_matrices_mapping)):
+            # Perform other DB queries to raise the proper exception if needed.
+            for matrix_id in normalized_matrices_mapping:
+                if not matrix_service.exists(matrix_id):
+                    raise ValueError(f"Matrix {matrix_id} does not exist")
+
+        # We simply save the matrix in the matrix mapper of each node.
+        for matrix_id, matrix_nodes in normalized_matrices_mapping.items():
+            for matrix_node in matrix_nodes:
+                matrix_node.matrix_mapper.save_matrix(matrix_node, matrix_id)
+
+        ########## Denormalized matrices ##########
+        # The `yield_matrices` allows us to perform only 1 DB query for all our matrix ids.
+        for matrix_content in matrix_service.yield_matrices(list(denormalized_matrices_mapping)):
+            dataframe = matrix_content.data
+            for node in denormalized_matrices_mapping[matrix_content.id]:
+                node.write_dataframe(dataframe)
