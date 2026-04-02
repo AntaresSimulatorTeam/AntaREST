9f0081ca6b6c72fd3382611317e2a058ce64468c
belthlemar <martin.belthle@rte-france.com>
2026-04-02 16:43:13 +0200

    fix bug

--- antarest/study/dao/file/file_study_factory_dao.py
+++ antarest/study/dao/file/file_study_factory_dao.py
@@ -20,6 +20,7 @@ from antarest.study.model import Study
 from antarest.study.storage.rawstudy.model.filesystem.factory import StudyFactory
 from antarest.study.storage.utils import create_new_empty_study, is_managed, update_antares_info
 from antarest.study.storage.variantstudy.model.command_context import CommandContext
+from antarest.study.storage.variantstudy.model.dbmodel import VariantStudy
 
 
 class FileStudyDaoFactory(StudyFactoryDao):
@@ -33,10 +34,15 @@ class FileStudyDaoFactory(StudyFactoryDao):
 
     @override
     def create_study_dao(self, study: Study) -> FileStudyTreeDao:
-        path_study = Path(study.path)
+        study_path = Path(study.path)
+        if isinstance(study, VariantStudy):
+            study_path = study_path / "snapshot"
 
-        create_new_empty_study(version=StudyVersion.parse(study.version), path_study=path_study)
-        file_study = self._study_factory.create_from_fs(path_study, is_managed(study), study.id)
+        if not study_path.exists():
+            # Will not override existing study, but instead use the existing one to build the DAO object
+            create_new_empty_study(version=StudyVersion.parse(study.version), path_study=study_path)
+
+        file_study = self._study_factory.create_from_fs(study_path, is_managed(study), study.id)
         update_antares_info(study, file_study.tree, update_author=True)
 
         context = self._command_context
