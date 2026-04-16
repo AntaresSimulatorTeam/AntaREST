339c438161121dd0fe7483cf294b06b8da6569b4
belthlemar <martin.belthle@rte-france.com>
2026-04-16 16:24:07 +0200

    add code except for binding constraints

--- antarest/study/storage/rawstudy/raw_path_to_matrix_mapper.py
+++ antarest/study/storage/rawstudy/raw_path_to_matrix_mapper.py
@@ -20,7 +20,7 @@ from antarest.core.exceptions import IncorrectPathError
 from antarest.study.business.model.binding_constraint_model import ConstraintId
 from antarest.study.business.model.xpansion_model import XpansionResourceFileType
 from antarest.study.dao.api.study_dao import StudyDao
-from antarest.study.model import STUDY_VERSION_8_2, STUDY_VERSION_8_7
+from antarest.study.model import STUDY_VERSION_8_2, STUDY_VERSION_8_7, MatrixFrequency
 
 
 @dataclass(frozen=True)
@@ -28,6 +28,9 @@ class RegexMatcher:
     pattern: re.Pattern[str]
     getter: Callable[..., pl.DataFrame]
     setter: Callable[..., None]
+    # Allows getting a matrix frequency from a path. Used inside the GET /matrixindex endpoint.
+    # Some matrices do not have a frequency, in which case the frequency is None.
+    frequency: Callable[..., MatrixFrequency | None]
 
 
 class RawPathToMatrixMapper:
@@ -178,86 +181,103 @@ class RawPathToMatrixMapper:
                 pattern=re.compile(r"user/expansion/capa/(?P<filename>[^/]+)"),
                 getter=lambda filename: dao.get_xpansion_resource(XpansionResourceFileType.CAPACITIES, filename),  # type: ignore
                 setter=_save_xpansion_capacity,
+                frequency=lambda: None,
             ),
             RegexMatcher(
                 pattern=re.compile(r"user/expansion/weights/(?P<filename>[^/]+)"),
                 getter=lambda filename: dao.get_xpansion_resource(XpansionResourceFileType.WEIGHTS, filename),  # type: ignore
                 setter=_save_xpansion_weight,
+                frequency=lambda: None,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/load/series/load_(?P<area_id>[^/]+)"),
                 getter=dao.get_load,
                 setter=_save_load,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/wind/series/wind_(?P<area_id>[^/]+)"),
                 getter=dao.get_wind,
                 setter=_save_wind,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/solar/series/solar_(?P<area_id>[^/]+)"),
                 getter=dao.get_solar,
                 setter=_save_solar,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/misc-gen/miscgen-(?P<area_id>[^/]+)"),
                 getter=dao.get_misc_gen,
                 setter=_save_misc_gen,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/reserves/(?P<area_id>[^/]+)"),
                 getter=dao.get_reserves,
                 setter=_save_reserves,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/links/(?P<area_from>[^/]+)/capacities/(?P<area_to>[^/]+)_direct"),
                 getter=dao.get_link_direct_capacities,
                 setter=_save_link_direct_capacities,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/links/(?P<area_from>[^/]+)/capacities/(?P<area_to>[^/]+)_indirect"),
                 getter=dao.get_link_indirect_capacities,
                 setter=_save_link_indirect_capacities,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/thermal/prepro/(?P<area_id>[^/]+)/(?P<thermal_id>[^/]+)/data"),
                 getter=dao.get_thermal_prepro,
                 setter=_save_thermal_prepro,
+                frequency=lambda: MatrixFrequency.DAILY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/thermal/prepro/(?P<area_id>[^/]+)/(?P<thermal_id>[^/]+)/modulation"),
                 getter=dao.get_thermal_modulation,
                 setter=_save_thermal_modulation,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/thermal/series/(?P<area_id>[^/]+)/(?P<thermal_id>[^/]+)/series"),
                 getter=dao.get_thermal_series,
                 setter=_save_thermal_series,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/thermal/series/(?P<area_id>[^/]+)/(?P<thermal_id>[^/]+)/fuelCost"),
                 getter=dao.get_thermal_fuel_cost,
                 setter=_save_thermal_fuel_cost,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/thermal/series/(?P<area_id>[^/]+)/(?P<thermal_id>[^/]+)/CO2Cost"),
                 getter=dao.get_thermal_co2_cost,
                 setter=_save_thermal_co2_cost,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/renewables/series/(?P<area_id>[^/]+)/(?P<renewable_id>[^/]+)/series"),
                 getter=dao.get_renewable_series,
                 setter=_save_renewable_series,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/st-storage/series/(?P<area_id>[^/]+)/(?P<storage_id>[^/]+)/pmax_injection"),
                 getter=dao.get_st_storage_pmax_injection,
                 setter=_save_st_storage_pmax_injection,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/st-storage/series/(?P<area_id>[^/]+)/(?P<storage_id>[^/]+)/pmax_withdrawal"),
                 getter=dao.get_st_storage_pmax_withdrawal,
                 setter=_save_st_storage_pmax_withdrawal,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(
@@ -265,6 +285,7 @@ class RawPathToMatrixMapper:
                 ),
                 getter=dao.get_st_storage_lower_rule_curve,
                 setter=_save_st_storage_lower_rule_curve,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(
@@ -272,26 +293,31 @@ class RawPathToMatrixMapper:
                 ),
                 getter=dao.get_st_storage_upper_rule_curve,
                 setter=_save_st_storage_upper_rule_curve,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/st-storage/series/(?P<area_id>[^/]+)/(?P<storage_id>[^/]+)/inflows"),
                 getter=dao.get_st_storage_inflows,
                 setter=_save_st_storage_inflows,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/st-storage/series/(?P<area_id>[^/]+)/(?P<storage_id>[^/]+)/cost_injection"),
                 getter=dao.get_st_storage_cost_injection,
                 setter=_save_st_storage_cost_injection,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/st-storage/series/(?P<area_id>[^/]+)/(?P<storage_id>[^/]+)/cost_withdrawal"),
                 getter=dao.get_st_storage_cost_withdrawal,
                 setter=_save_st_storage_cost_withdrawal,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/st-storage/series/(?P<area_id>[^/]+)/(?P<storage_id>[^/]+)/cost_level"),
                 getter=dao.get_st_storage_cost_level,
                 setter=_save_st_storage_cost_level,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(
@@ -299,6 +325,7 @@ class RawPathToMatrixMapper:
                 ),
                 getter=dao.get_st_storage_cost_variation_injection,
                 setter=_save_st_storage_cost_variation_injection,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(
@@ -306,71 +333,85 @@ class RawPathToMatrixMapper:
                 ),
                 getter=dao.get_st_storage_cost_variation_withdrawal,
                 setter=_save_st_storage_cost_variation_withdrawal,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/common/capacity/maxpower_(?P<area_id>[^/]+)"),
                 getter=dao.get_hydro_maxpower,
                 setter=_save_hydro_max_power,
+                frequency=lambda: MatrixFrequency.DAILY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/common/capacity/reservoir_(?P<area_id>[^/]+)"),
                 getter=dao.get_hydro_reservoir,
                 setter=_save_hydro_reservoir,
+                frequency=lambda: MatrixFrequency.DAILY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/prepro/(?P<area_id>[^/]+)/energy"),
                 getter=dao.get_hydro_energy,
                 setter=_save_hydro_energy,
+                frequency=lambda: MatrixFrequency.HOURLY,  # Weird but retro-compatible
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/series/(?P<area_id>[^/]+)/ror"),
                 getter=dao.get_hydro_run_of_river,
                 setter=_save_hydro_run_of_river,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/series/(?P<area_id>[^/]+)/mod"),
                 getter=dao.get_hydro_modulation,
                 setter=_save_hydro_modulation,
+                frequency=lambda: MatrixFrequency.DAILY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/series/(?P<area_id>[^/]+)/mingen"),
                 getter=dao.get_hydro_mingen,
                 setter=_save_hydro_mingen,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/series/(?P<area_id>[^/]+)/maxHourlyGenPower"),
                 getter=dao.get_hydro_max_hourly_gen_power,
                 setter=_save_hydro_max_hourly_gen_power,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/series/(?P<area_id>[^/]+)/maxHourlyPumpPower"),
                 getter=dao.get_hydro_max_hourly_pump_power,
                 setter=_save_hydro_max_hourly_pump_power,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/common/capacity/creditmodulations_(?P<area_id>[^/]+)"),
                 getter=dao.get_hydro_credit_modulations,
                 setter=_save_hydro_credit_modulations,
+                frequency=lambda: MatrixFrequency.HOURLY,  # Weird but retro-compatible
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/common/capacity/inflowPattern_(?P<area_id>[^/]+)"),
                 getter=dao.get_hydro_inflow_pattern,
                 setter=_save_hydro_inflow_pattern,
+                frequency=lambda: MatrixFrequency.DAILY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/common/capacity/waterValues_(?P<area_id>[^/]+)"),
                 getter=dao.get_hydro_water_values,
                 setter=_save_hydro_water_values,
+                frequency=lambda: MatrixFrequency.DAILY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/common/capacity/maxDailyGenEnergy_(?P<area_id>[^/]+)"),
                 getter=dao.get_hydro_max_daily_gen_energy,
                 setter=_save_hydro_max_daily_gen_energy,
+                frequency=lambda: MatrixFrequency.DAILY,
             ),
             RegexMatcher(
                 pattern=re.compile(r"input/hydro/common/capacity/maxDailyPumpEnergy_(?P<area_id>[^/]+)"),
                 getter=dao.get_hydro_max_daily_pump_energy,
                 setter=_save_hydro_max_daily_pump_energy,
+                frequency=lambda: MatrixFrequency.DAILY,
             ),
             RegexMatcher(
                 pattern=re.compile(
@@ -378,6 +419,7 @@ class RawPathToMatrixMapper:
                 ),
                 getter=dao.get_st_storage_additional_constraint_matrix,
                 setter=_save_st_storage_constraint,
+                frequency=lambda: MatrixFrequency.HOURLY,
             ),
         ]
         # Handle version specific patterns
@@ -388,6 +430,7 @@ class RawPathToMatrixMapper:
                     pattern=re.compile(r"input/links/(?P<area_from>[^/]+)/(?P<area_to>[^/]+)"),
                     getter=dao.get_link_series,
                     setter=_save_link_series,
+                    frequency=lambda: MatrixFrequency.HOURLY,
                 )
             )
         else:
@@ -396,6 +439,7 @@ class RawPathToMatrixMapper:
                     pattern=re.compile(r"input/links/(?P<area_from>[^/]+)/(?P<area_to>[^/]+)_parameters"),
                     getter=dao.get_link_series,
                     setter=_save_link_series,
+                    frequency=lambda: MatrixFrequency.HOURLY,
                 )
             )
         if study_version < STUDY_VERSION_8_7:
