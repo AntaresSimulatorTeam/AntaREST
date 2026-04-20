df3645d0ad0a78585319d05170bd92a86d87a7cc
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2026-04-20 15:56:20 +0200

    Revert "refactor(version)!: change format inside some endpoints (#3124)"
    This reverts commit 018cb6a9fe1510c4a53ba8019c4b7fe1e316298c.

--- tests/storage/integration/test_STA_mini.py
+++ tests/storage/integration/test_STA_mini.py
@@ -465,7 +465,7 @@ def test_sta_mini_list_studies(client: TestClient) -> None:
         UUID: {
             "id": UUID,
             "name": "STA-mini",
-            "version": "7.0",
+            "version": 700,
             "author": "Andrea SGATTONI",
             "editor": None,
             "created": str(datetime.fromtimestamp(1480683452)),
