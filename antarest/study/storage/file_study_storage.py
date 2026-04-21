67cd73ac01a73ddd106445330370f722efd9eeb6
belthlemar <martin.belthle@rte-france.com>
2026-04-21 17:01:45 +0200

    issues

--- antarest/study/storage/file_study_storage.py
+++ antarest/study/storage/file_study_storage.py
@@ -13,7 +13,11 @@ import logging
 
 from antarest.core.config import Config
 from antarest.core.interfaces.cache import ICache
+from antarest.matrixstore.matrix_uri_mapper import extract_matrix_id
 from antarest.matrixstore.service import ISimpleMatrixService
+from antarest.study.model import Study
+from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
+from antarest.study.storage.rawstudy.model.filesystem.matrix.matrix import MatrixNode
 from antarest.study.storage.study_storage_interface import IStudyStorage
 
 logger = logging.getLogger(__name__)
@@ -25,3 +29,22 @@ class FileStudyStorage(IStudyStorage):
         self._matrix_service = matrix_service
         self.cache = cache
         self.config = config
+
+    def denormalize_study(self, study: Study) -> None:
+        file_study = self.get_raw(study)
+        self.denormalize_file_study(file_study)
+
+    def denormalize_file_study(self, file_study: FileStudy) -> None:
+        matrix_nodes = file_study.tree.get_matrix_nodes_to_denormalize()
+        if not matrix_nodes:
+            return
+
+        matrices_mapping: dict[str, list[MatrixNode]] = {}
+        for node in matrix_nodes:
+            link_content = node.matrix_mapper.get_link_content(node)
+            assert link_content is not None
+            matrices_mapping.setdefault(extract_matrix_id(link_content), []).append(node)
+
+        for matrix_content in self._matrix_service.yield_matrices(list(matrices_mapping)):
+            for node in matrices_mapping[matrix_content.id]:
+                node.write_dataframe(matrix_content.data)
