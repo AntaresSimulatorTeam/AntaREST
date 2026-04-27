62117fdf55258c02f24e46d89f7507bc411739a9
smailio <smail.a.anis@gmail.com>
2026-04-27 16:29:01 +0200

    fix test

--- tests/integration/study_data_blueprint/test_st_storage.py
+++ tests/integration/study_data_blueprint/test_st_storage.py
@@ -405,8 +405,8 @@ class TestSTStorage:
         )
         assert res.status_code == 404
         obj = res.json()
-        assert obj["description"] == f"'{bad_area_id}' not a child of InputSTStorageClusters"
-        assert obj["exception"] == "ChildNotFoundError"
+        assert obj["description"] == f"Area is not found: '{bad_area_id}'"
+        assert obj["exception"] == "AreaNotFound"
 
         # Check PATCH with the wrong `storage_id`
         bad_storage_id = "bad_storage"
