3e059c7f1aa193f527e6f2aea94b95a75848ca73
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2026-04-29 17:06:27 +0200

    fix(download): use short transactions when polling for download readiness (#3196)
    ANT-4939
    
    Using a long transaction for polling block un-necessarily a DB session
    for a long time.
    
    It happens to cause deadlocks from time to time with `set_ready`,
    probably because
    of a bug in our old version of pgpool.
    
    ---------
    
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- tests/core/utils/test_db_session_middleware.py
+++ tests/core/utils/test_db_session_middleware.py
@@ -9,59 +9,29 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-from dataclasses import dataclass
 
 import pytest
 from sqlalchemy import create_engine, text
-from sqlalchemy.event import listens_for
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 
 from antarest.core.utils.fastapi_sqlalchemy import db
-
-
-@dataclass
-class EventCounter:
-    transaction_start: int = 0
-    transaction_end: int = 0
-    begin: int = 0
-    rollback: int = 0
-    commit: int = 0
+from tests.test_helpers.db import DbEventCounter, create_db_event_counter
 
 
 @pytest.fixture
-def session_factory():
+def session_factory() -> sessionmaker[Session]:
     engine = create_engine("sqlite:///:memory:")
     return sessionmaker(bind=engine)
 
 
 @pytest.fixture
-def events_counter(session_factory):
-    counter = EventCounter()
-
-    @listens_for(session_factory, "after_begin")
-    def after_begin(session, transaction, connection):
-        counter.begin = counter.begin + 1
-
-    @listens_for(session_factory, "after_rollback")
-    def after_rollback(session):
-        counter.rollback = counter.rollback + 1
-
-    @listens_for(session_factory, "after_commit")
-    def after_commit(session):
-        counter.commit = counter.commit + 1
-
-    @listens_for(session_factory, "after_transaction_create")
-    def on_transaction_start(session, transaction):
-        counter.transaction_start = counter.transaction_start + 1
-
-    @listens_for(session_factory, "after_transaction_end")
-    def on_transaction_end(session, transaction):
-        counter.transaction_end = counter.transaction_end + 1
-
-    return counter
+def events_counter(session_factory: sessionmaker[Session]) -> DbEventCounter:
+    return create_db_event_counter(session_factory)
 
 
-def test_db_session_is_rollbacked_on_exit(session_factory, events_counter):
+def test_db_session_is_rollbacked_on_exit(
+    session_factory: sessionmaker[Session], events_counter: DbEventCounter
+) -> None:
     # it's important that transactions are rollbacked on exit, otherwise some
     # transactions can be seen as unclosed (in case an exception happens on close)
 
@@ -76,7 +46,9 @@ def test_db_session_is_rollbacked_on_exit(session_factory, events_counter):
 
 
 @pytest.mark.parametrize("commit_on_exit", [False, True])
-def test_db_session_is_rollbacked_on_exception(session_factory, events_counter, commit_on_exit):
+def test_db_session_is_rollbacked_on_exception(
+    session_factory: sessionmaker[Session], events_counter: DbEventCounter, commit_on_exit: bool
+) -> None:
     with db(session_factory):
         db.session.execute(text("CREATE TABLE test (id INTEGER PRIMARY KEY)"))
         assert events_counter.begin == 1
@@ -87,7 +59,9 @@ def test_db_session_is_rollbacked_on_exception(session_factory, events_counter, 
     assert events_counter.commit == 0
 
 
-def test_db_session_is_committed_on_autocommit(session_factory, events_counter):
+def test_db_session_is_committed_on_autocommit(
+    session_factory: sessionmaker[Session], events_counter: DbEventCounter
+) -> None:
     with db(session_factory, commit_on_exit=True):
         db.session.execute(text("CREATE TABLE test (id INTEGER PRIMARY KEY)"))
         assert events_counter.begin == 1
