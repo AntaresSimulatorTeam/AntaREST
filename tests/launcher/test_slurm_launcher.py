AntaresSimulatorTeam/AntaREST

commit ed4e379c2a75ba8963ccf63d38ba042cfdad93a7

feat(slurm,metrics): stable id for workers, used in metrics and slurm workspace (#3177)
This PR assigns to each worker a stable integer ID between 1 and workers
count.

This is used for:
- worker ID in metrics
- slurm workspace name

In particular, this will solve the following issue:
- on worker kill (OOM typically), gunicorn will restart it, but will not
clean up the corresponding slurm lock file.
Therefore the workspace monitored by this worker will not be picked up
on startup of the replacing worker,
  and **computations will be lost**

Implementation:
- in single gunicorn - multiple workers env, the master process manages
the list of available identifiers,
then the worker picks the identifier and puts it in the
ANTAREST_WORKER_ID env var
- in multiple gunicorn - single worker per gunicorn, the startup script
defines the env variable ANTAREST_WORKER_ID
- else, the worker ID is 0

---------

Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

antarest/launcher/adapters/slurm_launcher/slurm_launcher.py
@@ -15,7 +15,6 @@ import logging
 import os
 import re
 import shutil
-import tempfile
 import threading
 import time
 import traceback
@@ -28,7 +27,6 @@ from antareslauncher.data_repo.data_repo_tinydb import DataRepoTinydb
 from antareslauncher.main import MainParameters, run_with
 from antareslauncher.main_option_parser import MainOptionParser, ParserParameters
 from antareslauncher.study_dto import StudyDTO
-from filelock import FileLock
 from typing_extensions import override
 
 from antarest.core.config import NbCoresConfig, SlurmConfig, TimeLimitConfig
@@ -40,6 +38,7 @@ from antarest.core.serde.ini_reader import read_ini
 from antarest.core.serde.ini_writer import write_ini_file
 from antarest.core.utils.archives import unzip
 from antarest.core.utils.utils import assert_this
+from antarest.globals import ANTAREST_WORKER_ID
 from antarest.launcher.adapters.abstractlauncher import AbstractLauncher, LauncherCallbacks, SimulationLogs
 from antarest.launcher.adapters.log_manager import LogTailManager
 from antarest.launcher.model import JobStatus, LauncherLoadDTO, LauncherParametersDTO, LogType, XpansionParametersDTO
@@ -50,7 +49,6 @@ from antarest.login.utils import current_user_context, require_current_user
 logger = logging.getLogger(__name__)
 logging.getLogger("paramiko").setLevel("WARN")
 
-WORKSPACE_LOCK_FILE_NAME = ".lock"
 LOCK_FILE_NAME = "slurm_launcher_init.lock"
 LOG_DIR_NAME = "LOGS"
 STUDIES_INPUT_DIR_NAME = "STUDIES_IN"
@@ -172,6 +170,7 @@ class SlurmLauncher(AbstractLauncher):
         cache: ICache,
         use_private_workspace: bool = True,
         retrieve_existing_jobs: bool = False,
+        workspace_id: str = f"workspace-{ANTAREST_WORKER_ID}",
     ) -> None:
         super().__init__(callbacks, event_bus, cache)
         self.slurm_config: SlurmConfig = config
@@ -183,14 +182,12 @@ class SlurmLauncher(AbstractLauncher):
         self._check_config()
         self.antares_launcher_lock = threading.Lock()
 
-        # use an absolute path instead of `LOCK_FILE_NAME`:
-        local_workspace_dir = Path(self.slurm_config.local_workspace)
-        with FileLock(local_workspace_dir.joinpath(LOCK_FILE_NAME)):
-            self.local_workspace = self._init_workspace(use_private_workspace)
-        self.log_tail_manager = LogTailManager(local_workspace_dir)
+        self.local_workspace = self._init_workspace(use_private_workspace, workspace_id)
 
-        self.launcher_args = self._init_launcher_arguments(self.local_workspace)
-        self.launcher_params = self._init_launcher_parameters(self.local_workspace)
+        self.log_tail_manager = LogTailManager()
+
+        self.launcher_args = self._init_launcher_arguments()
+        self.launcher_params = self._init_launcher_parameters()
 
         self.data_repo_tinydb = DataRepoTinydb(
             database_file_path=(self.launcher_params.json_dir / self.launcher_params.default_json_db_name),
@@ -204,26 +201,17 @@ class SlurmLauncher(AbstractLauncher):
             self.slurm_config.local_workspace.exists() and self.slurm_config.local_workspace.is_dir()
         )  # and check write permission
 
-    def _init_workspace(self, use_private_workspace: bool) -> Path:
+    def _init_workspace(self, use_private_workspace: bool, workspace_id: str) -> Path:
         if not use_private_workspace:
             return Path(self.slurm_config.local_workspace)
 
-        for existing_workspace in self.slurm_config.local_workspace.iterdir():
-            lock_file = existing_workspace / WORKSPACE_LOCK_FILE_NAME
-            if (
-                existing_workspace.is_dir()
-                and existing_workspace != self.slurm_config.local_workspace / LOG_DIR_NAME
-                and not lock_file.exists()
-            ):
-                logger.info(f"Initiating slurm workspace into existing directory {existing_workspace}")
-                lock_file.touch()
-                return existing_workspace
-
-        new_workspace = Path(tempfile.mkdtemp(dir=str(self.slurm_config.local_workspace)))
-        lock_file = new_workspace / WORKSPACE_LOCK_FILE_NAME
-        lock_file.touch()
-        logger.info(f"Initiating slurm workspace in new directory {new_workspace}")
-        return new_workspace
+        workspace_dir = self.slurm_config.local_workspace / workspace_id
+        if workspace_dir.exists() and workspace_dir.is_dir():
+            logger.info(f"Initiating slurm workspace into existing directory {workspace_dir}")
+        else:
+            logger.info(f"Initiating slurm workspace in new directory {workspace_dir}")
+            os.makedirs(workspace_dir)
+        return workspace_dir
 
     def _retrieve_running_jobs(self) -> None:
         if len(self.data_repo_tinydb.get_list_of_studies()) > 0:
@@ -263,14 +251,14 @@ class SlurmLauncher(AbstractLauncher):
         self.thread = None
         logger.info("slurm_launcher loop stopped")
 
-    def _init_launcher_arguments(self, local_workspace: Path | None = None) -> argparse.Namespace:
+    def _init_launcher_arguments(self) -> argparse.Namespace:
         main_options_parameters = ParserParameters(
             default_wait_time=self.slurm_config.default_wait_time,
             default_time_limit=self.slurm_config.time_limit.default * 3600,
             default_n_cpu=self.slurm_config.nb_cores.default,
-            studies_in_dir=str(Path(local_workspace or self.slurm_config.local_workspace) / STUDIES_INPUT_DIR_NAME),
-            log_dir=str(Path(self.slurm_config.local_workspace) / LOG_DIR_NAME),
-            finished_dir=str(Path(local_workspace or self.slurm_config.local_workspace) / STUDIES_OUTPUT_DIR_NAME),
+            studies_in_dir=str(self.local_workspace / STUDIES_INPUT_DIR_NAME),
+            log_dir=str(self.local_workspace / LOG_DIR_NAME),
+            finished_dir=str(self.local_workspace / STUDIES_OUTPUT_DIR_NAME),
             ssh_config_file_is_required=False,
             ssh_configfile_path_alternate1=None,
             ssh_configfile_path_alternate2=None,
@@ -292,9 +280,9 @@ class SlurmLauncher(AbstractLauncher):
 
         return arguments
 
-    def _init_launcher_parameters(self, local_workspace: Path | None = None) -> MainParameters:
+    def _init_launcher_parameters(self) -> MainParameters:
         return MainParameters(
-            json_dir=local_workspace or self.slurm_config.local_workspace,
+            json_dir=self.local_workspace,
             default_json_db_name=self.slurm_config.default_json_db_name,
             slurm_script_path=self.slurm_config.slurm_script_path,
             partition=self.slurm_config.partition,

tests/launcher/test_slurm_launcher.py
@@ -33,7 +33,6 @@ from antarest.core.jwt import JWTUser
 from antarest.launcher.adapters.abstractlauncher import SimulationLogs
 from antarest.launcher.adapters.slurm_launcher.slurm_launcher import (
     LOG_DIR_NAME,
-    WORKSPACE_LOCK_FILE_NAME,
     SlurmLauncher,
     VersionNotSupportedError,
 )
@@ -76,7 +75,9 @@ def test_init_slurm_launcher_arguments(tmp_path: Path) -> None:
         local_workspace=tmp_path,
     )
 
-    slurm_launcher = SlurmLauncher(config=config, callbacks=Mock(), event_bus=Mock(), cache=Mock())
+    slurm_launcher = SlurmLauncher(
+        config=config, callbacks=Mock(), event_bus=Mock(), cache=Mock(), use_private_workspace=False
+    )
 
     arguments = slurm_launcher._init_launcher_arguments()
 
@@ -113,7 +114,9 @@ def test_init_slurm_launcher_parameters(tmp_path: Path) -> None:
         password="password",
     )
 
-    slurm_launcher = SlurmLauncher(config=config, callbacks=Mock(), event_bus=Mock(), cache=Mock())
+    slurm_launcher = SlurmLauncher(
+        config=config, callbacks=Mock(), event_bus=Mock(), cache=Mock(), use_private_workspace=False
+    )
 
     main_parameters = slurm_launcher._init_launcher_parameters()
     assert config is not None
@@ -486,10 +489,11 @@ def test_launcher_workspace_init(run_with_mock: Any, tmp_path: Path, launcher_co
         event_bus=Mock(),
         retrieve_existing_jobs=True,
         cache=Mock(),
+        workspace_id="workspace-12",
     )
     workspaces = [p for p in tmp_path.iterdir() if p.is_dir() and p.name != LOG_DIR_NAME]
-    assert len(workspaces) == 1
-    assert (workspaces[0] / WORKSPACE_LOCK_FILE_NAME).exists()
+    assert workspaces[0] == tmp_path / "workspace-12"
+    assert workspaces[0].is_dir()
 
     slurm_launcher.data_repo_tinydb.save_study(StudyDTO(path="some_path"))
     run_with_mock.assert_not_called()
@@ -501,19 +505,8 @@ def test_launcher_workspace_init(run_with_mock: Any, tmp_path: Path, launcher_co
         event_bus=Mock(),
         retrieve_existing_jobs=True,
         cache=Mock(),
+        workspace_id="workspace-12",
     )
     workspaces = [p for p in tmp_path.iterdir() if p.is_dir() and p.name != LOG_DIR_NAME]
-    assert len(workspaces) == 2
-
-    run_with_mock.reset_mock()
-    # will create a new one since there is a lock on previous one
-    SlurmLauncher(
-        config=launcher_config,
-        callbacks=callbacks,
-        event_bus=Mock(),
-        retrieve_existing_jobs=True,
-        cache=Mock(),
-    )
-    workspaces = [p for p in tmp_path.iterdir() if p.is_dir() and p.name != LOG_DIR_NAME]
-    assert len(workspaces) == 3
-    run_with_mock.assert_not_called()
+    assert len(workspaces) == 1
+    assert workspaces[0] == tmp_path / "workspace-12"

antarest/core/logging/utils.py
@@ -25,6 +25,7 @@ from typing_extensions import override
 
 from antarest.core.config import Config
 from antarest.core.jwt import JWTUser
+from antarest.globals import ANTAREST_WORKER_ID
 from antarest.login.utils import current_user_context, get_current_user
 
 _request: ContextVar[Request | None] = ContextVar("_request", default=None)
@@ -96,6 +97,7 @@ def configure_logger(config: Config, handler_cls: str = "logging.FileHandler") -
                 "format": (
                     "%(asctime)s"
                     " - %(process)s"
+                    " - %(worker_id)s"
                     " - %(name)s"
                     " - %(trace_id)s"
                     " - %(task_id)s"
@@ -201,6 +203,7 @@ class ContextFilter(logging.Filter):
             record.task_id = task_id
         if current_user := get_current_user():
             record.user = current_user.id
+        record.worker_id = ANTAREST_WORKER_ID
         return True
 
 

antarest/core/metrics.py
@@ -37,13 +37,14 @@ from antarest.core.config import Config
 from antarest.core.exceptions import ConfigurationError
 from antarest.core.tasks.model import TaskStatus, TaskType
 from antarest.core.tasks.service import TaskServiceListener
+from antarest.globals import ANTAREST_WORKER_ID
 
 logger = logging.getLogger(__name__)
 
 
 _PROMETHEUS_MULTIPROCESS_ENV_VAR = "PROMETHEUS_MULTIPROC_DIR"
 
-WORKER_ID = str(os.getpid())
+WORKER_ID = str(ANTAREST_WORKER_ID)
 
 
 def add_db_metrics(config: Config) -> None:

antarest/globals.py
@@ -1,0 +1,20 @@
+# Copyright (c) 2026, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+
+import os
+
+# A unique ID for each worker process, in a multi process environment (gunicorn or any other way)
+# The system starting the workers is responsible for defining a value for the env var ANTAREST_WORKER_ID
+# Currently, see startup script start.sh, or gunicorn configuration conf/gunicorn.py.
+#
+# The worker ID may be used a stable ID for metrics or other purposes.
+ANTAREST_WORKER_ID: int = int(os.environ.get("ANTAREST_WORKER_ID", 0))

antarest/launcher/adapters/local_launcher/local_launcher.py
@@ -68,7 +68,7 @@ class LocalLauncher(AbstractLauncher):
         logs_path = self.local_workspace / "LOGS"
         logs_path.mkdir(parents=True, exist_ok=True)
         self.log_directory = logs_path
-        self.log_tail_manager = LogTailManager(self.local_workspace)
+        self.log_tail_manager = LogTailManager()
         self.submitted_jobs: dict[str, LauncherParametersDTO] = {}
         self.job_id_to_study_id: dict[str, tuple[str, Path, subprocess.Popen]] = {}  # type: ignore
         self.logs: dict[str, str] = {}

antarest/launcher/adapters/log_manager.py
@@ -25,9 +25,8 @@ logger = logging.getLogger(__name__)
 class LogTailManager:
     BATCH_SIZE = 10
 
-    def __init__(self, log_base_dir: Path) -> None:
+    def __init__(self) -> None:
         logger.info("Initiating Log manager")
-        self.log_base_dir = log_base_dir
         self.tracked_logs: dict[str, Thread] = {}
 
     def track(self, log_path: Path | None, handler: Callable[[str], None]) -> None:

antarest/main.py
@@ -49,6 +49,7 @@ from antarest.eventbus.web import register_websocket_routes
 from antarest.fastapi_jwt_auth.exceptions import AuthJWTException
 from antarest.favorite.web import create_favorite_routes
 from antarest.front import add_front_app
+from antarest.globals import ANTAREST_WORKER_ID
 from antarest.launcher.web import create_launcher_api
 from antarest.login.model import init_admin_user
 from antarest.login.web import create_login_api, create_user_api
@@ -70,7 +71,6 @@ from antarest.study.web.study_data_blueprint import create_study_data_routes
 from antarest.study.web.variant_blueprint import create_study_variant_routes
 from antarest.study.web.watcher_blueprint import create_watcher_routes
 from antarest.study.web.xpansion_studies_blueprint import create_xpansion_routes
-from antarest.tools.admin_lib import clean_locks
 
 logger = logging.getLogger(__name__)
 
@@ -378,7 +378,7 @@ def fastapi_app(
 
     configure_logger(config)
 
-    logger.info("Initiating application")
+    logger.info(f"Initiating application (worker {ANTAREST_WORKER_ID})")
 
     app = base_fastapi_app(config.api_prefix, config.root_path)
 
@@ -411,7 +411,6 @@ LOGGING_CONFIG["formatters"]["access"]["fmt"] = (
 def main() -> None:
     arguments = parse_arguments()
     if arguments.module == Module.APP:
-        clean_locks(arguments.config_file)
         app = fastapi_app(
             arguments.config_file,
             mount_front=not arguments.no_front,

commit 86b05567027cad851646b4038b100325ec65d555

fix(logs): logs dir must be shared between workers (#3197)
Fixes regression introduced in
https://github.com/AntaresSimulatorTeam/AntaREST/pull/3177

In a multi-worker environment, all workers need to be able to retrieve
logs of any simulation,
including simulations running in other workers.
This was not the case any more, returning empty logs for simulations of
other workers.

The behaviour is restored by having a log directory shared between
workers.

Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

antarest/launcher/adapters/slurm_launcher/slurm_launcher.py
@@ -49,7 +49,6 @@ from antarest.login.utils import current_user_context, require_current_user
 logger = logging.getLogger(__name__)
 logging.getLogger("paramiko").setLevel("WARN")
 
-LOCK_FILE_NAME = "slurm_launcher_init.lock"
 LOG_DIR_NAME = "LOGS"
 STUDIES_INPUT_DIR_NAME = "STUDIES_IN"
 STUDIES_OUTPUT_DIR_NAME = "OUTPUT"
@@ -184,6 +183,10 @@ class SlurmLauncher(AbstractLauncher):
 
         self.local_workspace = self._init_workspace(use_private_workspace, workspace_id)
 
+        # Important: in a multi-process environment, the log directory must be shared between workers,
+        # so that all workers can expose logs of any simulation in the API
+        self.log_dir = config.local_workspace / LOG_DIR_NAME
+
         self.log_tail_manager = LogTailManager()
 
         self.launcher_args = self._init_launcher_arguments()
@@ -257,7 +260,7 @@ class SlurmLauncher(AbstractLauncher):
             default_time_limit=self.slurm_config.time_limit.default * 3600,
             default_n_cpu=self.slurm_config.nb_cores.default,
             studies_in_dir=str(self.local_workspace / STUDIES_INPUT_DIR_NAME),
-            log_dir=str(self.local_workspace / LOG_DIR_NAME),
+            log_dir=str(self.log_dir),
             finished_dir=str(self.local_workspace / STUDIES_OUTPUT_DIR_NAME),
             ssh_config_file_is_required=False,
             ssh_configfile_path_alternate1=None,

tests/launcher/test_slurm_launcher.py
@@ -97,6 +97,38 @@ def test_init_slurm_launcher_arguments(tmp_path: Path) -> None:
     assert Path(arguments.log_dir) == config.local_workspace / "LOGS"
 
 
+def test_init_slurm_launcher_arguments_multi_worker(tmp_path: Path) -> None:
+    config = SlurmConfig(
+        id="slurm_id",
+        name="slurm",
+        default_wait_time=42,
+        time_limit=TimeLimitConfig(),
+        nb_cores=NbCoresConfig(min=1, default=30, max=36),
+        local_workspace=tmp_path,
+    )
+
+    slurm_launcher = SlurmLauncher(
+        config=config, callbacks=Mock(), event_bus=Mock(), cache=Mock(), workspace_id="workspace-5"
+    )
+
+    arguments = slurm_launcher._init_launcher_arguments()
+
+    assert not arguments.wait_mode
+    assert not arguments.check_queue
+    assert not arguments.wait_mode
+    assert not arguments.check_queue
+    assert arguments.json_ssh_config is None
+    assert arguments.job_id_to_kill is None
+    assert not arguments.xpansion_mode
+    assert not arguments.version
+    assert not arguments.post_processing
+
+    assert config is not None
+    assert Path(arguments.studies_in) == config.local_workspace / "workspace-5" / "STUDIES_IN"
+    assert Path(arguments.output_dir) == config.local_workspace / "workspace-5" / "OUTPUT"
+    assert Path(arguments.log_dir) == config.local_workspace / "LOGS"  # Log dir is shared between workers
+
+
 def test_init_slurm_launcher_parameters(tmp_path: Path) -> None:
     config = SlurmConfig(
         id="slurm_id",
@@ -121,6 +153,45 @@ def test_init_slurm_launcher_parameters(tmp_path: Path) -> None:
     main_parameters = slurm_launcher._init_launcher_parameters()
     assert config is not None
     assert main_parameters.json_dir == config.local_workspace
+    assert main_parameters.default_json_db_name == config.default_json_db_name
+    assert main_parameters.slurm_script_path == config.slurm_script_path
+    assert main_parameters.partition == config.partition
+    assert main_parameters.antares_versions_on_remote_server == config.antares_versions_on_remote_server
+    assert main_parameters.default_ssh_dict == {
+        "username": config.username,
+        "hostname": config.hostname,
+        "port": config.port,
+        "private_key_file": config.private_key_file,
+        "key_password": config.key_password,
+        "password": config.password,
+    }
+    assert main_parameters.db_primary_key == "name"
+
+
+def test_init_slurm_launcher_parameters_multi_worker(tmp_path: Path) -> None:
+    config = SlurmConfig(
+        id="slurm_id",
+        name="slurm",
+        local_workspace=tmp_path,
+        default_json_db_name="default_json_db_name",
+        slurm_script_path="slurm_script_path",
+        partition="fake_partition",
+        antares_versions_on_remote_server=[SolverVersion.parse(840), SolverVersion.parse("10.0")],
+        username="username",
+        hostname="hostname",
+        port=42,
+        private_key_file=Path("private_key_file"),
+        key_password="key_password",
+        password="password",
+    )
+
+    slurm_launcher = SlurmLauncher(
+        config=config, callbacks=Mock(), event_bus=Mock(), cache=Mock(), workspace_id="workspace-5"
+    )
+
+    main_parameters = slurm_launcher._init_launcher_parameters()
+    assert config is not None
+    assert main_parameters.json_dir == config.local_workspace / "workspace-5"
     assert main_parameters.default_json_db_name == config.default_json_db_name
     assert main_parameters.slurm_script_path == config.slurm_script_path
     assert main_parameters.partition == config.partition
@@ -481,7 +552,6 @@ def test_kill_job(
 @patch("antarest.launcher.adapters.slurm_launcher.slurm_launcher.run_with")
 def test_launcher_workspace_init(run_with_mock: Any, tmp_path: Path, launcher_config: SlurmConfig) -> None:
     callbacks = Mock()
-    (tmp_path / LOG_DIR_NAME).mkdir()
 
     slurm_launcher = SlurmLauncher(
         config=launcher_config,