23d711be6edf3b558d85abdce291c5522547d144
smailio <smail.a.anis@gmail.com>
2026-05-06 11:22:44 +0200

    revert something

--- tests/integration/raw_studies_blueprint/test_reserves_need_matrix.py
+++ tests/integration/raw_studies_blueprint/test_reserves_need_matrix.py
@@ -22,7 +22,7 @@ class TestReserveNeedMatrix:
     def test_crud_on_reserve_need_matrix_via_raw(self, client: TestClient, user_access_token: str) -> None:
         preparer = PreparerProxy(client, user_access_token)
 
-        study_id = preparer.create_study("reserve-need-test", version=930, storage_mode="database")
+        study_id = preparer.create_study("reserve-need-test", version=1000, storage_mode="database")
 
         preparer.create_area(study_id, name="fr")
 
