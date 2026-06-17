a16f0d301785a49edac96bd786f3ccb7623d6e83
wahadameh <mehdi.wahada@rte-france.com>
2026-06-17 15:02:56 +0200

    feat(favorite_external_directory): finally corrected the path problem (test variable not adapting)
    Beware : put only favorite_blueprint in testing for the sake of this test

--- tests/integration/favorite_blueprint/test_favorite_external_directory.py
+++ tests/integration/favorite_blueprint/test_favorite_external_directory.py
@@ -9,7 +9,7 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-from pathlib import Path, PurePosixPath
+from pathlib import Path
 
 from starlette.testclient import TestClient
 
