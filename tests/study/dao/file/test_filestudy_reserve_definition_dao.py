f5b50436c9025837c1bdab49ecd3db71d0698fb0
belthlemar <martin.belthle@rte-france.com>
2026-06-23 09:58:27 +0200

    add test for the YML format

--- tests/study/dao/file/test_filestudy_reserve_definition_dao.py
+++ tests/study/dao/file/test_filestudy_reserve_definition_dao.py
@@ -10,6 +10,7 @@
 #
 # This file is part of the Antares project.
 
+from typing import Any
 from unittest.mock import Mock
 
 import pytest
@@ -21,6 +22,7 @@ from antarest.study.business.model.reserves_global_parameters_model import Reser
 from antarest.study.dao.file.file_study_dao import FileStudyTreeDao
 from antarest.study.model import STUDY_VERSION_10_0
 from antarest.study.storage.rawstudy.model.filesystem.factory import FileStudy
+from antarest.study.storage.rawstudy.model.filesystem.yaml_file_node import YAMLReader
 from antarest.study.storage.variantstudy.business.matrix_constants_generator import GeneratorMatrixConstants
 from tests.study.dao.utils import save_area
 
@@ -118,3 +120,46 @@ class TestCoexistenceWithGlobalParameters:
         assert filestudy_dao_v10.get_reserve_definition("paris", "r1").failure_cost == 999.0
         assert filestudy_dao_v10.reserve_definition_exists("paris", "r2") is True
         assert filestudy_dao_v10.reserve_definition_exists("paris", "r3") is True
+
+    def test_yaml_file_is_written_and_read_correctly(self, filestudy_dao_v10: FileStudyTreeDao) -> None:
+        save_area(filestudy_dao_v10, "paris")
+        global_params = ReservesGlobalParameters(
+            reference_activation_duration_down=5,
+            energy_activation_ratio_down=0.33,
+            reference_activation_duration_up=11,
+            energy_activation_ratio_up=0.66,
+        )
+        filestudy_dao_v10.save_reserves_global_parameters({"paris": global_params})
+
+        # Check the YML content
+        study_path = (
+            filestudy_dao_v10.get_file_study().config.study_path / "input" / "reserves" / "paris" / "reserves.yml"
+        )
+        content = YAMLReader().read(study_path)
+        expected_content: dict[str, Any] = {
+            "globalparameters": {
+                "energy-activation-ratio-down": 0.33,
+                "energy-activation-ratio-up": 0.66,
+                "reference-activation-duration-down": 5,
+                "reference-activation-duration-up": 11,
+            }
+        }
+        assert content == expected_content
+
+        # Add a reserve definition
+        filestudy_dao_v10.save_reserve_definitions({"paris": [_make_reserve("R1", failure_cost=999)]})
+
+        # Check the YML content
+        content = YAMLReader().read(study_path)
+        expected_content["reserves"] = [
+            {
+                "energy-activation-ratio": 0.9,
+                "failure-cost": 999.0,
+                "name": "R1",
+                "power-activation-ratio": 0.4,
+                "reference-activation-duration": 3,
+                "spillage-cost": 5.0,
+                "type": "up",
+            }
+        ]
+        assert content == expected_content
