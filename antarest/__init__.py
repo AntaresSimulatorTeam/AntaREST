922b8e10fd5f9e791bb6e2ae5d0d41cc2c2f461f
belthlemar <martin.belthle@rte-france.com>
2026-06-24 09:41:23 +0200

    chore(release): v2.33.0 (2026-06-24)

--- antarest/__init__.py
+++ antarest/__init__.py
@@ -20,9 +20,9 @@ from pathlib import Path
 
 # Standard project metadata
 
-__version__ = "2.32.0"
+__version__ = "2.33.0"
 __author__ = "RTE, Antares Web Team"
-__date__ = "2026-05-26"
+__date__ = "2026-06-24"
 # noinspection SpellCheckingInspection
 __credits__ = "(c) Réseau de Transport de l’Électricité (RTE)"
 
