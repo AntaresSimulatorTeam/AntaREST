e59ff97da6bdb7b8db352940c5561cbebda7fe46
belthlemar <martin.belthle@rte-france.com>
2026-06-26 10:41:59 +0200

    add UTs for merge function

--- antarest/study/business/model/thermal_reserve_symmetries_model.py
+++ antarest/study/business/model/thermal_reserve_symmetries_model.py
@@ -19,3 +19,30 @@ ThermalReserveSymmetry: TypeAlias = list[ReserveDefinitionId]
 
 # Update map: area -> thermal -> list of reserve ids
 ThermalReserveSymmetriesUpdate = dict[AreaId, dict[ThermalId, ThermalReserveSymmetry]]
+
+
+def merge_symmetries(symmetries: list[ThermalReserveSymmetry]) -> list[ThermalReserveSymmetry]:
+    """
+    The Simulator accepts symmetries to be written as [a, b] and [b, c] separately, even if it means the same thing as [a, b, c].
+    This function merges symmetries to have the least number of them.
+    """
+    if not symmetries:
+        return []
+
+    merged_sets = [set(symmetries[0])]
+    for symmetry in symmetries[1:]:
+        merged = False
+        for reserve_id in symmetry:
+            for k, merged_set in enumerate(merged_sets):
+                if reserve_id in merged_set:
+                    merged_sets[k] = merged_set | set(symmetry)
+                    merged = True
+                    break
+            if merged:
+                break
+        if not merged:
+            merged_sets.append(set(symmetry))
+
+    # Replace sets with lists
+    merged_sets = [sorted(list(merged_set)) for merged_set in merged_sets]
+    return merged_sets
---
+++ antarest/study/storage/rawstudy/model/filesystem/config/thermal_reserve_symmetries.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2026, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+from typing import Any
+
+from antarest.study.business.model.reserve_definition_model import ReserveDefinitionId
+from antarest.study.business.model.thermal_reserve_symmetries_model import ThermalReserveSymmetry, merge_symmetries
+from antarest.study.storage.rawstudy.model.filesystem.config.identifier import transform_name_to_id
+
+
+def parse_thermal_reserves_symmetries(data: dict[str, Any]) -> dict[str, list[ThermalReserveSymmetry]]:
+    result: dict[str, list[ThermalReserveSymmetry]] = {}
+    for content in data.get("participations", []):
+        thermal_id = transform_name_to_id(content["cluster"])
+        if thermal_id in result:
+            raise ValueError(f"Duplicate thermal cluster id: {thermal_id}")
+        result[thermal_id] = []
+        for symmetry in content.get("symmetries", []):
+            reserve_ids = [ReserveDefinitionId(s) for s in symmetry["reserves"]]
+            result[thermal_id].append(reserve_ids)
+
+        # Merge symmetries
+        result[thermal_id] = merge_symmetries(result[thermal_id])
+
+    return result
+
+
+def serialize_thermal_reserve_symmetries(symmetries: list[ThermalReserveSymmetry]) -> list[dict[str, Any]]:
+    merged_symmetries = merge_symmetries(symmetries)
+    return [{"reserves": [symmetry]} for symmetry in merged_symmetries]
