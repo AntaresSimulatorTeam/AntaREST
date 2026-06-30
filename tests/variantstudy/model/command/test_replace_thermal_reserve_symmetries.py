37bb4fb9984794f9e8660b9fca5690de316a353c
belthlemar <martin.belthle@rte-france.com>
2026-06-30 16:24:59 +0200

    finalize set up code

--- tests/variantstudy/model/command/test_replace_thermal_reserve_symmetries.py
+++ tests/variantstudy/model/command/test_replace_thermal_reserve_symmetries.py
@@ -9,11 +9,12 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-
+from antarest.study.business.model.reserve_definition_model import ReserveDefinitionCreation, ReserveType
 from antarest.study.business.model.thermal_cluster_model import ThermalClusterCreation
 from antarest.study.dao.api.study_dao import StudyDao
 from antarest.study.storage.variantstudy.model.command.create_area import CreateArea
 from antarest.study.storage.variantstudy.model.command.create_cluster import CreateCluster
+from antarest.study.storage.variantstudy.model.command.create_reserve_definition import CreateReserveDefinition
 from antarest.study.storage.variantstudy.model.command_context import CommandContext
 
 
@@ -22,8 +23,10 @@ def _set_up(dao: StudyDao, command_context: CommandContext) -> None:
     # Create 2 areas
     cmd1 = CreateArea(area_name="FR", command_context=command_context, study_version=version)
     cmd2 = CreateArea(area_name="de", command_context=command_context, study_version=version)
-    cmd1.apply(dao)
-    cmd2.apply(dao)
+    output = cmd1.apply(dao)
+    assert output.status
+    output = cmd2.apply(dao)
+    assert output.status
     # Create 2 thermals inside area `fr` and 1 inside area `de`
     cmd = CreateCluster(
         area_id="fr",
@@ -31,21 +34,49 @@ def _set_up(dao: StudyDao, command_context: CommandContext) -> None:
         command_context=command_context,
         study_version=version,
     )
-    cmd.apply(dao)
+    output = cmd.apply(dao)
+    assert output.status
     cmd = CreateCluster(
         area_id="fr",
         parameters=ThermalClusterCreation(name="th2"),
         command_context=command_context,
         study_version=version,
     )
-    cmd.apply(dao)
+    output = cmd.apply(dao)
+    assert output.status
     cmd = CreateCluster(
         area_id="de",
         parameters=ThermalClusterCreation(name="th_de"),
         command_context=command_context,
         study_version=version,
     )
-    cmd.apply(dao)
+    output = cmd.apply(dao)
+    assert output.status
+    # Create 2 reserves inside area `fr` and 1 inside area `de`
+    cmd = CreateReserveDefinition(
+        area_id="fr",
+        parameters=ReserveDefinitionCreation(name="r1", type=ReserveType.UP),
+        command_context=command_context,
+        study_version=version,
+    )
+    output = cmd.apply(dao)
+    assert output.status
+    cmd = CreateReserveDefinition(
+        area_id="fr",
+        parameters=ReserveDefinitionCreation(name="r2", type=ReserveType.DOWN),
+        command_context=command_context,
+        study_version=version,
+    )
+    output = cmd.apply(dao)
+    assert output.status
+    cmd = CreateReserveDefinition(
+        area_id="de",
+        parameters=ReserveDefinitionCreation(name="reserve_DE", type=ReserveType.UP),
+        command_context=command_context,
+        study_version=version,
+    )
+    output = cmd.apply(dao)
+    assert output.status
 
 
 def test_nominal_case(dao_10_0: StudyDao, command_context: CommandContext) -> None:
