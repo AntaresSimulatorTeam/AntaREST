0a04bd8b33f421223cbe391c033dfaa20fe3c855
belthlemar <martin.belthle@rte-france.com>
2026-07-03 16:47:47 +0200

    write UT that currently fails

--- tests/study/dao/file/test_filestudy_thermal_reserves_dao.py
+++ tests/study/dao/file/test_filestudy_thermal_reserves_dao.py
@@ -15,12 +15,12 @@ from antarest.study.business.model.area_properties_model import AreaProperties
 from antarest.study.business.model.reserve_definition_model import ReserveDefinition, ReserveType
 from antarest.study.business.model.thermal_cluster_model import ThermalCluster
 from antarest.study.business.model.thermal_reserve_certification_model import ThermalReserveCertification
+from antarest.study.dao.file.file_study_dao import FileStudyTreeDao
 from antarest.study.model import STUDY_VERSION_10_0
 
 
-def test_symmetries_and_certifications_do_not_overwrite_each_other(fs_dao_930_and_matrix_service) -> None:
+def _set_up(dao: FileStudyTreeDao) -> FileStudyTreeDao:
     # Build a v10.0 FS DAO.
-    dao, _ = fs_dao_930_and_matrix_service
     dao.get_file_study().config.version = STUDY_VERSION_10_0
     # Create 1 area with 2 thermal clusters and 4 reserves
     dao.save_areas_with_properties({"fr": AreaProperties()})
@@ -29,6 +29,12 @@ def test_symmetries_and_certifications_do_not_overwrite_each_other(fs_dao_930_an
     for reserve_name in ["r1", "r2", "r3", "r4"]:
         reserves.append(ReserveDefinition(name=reserve_name, type=ReserveType.DOWN))
     dao.save_reserve_definitions({"fr": reserves})
+    return dao
+
+
+def test_symmetries_and_certifications_do_not_overwrite_each_other(fs_dao_930_and_matrix_service) -> None:
+    dao, _ = fs_dao_930_and_matrix_service
+    _set_up(dao)
 
     # Save 2 symmetries. Then 1 certification. Ensures the certification writing didn't affect the symmetries.
     dao.save_thermal_reserve_symmetries({"fr": {"th1": [["r1", "r2"], ["r3", "r4"]]}})
@@ -45,3 +51,19 @@ def test_symmetries_and_certifications_do_not_overwrite_each_other(fs_dao_930_an
         "th1": [["r1", "r2"], ["r3", "r4"]],
         "th2": [["r1", "r2", "r3"]],
     }
+
+
+def test_removing_a_reserve_cascades_on_symmetries_and_certifications(fs_dao_930_and_matrix_service) -> None:
+    dao, _ = fs_dao_930_and_matrix_service
+    _set_up(dao)
+
+    # Save 1 symmetry and 1 certitification.
+    dao.save_thermal_reserve_symmetries({"fr": {"th1": [["r1", "r2"]]}})
+    certification = ThermalReserveCertification()
+    dao.save_thermal_reserve_certifications({"fr": {"r1": {"th2": certification}, "r2": {"th1": certification}}})
+
+    # Remove the reserve `r1`. We should no longer see `r1` in the symmetries and certifications.
+    dao.delete_reserve_definitions("fr", ["r1"])
+
+    assert dao.get_thermal_reserve_symmetries("fr") == {}
+    assert dao.get_thermal_reserve_certifications("fr") == {"r2": {"th1": certification}}
