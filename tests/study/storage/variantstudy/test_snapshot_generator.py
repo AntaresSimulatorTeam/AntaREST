55ef92009e73ca76d6a128471cb159fbffc028ca
belthlemar <martin.belthle@rte-france.com>
2026-07-16 15:29:05 +0200

    add msg in test with all db queries performed

--- tests/study/storage/variantstudy/test_snapshot_generator.py
+++ tests/study/storage/variantstudy/test_snapshot_generator.py
@@ -724,6 +724,16 @@ class TestSnapshotGenerator:
                 variant_study_id, dao_factory=factory, from_scratch=False, notifier=notifier
             )
 
+        # todo: Note of what's currently happening
+        # We currently have 6 database queries
+        # - 1 query to fetch the whole tree of studies (with owner and groups for permission check)
+        # - 1 query to fetch the current variant snapshot
+        # - 1 query to fetch the commands and their associated version
+        # - 0 query in this example, but N+1 issue due to `is_snapshot_up_to_date` for deep variant trees
+        # - 1 query to fetch the commands and their assciated version for potential variant parents
+        # - 1 query to fetch the paths inside the `get_study_dao` method
+        # - 1 query to insert the variant study snapshot
+
         # Check: the number of database queries is kept as low as possible.
         # We expect 4 queries:
         # - 1 query to fetch the ancestors of a variant study,
