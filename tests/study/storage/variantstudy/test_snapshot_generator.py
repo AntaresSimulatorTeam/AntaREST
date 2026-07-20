091485b3332703a117871d71e8f9f1b1ed1f9211
belthlemar <martin.belthle@rte-france.com>
2026-07-20 15:42:08 +0200

    add a todo in the test

--- tests/study/storage/variantstudy/test_snapshot_generator.py
+++ tests/study/storage/variantstudy/test_snapshot_generator.py
@@ -1128,29 +1128,6 @@ class TestSnapshotGenerator:
 
             assert len(db_recorder.sql_statements) == 5, str(db_recorder)
 
-        # Generate when the last snapshot is up to date
-        with DBStatementRecorder(db.session.bind) as db_recorder:
-            results = generator.generate_snapshot(variant_study_id, dao_factory=factory, from_scratch=False)
-            assert results.success is True
-
-            # We expect 5 queries:
-            # - 2 queries to fetch the whole tree of studies (with owner and groups for permission check)
-            # - 1 query to fetch the commands and their assciated version for all variant parents
-            # - 1 query to fetch the paths inside the `get_study_dao` method (as we're generating an FS variant)
-            # - 1 query to insert the variant study snapshot
-
-            assert len(db_recorder.sql_statements) == 5, str(db_recorder)
-
-        # todo: create a new variant of 2nd level to ensure we do not perform
-
-        # todo: Note of what's currently happening when not asking for from_scratch = True.
-        # We currently have 6 database queries
-        # - 1 query to fetch the whole tree of studies (with owner and groups for permission check)
-        # - 1 query to fetch the current variant snapshot
-
-        # - 1 query to fetch the commands and their associated version
-        # - 0 query in this example, but N+1 issue due to `is_snapshot_up_to_date` for deep variant trees
-        # - 1 query to fetch the commands and their assciated version for potential variant parents
-
-        # - 1 query to fetch the paths inside the `get_study_dao` method
-        # - 1 query to insert the variant study snapshot
+        # todo: enrich the test with other cases:
+        # - Not from scratch
+        # - Variant that has multiple parents
