f01d1ead6a4cc9b8e35eb37b691a7ff7a78d2694
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2026-07-29 15:35:20 +0200

    fix(variant): invalidate snapshot during generation (#3320)
    We now invalidate the previous snapshot at the start of generation.
    
    This will avoid to keep the previous snapshot as "valid" if the
    generation starts modifying it and is interrupted, for example by a
    process crash. Could not find an easy way to unit test that.
    
    Also set expire_on_commit=False in unit tests, in consistence with the
    actual application configuration.
    
    ---------
    
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- antarest/study/storage/variantstudy/snapshot/snapshot_generator.py
+++ antarest/study/storage/variantstudy/snapshot/snapshot_generator.py
@@ -118,6 +118,10 @@ class SnapshotGenerator:
         variant_study = descendants[-1]
 
         try:
+            # we need to invalidate the current snapshot, since we start to modify the underlying data.
+            # if the process crashes during generation, the snapshot will be invalid and will be regenerated on the next request.
+            self.variant_study_service.invalidate_snapshot(variant_study)
+
             if search_result.force_regenerate:
                 self.variant_study_service.create_snapshot(ref_study, variant_study)
 
