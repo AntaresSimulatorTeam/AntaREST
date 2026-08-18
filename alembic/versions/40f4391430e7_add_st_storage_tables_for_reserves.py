91dfaf2dcc6916d50328de897383fc2c75db4373
pigeonoli <olivier.pigeon@rte-france.com>
2026-08-18 14:43:00 +0200

    feat(study): update migration commit and delete integration tests for reserve

--- alembic/versions/40f4391430e7_add_st_storage_tables_for_reserves.py
+++ alembic/versions/40f4391430e7_add_st_storage_tables_for_reserves.py
@@ -1,7 +1,7 @@
 """add_st_storage_tables_for_reserves
 
 Revision ID: 40f4391430e7
-Revises: 6012b0407e38
+Revises: 1f0c9b2e7a34
 Create Date: 2026-08-11 15:49:32.314554
 
 """
@@ -10,9 +10,11 @@ from sqlalchemy import Column, Float, ForeignKeyConstraint, String
 
 from alembic import op
 
+from antarest.study.dao.database.models import study_data_id_col
+
 # revision identifiers, used by Alembic.
 revision = '40f4391430e7'
-down_revision = '6012b0407e38'
+down_revision = '1f0c9b2e7a34'
 branch_labels = None
 depends_on = None
 
@@ -21,7 +23,7 @@ def upgrade():
     # Certifications
     op.create_table(
         "st_storage_reserve_certifications",
-        Column("study_id", String(36), nullable=False, primary_key=True),
+        study_data_id_col(),
         Column("area_id", String(255), nullable=False, primary_key=True),
         Column("st_storage_id", String(255), nullable=False, primary_key=True),
         Column("reserve_id", String(255), nullable=False, primary_key=True),
@@ -29,13 +31,13 @@ def upgrade():
         Column("max_release", Float, nullable=False),
         Column("max_store", Float, nullable=False),
         ForeignKeyConstraint(
-            ["study_id", "area_id", "st_storage_id"],
-            ["st_storage.study_id", "st_storage.area_id", "st_storage.st_storage_id"],
+            ["study_data_id", "area_id", "st_storage_id"],
+            ["st_storage.study_data_id", "st_storage.area_id", "st_storage.st_storage_id"],
             ondelete="CASCADE",
         ),
         ForeignKeyConstraint(
-            ["study_id", "area_id", "reserve_id"],
-            ["reserve_definition.study_id", "reserve_definition.area_id", "reserve_definition.reserve_id"],
+            ["study_data_id", "area_id", "reserve_id"],
+            ["reserve_definition.study_data_id", "reserve_definition.area_id", "reserve_definition.reserve_id"],
             ondelete="CASCADE",
         ),
     )
@@ -43,13 +45,13 @@ def upgrade():
     # Symmetries
     op.create_table(
         "st_storage_reserve_symmetries",
-        Column("study_id", String(36), nullable=False, primary_key=True),
+        study_data_id_col(),
         Column("area_id", String(255), nullable=False, primary_key=True),
         Column("st_storage_id", String(255), nullable=False, primary_key=True),
         Column("symmetries", String(), nullable=False),
         ForeignKeyConstraint(
-            ["study_id", "area_id", "st_storage_id"],
-            ["st_storage.study_id", "st_storage.area_id", "st_storage.st_storage_id"],
+            ["study_data_id", "area_id", "st_storage_id"],
+            ["st_storage.study_data_id", "st_storage.area_id", "st_storage.st_storage_id"],
             ondelete="CASCADE",
         ),
     )
