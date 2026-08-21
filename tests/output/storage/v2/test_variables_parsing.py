0f46f4994c9a77d6393907085e51ac4cc62895d4
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2026-08-21 14:49:11 +0200

    remove test, add mc-all files
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- tests/output/storage/v2/test_variables_parsing.py
+++ tests/output/storage/v2/test_variables_parsing.py
@@ -201,32 +201,3 @@ def test_extract_area_variables(output_dir: Path) -> None:
             23,
         ],
     }
-
-
-def test_extract_area_variables_to_db(output_dir: Path) -> None:
-    output = FileOutput(output_dir)
-    parsing_result = parse_output_variables(output)
-
-    # Notes: weird stuff in input data: CO2 emissions in MWh, and different naming for thermal production groups
-    assert parsing_result.mc_ind_areas.variables == [
-        VariableDescription(name="CO2 EMIS.", unit="MWh", statistic_type=None),
-        VariableDescription(name="AVL DTG", unit="MWh", statistic_type=None),
-        VariableDescription(name="DTG MRG", unit="MWh", statistic_type=None),
-        VariableDescription(name="MAX MRG", unit="MWh", statistic_type=None),
-        VariableDescription(name="NP COST", unit="Euro", statistic_type=None),
-        VariableDescription(name="RES LOAD", unit="MWh", statistic_type=None),
-        VariableDescription(name="NODU", unit=None, statistic_type=None),
-        VariableDescription(name="ES_NUCLEAR_TH_PROD", unit="MWh", statistic_type=None),
-        VariableDescription(name="FR_NUCLEAR_TH_PROD", unit="MWh", statistic_type=None),
-        VariableDescription(name="CO2 EMIS.", unit="Tons", statistic_type=None),
-        VariableDescription(name="ES_NUCLEAR", unit="MWh", statistic_type=None),
-        VariableDescription(name="FR_NUCLEAR", unit="MWh", statistic_type=None),
-    ]
-    assert parsing_result.mc_ind_areas.area_vars == {
-        "@ all areas": [0, 1, 2, 3, 4, 5, 6, 7, 8],
-        "es": [9, 10, 1, 2, 3, 4, 6, 5],  # we correctly get ES_NUCLEAR as 10
-        "fr": [9, 11, 1, 2, 3, 4, 6, 5],  # we correctly get FR_NUCLEAR as 11
-    }
-
-    assert parsing_result.mc_all_areas.variables == []
-    assert parsing_result.mc_all_areas.area_vars == {}
