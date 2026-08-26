a8edbdda993420beaca8079faf5b265867ae22f7
pigeonoli <olivier.pigeon@rte-france.com>
2026-08-26 14:41:16 +0200

    fix(study): set path to None for existing DB studies

--- alembic/versions/1bb1c26c70d8_make_studies_path_nullable.py
+++ alembic/versions/1bb1c26c70d8_make_studies_path_nullable.py
@@ -20,6 +20,17 @@ def upgrade():
     with op.batch_alter_table("study", schema=None) as batch_op:
         batch_op.alter_column("path", existing_type=sa.String(), nullable=True)
 
+    # Set the path to None for existing database-mode studies
+    study_table = sa.table("study", sa.column("path", sa.String()), sa.column("storage_mode", sa.String()))
+    op.execute(study_table.update().where(study_table.c.storage_mode == "DATABASE").values(path=None))
+
 def downgrade():
+
+    # Set the path to the study ID for existing database-mode studies
+    study_table = sa.table("study", sa.column("id", sa.String()), sa.column("path", sa.String()))
+    op.execute(
+        study_table.update().where(study_table.c.path.is_(None)).values(path=study_table.c.id)
+    )
+
     with op.batch_alter_table("study", schema=None) as batch_op:
         batch_op.alter_column("path", existing_type=sa.String(), nullable=False)
