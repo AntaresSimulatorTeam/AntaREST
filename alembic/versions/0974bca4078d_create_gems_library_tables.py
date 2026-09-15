be54daf1d0a0a9d1cd228430facd936ff5785410
belthlemar <martin.belthle@rte-france.com>
2026-09-15 13:57:58 +0200

    add txonoomy

--- alembic/versions/0974bca4078d_create_gems_library_tables.py
+++ alembic/versions/0974bca4078d_create_gems_library_tables.py
@@ -41,6 +41,7 @@ def upgrade():
         study_data_id_col(),
         Column("id", String(255), primary_key=True),
         Column("description", String(), nullable=True),
+        Column("taxonomy_category", String(), nullable=True),
         Column("variables", String(), nullable=False),
         Column("binding_constraints", String(), nullable=False),
         Column("constraints", String(), nullable=False),
--- antarest/study/dao/database/models/gems/library.py
+++ antarest/study/dao/database/models/gems/library.py
@@ -42,6 +42,7 @@ GEMS_MODELS_TABLE = Table(
     study_data_id_col(),
     Column("id", String(255), primary_key=True),
     Column("description", String(), nullable=True),
+    Column("taxonomy_category", String(), nullable=True),
     Column("variables", String(), nullable=False),
     Column("binding_constraints", String(), nullable=False),
     Column("constraints", String(), nullable=False),
