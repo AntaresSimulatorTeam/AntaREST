d97609907cc83f6efebc9f34b743b4f4d3460323
belthlemar <martin.belthle@rte-france.com>
2026-09-16 16:50:06 +0200

    remove duplicate copyright

--- antarest/study/dao/database/gems/database_library_dao.py
+++ antarest/study/dao/database/gems/database_library_dao.py
@@ -13,18 +13,6 @@ import json
 from typing import Any
 
 from sqlalchemy import insert, select
-
-# Copyright (c) 2026, RTE (https://www.rte-france.com)
-#
-# See AUTHORS.txt
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-# SPDX-License-Identifier: MPL-2.0
-#
-# This file is part of the Antares project.
 from typing_extensions import override
 
 from antarest.core.exceptions import GemsLibraryAlreadyExists
