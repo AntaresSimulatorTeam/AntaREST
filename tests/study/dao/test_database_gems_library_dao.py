e797c560492ef7ea74988d9b3034f4b47463c8f8
belthlemar <martin.belthle@rte-france.com>
2026-09-16 10:32:57 +0200

    start test

--- tests/study/dao/test_database_gems_library_dao.py
+++ tests/study/dao/test_database_gems_library_dao.py
@@ -11,8 +11,29 @@
 # This file is part of the Antares project.
 from pathlib import Path
 
+import pytest
+
+from antarest.core.exceptions import GemsLibraryAlreadyExists
+from antarest.study.business.model.gems.library import GemsLibrary
+from antarest.study.dao.api.study_dao import StudyDao
+from antarest.study.storage.rawstudy.model.filesystem.yaml_file_node import YAMLReader
+
 ASSETS_PATH = Path(__file__).parent / "assets"
 
 
-def test_martin():
-    assert ASSETS_PATH.exists()
+@pytest.mark.parametrize("dao_10_2", ["db"], indirect=True)
+def test_nominal_cases(dao_10_2: StudyDao) -> None:
+    dao = dao_10_2
+
+    # First ensure there is no library in the study
+    assert dao.get_library() is None
+
+    # Saves a library
+    gems_library_asset_path = ASSETS_PATH / "gems" / "libraries" / "8_1_simulator_nr_tests.yml"
+    content = YAMLReader().read(gems_library_asset_path)["library"]
+    library = GemsLibrary.model_validate(content)
+    dao.save_library(library)
+
+    # Ensures we cannot replace a library once it already exists in a study
+    with pytest.raises(GemsLibraryAlreadyExists):
+        dao.save_library(library)
