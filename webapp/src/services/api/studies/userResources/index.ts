11c428dfd35dc3d7d4e0047b99c53ec8e7efae91
Samir Kamal <1954121+skamril@users.noreply.github.com>
2026-09-17 12:37:26 +0100

    review

--- webapp/src/services/api/studies/userResources/index.ts
+++ webapp/src/services/api/studies/userResources/index.ts
@@ -25,6 +25,13 @@ import type {
 
 const BASE_URL = "/v1/studies/{studyId}/user-resources";
 
+/**
+ * GET /v1/studies/{studyId}/user-resources - Gets the tree of user resources for the specified study.
+ *
+ * @param params - Parameters.
+ * @param params.studyId - The ID of the study.
+ * @returns The tree of user resources for the specified study.
+ */
 export async function getUserResourcesTree({
   studyId,
 }: GetUserResourceTreeParams): Promise<UserResourcesTree> {
@@ -32,6 +39,15 @@ export async function getUserResourcesTree({
   return userResourcesTreeSchema.parse(data);
 }
 
+/**
+ * GET /v1/studies/{studyId}/user-resources/content - Gets the content of a user resource
+ * for the specified study.
+ *
+ * @param params - Parameters.
+ * @param params.studyId - The ID of the study.
+ * @param params.path - The path of the user resource.
+ * @returns The content of the user resource.
+ */
 export async function getUserResourceContent({ studyId, path }: GetUserResourceContentParams) {
   const { data } = await client.get<Blob>(format(`${BASE_URL}/content`, { studyId }), {
     params: { path },
@@ -40,6 +56,16 @@ export async function getUserResourceContent({ studyId, path }: GetUserResourceC
   return data;
 }
 
+/**
+ * PUT /v1/studies/{studyId}/user-resources - Creates or replaces a user resource
+ * for the specified study.
+ *
+ * @param params - Parameters.
+ * @param params.studyId - The ID of the study.
+ * @param params.path - The path of the user resource.
+ * @param params.resourceType - The type of the user resource.
+ * @param params.file - The file to upload if the resource type is "file".
+ */
 export async function createOrReplaceUserResource({
   studyId,
   path,
